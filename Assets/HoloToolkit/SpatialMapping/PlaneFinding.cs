95030dc830a4ad93dc0918d160c0b1c3136c0935
ahillier <ahillier@microsoft.com>
2016-03-03 14:50:17 -0800

    Disable warning 618 in PlaneFinding.cs
    This change disables the warning that appears when calling
    Marshal.SizeOf, because Unity does not support .Net 4.5.1+ (which
    supports Marshal.SizeOf<T>).

--- Assets/HoloToolkit/SpatialMapping/PlaneFinding.cs
+++ Assets/HoloToolkit/SpatialMapping/PlaneFinding.cs
@@ -251,16 +251,22 @@ namespace HoloToolkit.Unity
 
         /// <summary>
         /// Marshals BoundedPlane data returned from a DLL API call into a managed BoundedPlane array
-        /// and the frees the memory that was allocated within the DLL.
+        /// and then frees the memory that was allocated within the DLL.
         /// </summary>
+        /// <remarks>Disabling warning 618 when calling Marshal.SizeOf(), because
+        /// Unity does not support .Net 4.5.1+ for using the preferred Marshal.SizeOf(T) method."/>, </remarks>
         private static BoundedPlane[] MarshalBoundedPlanesFromIntPtr(IntPtr outArray, int size)
         {
             BoundedPlane[] resArray = new BoundedPlane[size];
+#pragma warning disable 618
             int structsize = Marshal.SizeOf(typeof(BoundedPlane));
+#pragma warning restore 618
             IntPtr current = outArray;
             for (int i = 0; i < size; i++)
             {
+#pragma warning disable 618
                 resArray[i] = (BoundedPlane)Marshal.PtrToStructure(current, typeof(BoundedPlane));
+#pragma warning restore 618
                 current = (IntPtr)((long)current + structsize);
             }
             Marshal.FreeCoTaskMem(outArray);
