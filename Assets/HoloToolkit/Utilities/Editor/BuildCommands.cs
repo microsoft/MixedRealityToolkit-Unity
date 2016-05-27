fabb0579d6c461da13ee71e4c2b54f4a8cd734da
Josh Wittner <jowitt@microsoft.com>
2016-05-27 11:57:37 -0700

    Added build commands and menu.
    Menu allows for building the project with preferred settings.
    Menu allows for opening the solution from the build.
    
    Both commands ask about executing the other at appropriate times.

---
+++ Assets/HoloToolkit/Utilities/Editor/BuildCommands.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace HoloToolkit.Unity
+{
+    /// <summary>
+    /// Implements functionality for building HoloLens applications
+    /// </summary>
+    public static class BuildCommands
+    {
+        public static readonly string BuildLocation = "Builds/HoloLens";
+
+        /// <summary>
+        /// Do a build configured for the HoloLens, returns the error from BuildPipeline.BuildPlayer
+        /// </summary>
+        public static string BuildForHololens()
+        {
+            var scenes = EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path);
+
+            // Cache the current settings
+            BuildTarget oldBuildTarget = EditorUserBuildSettings.activeBuildTarget;
+            WSASDK oldSDK = EditorUserBuildSettings.wsaSDK;
+            WSAUWPBuildType oldBuildType = EditorUserBuildSettings.wsaUWPBuildType;
+
+            // Set the sdk and build type
+            EditorUserBuildSettings.wsaSDK = WSASDK.UWP;
+            EditorUserBuildSettings.wsaUWPBuildType = WSAUWPBuildType.D3D;
+
+            string error = BuildPipeline.BuildPlayer(scenes.ToArray(), BuildLocation, BuildTarget.WSAPlayer, BuildOptions.None);
+
+            // Restore the sdk and build type
+            EditorUserBuildSettings.wsaSDK = oldSDK;
+            EditorUserBuildSettings.wsaUWPBuildType = oldBuildType;
+            EditorUserBuildSettings.SwitchActiveBuildTarget(oldBuildTarget);
+
+            return error;
+        }
+    }
+}
