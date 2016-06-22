58f674129b82a03f13748406c37aa2475c23ae1d
Dmitri Bouianov <dmitri@rocketalien.com>
2016-06-22 14:34:34 -0700

    Adding method in GazeManager to send a message to object that is being focused on, which then can be added as a response in KeywordRecognizer.
    Addressing code review comments
    
    Removing SendMessageToFocusedObject from GazeManager, adding SelectedObjectMessageSender class, adding 2 custom keyword manager prefabs
    
    Adding focused and selected object keyword test scenes

---
+++ Assets/HoloToolkit/Input/Scripts/FocusedObjectMessageSender.cs
@@ -0,0 +1,21 @@
+﻿using HoloToolkit.Unity;
+using UnityEngine;
+
+/// <summary>
+/// FocusedObjectMessageSender class sends Unity message to object currently focused on by GazeManager.
+/// Focused object messages can be triggered using keyword responses registered in KeywordManager.
+/// </summary>
+public class FocusedObjectMessageSender : MonoBehaviour
+{
+    /// <summary>
+    /// Sends message to the object currently focused on by GazeManager.
+    /// </summary>
+    /// <param name="message">Message to send</param>
+    public void SendMessageToFocusedObject(string message)
+    {
+        if (GazeManager.Instance.FocusedObject != null)
+        {
+            GazeManager.Instance.FocusedObject.SendMessage(message, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
