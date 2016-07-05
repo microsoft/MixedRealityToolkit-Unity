0bb2c5986ecbc67d037ee989a820998441993a86
Ben Strukus (H10 Capital) <v-benjas@microsoft.com>
2016-07-05 13:43:27 -0700

    Reverted changes to GazeManager.cs

--- Assets/HoloToolkit/Input/Scripts/GazeManager.cs
+++ Assets/HoloToolkit/Input/Scripts/GazeManager.cs
@@ -2,27 +2,31 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.VR.WSA;
 
 namespace HoloToolkit.Unity
 {
     /// <summary>
-    /// GazeManager makes the location of the user's gaze, hit position and normals accessible to other components.
+    /// GazeManager determines the location of the user's gaze, hit position and normals.
     /// </summary>
     public partial class GazeManager : Singleton<GazeManager>
     {
-        public GGVInputModule InputModule;
+        [Tooltip("Maximum gaze distance, in meters, for calculating a hit.")]
+        public float MaxGazeDistance = 15.0f;
+
+        [Tooltip("Select the layers raycast should target.")]
+        public LayerMask RaycastLayerMask = Physics.DefaultRaycastLayers;
 
         /// <summary>
-        /// Indicates whether the user is currently gazing at a hologram.
+        /// Physics.Raycast result is true if it hits a hologram.
         /// </summary>
-        public bool IsGazingAtHologram { get; private set; }
+        public bool Hit { get; private set; }
 
         /// <summary>
-        /// HitInfo property gives access to information at the hologram being gazed at, if any.
+        /// HitInfo property gives access
+        /// to RaycastHit public members.
         /// </summary>
-        public RaycastResult HitInfo { get; private set; }
+        public RaycastHit HitInfo { get; private set; }
 
         /// <summary>
         /// Position of the intersection of the user's gaze and the holograms in the scene.
@@ -30,13 +34,25 @@ namespace HoloToolkit.Unity
         public Vector3 Position { get; private set; }
 
         /// <summary>
-        /// Normal direction.
+        /// RaycastHit Normal direction.
         /// </summary>
         public Vector3 Normal { get; private set; }
 
+        /// <summary>
+        /// Object currently being focused on.
+        /// </summary>
+        public GameObject FocusedObject { get; private set; }
+
+        [Tooltip("Checking enables SetFocusPointForFrame to set the stabilization plane.")]
+        public bool SetStabilizationPlane = true;
+        [Tooltip("Lerp speed when moving focus point closer.")]
+        public float LerpStabilizationPlanePowerCloser = 4.0f;
+        [Tooltip("Lerp speed when moving focus point farther away.")]
+        public float LerpStabilizationPlanePowerFarther = 7.0f;
+
         private Vector3 gazeOrigin;
         private Vector3 gazeDirection;
-        private float lastHitDistance = 4.0f;
+        private float lastHitDistance = 15.0f;
 
         private void Update()
         {
@@ -44,6 +60,8 @@ namespace HoloToolkit.Unity
             gazeDirection = Camera.main.transform.forward;
 
             UpdateRaycast();
+
+            UpdateStabilizationPlane();
         }
 
         /// <summary>
@@ -51,32 +69,68 @@ namespace HoloToolkit.Unity
         /// </summary>
         private void UpdateRaycast()
         {
-            // Update the HitInfo property so other classes can use this hit information.
-            HitInfo = InputModule.RaycastResult;
-            IsGazingAtHologram = HitInfo.isValid;
+            // Get the raycast hit information from Unity's physics system.
+            RaycastHit hitInfo;
+            Hit = Physics.Raycast(gazeOrigin,
+                           gazeDirection,
+                           out hitInfo,
+                           MaxGazeDistance,
+                           RaycastLayerMask);
 
-            if (this.IsGazingAtHologram)
+            GameObject oldFocusedObject = FocusedObject;
+            // Update the HitInfo property so other classes can use this hit information.
+            HitInfo = hitInfo;
+
+            if (Hit)
             {
                 // If the raycast hits a hologram, set the position and normal to match the intersection point.
-                Position = HitInfo.worldPosition;
-                Normal = HitInfo.worldNormal;
-                lastHitDistance = HitInfo.distance;
-
-                if (Position == Vector3.zero)
-                {
-                    Position = gazeOrigin + (gazeDirection * this.lastHitDistance);
-                }
-                if (Normal == Vector3.zero)
-                {
-                    Normal = gazeDirection;
-                }
+                Position = hitInfo.point;
+                Normal = hitInfo.normal;
+                lastHitDistance = hitInfo.distance;
+                FocusedObject = hitInfo.collider.gameObject;
             }
             else
             {
                 // If the raycast does not hit a hologram, default the position to last hit distance in front of the user,
                 // and the normal to face the user.
-                Position = gazeOrigin + (gazeDirection * this.lastHitDistance);
-                Normal = gazeDirection;
+                Position = gazeOrigin + (gazeDirection * lastHitDistance);
+                Normal = -gazeDirection;
+                FocusedObject = null;
+            }
+
+            // Check if the currently hit object has changed
+            if (oldFocusedObject != FocusedObject)
+            {
+                if (oldFocusedObject != null)
+                {
+                    oldFocusedObject.SendMessage("OnGazeLeave", SendMessageOptions.DontRequireReceiver);
+                }
+                if (FocusedObject != null)
+                {
+                    FocusedObject.SendMessage("OnGazeEnter", SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Updates the focus point for every frame.
+        /// </summary>
+        private void UpdateStabilizationPlane()
+        {
+            if (SetStabilizationPlane)
+            {
+                // Calculate the delta between camera's position and current hit position.
+                float focusPointDistance = (gazeOrigin - Position).magnitude;
+                float lerpPower = focusPointDistance > lastHitDistance
+                    ? LerpStabilizationPlanePowerFarther
+                    : LerpStabilizationPlanePowerCloser;
+
+                // Smoothly move the focus point from previous hit position to new position.
+                lastHitDistance = Mathf.Lerp(lastHitDistance, focusPointDistance, lerpPower * Time.deltaTime);
+
+                Vector3 newFocusPointPosition = gazeOrigin + (gazeDirection * lastHitDistance);
+
+                HolographicSettings.SetFocusPointForFrame(newFocusPointPosition, -gazeDirection);
             }
         }
     }
