1dcae9f6f91ccaf044fb4529ec785f1b22added7
Kurtis Eveleigh <kurtie@microsoft.com>
2016-08-12 15:20:53 -0700

    Adding KeywordManager test scene

---
+++ Assets/HoloToolkit/Input/Tests/Scripts/DisplayKeywords.cs
@@ -0,0 +1,31 @@
+﻿using HoloToolkit.Unity;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayKeywords : MonoBehaviour
+{
+    public KeywordManager keywordManager;
+    public Text textPanel;
+
+    void Start()
+    {
+        if (keywordManager == null)
+        {
+            Debug.Log("Please specify a KeywordManager in the Inspector on DisplayKeywords.cs on" + name + ".");
+        }
+        if (textPanel == null)
+        {
+            Debug.Log("Please specify a Text field in the Inspector on DisplayKeywords.cs on " + name + ".");
+        }
+        if (textPanel == null || keywordManager == null)
+        {
+            return;
+        }
+
+        textPanel.text = "Try saying:\n";
+        foreach (KeywordManager.KeywordAndResponse k in keywordManager.KeywordsAndResponses)
+        {
+            textPanel.text += k.Keyword + "\n";
+        }
+    }
+}
