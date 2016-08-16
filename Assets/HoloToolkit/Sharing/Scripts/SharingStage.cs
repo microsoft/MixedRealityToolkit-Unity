f5fd4bcc794145ff7e715fe79dbbf14045922203
Alex Drenea <Alex Drenea>
2016-08-16 11:18:27 -0400

    AutoDiscover in Sharing Stage Added a flag in SharingStage to enable server autodiscovery. Uses the first server that responds to connect to.

--- Assets/HoloToolkit/Sharing/Scripts/SharingStage.cs
+++ Assets/HoloToolkit/Sharing/Scripts/SharingStage.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace HoloToolkit.Sharing
 {
     public class SharingStage : MonoBehaviour
     {
+        /// <summary>
+        /// SharingManagerConnected event notifies when the sharing manager is created and connected.
+        /// </summary>
+        public event EventHandler SharingManagerConnected;
+
         public static SharingStage Instance = null;
 
         /// <summary>
@@ -24,10 +30,27 @@ namespace HoloToolkit.Sharing
         /// </summary>
         public bool IsAudioEndpoint = true;
 
+        public bool AutoDiscoverServer = false;
+
         /// <summary>
         /// Pipes XTools console output to Unity's output window for debugging
         /// </summary>
         private ConsoleLogWriter logWriter;
+
+        /// <summary>
+        /// Enables Server Discovery on the network
+        /// </summary>
+        private DiscoveryClient discoveryClient;
+        /// <summary>
+        /// Provides callbacks when server is discovered or lost.
+        /// </summary>
+        private DiscoveryClientAdapter listener;
+        /// <summary>
+        /// Determines how often the discovery service should ping the network in search of a server.
+        /// </summary>
+        public float PingIntervalSec = 2;
+        private float PingIntervalCurrent = 0;
+        private bool IsTryingToFindServer = false;
 
         private void Awake()
         {
@@ -35,34 +58,53 @@ namespace HoloToolkit.Sharing
 
             this.logWriter = new ConsoleLogWriter();
 
-            ClientConfig config = new ClientConfig(this.ClientRole);
-            config.SetIsAudioEndpoint(this.IsAudioEndpoint);
-            config.SetLogWriter(this.logWriter);
-            config.SetServerAddress(this.ServerAddress);
-            config.SetServerPort(this.ServerPort);
-            config.SetProfilerEnabled(false);
-
-            this.sharingMgr = SharingManager.Create(config);
+            if (AutoDiscoverServer)
+            {
+                AutoDiscoverInit();
+            }
+            else
+            {
+                Connect();
+            }
         }
 
         protected void OnDestroy()
         {
             Instance = null;
 
-            // Force a disconnection so that we can stop and start Unity without connections hanging around
-            this.sharingMgr.GetPairedConnection().Disconnect();
-            this.sharingMgr.GetServerConnection().Disconnect();
+            if (this.discoveryClient != null)
+            {
+                discoveryClient.RemoveListener(listener);
+                discoveryClient.Dispose();
+                discoveryClient = null;
 
-            // Release the XTools manager so that it cleans up the C++ copy
-            this.sharingMgr.Dispose();
-            this.sharingMgr = null;
+                if (listener != null)
+                {
+                    listener.Dispose();
+                    listener = null;
+                }
+            }
 
+            if (this.sharingMgr != null)
+            {
+                // Force a disconnection so that we can stop and start Unity without connections hanging around
+                this.sharingMgr.GetPairedConnection().Disconnect();
+                this.sharingMgr.GetServerConnection().Disconnect();
+
+                // Release the XTools manager so that it cleans up the C++ copy
+                this.sharingMgr.Dispose();
+                this.sharingMgr = null;
+            }
             // Forces a garbage collection to try to clean up any additional reference to SWIG-wrapped objects
             System.GC.Collect();
         }
 
         private void LateUpdate()
         {
+            if (this.IsTryingToFindServer)
+            {
+                AutoDiscoverUpdate();
+            }
             if (this.sharingMgr != null)
             {
                 // Update the XToolsManager to processes any network messages that have arrived
@@ -78,6 +120,75 @@ namespace HoloToolkit.Sharing
         private void OnDisable()
         {
             Application.logMessageReceived -= HandleLog;
+        }
+
+        private void Connect()
+        {
+            ClientConfig config = new ClientConfig(this.ClientRole);
+            config.SetIsAudioEndpoint(this.IsAudioEndpoint);
+            config.SetLogWriter(this.logWriter);
+            config.SetServerAddress(this.ServerAddress);
+            config.SetServerPort(this.ServerPort);
+            config.SetProfilerEnabled(false);
+
+            this.sharingMgr = SharingManager.Create(config);
+
+            //delay sending notification so everything is initialized properly
+            Invoke("SendConnectedNotification", 1);
+        }
+
+        private void SendConnectedNotification()
+        {
+            if (Manager.GetServerConnection().IsConnected())
+            {
+                //Send notification that we're connected
+                EventHandler connectedEvent = SharingManagerConnected;
+                if (connectedEvent != null)
+                {
+                    connectedEvent(this, EventArgs.Empty);
+                }
+            }
+            else
+            {
+                Log.Error(string.Format("Cannot connect to server {0}:{1}", ServerAddress, ServerPort));
+            }
+        }
+
+        private void AutoDiscoverInit()
+        {
+            listener = new DiscoveryClientAdapter();
+            listener.DiscoveredEvent += OnSystemDiscovered;
+
+            discoveryClient = DiscoveryClient.Create();
+            discoveryClient.AddListener(listener);
+
+            //Start Finding Server
+            IsTryingToFindServer = true;
+        }
+
+        private void AutoDiscoverUpdate()
+        {
+            //Searching Enabled-> Update DiscoveryClient to check results, Wait Interval then Ping network.
+            PingIntervalCurrent += Time.deltaTime;
+            if (PingIntervalCurrent > PingIntervalSec)
+            {
+                PingIntervalCurrent = 0;
+                discoveryClient.Ping();
+            }
+            discoveryClient.Update();
+        }
+
+        private void OnSystemDiscovered(DiscoveredSystem obj)
+        {
+            if (obj.GetRole() == SystemRole.SessionDiscoveryServerRole)
+            {
+                //Found a server. Stop pinging the network and connect
+                IsTryingToFindServer = false;
+                this.ServerAddress = obj.GetAddress();
+                Debug.Log("System Discovered at: " + this.ServerAddress);
+                Connect();
+                Debug.Log(string.Format("Connected to: {0}:{1}", this.ServerAddress, this.ServerPort));
+            }
         }
 
         void HandleLog(string logString, string stackTrace, LogType type)
