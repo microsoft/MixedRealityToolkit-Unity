1f11fde727e3dc47ff69e3de7ef7936dcdc306f6
Julia Schwarz <jschwarz@microsoft.com>
2016-08-22 00:46:28 -0700

    Build Window - Add option to auto-increment appx version
    - Add checkbox "Increment version" in build window
    - If checked, build a new appx, incrementing the last number in
      Major.Minor.Build.Revision. Example: 1.0.0.3 => 1.0.0.4
    
    This can be useful for easily checkpointing your work, especially when
    you're actively working on something and somebody asks for a demo it's
    easy to just install an older version.

--- Assets/HoloToolkit/Build/Editor/BuildDeployPrefs.cs
+++ Assets/HoloToolkit/Build/Editor/BuildDeployPrefs.cs
@@ -16,6 +16,7 @@ namespace HoloToolkit.Unity
         private const string EditorPrefs_BuildDir = "BuildDeployWindow_BuildDir";
         private const string EditorPrefs_BuildConfig = "BuildDeployWindow_BuildConfig";
         private const string EditorPrefs_ForceRebuild = "BuildDeployWindow_ForceBuild";
+        private const string EditorPrefs_IncrementBuildVersion = "BuildDeployWindow_IncrementBuildVersion";
         private const string EditorPrefs_MSBuildVer = "BuildDeployWindow_MSBuildVer";
         private const string EditorPrefs_TargetIPs = "BuildDeployWindow_DestIPs";
         private const string EditorPrefs_DeviceUser = "BuildDeployWindow_DeviceUser";
@@ -45,6 +46,11 @@ namespace HoloToolkit.Unity
         {
             get { return GetEditorPref(EditorPrefs_ForceRebuild, false); }
             set { EditorPrefs.SetBool(EditorPrefs_ForceRebuild, value); }
+        }
+        public static bool IncrementBuildVersion
+        {
+            get { return GetEditorPref(EditorPrefs_IncrementBuildVersion, true); }
+            set { EditorPrefs.SetBool(EditorPrefs_IncrementBuildVersion, value); }
         }
         public static string TargetIPs
         {
