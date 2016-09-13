c2a0a42db0110a4dc5a1280e150f13e0a1ba2bb1
András Velvárt <github@response.hu>
2016-09-13 13:21:06 +0200

    fixed cosmetic issues as per the requests of the reviewers.

--- Assets/HoloToolkit/Input/Scripts/GameControllerManipulator.cs
+++ Assets/HoloToolkit/Input/Scripts/GameControllerManipulator.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using UnityEngine;
-using System.Collections;
-using HoloToolkit.Unity;
+﻿using UnityEngine;
 
 namespace HoloToolkit.Unity
 {
@@ -50,7 +47,7 @@ namespace HoloToolkit.Unity
         [Tooltip("Move the gaze target instead of the GameObject this script is attached to")]
         public bool MoveGazeTarget = false;
 
-        private GameObject lastManipulatedObject;
+        private GameObject lastAffectedObject;
         private float stickDistance = 0;
 
         // Update is called once per frame
@@ -59,12 +56,18 @@ namespace HoloToolkit.Unity
             GameObject go;
 
             if (!MoveGazeTarget)
+            {
                 go = gameObject;
+            }
             else
-                go = lastManipulatedObject ?? GazeManager.Instance.FocusedObject;
+            {
+                go = lastAffectedObject ?? GazeManager.Instance.FocusedObject;
+            }
 
             if (go == null)
+            {
                 return;
+            }
 
             var cameraTransform = Camera.main.transform;
 
@@ -75,6 +78,7 @@ namespace HoloToolkit.Unity
             float rz = 0;
             float ry = 0;
             float rx = 0;
+
             if (enableRotate)
             {
                 rz = RotateAroundAxis(go, RotateAroundZAxisName, cameraTransform.forward, RotateAroundZAxisSpeed);
@@ -84,8 +88,8 @@ namespace HoloToolkit.Unity
 
             //Move
             bool usingSameAxisForRotateAndMove = RotateAroundYAxisName == MoveXAxisName ||
-                                                 RotateAroundYAxisName == MoveZAxisName
-                                                 || RotateAroundZAxisName == MoveXAxisName ||
+                                                 RotateAroundYAxisName == MoveZAxisName || 
+                                                 RotateAroundZAxisName == MoveXAxisName ||
                                                  RotateAroundZAxisName == MoveYAxisName;
 
             float x = 0;
@@ -94,33 +98,42 @@ namespace HoloToolkit.Unity
             //only move if the movement axis has not been used for rotation
             if (!(enableRotate && usingSameAxisForRotateAndMove))
             {
-                x = Input.GetAxis(MoveXAxisName)*MoveXAxisSpeed * 60 * Time.deltaTime;
+                x = Input.GetAxis(MoveXAxisName) * MoveXAxisSpeed * 60 * Time.deltaTime;
                 go.transform.RotateAround(cameraTransform.position, cameraTransform.up, x);
 
-                y = Input.GetAxis(MoveYAxisName)*MoveYAxisSpeed * 60 * Time.deltaTime;
+                y = Input.GetAxis(MoveYAxisName) * MoveYAxisSpeed * 60 * Time.deltaTime;
                 go.transform.RotateAround(cameraTransform.position, cameraTransform.right, y);
             }
 
-            var z = Input.GetAxis(MoveZAxisName)*MoveZAxisSpeed * 60 * Time.deltaTime;
+            var z = Input.GetAxis(MoveZAxisName) * MoveZAxisSpeed * 60 * Time.deltaTime;
             go.transform.position += cameraTransform.forward*z*-0.03f;
 
             var stickInFrontOfMe = Input.GetButton(MoveWithGazeButtonName);
+
             if (stickInFrontOfMe)
             {
                 if (stickDistance == 0)
+                {
                     stickDistance = (go.transform.position - cameraTransform.position).magnitude;
+                }
 
                 go.transform.position = cameraTransform.position + cameraTransform.forward * stickDistance;
             }
             else
+            {
                 stickDistance = 0;
+            }
 
 
             if (stickInFrontOfMe || x != 0 || y != 0 || z != 0 || rz != 0 || ry != 0 || rx != 0)
-                lastManipulatedObject = go;
+            {
+                lastAffectedObject = go;
+            }
 #if !UNITY_EDITOR
-        else
-           lastManipulatedObject = null; //easier to test in Unity this way
+            else
+            {
+                lastAffectedObject = null; //easier to test in Unity this way
+            }
 #endif
         }
 
@@ -135,9 +148,11 @@ namespace HoloToolkit.Unity
         private float RotateAroundAxis(GameObject go, string joyAxisName, Vector3 vectorToRotateAround, float speed)
         {
             if (string.IsNullOrEmpty(joyAxisName))
+            {
                 return 0;
+            }
 
-            var result = Input.GetAxis(joyAxisName)*speed * 60 * Time.deltaTime;
+            var result = Input.GetAxis(joyAxisName) * speed * 60 * Time.deltaTime;
             go.transform.rotation = Quaternion.Euler(vectorToRotateAround * result) * go.transform.rotation;
             return result;
         }
