40fd264396f91591e64e84a3d84cbbf4ae6a1e10
Patrick Cook <pcook@microsoft.com>
2016-09-26 09:28:23 -0700

    Create WorldAnchorManager to abstract saving and loading of persisted anchors Updated TapToPlace to use this script. Updated TapToPlace test scene to include the WorldAnchorManager on the Managers GameObject.

--- Assets/HoloToolkit/SpatialMapping/Scripts/TapToPlace.cs
+++ Assets/HoloToolkit/SpatialMapping/Scripts/TapToPlace.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using UnityEngine;
-using UnityEngine.VR.WSA;
-using UnityEngine.VR.WSA.Persistence;
 
 namespace HoloToolkit.Unity
 {
@@ -24,44 +22,16 @@ namespace HoloToolkit.Unity
         public string SavedAnchorFriendlyName = "SavedAnchorFriendlyName";
 
         /// <summary>
-        /// Keeps track of anchors stored on local device.
+        /// Manages persisted anchors.
         /// </summary>
-        WorldAnchorStore anchorStore = null;
-
-        /// <summary>
-        /// Locally saved wold anchor.
-        /// </summary>
-        WorldAnchor savedAnchor;
+        WorldAnchorManager anchorManager;
 
         bool placing = false;
 
         void Start()
         {
-            WorldAnchorStore.GetAsync(AnchorStoreReady);
-        }
-
-        /// <summary>
-        /// Called when the local anchor store is ready.
-        /// </summary>
-        /// <param name="store"></param>
-        void AnchorStoreReady(WorldAnchorStore store)
-        {
-            anchorStore = store;
-
-            // Try to load a previously saved world anchor.
-            savedAnchor = anchorStore.Load(SavedAnchorFriendlyName, gameObject);
-            if (savedAnchor == null)
-            {
-                // Either world anchor was not saved / does not exist or has a different name.
-                Debug.Log(gameObject.name + " : "+ "World anchor could not be loaded for this game object. Creating a new anchor.");
-
-                // Create anchor since one does not exist.
-                CreateAnchor();
-            }
-            else
-            {
-                Debug.Log(gameObject.name + " : " + "World anchor loaded from anchor store and updated for this game object.");
-            }
+            anchorManager = WorldAnchorManager.Instance;
+            anchorManager.AttachAnchor(this.gameObject, SavedAnchorFriendlyName);
         }
 
         // Called by GazeGestureManager when the user performs a tap gesture.
@@ -77,24 +47,16 @@ namespace HoloToolkit.Unity
                 {
                     SpatialMappingManager.Instance.DrawVisualMeshes = true;
 
-                    Debug.Log(gameObject.name + " : " + "Removing existing world anchor if any.");
+                    Debug.Log(gameObject.name + " : Removing existing world anchor if any.");
 
-                    // Remove existing world anchor when moving an object.
-                    DestroyImmediate(gameObject.GetComponent<WorldAnchor>());
-
-                    // Delete existing world anchor from anchor store when moving an object.
-                    if (anchorStore != null)
-                    {
-                        anchorStore.Delete(SavedAnchorFriendlyName);
-                    }
+                    anchorManager.RemoveAnchor(gameObject);
                 }
                 // If the user is not in placing mode, hide the spatial mapping mesh.
                 else
                 {
                     SpatialMappingManager.Instance.DrawVisualMeshes = false;
-
                     // Add world anchor when object placement is done.
-                    CreateAnchor();
+                    anchorManager.AttachAnchor(gameObject, SavedAnchorFriendlyName);
                 }
             }
             else
@@ -103,57 +65,11 @@ namespace HoloToolkit.Unity
             }
         }
 
-        private void CreateAnchor()
-        {
-            // NOTE: It's good practice to ensure your parent hierarchy or root game object does not already have a World Anchor.
-            // You can handle this in a way that works best for your application.
-            // For example: gameObject.transform.root.GetComponent<WorldAnchor>();
-
-            // Add the world anchor component when done moving an object.
-            WorldAnchor anchor = gameObject.AddComponent<WorldAnchor>();
-            if (anchor.isLocated)
-            {
-                SaveAnchor(anchor);
-            }
-            else
-            {
-                anchor.OnTrackingChanged += Anchor_OnTrackingChanged;
-            }
-        }
-
-        private void SaveAnchor(WorldAnchor anchor)
-        {
-            // Save the anchor to persist holograms across sessions.
-            if (anchorStore.Save(SavedAnchorFriendlyName, anchor))
-            {
-                Debug.Log(gameObject.name + " : " + "World anchor saved successfully.");
-            }
-            else
-            {
-                Debug.LogError(gameObject.name + " : " + "World anchor save failed.");
-            }
-        }
-
-        private void Anchor_OnTrackingChanged(WorldAnchor self, bool located)
-        {
-            if (located)
-            {
-                Debug.Log(gameObject.name + " : " + "World anchor located successfully.");
-
-                SaveAnchor(self);
-                self.OnTrackingChanged -= Anchor_OnTrackingChanged;
-            }
-            else
-            {
-                Debug.LogError(gameObject.name + " : " + "World anchor failed to locate.");
-            }
-        }
-
         void Update()
         {
-                // If the user is in placing mode,
-                // update the placement to match the user's gaze.
-                if (placing)
+            // If the user is in placing mode,
+            // update the placement to match the user's gaze.
+            if (placing)
             {
                 // Do a raycast into the world that will only hit the Spatial Mapping mesh.
                 var headPosition = Camera.main.transform.position;
---
+++ Assets/HoloToolkit/Utilities/Scripts/WorldAnchorManager.cs
@@ -0,0 +1,216 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using UnityEngine.VR.WSA.Persistence;
+using System.Collections.Generic;
+using UnityEngine.VR.WSA;
+
+namespace HoloToolkit.Unity
+{
+    /// <summary>
+    /// Wrapper around world anchor store to streamline some of the
+    /// persistence api busy work.
+    /// </summary>
+    public class WorldAnchorManager : Singleton<WorldAnchorManager>
+    {
+        /// <summary>
+        /// To prevent initializing too many anchors at once
+        /// and to allow for the WorldAnchorStore to load asyncronously
+        /// without callers handling the case where the store isn't loaded yet
+        /// we'll setup a queue of anchor attachment operations.  
+        /// The AnchorAttachmentInfo struct has the data needed to do this.
+        /// </summary>
+        private struct AnchorAttachmentInfo
+        {
+            public GameObject gameObjectToAnchor { get; set; }
+            public string anchorName { get; set; }
+        }
+
+        /// <summary>
+        /// The queue mentioned above.
+        /// </summary>
+        private Queue<AnchorAttachmentInfo> anchorOperations = new Queue<AnchorAttachmentInfo>();
+
+        /// <summary>
+        /// The WorldAnchorStore for the current application.  
+        /// Can be null when the application starts.
+        /// </summary>
+        public WorldAnchorStore anchorStore { get; set; }
+
+        /// <summary>
+        /// Callback function that contains the WorldAnchorStore object.
+        /// </summary>
+        /// <param name="Store">The WorldAnchorStore to cache.</param>
+        void AnchorStoreReady(WorldAnchorStore Store)
+        {
+            anchorStore = Store;
+        }
+
+        /// <summary>
+        /// When the app starts grab the anchor store immediately.
+        /// </summary>
+        void Awake()
+        {
+            anchorStore = null;
+            WorldAnchorStore.GetAsync(AnchorStoreReady);
+        }
+
+        /// <summary>
+        /// Each frame see if there is work to do and if we can do a unit, do it.
+        /// </summary>
+        void Update()
+        {
+            if (anchorStore != null && anchorOperations.Count > 0)
+            {
+                DoAnchorOperation(anchorOperations.Dequeue());
+            }
+        }
+
+        /// <summary>
+        /// Attaches an anchor to the game object.  If the anchor store has
+        /// an anchor with the specified name it will load the acnhor, otherwise
+        /// a new anchor will be saved under the specified name.
+        /// </summary>
+        /// <param name="gameObjectToAnchor">The Gameobject to attach the anchor to.</param>
+        /// <param name="AnchorName">Name of the anchor.</param>
+        public void AttachAnchor(GameObject gameObjectToAnchor, string AnchorName)
+        {
+            if (gameObjectToAnchor == null)
+            {
+                Debug.LogError("Must pass in a valid gameObject");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(AnchorName))
+            {
+                Debug.LogError("Must supply an AnchorName.");
+                return;
+            }
+
+            anchorOperations.Enqueue(
+                new AnchorAttachmentInfo()
+                {
+                    gameObjectToAnchor = gameObjectToAnchor,
+                    anchorName = AnchorName
+                }
+                );
+        }
+
+        /// <summary>
+        /// Removes the anchor from the game object and deletes the anchor
+        /// from the anchor store.
+        /// </summary>
+        /// <param name="gameObjectToUnanchor">gameObject to remove the anchor from.</param>
+        public void RemoveAnchor(GameObject gameObjectToUnanchor)
+        {
+            // This case is unexpected, but just in case.
+            if (anchorStore == null)
+            {
+                Debug.LogError("remove anchor called before anchor store is ready.");
+            }
+
+            WorldAnchor anchor = gameObjectToUnanchor.GetComponent<WorldAnchor>();
+
+            if (anchor != null)
+            {
+                anchorStore.Delete(anchor.name);
+                DestroyImmediate(anchor);
+            }
+        }
+
+        /// <summary>
+        /// Function that actually adds the anchor to the game object.
+        /// </summary>
+        /// <param name="anchorAttachmentInfo">Parameters for attaching the anchor.</param>
+        void DoAnchorOperation(AnchorAttachmentInfo anchorAttachmentInfo)
+        {
+            string AnchorName = anchorAttachmentInfo.anchorName;
+            GameObject gameObjectToAnchor = anchorAttachmentInfo.gameObjectToAnchor;
+
+            if (gameObjectToAnchor == null)
+            {
+                Debug.Log("GameObject must have been destroyed before we got a chance to anchor it.");
+                return;
+            }
+
+            // Try to load a previously saved world anchor.
+            WorldAnchor savedAnchor = anchorStore.Load(AnchorName, gameObjectToAnchor);
+            if (savedAnchor == null)
+            {
+                // Either world anchor was not saved / does not exist or has a different name.
+                Debug.Log(gameObjectToAnchor.name + " : World anchor could not be loaded for this game object. Creating a new anchor.");
+
+                // Create anchor since one does not exist.
+                CreateAnchor(gameObjectToAnchor, AnchorName);
+            }
+            else
+            {
+                savedAnchor.name = AnchorName;
+                Debug.Log(gameObjectToAnchor.name + " : World anchor loaded from anchor store and updated for this game object.");
+            }
+        }
+
+        /// <summary>
+        /// Creates an anchor, attaches it to the gameObjectToAnchor, and saves the anchor to the anchor store.
+        /// </summary>
+        /// <param name="gameObjectToAnchor">The GameObject to attach the anchor to.</param>
+        /// <param name="anchorName">The name to give to the anchor.</param>
+        void CreateAnchor(GameObject gameObjectToAnchor, string anchorName)
+        {
+            WorldAnchor anchor = gameObjectToAnchor.AddComponent<WorldAnchor>();
+            anchor.name = anchorName;
+
+            // Sometimes the anchor is located immediately. In that case it can be saved immediately.
+            if (anchor.isLocated)
+            {
+                SaveAnchor(anchor);
+            }
+            else
+            {
+                // Othertimes we must wait for the 
+                anchor.OnTrackingChanged += Anchor_OnTrackingChanged;
+            }
+        }
+
+        /// <summary>
+        /// When an anchor isn't located immediately we subscribe to this event so
+        /// we can save the anchor when it is finally located.
+        /// </summary>
+        /// <param name="self">The anchor that is reporting a tracking changed event.</param>
+        /// <param name="located">Indicates if the anchor is located or not located.</param>
+        private void Anchor_OnTrackingChanged(WorldAnchor self, bool located)
+        {
+            if (located)
+            {
+                Debug.Log(gameObject.name + " : World anchor located successfully.");
+
+                SaveAnchor(self);
+
+                // Once the anchor is located we can unsubscribe from this event.
+                self.OnTrackingChanged -= Anchor_OnTrackingChanged;
+            }
+            else
+            {
+                Debug.LogError(gameObject.name + " : World anchor failed to locate.");
+            }
+        }
+
+        /// <summary>
+        /// Saves the anchor to the anchor store.
+        /// </summary>
+        /// <param name="anchor"></param>
+        private void SaveAnchor(WorldAnchor anchor)
+        {
+            // Save the anchor to persist holograms across sessions.
+            if (anchorStore.Save(anchor.name, anchor))
+            {
+                Debug.Log(gameObject.name + " : World anchor saved successfully.");
+            }
+            else
+            {
+                Debug.LogError(gameObject.name + " : World anchor save failed.");
+            }
+        }
+    }
+}
