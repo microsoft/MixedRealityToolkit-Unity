4ec9da93ea77a32c6ec9c62ee51a304e8daf9b5d
Steven Liss <stlis@microsoft.com>
2016-09-27 13:15:46 -0700

    Clean up HeadsUpDirectionIndicator code

--- Assets/HoloToolkit/Utilities/Scripts/HeadsUpDirectionIndicator.cs
+++ Assets/HoloToolkit/Utilities/Scripts/HeadsUpDirectionIndicator.cs
@@ -1,146 +1,125 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class HeadsUpDirectionIndicator : MonoBehaviour {
-    public GameObject targetObject;
-
-	// Use this for initialization
-	void Start () {
-	}
-
-    void UpdateAsPlane()
+namespace HoloToolkit.Unity
+{
+    public class HeadsUpDirectionIndicator : MonoBehaviour
     {
-        var targetPosition = targetObject.GetComponent<Transform>().position;
-        var vecToTarget = targetPosition - Camera.main.transform.position;
-        var planes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+        public GameObject targetObject;
 
-        float hit = 0.0f;
-        if (planes[4].Raycast(new Ray(Camera.main.transform.position, vecToTarget), out hit))
+        void Start() { }
+
+        // Update is called once per frame
+        void Update()
         {
-            var hitPosition = Camera.main.transform.position + hit * vecToTarget;
-            var screenPoint = Camera.main.WorldToScreenPoint(hitPosition);
-
-            var screenPointInView = screenPoint;
-            screenPointInView.x = Mathf.Clamp(screenPoint.x, 0, Screen.width);
-            screenPointInView.y = Mathf.Clamp(screenPoint.y, 0, Screen.height);
-
-            screenPoint.x = screenPoint.x - Screen.width / 2;
-            screenPoint.y = screenPoint.y - Screen.height / 2;
-
-            screenPointInView.x = screenPointInView.x - Screen.width / 2;
-            screenPointInView.y = screenPointInView.y - Screen.height / 2;
-
-            var adjustDirection = screenPoint - screenPointInView;
-            //if (adjustDirection == Vector3.zero)
+            if (gameObject == null)
             {
-                adjustDirection = screenPointInView - Vector3.zero;
-                adjustDirection.z = 0.0f;
+                // no target
+                return;
             }
 
-            this.GetComponent<RectTransform>().anchoredPosition = screenPointInView;
-            this.GetComponent<RectTransform>().up = adjustDirection.normalized;
+            Vector3 position = Vector3.zero;
+
+            var targetPosition = targetObject.GetComponent<Transform>().position;
+
+            if (this.PointInInfiniteFrustum(targetPosition))
+            {
+                position = GetIndicatorPositionInView(targetPosition, Camera.main, Screen.width, Screen.height);
+            }
+            else
+            {
+                position = GetIndicatorPositionNotInView(targetPosition, Camera.main, Screen.width, Screen.height);
+            }
+
+            var alignment = position;
+            alignment.z = 0.0f;
+
+            this.GetComponent<RectTransform>().anchoredPosition = position;
+            this.GetComponent<RectTransform>().up = alignment.normalized;
+        }
+
+        // Provides the GUI space position of the indicator assuming it should be point off-screen.
+        private Vector3 GetIndicatorPositionNotInView(Vector3 targetPosition, Camera camera, int viewportWidth, int viewportHeight)
+        {
+            var screenPoint = camera.WorldToScreenPoint(targetPosition);
+
+            // negative z means the point is behind the camera and the coordinates flip.
+            if (screenPoint.z < 0.0f)
+            {
+                screenPoint = -screenPoint;
+            }
+
+            // screen space maps 0,0 to the lower left corner but canvase coordinates
+            // map 0,0 to the center of the view.
+            screenPoint.x = screenPoint.x - viewportWidth / 2;
+            screenPoint.y = screenPoint.y - viewportHeight / 2;
+
+            // used to determine which edge of the screen the indicator vector
+            // would exit through.
+            float hRatio = screenPoint.x / (float)viewportWidth;
+            float vRatio = screenPoint.y / (float)viewportHeight;
+
+            if (Mathf.Abs(hRatio) > Mathf.Abs(vRatio))
+            {
+                // in this case, the vector collides with the sides first.
+                float slope = screenPoint.y / screenPoint.x;
+                if (screenPoint.x > 0.0)
+                {
+                    screenPoint.x = viewportWidth / 2;
+                }
+                else
+                {
+                    screenPoint.x = -viewportWidth / 2;
+                }
+
+                screenPoint.y = slope * screenPoint.x;
+            }
+            else
+            {
+                // in this case, the vector collides with the top or bottom first;
+                float invSlope = screenPoint.x / screenPoint.y;
+                if (screenPoint.y > 0.0f)
+                {
+                    screenPoint.y = viewportHeight / 2;
+                }
+                else
+                {
+                    screenPoint.y = -viewportHeight / 2;
+                }
+
+                screenPoint.x = invSlope * screenPoint.y;
+            }
+
+            return screenPoint;
+        }
+
+        // Provides the GUI space position of the indicator assuming it should point to a position on-screen.
+        private Vector3 GetIndicatorPositionInView(Vector3 targetPosition, Camera camera, int viewportWidth, int viewportHeight)
+        {
+            var screenPoint = camera.WorldToScreenPoint(targetPosition);
+
+            // screen space maps 0,0 to the lower left corner but canvase coordinates
+            // map 0,0 to the center of the view.
+            screenPoint.x = screenPoint.x - viewportWidth / 2;
+            screenPoint.y = screenPoint.y - viewportHeight / 2;
+
+            return screenPoint;
+        }
+
+        private bool PointInInfiniteFrustum(Vector3 point)
+        {
+            var frustumPlanes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+
+            // Don't test against the far plane, which is the last in the array.
+            for (int i = 0; i < 5; ++i)
+            {
+                var plane = frustumPlanes[i];
+                if (!plane.GetSide(point))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
-
-    Vector3 ProjectPointToPlane(Vector3 planeNormal, Vector3 pointOnPlane, Vector3 v) {
-        var vRelative = v - pointOnPlane;
-        float normalBias = Vector3.Dot(planeNormal, vRelative);
-        var vProjected = vRelative - normalBias * planeNormal;
-        var vDisplaced = pointOnPlane + vProjected;
-        return vDisplaced;
-    }
-
-    bool PointInFrustum(Vector3 point)
-    {
-        var frustumPlanes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
-
-        // Don't test against the far plane, which is the last in the array.
-        for (int i = 0; i < 5; ++i) {
-            var plane = frustumPlanes[i];
-            if (!plane.GetSide(point)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    Vector3 GetIndicatorDispositionNotInView() {
-        var targetPosition = targetObject.GetComponent<Transform>().position;
-        var screenPoint = Camera.main.WorldToScreenPoint(targetPosition);
-
-        // negative z means the point is behind the camera and the coordinates flip.
-        if (screenPoint.z < 0.0f) {
-            screenPoint = -screenPoint;
-        }
-
-        var screenPointInView = screenPoint;
-        screenPointInView.x = screenPointInView.x - Screen.width / 2;
-        screenPointInView.y = screenPointInView.y - Screen.height / 2;
-
-        float hRatio = screenPointInView.x / (float)Screen.width;
-        float vRatio = screenPointInView.y / (float)Screen.height;
-
-        if (Mathf.Abs(hRatio) > Mathf.Abs(vRatio)) {
-            // in this case, the vector collides with the sides first.
-            float slope = screenPointInView.y / screenPointInView.x;
-            if (screenPointInView.x > 0.0) {
-                screenPointInView.x = Screen.width / 2;
-            }
-            else {
-                screenPointInView.x = -Screen.width / 2;
-            }
-
-            screenPointInView.y = slope * screenPointInView.x;
-        }
-        else {
-            // in this case, the vector collides with the top or bottom first;
-            float invSlope = screenPointInView.x / screenPointInView.y;
-            if (screenPointInView.y > 0.0f) {
-                screenPointInView.y = Screen.height / 2;
-            }
-            else {
-                screenPointInView.y = -Screen.height / 2;
-            }
-
-            screenPointInView.x = invSlope * screenPointInView.y;
-        }
-
-        return screenPointInView;
-    }
-
-    Vector3 GetIndicatorDispositionInView() {
-        var targetPosition = targetObject.GetComponent<Transform>().position;
-        var screenPoint = Camera.main.WorldToScreenPoint(targetPosition);
-
-        var screenPointInView = screenPoint;
-        screenPointInView.x = screenPointInView.x - Screen.width / 2;
-        screenPointInView.y = screenPointInView.y - Screen.height / 2;
-
-        var adjustDirection = screenPointInView;
-        adjustDirection.z = 0.0f;
-
-        return screenPointInView;
-    }
-
-	// Update is called once per frame
-	void Update () {
-        Vector3 position = Vector3.zero;
-
-        var targetPosition = targetObject.GetComponent<Transform>().position;
-
-        if (this.PointInFrustum(targetPosition)) {
-            position = GetIndicatorDispositionInView();
-        }
-        else {
-            position = GetIndicatorDispositionNotInView();
-        }
-
-        var alignment = position;
-        alignment.z = 0.0f;
-
-        this.GetComponent<RectTransform>().anchoredPosition = position;
-        this.GetComponent<RectTransform>().up = alignment.normalized;
-	}
 }
-
