111062de4a0a81e2a3540cb7d751f674d9fb7b3f
Stephen Hodgson <Stephen.Hodgson@saabusa.com>
2016-09-27 11:36:45 -0400

    updates to gazemanager from merge conflicts

--- Assets/HoloToolkit/Input/Scripts/GazeManager.cs
+++ Assets/HoloToolkit/Input/Scripts/GazeManager.cs
@@ -24,11 +24,25 @@ namespace HoloToolkit.Unity
         public delegate void OnGazeExitEvent(GameObject focusedObject);
         public event OnGazeExitEvent OnGazeExit;
 
+        /// <summary>
+        /// Maximum gaze distance, in meters, for calculating a hit from a GameOjects Collider.
+        /// </summary>
         [Tooltip("Maximum gaze distance, in meters, for calculating a hit.")]
         public float MaxGazeDistance = 15.0f;
 
+        /// <summary>
+        /// Select the layers raycast should target.
+        /// </summary>
         [Tooltip("Select the layers raycast should target.")]
         public LayerMask RaycastLayerMask = Physics.DefaultRaycastLayers;
+
+        /// <summary>
+        /// Use built in gaze stabilization that utilizes gavity wells.
+        /// Change to false if you wish to use your own gabilization calculation
+        /// and extend this class.
+        /// </summary>
+        [Tooltip("Use built in gaze stabilization that utilizes gavity wells.")]
+        public bool UseBuiltInGazeStabilization = true;
 
         /// <summary>
         /// Physics.Raycast result is true if it hits a hologram.
@@ -36,8 +50,7 @@ namespace HoloToolkit.Unity
         public bool Hit { get; private set; }
 
         /// <summary>
-        /// HitInfo property gives access
-        /// to RaycastHit public members.
+        /// HitInfo property gives access to RaycastHit public members.
         /// </summary>
         public RaycastHit HitInfo { get; private set; }
 
@@ -56,21 +69,52 @@ namespace HoloToolkit.Unity
         /// </summary>
         public GameObject FocusedObject { get; private set; }
 
+        /// <summary>
+        /// Checking enables SetFocusPointForFrame to set the stabilization plane.
+        /// </summary>
         [Tooltip("Checking enables SetFocusPointForFrame to set the stabilization plane.")]
         public bool SetStabilizationPlane = true;
+        /// <summary>
+        /// Lerp speed when moving focus point closer.
+        /// </summary>
         [Tooltip("Lerp speed when moving focus point closer.")]
         public float LerpStabilizationPlanePowerCloser = 4.0f;
+
+        /// <summary>
+        /// Lerp speed when moving focus point farther away.
+        /// </summary>
         [Tooltip("Lerp speed when moving focus point farther away.")]
         public float LerpStabilizationPlanePowerFarther = 7.0f;
 
+        /// <summary>
+        /// Helper class that stabilizes gaze using gravity wells
+        /// </summary>
+        public GazeStabilizer GazeStabilization { get; private set; }
+
         private Vector3 gazeOrigin;
         private Vector3 gazeDirection;
+        private Quaternion gazeRotation;
         private float lastHitDistance = 15.0f;
+
+        private void Awake()
+        {
+            if (UseBuiltInGazeStabilization)
+            {
+                GazeStabilization = gameObject.GetComponent<GazeStabilizer>() ??
+                                    gameObject.AddComponent<GazeStabilizer>();
+            }
+        }
 
         private void Update()
         {
             gazeOrigin = Camera.main.transform.position;
             gazeDirection = Camera.main.transform.forward;
+            gazeRotation = Camera.main.transform.rotation;
+
+            if (GazeStabilization != null)
+            {
+                GazeStabilization.UpdateHeadStability(gazeOrigin, gazeRotation);
+            }
 
             UpdateRaycast();
             UpdateStabilizationPlane();
@@ -83,13 +127,18 @@ namespace HoloToolkit.Unity
         {
             // Get the raycast hit information from Unity's physics system.
             RaycastHit hitInfo;
-            Hit = Physics.Raycast(gazeOrigin,
-                           gazeDirection,
-                           out hitInfo,
-                           MaxGazeDistance,
-                           RaycastLayerMask);
+
+            if (GazeStabilization != null)
+            {
+                Hit = Physics.Raycast(GazeStabilization.StableHeadRay, out hitInfo, MaxGazeDistance, RaycastLayerMask);
+            }
+            else
+            {
+                Hit = Physics.Raycast(gazeOrigin, gazeDirection, out hitInfo, MaxGazeDistance, RaycastLayerMask);
+            }
 
             GameObject oldFocusedObject = FocusedObject;
+
             // Update the HitInfo property so other classes can use this hit information.
             HitInfo = hitInfo;
 
