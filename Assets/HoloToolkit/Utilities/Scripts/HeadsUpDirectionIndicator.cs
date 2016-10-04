101eedd0e296d7f7903ed81e88b256c6104abe20
Steven Liss <stlis@microsoft.com>
2016-10-04 14:23:55 -0700

    Address various PR comments

--- Assets/HoloToolkit/Utilities/Scripts/HeadsUpDirectionIndicator.cs
+++ Assets/HoloToolkit/Utilities/Scripts/HeadsUpDirectionIndicator.cs
@@ -15,52 +15,69 @@ namespace HoloToolkit.Unity
     public class HeadsUpDirectionIndicator : MonoBehaviour
     {
         [Tooltip("The object the direction indicator will point to.")]
-        public GameObject targetObject;
+        public GameObject TargetObject { get; set; }
 
         [Tooltip("Determines what percentage of the visible field should be margin.")]
         [Range(0.0f, 100.0f)]
-        public float IndicatorMarginPercent;
+        [SerializeField]
+        private float indicatorMarginPercent;
 
-        void Start() { }
+        private RectTransform rectTransform;
+
+        private const string MissingRectTransformErrorMessage = "The use of the HeadsUpDirectionIndicator component requires a RectTransform attached to the same game object.";
+
+        private void Start()
+        {
+            this.rectTransform = this.GetComponent<RectTransform>();
+            
+            if (this.rectTransform == null)
+            {
+                this.gameObject.SetActive(false);
+                Debug.LogError(MissingRectTransformErrorMessage);
+            }
+        }
 
         // Update the direction indicator's position and orientation every frame.
-        void Update()
+        private void Update()
         {
-            if (gameObject == null)
+            if (TargetObject == null)
             {
                 // no target, bail out
                 return;
             }
 
-            float marginFactor = (100.0f - IndicatorMarginPercent) / 100.0f;
+            float marginFactor = (100.0f - indicatorMarginPercent) / 100.0f;
             int indicatorFieldWidth = (int)(marginFactor * (float)Screen.width);
             int indicatorFieldHeight = (int)(marginFactor * (float)Screen.height);
 
-            var targetPosition = targetObject.GetComponent<Transform>().position;
-            var position = GetIndicatorPositionInView(targetPosition, Camera.main, indicatorFieldWidth, indicatorFieldHeight);
+            Vector3 targetPosition = TargetObject.transform.position;
+            Vector3 position = GetIndicatorPositionInView(targetPosition, Camera.main, indicatorFieldWidth, indicatorFieldHeight);
 
-            var alignment = position;
+            Vector3 alignment = position;
             alignment.z = 0.0f;
 
             // With camera space UI we can use the canvas space position
-            this.GetComponent<RectTransform>().anchoredPosition = position;
+            this.rectTransform.anchoredPosition = position;
 
             // but the orientation is in world space. So here we first apply the screen space
             // rotation and then rotate it into alignment with the camera.
-            this.GetComponent<RectTransform>().rotation = Camera.main.transform.rotation * Quaternion.FromToRotation(Vector3.up, alignment.normalized);
+            this.rectTransform.rotation = Camera.main.transform.rotation * Quaternion.FromToRotation(Vector3.up, alignment.normalized);
         }
 
         // Provides the GUI space position of the indicator
         private Vector3 GetIndicatorPositionInView(Vector3 targetPosition, Camera camera, int indicatorAreaWidth, int indicatorAreaHeight)
         {
-            var screenPoint = camera.WorldToScreenPoint(targetPosition);
+            Vector3 screenPoint = camera.WorldToScreenPoint(targetPosition);
 
             // screen space maps 0,0 to the lower left corner but canvase coordinates
             // map 0,0 to the center of the view.
             screenPoint.x = screenPoint.x - Screen.width / 2;
             screenPoint.y = screenPoint.y - Screen.height / 2;
 
-            bool pointNotInsideIndicatorField = screenPoint.x < -indicatorAreaWidth / 2 || screenPoint.y < -indicatorAreaHeight / 2 || screenPoint.x > indicatorAreaWidth / 2 || screenPoint.y > indicatorAreaHeight / 2;
+            bool pointNotInsideIndicatorField = screenPoint.x < -indicatorAreaWidth / 2
+                || screenPoint.y < -indicatorAreaHeight / 2
+                || screenPoint.x > indicatorAreaWidth / 2
+                || screenPoint.y > indicatorAreaHeight / 2;
 
             // negative z means the point is behind the camera and the coordinates flip.
             if (screenPoint.z < 0.0f)
@@ -83,7 +100,7 @@ namespace HoloToolkit.Unity
                 {
                     // in this case, the vector collides with the sides first.
                     float slope = screenPoint.y / screenPoint.x;
-                    if (screenPoint.x > 0.0)
+                    if (screenPoint.x > 0.0f)
                     {
                         screenPoint.x = indicatorAreaWidth / 2;
                     }
