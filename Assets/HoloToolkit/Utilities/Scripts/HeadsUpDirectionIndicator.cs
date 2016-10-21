66255f8e9c4687416646467297017cf5d277a73b
Steven Liss <stlis@microsoft.com>
2016-10-21 14:14:15 -0700

    Add HeadsUpDirectionIndicator file

---
+++ Assets/HoloToolkit/Utilities/Scripts/HeadsUpDirectionIndicator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class HeadsUpDirectionIndicator : MonoBehaviour
+{
+    [Tooltip("The object the direction indicator will point to.")]
+    public GameObject targetObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
