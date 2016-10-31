b5529b78ecc8107223ae6efb5fe7d6bd690f4edb
Patrick Cook <pcook@microsoft.com>
2016-10-31 16:06:23 -0700

    Example scene to show how to do world anchor sharing over UNet.

---
+++ Assets/HoloToolkit-Examples/SharingWithUNET/Scripts/BulletController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Controls little bullets fired into the world.
+/// </summary>
+public class BulletController : MonoBehaviour
+{
+	private void Start ()
+    {
+        // The bullet's transform should be in local space to the Shared Anchor.
+        // Make the shared anchor the parent, but we don't want the transform to try
+        // to 'preserve' the position, so we set false in SetParent.
+        transform.SetParent(SharedCollection.Instance.transform, false);
+
+        // The rigid body has a velocity that needs to be transformed into 
+        // the shared coordinate system.
+        Rigidbody rb = GetComponentInChildren<Rigidbody>();
+        rb.velocity = transform.parent.TransformDirection(rb.velocity);
+    }
+}
