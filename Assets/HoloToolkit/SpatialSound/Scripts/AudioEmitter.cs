9b6bf60278ccb038ebcb14e897ac9dfe7b3f85cf
David Kline <davidkl_work@live.com>
2016-11-04 12:54:29 -0700

    add audio occlusion and test

---
+++ Assets/HoloToolkit/SpatialSound/Scripts/AudioEmitter.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HoloToolkit.Unity
+{
+    [RequireComponent(typeof(AudioSource))]    
+    public class AudioEmitter : MonoBehaviour 
+    {
+        [Tooltip("Time, in seconds, between audio influence updates.  0 indicates to update every frame.")]
+        [Range(0.0f, 1.0f)]
+        public float UpdateInterval = 0.25f;
+
+        [Tooltip("Maximum distance, in m, to look when attempting to find the user and any influencers.")]
+        [Range(1.0f, 50.0f)]
+        public float MaxDistance = 20.0f;
+
+        [Tooltip("Maximum number of objects that will be considered when looking for influencers.")]
+        [Range(5, 25)]
+        [SerializeField]
+        private int MaxObjects = 10;
+
+        // Time of last audio processing update.
+        private DateTime lastUpdate = DateTime.MinValue;
+
+        private AudioSource audioSource;
+        private float initialAudioSourceVolume;
+
+        private RaycastHit[] hits;
+
+	    private void Awake() 
+        {           
+            audioSource = gameObject.GetComponent<AudioSource>();
+            initialAudioSourceVolume = audioSource.volume;
+
+            // Pre-allocate the array that will be used to collect RaycastHit structures.
+            hits = new RaycastHit[MaxObjects];
+        }
+	
+        List<IAudioInfluencer> previousInfluencers = new List<IAudioInfluencer>();
+	    private void Update() 
+        {
+            // Audio influences do not need to be updated every frame.
+            DateTime now = DateTime.Now;
+
+            if ((UpdateInterval * 1000.0f) <= (now - lastUpdate).Milliseconds)
+            {
+                audioSource.volume = initialAudioSourceVolume;
+
+                List<IAudioInfluencer> influencers = GetInfluencers();
+                foreach (IAudioInfluencer influencer in influencers)
+                {
+                    influencer.ApplyEffect(gameObject, audioSource);
+                }
+
+                List<IAudioInfluencer> influencersToRemove = new List<IAudioInfluencer>();
+                foreach (IAudioInfluencer prev in previousInfluencers)
+                {
+                    if (!influencers.Contains(prev))
+                    {
+                        influencersToRemove.Add(prev);
+                    }
+                }
+                RemoveInfluencers(influencersToRemove);
+
+                previousInfluencers = influencers;
+                lastUpdate = now;
+            }
+	    }
+
+        private void RemoveInfluencers(List<IAudioInfluencer> influencers)
+        {
+            foreach (IAudioInfluencer influencer in influencers)
+            {
+                influencer.RemoveEffect(gameObject, audioSource);
+            }
+        }
+
+        private List<IAudioInfluencer> GetInfluencers()
+        {
+            List<IAudioInfluencer> influencers = new List<IAudioInfluencer>();
+
+            // For influencers that take effect only when between the emitter and the user, perform a raycast
+            // from the user in our directiond.
+            Vector3 direction = (gameObject.transform.position - Camera.main.transform.position).normalized;
+            float distance = Vector3.Distance(Camera.main.transform.position, gameObject.transform.position);
+
+            int count = Physics.RaycastNonAlloc(Camera.main.transform.position,
+                                                direction,
+                                                hits,
+                                                distance,
+                                                Physics.DefaultRaycastLayers,
+                                                QueryTriggerInteraction.Ignore);
+            
+            for (int i = 0; i < count; i++)
+            {
+                IAudioInfluencer ai = hits[i].collider.gameObject.GetComponentInParent<IAudioInfluencer>();
+                if (ai != null)
+                {
+                    influencers.Add(ai);
+                }
+            }
+
+            return influencers;
+        }
+    }
+}
---
+++ Assets/HoloToolkit/SpatialSound/Scripts/AudioOccluder.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using UnityEngine;
+
+namespace HoloToolkit.Unity
+{
+    public class AudioOccluder : MonoBehaviour, IAudioInfluencer
+    {
+        private readonly float NeutralFrequency = 22000f;
+
+        [Range(10.0f, 22000.0f)]
+        public Single CutoffFrequency = 5000.0f;
+        
+        [Range(0.0f, 1.0f)]
+        public Single VolumePassThrough = 1.0f;
+
+        // Update is not used, but is kept so that this component can be enabled/disabled.
+        private void Update() 
+        { }
+
+        /// <summary>
+        /// Applies an audio effect.
+        /// </summary>
+        /// <param name="emitter">The GameObject on which the effect is to be applied.</param>
+        /// <param name="audioSource">The AudioSource that will be impacted by the effect.</param>
+        public void ApplyEffect(GameObject emitter,
+                                AudioSource audioSource)
+        {
+            if (!isActiveAndEnabled)
+            { return; }
+
+            if (audioSource == null)
+            {
+                Debug.LogWarning("The specified emitter does not have an attached AudioSource component.");
+                return;
+            }
+
+            // Audio occlusion is performed using a low pass filter.                
+            AudioLowPassFilter lowPass = emitter.GetComponent<AudioLowPassFilter>();
+            if (lowPass == null)
+            { 
+                lowPass = emitter.AddComponent<AudioLowPassFilter>();
+            }
+            lowPass.enabled = true;
+
+            // In the real world, chaining multiple low-pass filters will result in the 
+            // lowest of the cutoff frequencies being the highest pitches heard.
+            lowPass.cutoffFrequency = Mathf.Min(lowPass.cutoffFrequency, CutoffFrequency);
+
+            // Unlike the cutoff frequency, volume pass-through is cumulative.
+            audioSource.volume *= VolumePassThrough;
+        }
+
+        /// <summary>
+        /// Removes a previously applied audio effect.
+        /// </summary>
+        /// <param name="emitter">The GameObject from which the effect is to be removed.</param>
+        /// <param name="audioSource">The AudioSource that will be impacted by the effect.</param>
+        public void RemoveEffect(GameObject emitter,
+                                AudioSource audioSource)
+        {
+            // Note: The audioSource parameter is unused.
+
+            // Audio occlusion is performed using a low pass filter.                
+            AudioLowPassFilter lowPass = emitter.GetComponent<AudioLowPassFilter>();
+            if (lowPass == null) { return; }
+            lowPass.cutoffFrequency = NeutralFrequency;
+            lowPass.enabled = false;
+
+            // Volume attenuation is reset in the emitter
+        }
+    }
+}
