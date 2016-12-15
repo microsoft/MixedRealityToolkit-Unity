7b150380370bc142ca0624ea3b74183edfe77dc0
Antao Almada <antao.almada@gmail.com>
2016-12-15 09:39:34 +0000

    Added a custom editor for SpeechInputSource showing a warning when no keywords are assigned.

---
+++ Assets/HoloToolkit/Input/Scripts/Voice/Editor/SpeechInputSourceEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace HoloToolkit.Unity.InputModule
+{
+    [CustomEditor(typeof(SpeechInputSource)), CanEditMultipleObjects]
+    public class SpeechInputSourceEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            SerializedProperty recognizerStart = serializedObject.FindProperty("RecognizerStart");
+            SerializedProperty keywordsAndKeys = serializedObject.FindProperty("KeywordsAndKeys");
+
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(recognizerStart);
+            EditorGUILayout.PropertyField(keywordsAndKeys, true);
+            serializedObject.ApplyModifiedProperties();
+
+            if (keywordsAndKeys.arraySize == 0)
+            {
+                EditorGUILayout.HelpBox("No keywords have been assigned!", MessageType.Warning);
+            }
+        }
+    }
+}
