a8dc9cab3326af8a0525e88d96df64d032ae8075
Stephen Hodgson <Stephen.Hodgson@saabusa.com>
2016-12-16 16:48:33 -0500

    stubbed out spatial mapping pulse script

--- Assets/HoloToolkit/SpatialMapping/Scripts/Cursor/SpatialMappingPulse.cs
+++ Assets/HoloToolkit/SpatialMapping/Scripts/Cursor/SpatialMappingPulse.cs
@@ -7,6 +7,23 @@ namespace HoloToolkit.Unity.SpatialMapping
 {
     public class SpatialMappingPulse : MonoBehaviour
     {
+        private Renderer cachedRenderer;
+        private MaterialPropertyBlock matPropBlock;
 
+        private void Awake()
+        {
+            matPropBlock = new MaterialPropertyBlock();
+            cachedRenderer = GetComponent<Renderer>();
+        }
+
+        private void DoPulse(Color color)
+        {
+            // Get the current value of the material properties in the renderer.
+            cachedRenderer.GetPropertyBlock(matPropBlock);
+            // Assign our new value.
+            matPropBlock.SetColor("_Color", color);
+            // Apply the edited values to the renderer.
+            cachedRenderer.SetPropertyBlock(matPropBlock);
+        }
     }
 }
