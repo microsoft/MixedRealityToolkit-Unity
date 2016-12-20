bbbd2ad5f872827455a52e279cae8b57678a7add
Stephen Hodgson <Stephen.Hodgson@saabusa.com>
2016-12-20 11:01:35 -0500

    Added Material options to spatial mapping manager editor inspector

--- Assets/HoloToolkit/SpatialMapping/Scripts/Editor/SpatialMappingManagerEditor.cs
+++ Assets/HoloToolkit/SpatialMapping/Scripts/Editor/SpatialMappingManagerEditor.cs
@@ -17,16 +17,67 @@ namespace HoloToolkit.Unity.SpatialMapping
                 spatialMappingManager.SurfaceMaterial);
 
             spatialMappingManager.autoStartObserver = EditorGUILayout.Toggle(
-                    new GUIContent("Auto Start", "Determines if the surface observer should be automatically started."),
-                    spatialMappingManager.autoStartObserver);
+                new GUIContent("Auto Start", "Determines if the surface observer should be automatically started."),
+                spatialMappingManager.autoStartObserver);
 
             spatialMappingManager.DrawVisualMeshes = EditorGUILayout.Toggle(
-                    new GUIContent("Draw Visual Meshes", "Determines if spatial mapping data will be rendered."),
-                    spatialMappingManager.DrawVisualMeshes);
+                new GUIContent("Draw Visual Meshes", "Determines if spatial mapping data will be rendered."),
+                spatialMappingManager.DrawVisualMeshes);
 
             spatialMappingManager.CastShadows = EditorGUILayout.Toggle(
                 new GUIContent("Cast Shadows", "Determines if spatial mapping data will cast shadows."),
                 spatialMappingManager.CastShadows);
+
+            if (spatialMappingManager.SurfaceMaterial.HasProperty("_PulseColor"))
+            {
+                spatialMappingManager.SurfaceMaterial.SetColor(
+                    "_PulseColor",
+                    EditorGUILayout.ColorField(
+                        new GUIContent("Pulse Color", "The Color the Pulse will have when user taps the spatial mesh"),
+                        spatialMappingManager.SurfaceMaterial.GetColor("_PulseColor")
+                    )
+                );
+            }
+
+            if (spatialMappingManager.SurfaceMaterial.HasProperty("_PulseWidth"))
+            {
+                spatialMappingManager.SurfaceMaterial.SetFloat(
+                    "_PulseWidth",
+                    EditorGUILayout.FloatField(
+                        new GUIContent("Pulse Width", "Width of Pulse when flowing over spatial map"),
+                        spatialMappingManager.SurfaceMaterial.GetFloat("_PulseWidth")
+                    )
+                );
+            }
+
+            if (spatialMappingManager.SurfaceMaterial.HasProperty("_UseWireframe"))
+            {
+                bool useWireframe = spatialMappingManager.SurfaceMaterial.GetInt("_UseWireframe") == 1;
+                if (useWireframe)
+                {
+                    if (spatialMappingManager.SurfaceMaterial.HasProperty("_WireframeColor"))
+                    {
+                        spatialMappingManager.SurfaceMaterial.SetColor(
+                            "_WireframeColor",
+                            EditorGUILayout.ColorField(
+                                new GUIContent("Wireframe Color", "The Color the wireframe on the spatial map will have"),
+                                spatialMappingManager.SurfaceMaterial.GetColor("_WireframeColor")
+                            )
+                        );
+                    }
+
+                    if (spatialMappingManager.SurfaceMaterial.HasProperty("_WireframeFill"))
+                    {
+                        spatialMappingManager.SurfaceMaterial.SetFloat(
+                            "_WireframeFill",
+                            EditorGUILayout.FloatField(
+                                new GUIContent("Wireframe thickness", "Thickness of wireframe lines on spatial map"),
+                                spatialMappingManager.SurfaceMaterial.GetFloat("_WireframeFill")
+                            )
+                        );
+                    }
+                }
+            }
         }
     }
 }
