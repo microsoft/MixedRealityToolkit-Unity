05387078e08c96a44450b4d9ba4efce40a446af8
Antao Almada <antao.almada@gmail.com>
2016-12-20 15:03:00 +0000

    Added a basic custom editor for SpeechInputHandler

---
+++ Assets/HoloToolkit/Input/Scripts/Voice/Editor/SpeechInputHandlerEditor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace HoloToolkit.Unity.InputModule
+{
+    [CustomEditor(typeof(SpeechInputHandler))]
+    public class SpeechInputHandlerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            SerializedProperty keywordsAndResponses = serializedObject.FindProperty("KeywordsAndResponses");
+
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(keywordsAndResponses, true);
+            serializedObject.ApplyModifiedProperties();
+
+            // error and warning messages
+            if (keywordsAndResponses.arraySize == 0)
+            {
+                EditorGUILayout.HelpBox("No keywords have been assigned!", MessageType.Warning);
+            }
+        }
+    }
+}
