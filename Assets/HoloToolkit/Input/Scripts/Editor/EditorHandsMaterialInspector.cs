e2183b2fb92c89f1dcf414a61a35bc0d1b8361d6
Stephen Hodgson <Stephen.Hodgson@saabusa.com>
2017-01-26 16:24:06 -0500

    first a little fix for EditorHandsControl

---
+++ Assets/HoloToolkit/Input/Scripts/Editor/EditorHandsMaterialInspector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class EditorHandsMaterialInspector : ShaderGUI
+{
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        foreach (MaterialProperty materialProperty in properties)
+        {
+            if (materialProperty.type != MaterialProperty.PropType.Texture && materialProperty.type != MaterialProperty.PropType.Color || materialProperty.flags != MaterialProperty.PropFlags.PerRendererData)
+            {
+                materialEditor.ShaderProperty(materialProperty, materialProperty.displayName);
+            }
+        }
+
+    }
+}
