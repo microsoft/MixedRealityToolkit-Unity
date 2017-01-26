fe6da230b93f5a2eafd6f3825baa485c42a4c064
Stephen Hodgson <Stephen.Hodgson@saabusa.com>
2017-01-26 16:37:51 -0500

    Simplified property flags check before displaying material properties.

--- Assets/HoloToolkit/Input/Scripts/Editor/EditorHandsMaterialInspector.cs
+++ Assets/HoloToolkit/Input/Scripts/Editor/EditorHandsMaterialInspector.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEditor;
-using UnityEngine;
 
 public class EditorHandsMaterialInspector : ShaderGUI
 {
@@ -7,7 +6,7 @@ public class EditorHandsMaterialInspector : ShaderGUI
     {
         foreach (MaterialProperty materialProperty in properties)
         {
-            if (materialProperty.type != MaterialProperty.PropType.Texture && materialProperty.type != MaterialProperty.PropType.Color || materialProperty.flags != MaterialProperty.PropFlags.PerRendererData)
+            if (materialProperty.flags != MaterialProperty.PropFlags.PerRendererData)
             {
                 materialEditor.ShaderProperty(materialProperty, materialProperty.displayName);
             }
