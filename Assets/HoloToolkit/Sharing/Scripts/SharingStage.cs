0565c4d8b956bd04c7fdc172ea89206606bfafd0
Stephen Hodgson <stephen.hodgson@saabusa.com>
2017-01-30 00:23:23 -0500

    removed missing script reference from sharing.prefab

--- Assets/HoloToolkit/Sharing/Scripts/SharingStage.cs
+++ Assets/HoloToolkit/Sharing/Scripts/SharingStage.cs
@@ -2,9 +2,9 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
+using UnityEngine;
 using HoloToolkit.Sharing.Utilities;
 using HoloToolkit.Unity;
-using UnityEngine;
 
 namespace HoloToolkit.Sharing
 {
