f1a6f63e5245ae860073268b46a7aba016f6add7
András Velvárt <andras.velvart@response.hu>
2017-02-28 22:27:10 +0100

    Headset adjustment scene initial commit.

---
+++ Assets/HoloToolkit/Utilities/Scripts/HeadsetAdjustment.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HeadsetAdjustment : MonoBehaviour, IInputClickHandler, ISpeechHandler {
+
+    public string NextSceneName;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("Clicked");
+        GotoNextScene();
+    }
+
+    private void GotoNextScene()
+    {
+        if (!string.IsNullOrEmpty(NextSceneName))
+        {
+            SceneManager.LoadScene(NextSceneName);
+        }
+        else
+        {
+            var sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex + 1);
+        }
+    }
+
+    public void OnSpeechKeywordRecognized(SpeechKeywordRecognizedEventData eventData)
+    {
+        Debug.Log("Speech command recognized");
+        GotoNextScene();
+    }
+
+    // Use this for initialization
+    void Start () {
+        InputManager.Instance.AddGlobalListener(this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
