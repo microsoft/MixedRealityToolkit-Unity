6468cf384bee213963dd74cb6cc20f1031ef0357
toddwil <toddwil@DESKTOP-KVG660R>
2017-03-23 22:56:51 -0700

    Cleaned up code and added comments

--- Assets/HoloToolkit/Input/Scripts/Cursor/InteractiveCursor.cs
+++ Assets/HoloToolkit/Input/Scripts/Cursor/InteractiveCursor.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,27 +9,51 @@ namespace HoloToolkit.Unity.InputModule
 {
     public class InteractiveCursor : Cursor
     {
+        [Tooltip ("Model to show when collision or source is detected")]
         public GameObject Ring;
+        [Tooltip ("Default cursor model")]
         public GameObject Dot;
+
+        [Tooltip ("Scale when no source or interaction is detected")]
         public float DefaultScale = 0.75f;
+        [Tooltip ("Scale when a tap or gesture is detected")]
         public float DownScale = 0.5f;
+        [Tooltip ("Scale when a source is detected")]
         public float UpScale = 1;
+        [Tooltip ("The lerp time for scaling")]
         public float ScaleTime = 0.5f;
 
-        private bool mRingVisible = false;
-        private bool mScaleDown = false;
-        private float mTimer = 0;
-
+        /// <summary>
+        /// Chached properties for tracking input states over time
+        /// </summary>
         private bool mHasHover = false;
         private bool mHasHand = false;
         private bool mIsDown = false;
-        private Vector3 mBaseScale = new Vector3(1, 1, 1);
+
+        /// <summary>
+        /// Used for lerping the scale in responce to gesture or source
+        /// </summary>
+        private float mTimer = 0;
+
+        /// <summary>
+        /// Base scale for converting the scale values as floats to vector3
+        /// </summary>
+        private Vector3 mBaseScale = Vector3.one;
+
+        /// <summary>
+        /// Cached scale value to lerp toward
+        /// </summary>
         private Vector3 mTargetScale;
 
+        /// <summary>
+        /// Cursor change events for deriving interaction states
+        /// </summary>
+        /// <param name="state"></param>
         public override void OnCursorStateChange(CursorStateEnum state)
         {
             base.OnCursorStateChange(state);
 
+            // check for state changes - reset scale timer
             if (mHasHand != this.IsHandVisible || mIsDown != this.IsInputSourceDown || mHasHover != (this.TargetedObject != null))
             {
                 mTimer = 0;
@@ -36,6 +63,7 @@ namespace HoloToolkit.Unity.InputModule
             mIsDown = this.IsInputSourceDown;
             mHasHover = this.TargetedObject != null;
 
+            // determine if the ring or dot should be visible
             mTargetScale = mBaseScale * DefaultScale;
             bool showRing = false;
 
@@ -72,6 +100,9 @@ namespace HoloToolkit.Unity.InputModule
             
         }
 
+        /// <summary>
+        /// lerp the scale for the ring and dot to the mTargetScale
+        /// </summary>
         protected override void UpdateCursorTransform()
         {
             base.UpdateCursorTransform();
