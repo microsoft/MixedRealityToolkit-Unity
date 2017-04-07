9a28440dc278ff17b322912b4a5d22f6a74b9d36
Stephen Hodgson <hodgson.designs@gmail.com>
2017-04-07 15:54:53 -0400

    updated namespace in reflection extensions

--- Assets/HoloToolkit/CrossPlatform/Scripts/Reflection/ReflectionExtensions.cs
+++ Assets/HoloToolkit/CrossPlatform/Scripts/Reflection/ReflectionExtensions.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-namespace HoloToolkit
+namespace HoloToolkit.Unity
 {
 #if UNITY_METRO && !UNITY_EDITOR
 
