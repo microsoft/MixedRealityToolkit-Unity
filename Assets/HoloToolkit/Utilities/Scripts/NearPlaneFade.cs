0c1388d96cfee18ee22f592a6d111f0ad8151e59
dbastien <dbastien@DBASTIEN-LAPPY>
2017-04-12 15:10:17 -0700

    major update to fastconfigurable shader

--- Assets/HoloToolkit/Utilities/Scripts/NearPlaneFade.cs
+++ Assets/HoloToolkit/Utilities/Scripts/NearPlaneFade.cs
@@ -3,53 +3,52 @@
 
 using UnityEngine;
 
-namespace HoloToolkit.Unity
+/// <summary>
+/// Sets global shader variables relating to near plane fade
+/// </summary>
+[ExecuteInEditMode]
+public class NearPlaneFade : MonoBehaviour
 {
-    /// <summary>
-    /// Updates the shader parameters for use in near plade fading.
-    /// </summary>
-    [ExecuteInEditMode]
-    public class NearPlaneFade : MonoBehaviour
+    public float FadeDistanceStart = 0.85f;
+    public float FadeDistanceEnd = 0.5f;
+
+    public bool NearPlaneFadeOn = true;
+
+    private const string FadeKeywordOn = "_NEAR_PLANE_FADE_ON";
+
+    private int fadeDistancePropertyID;
+
+    void Awake()
     {
-        public float FadeDistanceStart = 0.85f;
-        public float FadeDistanceEnd = 0.5f;
+        fadeDistancePropertyID = Shader.PropertyToID("_NearPlaneFadeDistance");
+        UpdateShaderParams();
+    }
 
-        public bool NearPlaneFadeOn = true;
+    private void OnValidate()
+    {
+        UpdateShaderParams();
+    }
 
-        private const string FadeKeywordOn = "_NEAR_PLANE_FADE_ON";
+    private void UpdateShaderParams()
+    {
+        FadeDistanceStart = Mathf.Max(FadeDistanceStart, 0);
+        FadeDistanceEnd = Mathf.Max(FadeDistanceEnd, 0);
+        FadeDistanceStart = Mathf.Max(FadeDistanceStart, FadeDistanceEnd);
 
-        private int fadeDistancePropertyID;
-
-        private void Start()
-        {
-            fadeDistancePropertyID = Shader.PropertyToID("_NearPlaneFadeDistance");
-            UpdateShaderParams();
-        }
-
-        private void OnValidate()
-        {
-            FadeDistanceStart = Mathf.Max(FadeDistanceStart, 0);
-            FadeDistanceEnd = Mathf.Max(FadeDistanceEnd, 0);
-            FadeDistanceStart = Mathf.Max(FadeDistanceStart, FadeDistanceEnd);
-
-            UpdateShaderParams();
-        }
-
-        private void UpdateShaderParams()
+        if (FadeDistanceStart != FadeDistanceEnd)
         {
             float rangeInverse = 1.0f / (FadeDistanceStart - FadeDistanceEnd);
             var fadeDist = new Vector4(-FadeDistanceEnd * rangeInverse, rangeInverse, 0, 0);
-
             Shader.SetGlobalVector(fadeDistancePropertyID, fadeDist);
+        }
 
-            if (NearPlaneFadeOn)
-            {
-                Shader.EnableKeyword(FadeKeywordOn);
-            }
-            else
-            {
-                Shader.DisableKeyword(FadeKeywordOn);
-            }
+        if (NearPlaneFadeOn)
+        {
+            Shader.EnableKeyword(FadeKeywordOn);
+        }
+        else
+        {
+            Shader.DisableKeyword(FadeKeywordOn);
         }
     }
 }
