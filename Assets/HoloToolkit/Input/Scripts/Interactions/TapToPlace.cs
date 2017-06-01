f302fa762c009d8133e38c7d19200e1914a40294
Stephen Hodgson <hodgson.designs@gmail.com>
2017-05-31 20:05:14 -0400

    Moved TapToPlace from SpatialMapping/Scripts folder to Input/Scripts/Interactions now that we're not dependant on the Spatial Mapping Manager.

--- Assets/HoloToolkit/Input/Scripts/Interactions/TapToPlace.cs
+++ Assets/HoloToolkit/Input/Scripts/Interactions/TapToPlace.cs
@@ -1,10 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using HoloToolkit.Unity.InputModule;
 using UnityEngine;
 
-namespace HoloToolkit.Unity.SpatialMapping
+namespace HoloToolkit.Unity.InputModule
 {
     /// <summary>
     /// The TapToPlace class is a basic way to enable users to move objects 
