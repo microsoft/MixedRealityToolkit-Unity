46348b013a911b252d42dc295484218cee6fb6e2
Stephen Hodgson <hodgson.designs@gmail.com>
2017-06-11 02:23:12 -0400

    added missing license header.

--- Assets/HoloToolkit/Build/Editor/CertificatePasswordWindow.cs
+++ Assets/HoloToolkit/Build/Editor/CertificatePasswordWindow.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+
+using System;
 using UnityEditor;
 using UnityEngine;
 
