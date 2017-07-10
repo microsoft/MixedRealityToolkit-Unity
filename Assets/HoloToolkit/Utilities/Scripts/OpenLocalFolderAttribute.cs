038d0eb6a7ed224bdcbb6e5db9bd20a91999d89e
dbastienMS <dbastien@microsoft.com>
2017-07-10 10:24:47 -0700

    fix attribute duplication, move camera

--- Assets/HoloToolkit/Utilities/Scripts/OpenLocalFolderAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
-using UnityEngine;
-
-namespace HoloToolkit.Unity
-{
-    public class OpenLocalFolderAttribute : PropertyAttribute { }
-}
