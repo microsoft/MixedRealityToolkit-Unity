623825ba2ddcc7a3241b877da1c0d4da22db1ff3
Stephen Hodgson <StephenHodgson@users.noreply.github.com>
2017-07-12 14:15:31 -0700

    removed old commented out code.

--- Assets/HoloToolkit-Examples/SpatialUnderstanding/SpatialUnderstanding-FeatureOverview/Scripts/SpatialUnderstandingCursor.cs
+++ Assets/HoloToolkit-Examples/SpatialUnderstanding/SpatialUnderstanding-FeatureOverview/Scripts/SpatialUnderstandingCursor.cs
@@ -92,7 +92,6 @@ namespace HoloToolkit.Examples.SpatialUnderstandingFeatureOverview
                             Button button = canvasHits[i].gameObject.GetComponent<Button>();
                             if (button != null)
                             {
-                                //hitPos = hitInfo.point;
                                 hitPos = uiRayCastOrigin + uiRayCastDirection * canvasHits[i].distance;
                                 hitNormal = canvasHits[i].gameObject.transform.forward;
                                 hitButton = button;
@@ -117,9 +116,9 @@ namespace HoloToolkit.Examples.SpatialUnderstandingFeatureOverview
 
             // Basic checks
             if ((SpatialUnderstanding.Instance == null) ||
-                ((SpatialUnderstanding.Instance.ScanState != SpatialUnderstanding.ScanStates.Scanning) &&
-                 (SpatialUnderstanding.Instance.ScanState != SpatialUnderstanding.ScanStates.Finishing) &&
-                 (SpatialUnderstanding.Instance.ScanState != SpatialUnderstanding.ScanStates.Done)))
+               ((SpatialUnderstanding.Instance.ScanState != SpatialUnderstanding.ScanStates.Scanning) &&
+                (SpatialUnderstanding.Instance.ScanState != SpatialUnderstanding.ScanStates.Finishing) &&
+                (SpatialUnderstanding.Instance.ScanState != SpatialUnderstanding.ScanStates.Done)))
             {
                 CursorText.gameObject.SetActive(false);
                 return;
