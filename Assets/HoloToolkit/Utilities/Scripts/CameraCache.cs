43797da1884f10ad760c92a604c4c275c3d57e5c
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-08-12 17:40:39 +0200

    Create CameraCache utility class Camera.main calls a FindByTag on the whole scene to return the reference everytime. Cache the reference to the main camera instead and provide a method to overwrite it if necessary

---
+++ Assets/HoloToolkit/Utilities/Scripts/CameraCache.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace HoloToolkit.Unity
+{
+    public static class CameraCache
+    {
+        private static Camera cachedCamera;
+
+        /// <summary>
+        /// Returns a cached reference to the main camera and uses Camera.main if it hasn't been cached yet.
+        /// </summary>
+        public static Camera main
+        {
+            get
+            {
+                return cachedCamera ?? CacheMain(Camera.main);
+            }
+        }
+
+        /// <summary>
+        /// Set the cached camera to a new reference and return it
+        /// </summary>
+        /// <param name="newMain">New main camera to cache</param>
+        /// <returns></returns>
+        private static Camera CacheMain(Camera newMain)
+        {
+            return cachedCamera = newMain;
+        }
+    }
+}
