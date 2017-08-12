9d9eb4c5e0cc957c026f83f315408311429a444b
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-08-12 17:41:36 +0200

    Move camera related MathUtils in new extension Both GetHorizontalFieldOfViewRadians and IsInFov use Camera.main to get the reference to the main camera in their calculations. Set the old methods obsolte and move them into new camera extensions

---
+++ Assets/HoloToolkit/Utilities/Scripts/Extensions/CameraExtensions.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace HoloToolkit.Unity
+{
+    public static class CameraExtensions
+    {
+        /// <summary>
+        /// Get the horizontal FOV from the stereo camera
+        /// </summary>
+        /// <returns></returns>
+        public static float GetHorizontalFieldOfViewRadians(this Camera camera)
+        {
+            float horizontalFovRadians = 2 * Mathf.Atan(Mathf.Tan((camera.fieldOfView * Mathf.Deg2Rad) / 2) * camera.aspect);
+            return horizontalFovRadians;
+        }
+
+        /// <summary>
+        /// Returns if a point will be rendered on the screen in either eye
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public static bool IsInFOV(this Camera camera, Vector3 position)
+        {
+            float verticalFovHalf = camera.fieldOfView / 2;
+            float horizontalFovHalf = camera.GetHorizontalFieldOfViewRadians() * Mathf.Rad2Deg / 2;
+
+            Vector3 deltaPos = position - camera.transform.position;
+            Vector3 headDeltaPos = MathUtils.TransformDirectionFromTo(null, camera.transform, deltaPos).normalized;
+
+            float yaw = Mathf.Asin(headDeltaPos.x) * Mathf.Rad2Deg;
+            float pitch = Mathf.Asin(headDeltaPos.y) * Mathf.Rad2Deg;
+
+            return (Mathf.Abs(yaw) < horizontalFovHalf && Mathf.Abs(pitch) < verticalFovHalf);
+        }
+    }
+}
