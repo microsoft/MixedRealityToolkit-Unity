6a4dea047262968965661b272e7177944beb0fa3
Jan Reinhardt <jan.reinhardt@aitgmbh.de>
2017-08-14 19:01:24 +0200

    Added license header and namespace, changed access modifier

--- Assets/HoloToolkit-Tests/SpatialSound/Scripts/UAudioManagerTest.cs
+++ Assets/HoloToolkit-Tests/SpatialSound/Scripts/UAudioManagerTest.cs
@@ -1,27 +1,31 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 using System.Collections;
-using HoloToolkit.Unity;
 using UnityEngine;
 
-public class UAudioManagerTest : MonoBehaviour
+namespace HoloToolkit.Unity.Tests
 {
-	void Start ()
-	{
-	    StartCoroutine(ContinouslyPlaySounds());
-	}
-
-    private IEnumerator ContinouslyPlaySounds()
+    public class UAudioManagerTest : MonoBehaviour
     {
-        while (true)
+        private void Start()
         {
-            UAudioManager.Instance.PlayEvent("Laser");
-
-            yield return new WaitForSeconds(1.0f);
-
-            UAudioManager.Instance.PlayEvent("Vocals");
-
-            yield return new WaitForSeconds(10.0f);
+            StartCoroutine(ContinouslyPlaySounds());
         }
-    }
 
+        private IEnumerator ContinouslyPlaySounds()
+        {
+            while (true)
+            {
+                UAudioManager.Instance.PlayEvent("Laser");
+
+                yield return new WaitForSeconds(1.0f);
+
+                UAudioManager.Instance.PlayEvent("Vocals");
+
+                yield return new WaitForSeconds(10.0f);
+            }
+        }
+
+    }
 }
