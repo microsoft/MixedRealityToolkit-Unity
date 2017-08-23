133142ac06a56e3da9279f7363006aa00def8b29
Neeraj Wadhwa <neerajw@microsoft.com>
2017-08-23 12:45:01 -0700

    Experimental Boundary: Adding code to render floor and check if object is within established boundary
    1. Removed old stage code
    2. Started using the buggy new boundary APIs

---
+++ Assets/HoloToolkit/Boundary/Tests/Scripts/BoundaryTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace HoloToolkit.Unity.Boundary.Tests
+{
+    public class BoundaryTest : MonoBehaviour
+    {
+        private Material[] defaultMaterials;
+
+        private void Start()
+        {
+            BoundaryManager.Instance.RenderBoundary = true;
+            BoundaryManager.Instance.RenderFloor = true;
+
+            defaultMaterials = GetComponent<Renderer>().materials;
+        
+            if (BoundaryManager.Instance.ContainsObject(gameObject.transform.position))
+            {
+                Debug.LogFormat("Object is within established boundary. Position: {0}", gameObject.transform.position);
+
+                for (int i = 0; i < defaultMaterials.Length; i++)
+                {
+                    // Highlight the material if object is within specified boundary.
+                    defaultMaterials[i].SetFloat("_Highlight", 0.35f);
+                }
+            }
+        }
+    }
+}
