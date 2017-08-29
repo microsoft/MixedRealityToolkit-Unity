ad85fe52304426a0682ea31ca7bb9bbaa4f3f779
Neeraj Wadhwa <neerajw@microsoft.com>
2017-08-29 13:30:46 -0700

    Adding missing headers

--- Assets/HoloToolkit/Boundary/Tests/Scripts/BoundaryTest.cs
+++ Assets/HoloToolkit/Boundary/Tests/Scripts/BoundaryTest.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+using UnityEngine;
 
 namespace HoloToolkit.Unity.Boundary.Tests
 {
