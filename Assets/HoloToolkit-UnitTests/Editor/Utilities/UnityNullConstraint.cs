d74577cd42f66cb3d9d5601d5f9dff60ffe85df8
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-09-03 22:31:25 +0200

    License Headers

--- Assets/HoloToolkit-UnitTests/Editor/Utilities/UnityNullConstraint.cs
+++ Assets/HoloToolkit-UnitTests/Editor/Utilities/UnityNullConstraint.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework.Constraints;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 namespace HoloToolkit.Unity.Tests
--- Assets/HoloToolkit-UnitTests/Editor/Utilities/UnityNullConstraintTests.cs
+++ Assets/HoloToolkit-UnitTests/Editor/Utilities/UnityNullConstraintTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using NUnit.Framework;
 using NUnit.Framework.Constraints;
 using UnityEngine;
 
