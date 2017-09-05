9d60be45f72ee2d77e23e2b1affb4235b8834458
MSDNAndi <MSDNAndi@users.noreply.github.com>
2017-09-04 21:09:00 -0400

    Update HandGuidance.cs
    moved
    using UnityEngine;
    above #if UNITY_WSA
    to support editor integration/tooltip

--- Assets/HoloToolkit/Input/Scripts/HandGuidance.cs
+++ Assets/HoloToolkit/Input/Scripts/HandGuidance.cs
@@ -2,8 +2,8 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 
-#if UNITY_WSA
 using UnityEngine;
+#if UNITY_WSA
 using UnityEngine.VR.WSA.Input;
 #endif
 
