59d5edb3b1928ef82d47823bd76ff34ab295ae93
Stephen Hodgson <StephenHodgson@users.noreply.github.com>
2017-09-12 16:19:08 -0700

    updated header.

--- Assets/HoloToolkit/Input/Scripts/Editor/MixedRealityCameraMangerEditor.cs
+++ Assets/HoloToolkit/Input/Scripts/Editor/MixedRealityCameraMangerEditor.cs
@@ -1,4 +1,7 @@
-﻿using HoloToolkit.Unity.InputModule;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.﻿
+
+using HoloToolkit.Unity.InputModule;
 using UnityEditor;
 using UnityEngine;
 
