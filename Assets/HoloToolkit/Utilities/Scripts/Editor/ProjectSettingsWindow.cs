3627b5a97795716bdedb13e5e747beff9d189511
Stephen Hodgson <shodgson@valorem.com>
2017-09-17 12:02:47 -0700

    removed extra line.

--- Assets/HoloToolkit/Utilities/Scripts/Editor/ProjectSettingsWindow.cs
+++ Assets/HoloToolkit/Utilities/Scripts/Editor/ProjectSettingsWindow.cs
@@ -185,7 +185,6 @@ namespace HoloToolkit.Unity
                 PlayerSettings.WSA.SetCapability(PlayerSettings.WSACapability.PrivateNetworkClientServer, false);
             }
 
-
             Close();
         }
 
