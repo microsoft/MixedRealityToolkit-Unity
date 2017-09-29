a0366f1f81fdb0599ac25d4816a9854dcb715e1e
Lars Simkins <lars.simkins@gmail.com>
2017-09-29 15:36:07 -0700

    GrabbableMultiJoint

---
+++ Assets/HoloToolkit-Examples/MotionControllers-GrabMechanics/Scripts/GrabbableMultiJoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MRTK.Grabbables
+{
+    public class GrabbableMultiJoint : BaseGrabbable
+    {
+        protected override void OnGrabStay()
+        {
+            base.OnGrabStay();
+
+            Vector3 averagePosition = transform.position;
+            Quaternion averageRotation = transform.rotation;
+            int numGrabbers = activeGrabbers.Count;
+            float weightPerGrabber = 1f / numGrabbers;
+
+            foreach (Grabber activeGrabber in activeGrabbers)
+            {
+                averagePosition = Vector3.Lerp(averagePosition, activeGrabber.GrabHandle.position, weightPerGrabber);
+                averageRotation = Quaternion.Lerp(averageRotation, activeGrabber.GrabHandle.rotation, weightPerGrabber);
+            }
+
+            transform.position = Vector3.Lerp(transform.position, averagePosition, Time.deltaTime * blendSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, averageRotation, Time.deltaTime * blendSpeed);
+        }
+
+        [SerializeField]
+        private float blendSpeed = 10f;
+    }
+}
