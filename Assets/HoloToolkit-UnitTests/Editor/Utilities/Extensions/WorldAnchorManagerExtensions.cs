b343867db7b9ca672594eb8edc9c48fd53a56026
Esteban Luchsinger <eluchsingerm@hotmail.com>
2017-10-05 09:47:07 +0200

    Added UnitTests for WorldAnchor.

---
+++ Assets/HoloToolkit-UnitTests/Editor/Utilities/Extensions/WorldAnchorManagerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace HoloToolkit.Unity.Tests.Extensions
+{
+    /// <summary>
+    /// This class extends the world anchor manager with methods constrained to test logic.
+    /// </summary>
+    public static class WorldAnchorManagerExtensions
+    {
+        internal static string GenerateAnchorName(this WorldAnchorManager anchorManager, GameObject gameObjectToAnchor, string anchorName = null)
+        {
+            return string.IsNullOrEmpty(anchorName) ? gameObjectToAnchor.name : anchorName;
+        }
+    }
+}
---
+++ Assets/HoloToolkit-UnitTests/Editor/Utilities/WorldAnchorManagerTests.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+using HoloToolkit.Unity.Tests.Extensions;
+
+namespace HoloToolkit.Unity.Tests
+{
+    public class WorldAnchorManagerTests
+    {
+        [Test]
+        public void TestGenerateAnchorNameFromGameObject()
+        {
+            const string expected = "AnchorName";
+            var gameObject = new GameObject(expected);
+            var result = WorldAnchorManager.Instance.GenerateAnchorName(gameObject);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void TestGenerateAnchorNameFromParameter()
+        {
+            const string expected = "AnchorName";
+            var gameObject = new GameObject();
+            var result = WorldAnchorManager.Instance.GenerateAnchorName(gameObject, expected);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
