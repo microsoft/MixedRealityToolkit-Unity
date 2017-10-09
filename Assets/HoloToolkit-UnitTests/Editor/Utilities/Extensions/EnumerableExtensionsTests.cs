71b781a33b2b616f11062d1bfe81a40524de3b4f
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-10-10 00:27:51 +0200

    Add MaxOrDefault enumerable extension

---
+++ Assets/HoloToolkit-UnitTests/Editor/Utilities/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace HoloToolkit.Unity.Tests
+{
+    public class EnumerableExtensionsTests
+    {
+        [Test]
+        public void TestMaxOrDefault()
+        {
+            Assert.Throws<ArgumentNullException>(() => ((int[])null).MaxOrDefault());
+        }
+
+        [Test]
+        public void TestMaxOrDefaultEmpty()
+        {
+            var items = new int[0];
+
+            Assert.That(items.MaxOrDefault(), Is.Zero);
+        }
+
+        [Test]
+        public void TestMaxOrDefaultUnordered()
+        {
+            var items = new[]
+            {
+                -5, -20, 100, 5
+            };
+
+            Assert.That(items.MaxOrDefault(), Is.EqualTo(100));
+        }
+
+        [Test]
+        public void TestMaxOrDefaultOrdered()
+        {
+            var items = new[]
+            {
+                -20, -5, 5, 100
+            };
+
+            Assert.That(items.MaxOrDefault(), Is.EqualTo(100));
+        }
+    }
+}
