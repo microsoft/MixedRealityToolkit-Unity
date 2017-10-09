2e0d4cd40f8b7b0f54104d6facd62d84b6a848a1
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-10-09 21:49:36 +0200

    Add sorting order/layer comparison Also move comparer methods into a list

--- Assets/HoloToolkit/Utilities/Scripts/RaycastResultComparer.cs
+++ Assets/HoloToolkit/Utilities/Scripts/RaycastResultComparer.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using UnityEngine.EventSystems;
 
@@ -8,11 +9,37 @@ namespace HoloToolkit.Unity
 {
     public class RaycastResultComparer : IComparer<RaycastResult>
     {
+        private static readonly List<Func<RaycastResult, RaycastResult, int>> Comparers = new List<Func<RaycastResult, RaycastResult, int>>
+        {
+            CompareRaycastsBySortingLayer,
+            CompareRaycastsBySortingOrder,
+            CompareRaycastsByCanvasDepth,
+            CompareRaycastsByDistance,
+        };
+
         public int Compare(RaycastResult left, RaycastResult right)
         {
-            var result = CompareRaycastsByCanvasDepth(left, right);
-            if (result != 0) { return result; }
-            return CompareRaycastsByDistance(left, right);
+            for (var i = 0; i < Comparers.Count; i++)
+            {
+                var result = Comparers[i](left, right);
+                if (result != 0)
+                {
+                    return result;
+                }
+            }
+            return 0;
+        }
+
+        private static int CompareRaycastsBySortingOrder(RaycastResult left, RaycastResult right)
+        {
+            //Higher is better
+            return right.sortingOrder.CompareTo(left.sortingOrder);
+        }
+
+        private static int CompareRaycastsBySortingLayer(RaycastResult left, RaycastResult right)
+        {
+            //Higher is better
+            return right.sortingLayer.CompareTo(left.sortingLayer);
         }
 
         private static int CompareRaycastsByCanvasDepth(RaycastResult left, RaycastResult right)
@@ -20,6 +47,7 @@ namespace HoloToolkit.Unity
             //Module is the graphic raycaster on the canvases.
             if (left.module.transform.IsParentOrChildOf(right.module.transform))
             {
+                //Higher is better
                 return right.depth.CompareTo(left.depth);
             }
             return 0;
@@ -27,6 +55,7 @@ namespace HoloToolkit.Unity
 
         private static int CompareRaycastsByDistance(RaycastResult left, RaycastResult right)
         {
+            //Lower is better
             return left.distance.CompareTo(right.distance);
         }
     }
