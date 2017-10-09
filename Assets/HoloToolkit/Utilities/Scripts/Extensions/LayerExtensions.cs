c46608128940692d32e81ace8bd909d216a6ec48
Julian Hangstörfer <jhangstoerfer@gmail.com>
2017-10-09 21:03:58 +0200

    Add layer extension tests Also slightly rewrite FindLayerListIndex to accept all enumerable

--- Assets/HoloToolkit/Utilities/Scripts/Extensions/LayerExtensions.cs
+++ Assets/HoloToolkit/Utilities/Scripts/Extensions/LayerExtensions.cs
@@ -12,26 +12,35 @@ namespace HoloToolkit.Unity
         /// Look through the layerMaskList and find the index in that list for which the supplied layer is part of
         /// </summary>
         /// <param name="layer">Layer to search for</param>
-        /// <param name="layerMaskList">List of LayerMasks to search</param>
+        /// <param name="layerMasks">List of LayerMasks to search</param>
         /// <returns>LayerMaskList index, or -1 for not found</returns>
-        public static int FindLayerListIndex(this int layer, LayerMask[] layerMaskList)
+        public static int FindLayerListIndex(this int layer, IEnumerable<LayerMask> layerMasks)
         {
-            for (int i = 0; i < layerMaskList.Length; i++)
+            var i = 0;
+            foreach (var layerMask in layerMasks)
             {
-                if (IsInLayerMask(layer, layerMaskList[i].value))
+                if (layer.IsInLayerMask(layerMask))
                 {
                     return i;
                 }
+                i++;
             }
-
             return -1;
         }
 
+        /// <summary>
+        /// Checks whether a layer is in a layer mask
+        /// </summary>
+        /// <returns>True if the layer mask contains the layer</returns>
         public static bool IsInLayerMask(this int layer, int layerMask)
         {
             return ((1 << layer) & layerMask) != 0;
         }
 
+        /// <summary>
+        /// Combines provided layers into a single layer mask.
+        /// </summary>
+        /// <returns>The combined layer mask</returns>
         public static int Combine(this IEnumerable<LayerMask> layerMaskList)
         {
             int combinedLayerMask = 0;
