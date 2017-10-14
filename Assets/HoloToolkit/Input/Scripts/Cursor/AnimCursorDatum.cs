544664761f788d9f2a27ea900730f88cb7874c49
Stephen Hodgson <shodgson@valorem.com>
2017-10-13 17:33:34 -0700

    Added back in the Obsolete stuff.

--- Assets/HoloToolkit/Input/Scripts/Cursor/AnimCursorDatum.cs
+++ Assets/HoloToolkit/Input/Scripts/Cursor/AnimCursorDatum.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
+using UnityEngine;
 
 namespace HoloToolkit.Unity.InputModule
 {
@@ -15,5 +16,37 @@ namespace HoloToolkit.Unity.InputModule
         public string Name;
         public CursorStateEnum CursorState;
         public AnimatorParameter Parameter;
+
+        /// <summary>
+        /// Types that an animation parameter can have in the Unity animation system.
+        /// </summary>
+        [Obsolete("Use Parameter")]
+        public enum AnimInputTypeEnum
+        {
+            Int,
+            Trigger,
+            Bool,
+            Float
+        }
+
+        [Obsolete("Use Parameter")]
+        [Tooltip("Type of the animation parameter to modify.")]
+        public AnimInputTypeEnum AnimInputType;
+
+        [Obsolete("Use Parameter")]
+        [Tooltip("Name of the animation parameter to modify.")]
+        public string AnimParameterName;
+
+        [Obsolete("Use Parameter")]
+        [Tooltip("If the animation parameter type is a bool, value to set. Ignored otherwise.")]
+        public bool AnimBoolValue;
+
+        [Obsolete("Use Parameter")]
+        [Tooltip("If the animation parameter type is an int, value to set. Ignored otherwise.")]
+        public int AnimIntValue;
+
+        [Obsolete("Use Parameter")]
+        [Tooltip("If the animation parameter type is a float, value to set. Ignored otherwise.")]
+        public float AnimFloatValue;
     }
 }
