ae8de4d85be71d42df697e4604c893f505d857ce
REDMOND\dongpark <dongpark@microsoft.com>
2017-10-25 17:08:31 -0700

    Applying feedback from Simon - adding information text, unify the scene for HoloLens and IHMD.

---
+++ Assets/HoloToolkit-Examples/UX/Scripts/OptimizeSceneforDeviceType.cs
@@ -0,0 +1,45 @@
+﻿//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+using HoloToolkit.Unity;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA;
+
+namespace HoloToolkit.Unity.Examples
+{
+    public class OptimizeSceneforDeviceType : MonoBehaviour
+    {
+        public GameObject containerObject;
+        public GameObject environmentObject;
+        public GameObject cameraHoloLens;
+        public GameObject cameraImmersiveHMD;
+
+        void Start()
+        {
+            // Check if the device type is HoloLens or Immersive HMD
+            if (HolographicSettings.IsDisplayOpaque)
+            {
+                // Show IHMD camera
+                cameraHoloLens.SetActive(false);
+                cameraImmersiveHMD.SetActive(true);
+                environmentObject.SetActive(true);
+
+                // Optimize the default postion of the objects for Immersive HMD
+                containerObject.transform.position = new Vector3(0.05f, 1.2f, 1.08f);
+            }
+            else
+            {
+                // Show HoloLens camera, hide floor and table
+                cameraHoloLens.SetActive(true);
+                cameraImmersiveHMD.SetActive(false);
+                environmentObject.SetActive(false);
+
+                // Optimize the default postion of the objects for HoloLens
+                containerObject.transform.position = new Vector3(0.05f, -0.65f, 1.65f);
+            }
+        }
+
+    }
+}
