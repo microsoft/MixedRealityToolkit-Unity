a49e812369d8fcdb41fffc92524455a8c5278b5e
Stephen Hodgson <hodgson.designs@gmail.com>
2017-11-13 13:46:30 -0800

    Updated Simulated Hand show/hide logic.

--- Assets/HoloToolkit/Input/Scripts/Utilities/Interactions/CustomInputSelector.cs
+++ Assets/HoloToolkit/Input/Scripts/Utilities/Interactions/CustomInputSelector.cs
@@ -54,9 +54,9 @@ namespace HoloToolkit.Unity.InputModule
             bool spawnControllers = false;
 
 #if UNITY_2017_2_OR_NEWER
-            spawnControllers = (!XRDevice.isPresent && XRSettings.enabled || Application.isEditor) && simulateHandsInEditor;
+            spawnControllers = !XRDevice.isPresent && XRSettings.enabled && simulateHandsInEditor;
 #else
-            spawnControllers = !VRDevice.isPresent;
+            spawnControllers = simulateHandsInEditor;
 #endif
             if (spawnControllers)
             {
