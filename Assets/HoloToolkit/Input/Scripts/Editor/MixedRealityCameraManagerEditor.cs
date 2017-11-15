70a68cf7a3c4863cf16c09ee5de9944191aca792
Stephen Hodgson <hodgson.designs@gmail.com>
2017-11-15 14:06:46 -0800

    Moved the namespace to match the script this custom editor is for

--- Assets/HoloToolkit/Input/Scripts/Editor/MixedRealityCameraManagerEditor.cs
+++ Assets/HoloToolkit/Input/Scripts/Editor/MixedRealityCameraManagerEditor.cs
@@ -1,11 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.﻿
 
-using HoloToolkit.Unity.InputModule;
 using UnityEditor;
 using UnityEngine;
 
-namespace HoloToolkit.Unity
+namespace HoloToolkit.Unity.InputModule
 {
     [CustomEditor(typeof(MixedRealityCameraManager))]
     public class MixedRealityCameraManagerEditor : Editor
