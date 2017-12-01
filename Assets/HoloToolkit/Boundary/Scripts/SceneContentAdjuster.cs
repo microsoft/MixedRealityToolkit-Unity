fa2927720f54e72ca9dff82cee1cf775025b50df
Kurtis Eveleigh <kurtie@microsoft.com>
2017-11-30 16:02:31 -0800

    Adding documentation and attempting to clarify script settings.

--- Assets/HoloToolkit/Boundary/Scripts/SceneContentAdjuster.cs
+++ Assets/HoloToolkit/Boundary/Scripts/SceneContentAdjuster.cs
@@ -12,8 +12,26 @@ using UnityEngine.VR;
 
 namespace HoloToolkit.Unity.Boundary
 {
+    /// <summary>
+    /// Use this script on GameObjects you wish to be aligned in certain ways depending on the application space type.
+    /// For example, if you want to place an object at the height of the user's head in a room scale application, check alignWithHeadHeight.
+    /// In a stationary scale application, this is equivalent to placing the object at a height of 0.
+    /// You can also specify specific locations to place the object based on space type.
+    /// 
+    /// This script runs once, on GameObject creation.
+    /// 
+    /// See https://developer.microsoft.com/en-us/windows/mixed-reality/coordinate_systems_in_unity for more information.
+    /// <see cref="BoundaryManager"/> for TrackingSpaceType settings.
+    /// </summary>
     public class SceneContentAdjuster : MonoBehaviour
     {
+        private enum AlignmentType
+        {
+            AlignWithHeadHeight,
+            UsePresetPositions,
+            UsePresetXAndZWithHeadHeight
+        }
+
         private int frameWaitHack = 0;
 
         [SerializeField]
@@ -22,14 +40,14 @@ namespace HoloToolkit.Unity.Boundary
 
         [SerializeField]
         [Tooltip("Select this if the container should be placed in front of the head on app launch in a room scale app.")]
-        private bool alignWithHeadHeight = true;
+        private AlignmentType alignmentType = AlignmentType.AlignWithHeadHeight;
 
         [SerializeField]
-        [Tooltip("Use this to set the desired position of the container in a stationary app.")]
+        [Tooltip("Use this to set the desired position of the container in a stationary app. This will be ignored if AlignWithHeadHeight is set.")]
         private Vector3 stationarySpaceTypePosition = Vector3.zero;
 
         [SerializeField]
-        [Tooltip("Use this to set the desired position of the container in a room scale app, if alignWithInitialHeadHeight has not been set.")]
+        [Tooltip("Use this to set the desired position of the container in a room scale app. This will be ignored if AlignWithHeadHeight is set.")]
         private Vector3 roomScaleSpaceTypePosition = Vector3.zero;
 
         private Vector3 contentPosition = Vector3.zero;
@@ -67,24 +85,25 @@ namespace HoloToolkit.Unity.Boundary
                 yield return null;
             }
 
-            if (XRDevice.GetTrackingSpaceType() == TrackingSpaceType.RoomScale)
+            if (alignmentType == AlignmentType.UsePresetPositions || alignmentType == AlignmentType.UsePresetXAndZWithHeadHeight)
             {
-                if (alignWithHeadHeight)
-                {
-                    contentPosition.x = containerObject.position.x;
-                    contentPosition.y = containerObject.position.y + CameraCache.Main.transform.position.y;
-                    contentPosition.z = containerObject.position.z;
-
-                    containerObject.position = contentPosition;
-                }
-                else
+                if (XRDevice.GetTrackingSpaceType() == TrackingSpaceType.RoomScale)
                 {
                     containerObject.position = roomScaleSpaceTypePosition;
                 }
+                else if (XRDevice.GetTrackingSpaceType() == TrackingSpaceType.Stationary)
+                {
+                    containerObject.position = stationarySpaceTypePosition;
+                }
             }
-            else if (XRDevice.GetTrackingSpaceType() == TrackingSpaceType.Stationary)
+
+            if (alignmentType == AlignmentType.AlignWithHeadHeight || alignmentType == AlignmentType.UsePresetXAndZWithHeadHeight)
             {
-                containerObject.position = stationarySpaceTypePosition;
+                contentPosition.x = containerObject.position.x;
+                contentPosition.y = containerObject.position.y + CameraCache.Main.transform.position.y;
+                contentPosition.z = containerObject.position.z;
+
+                containerObject.position = contentPosition;
             }
         }
     }
