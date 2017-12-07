5338ae2286c9bfdf365a43f5f6f8638f8d8da9f0
Stephen Hodgson <hodgson.designs@gmail.com>
2017-12-07 15:51:53 -0800

    more typo fixes

--- Assets/HoloToolkit/BuildAndDeploy/Editor/BuildDeployTools.cs
+++ Assets/HoloToolkit/BuildAndDeploy/Editor/BuildDeployTools.cs
@@ -242,8 +242,8 @@ namespace HoloToolkit.Unity
 
             string nugetPath = Path.Combine(unity, @"Data\PlaybackEngines\MetroSupport\Tools\NuGet.exe");
             string assemblyCSharp = storePath + "/GeneratedProjects/UWP/Assembly-CSharp";
-            string assemblyCSharpFisrtpass = storePath + "/GeneratedProjects/UWP/Assembly-CSharp-firstpass";
-            bool restoreFirstPass = Directory.Exists(assemblyCSharpFisrtpass);
+            string assemblyCSharpFirstPass = storePath + "/GeneratedProjects/UWP/Assembly-CSharp-firstpass";
+            bool restoreFirstPass = Directory.Exists(assemblyCSharpFirstPass);
 
             // Before building, need to run a nuget restore to generate a json.lock file. Failing to do
             // this breaks the build in VS RTM
@@ -251,7 +251,7 @@ namespace HoloToolkit.Unity
                 (!RestoreNugetPackages(nugetPath, storePath) ||
                  !RestoreNugetPackages(nugetPath, storePath + "\\" + productName) ||
                  EditorUserBuildSettings.wsaGenerateReferenceProjects && !RestoreNugetPackages(nugetPath, assemblyCSharp) ||
-                 EditorUserBuildSettings.wsaGenerateReferenceProjects && restoreFirstPass && !RestoreNugetPackages(nugetPath, assemblyCSharpFisrtpass)))
+                 EditorUserBuildSettings.wsaGenerateReferenceProjects && restoreFirstPass && !RestoreNugetPackages(nugetPath, assemblyCSharpFirstPass)))
             {
                 Debug.LogError("Failed to restore nuget packages");
                 EditorUtility.ClearProgressBar();
