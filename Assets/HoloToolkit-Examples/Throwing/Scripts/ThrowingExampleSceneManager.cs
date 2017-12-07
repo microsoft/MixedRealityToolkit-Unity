7474bd8f4997acb08a7560a5b4ea3b2eccd5d426
Kevin Luecke <keluecke@microsoft.com>
2017-12-07 14:02:18 -0800

    Add throwing helper and example.

---
+++ Assets/HoloToolkit-Examples/Throwing/Scripts/ThrowingExampleSceneManager.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class ThrowingExampleSceneManager : MonoBehaviour
+{
+    public GameObject[] ThrowingAssets;
+    public InteractionSourceNode ControllerPose = InteractionSourceNode.Grip;
+    public Transform RealWorldRoot;
+
+    private readonly Dictionary<uint, Transform> devices = new Dictionary<uint, Transform>();
+    private readonly Dictionary<uint, int> modelIndecies = new Dictionary<uint, int>();
+    private readonly Dictionary<uint, bool> isDetatched = new Dictionary<uint, bool>();
+
+    // Use this for initialization
+    void Start ()
+    {
+		if (ThrowingAssets.Length == 0)
+        {
+            throw new System.Exception("ThrowingSceneManager needs to have some throwable objects!");
+        }   
+
+        InteractionManager.InteractionSourceReleased += InteractionManager_InteractionSourceReleased;
+        InteractionManager.InteractionSourcePressed += InteractionManager_InteractionSourcePressed;
+        Application.onBeforeRender += Application_onBeforeRender;
+    }
+
+    private void InteractionManager_InteractionSourcePressed(InteractionSourcePressedEventArgs args)
+    {
+        uint id = args.state.source.id;
+        if (args.pressType == InteractionSourcePressType.Menu || args.pressType == InteractionSourcePressType.Touchpad)
+        {
+            if (devices.ContainsKey(id))
+            {
+                int modelIndex = this.modelIndecies[id];
+                RemoveDevice(id);
+                AddDevice(id, ++modelIndex % ThrowingAssets.Length);
+            }
+            else if (modelIndecies.ContainsKey(id))
+            {
+                this.modelIndecies[id] = ++this.modelIndecies[id] % ThrowingAssets.Length;
+            }
+        }
+        else if (args.pressType == InteractionSourcePressType.Grasp || args.pressType == InteractionSourcePressType.Select)
+        {
+            if (isDetatched.ContainsKey(id))    
+            {
+                isDetatched[id] = false;
+            }
+        }
+    }
+
+    private void InteractionManager_InteractionSourceReleased(InteractionSourceReleasedEventArgs args)
+    {
+        if (args.pressType == InteractionSourcePressType.Grasp || args.pressType == InteractionSourcePressType.Select)
+        {
+            uint id = args.state.source.id;
+            if (devices.ContainsKey(id))
+            {
+                var go = devices[id];
+                var rigidbody = go.GetComponent<Rigidbody>();
+                if (rigidbody == null)
+                {
+                    rigidbody = go.GetComponentInChildren<Rigidbody>();
+                }
+                if (rigidbody.TryThrow(args.state.sourcePose))
+                {
+                    DetatchDevice(id);
+                }
+                else
+                {
+                    throw new System.Exception("Throw failed!!!");
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Unity will have an updated predicted rotation here, use this function to tweak
+    /// the rendered model last minute to have the smoothest visual experience.
+    /// </summary>
+    private void Application_onBeforeRender()
+    {
+        foreach (var sourceState in InteractionManager.GetCurrentReading())
+        {
+            uint id = sourceState.source.id;
+            var handedness = sourceState.source.handedness;
+            var sourcePose = sourceState.sourcePose;
+            Vector3 position;
+            Quaternion rotation;
+            if (devices.ContainsKey(id))
+            {
+                if (sourcePose.TryGetPosition(out position, this.ControllerPose) &&
+                    sourcePose.TryGetRotation(out rotation, this.ControllerPose)) // defaults to grip
+                {
+                    SetTransform(devices[id], position, rotation);
+                }
+            }
+            else if (sourceState.source.supportsPointing)
+            {
+                if (this.modelIndecies.ContainsKey(id))
+                {
+                    this.AddDevice(id, this.modelIndecies[id]);
+                }
+                else
+                {
+                    this.AddDevice(id);
+                }
+
+                if (!isDetatched.ContainsKey(id) || !isDetatched[id])
+                {
+                    if (sourcePose.TryGetPosition(out position, this.ControllerPose) &&
+                    sourcePose.TryGetRotation(out rotation, this.ControllerPose)) // defaults to grip
+                    {
+                        SetTransform(devices[id], position, rotation);
+                    }
+                }
+            }
+        }
+    }
+
+    private void AddDevice(uint id, int index = 0)
+    {
+        if (!devices.ContainsKey(id) && (!isDetatched.ContainsKey(id) || !isDetatched[id]))
+        {
+            GameObject go = Instantiate(this.ThrowingAssets[index], this.RealWorldRoot);
+            go.name = "Controller " + id;
+            devices[id] = go.transform;
+            modelIndecies[id] = index;
+            isDetatched[id] = false;
+        }
+    }
+
+    private void RemoveDevice(uint id)
+    {
+        if (devices.ContainsKey(id))
+        {
+            Destroy(devices[id].gameObject);
+            devices.Remove(id);
+        }
+    }
+
+    private void DetatchDevice(uint id)
+    { 
+        if (devices.ContainsKey(id))
+        {
+            devices[id].SetParent(null);
+            isDetatched[id] = true;
+            devices.Remove(id);
+        }
+    }
+
+    private void SetTransform(Transform t, Vector3 position, Quaternion rotation)
+    {
+        // This check shouldn't be necessary
+        if (!(float.IsNaN(position.x) || float.IsNaN(position.y) || float.IsNaN(position.z) ||
+            float.IsNaN(rotation.w) || float.IsNaN(rotation.x) || float.IsNaN(rotation.y) || float.IsNaN(rotation.z)))
+        {
+            t.localPosition = position;
+            t.localRotation = rotation;
+        }
+    }
+
+    void OnDestroy()
+    {
+        InteractionManager.InteractionSourceReleased -= InteractionManager_InteractionSourceReleased;
+        InteractionManager.InteractionSourcePressed -= InteractionManager_InteractionSourcePressed;
+        Application.onBeforeRender -= Application_onBeforeRender;
+    }
+}
---
+++ Assets/HoloToolkit/Utilities/Scripts/Throwing.cs
@@ -0,0 +1,116 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.WSA.Input;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Throwing
+{
+    public static void GetThrownObjectVelAngVel(Vector3 throwingControllerPos, Vector3 throwingControllerVelocity, Vector3 throwingControllerAngularVelocity,
+        Vector3 thrownObjectCenterOfMass, out Vector3 objectVelocity, out Vector3 objectAngularVelocity)
+    {
+        Vector3 radialVec = thrownObjectCenterOfMass - throwingControllerPos;
+
+        objectVelocity = throwingControllerVelocity + Vector3.Cross(throwingControllerAngularVelocity, radialVec);
+        objectAngularVelocity = throwingControllerAngularVelocity;
+    }
+
+    public static bool TryGetThrownObjectVelAngVel(InteractionSourcePose throwingControllerPose, Vector3 thrownObjectCenterOfMass, out Vector3 objectVelocity, out Vector3 objectAngularVelocity)
+    {
+        Vector3 controllerPos, controllerVelocity, controllerAngularVelocity;
+
+        if (!throwingControllerPose.TryGetPosition(out controllerPos) ||
+            !throwingControllerPose.TryGetVelocity(out controllerVelocity) ||
+            !throwingControllerPose.TryGetAngularVelocity(out controllerAngularVelocity))
+        {
+            objectVelocity = objectAngularVelocity = default(Vector3);
+            return false;
+        }
+
+        GetThrownObjectVelAngVel(controllerPos, controllerVelocity, controllerAngularVelocity, thrownObjectCenterOfMass, out objectVelocity, out objectAngularVelocity);
+        return true;
+    }
+
+    public static bool TryGetNodeState(XRNode node, out XRNodeState nodeState)
+    {
+        List<XRNodeState> nodeStates = new List<XRNodeState>();
+        InputTracking.GetNodeStates(nodeStates);
+        nodeState = nodeStates.Where(p => p.nodeType == node).FirstOrDefault();
+
+        return nodeState.tracked;
+    }
+
+    public static bool TryGetThrownObjectVelAngVel(XRNode throwingController, Vector3 thrownObjectCenterOfMass, out Vector3 objectVelocity, out Vector3 objectAngularVelocity)
+    {
+        XRNodeState throwingControllerState;
+        if (!TryGetNodeState(throwingController, out throwingControllerState))
+        { 
+            objectVelocity = objectAngularVelocity = default(Vector3);
+            return false;
+        }
+
+        return TryGetThrownObjectVelAngVel(throwingControllerState, thrownObjectCenterOfMass, out objectVelocity, out objectAngularVelocity);
+    }
+
+    public static bool TryGetThrownObjectVelAngVel(XRNodeState throwingControllerState, Vector3 thrownObjectCenterOfMass, out Vector3 objectVelocity, out Vector3 objectAngularVelocity)
+    {
+        Vector3 controllerPos, controllerVelocity, controllerAngularVelocity;
+
+        if (!throwingControllerState.TryGetPosition(out controllerPos) ||
+            !throwingControllerState.TryGetVelocity(out controllerVelocity) ||
+            !throwingControllerState.TryGetAngularVelocity(out controllerAngularVelocity))
+        {
+            objectVelocity = objectAngularVelocity = default(Vector3);
+            return false;
+        }
+
+        GetThrownObjectVelAngVel(controllerPos, controllerVelocity, controllerAngularVelocity, thrownObjectCenterOfMass, out objectVelocity, out objectAngularVelocity);
+        return true;
+    }
+
+    public static bool TryThrow(this Rigidbody rb, InteractionSourcePose throwingConctoller)
+    {
+        Vector3 velocity, angularVelocity;
+        if (!TryGetThrownObjectVelAngVel(throwingConctoller, rb.transform.TransformPoint(rb.centerOfMass), out velocity, out angularVelocity))
+        {
+            return false;
+        }
+
+        rb.angularVelocity = angularVelocity;
+        rb.velocity = velocity;
+        rb.isKinematic = false;
+        return true;
+    }
+
+    public static bool TryThrow(this Rigidbody rb, XRNodeState throwingConctoller)
+    {
+        Vector3 velocity, angularVelocity;
+        if (!TryGetThrownObjectVelAngVel(throwingConctoller, rb.transform.TransformPoint(rb.centerOfMass), out velocity, out angularVelocity))
+        {
+            return false;
+        }
+
+        rb.angularVelocity = angularVelocity;
+        rb.velocity = velocity;
+        rb.isKinematic = false;
+        return true;
+    }
+
+    public static bool TryThrow(this Rigidbody rb, XRNode throwingConctoller)
+    {
+        Vector3 velocity, angularVelocity;
+        if (!TryGetThrownObjectVelAngVel(throwingConctoller, rb.transform.TransformPoint(rb.centerOfMass), out velocity, out angularVelocity))
+        {
+            return false;
+        }
+
+        rb.angularVelocity = angularVelocity;
+        rb.velocity = velocity;
+        rb.isKinematic = false;
+        return true;
+    }
+}
