06027c69ba07f97f6d106893a41c2fdac4974bbd
Lars Simkins <lars.simkins@gmail.com>
2017-12-12 12:52:30 -0800

    Initial commit

---
+++ Assets/HoloToolkit/UX/Scripts/BoundingBoxes/BoundingBoxGizmo.cs
@@ -0,0 +1,37 @@
+﻿//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+using UnityEngine;
+
+namespace HoloToolkit.Unity.UX
+{
+    /// <summary>
+    /// Listens to a BoundingBoxManipulate object and draws a gizmo around the target object
+    /// </summary>
+    [ExecuteInEditMode]
+    public class BoundingBoxGizmo : MonoBehaviour
+    {
+        /// <summary>
+        /// Physics layer to use for rendering
+        /// </summary>
+        public int PhysicsLayer = 0;
+
+        /// <summary>
+        /// The bounding box we're following
+        /// </summary>
+        [SerializeField]
+        protected BoundingBox boundingBox;
+
+        /// <summary>
+        /// Draws any custom gizmo elements around the manipulator's target object
+        /// </summary>
+        protected virtual void DrawGizmoObjects() {
+            // empty
+        } 
+
+        protected virtual void LateUpdate() {
+            DrawGizmoObjects();
+        }
+    }
+}
