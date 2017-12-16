c44d809f8651514922b96a163525c61cde8ae789
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2017-12-16 13:35:56 +0000

    Initial project rename
    [x] Rename Core HT -> MRTK folder
    [x] Rename Examples HT -> Examples MRTK folder
    [x] Rename Unit Tests HT -> Unit Tests MRTK folder
    [x] Initial global NS rename

---
+++ Assets/MixedRealityToolkit-Examples/Prototyping/Scripts/Tests/CycleClicker.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using System.Collections;
+using MixedRealityToolkit.Unity.InputModule;
+using MixedRealityToolkit.Examples.Prototyping;
+
+namespace MixedRealityToolkit.Examples.InteractiveElements
+{
+    /// <summary>
+    /// Advances a iCycle component on click
+    /// </summary>
+    public class CycleClicker : MonoBehaviour, IInputClickHandler
+    {
+
+        public GameObject CycleObject;
+        private ICycle mCycleComp;
+
+        public void OnInputClicked(InputClickedEventData eventData)
+        {
+            mCycleComp = CycleObject.GetComponent<ICycle>();
+
+            if (mCycleComp != null)
+                mCycleComp.MoveNext();
+        }
+    }
+}
---
+++ Assets/MixedRealityToolkit/Sharing/Scripts/Unity/DefaultSyncModelAccessor.cs
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using MixedRealityToolkit.Sharing.SyncModel;
+
+namespace MixedRealityToolkit.Sharing
+{
+    /// <summary>
+    /// Default implementation of a MonoBehaviour that allows other components of a game object access the shared data model
+    /// as a raw SyncObject instance.
+    /// </summary>
+    public class DefaultSyncModelAccessor : MonoBehaviour, ISyncModelAccessor
+    {
+        public SyncObject SyncModel { get; private set; }
+
+        public void SetSyncModel(SyncObject syncObject)
+        {
+            SyncModel = syncObject;
+        }
+    }
+}
