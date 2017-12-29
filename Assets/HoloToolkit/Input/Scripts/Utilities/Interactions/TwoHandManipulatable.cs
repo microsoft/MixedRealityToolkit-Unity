d7040b25a947c0df37a59c28b34b3c91f9b98b58
Julia Schwarz <julenka.schwarz@gmail.com>
2017-12-28 17:53:48 -0800

    Add skeleton code for TwoHandManipulatable

---
+++ Assets/HoloToolkit/Input/Scripts/Utilities/Interactions/TwoHandManipulatable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+namespace HoloToolkit.Unity.InputModule
+{
+    /// <summary>
+    /// TO DO
+    /// </summary>
+    public class TwoHandManipulatable : MonoBehaviour, IInputHandler, ISourceStateHandler
+    {
+        public event Action StartedManipulating;
+        public event Action StoppedManipulating;
+
+        [Tooltip("Transform that will be dragged. Defaults to the object of the component.")]
+        public Transform HostTransform;
+
+        public void OnInputDown(InputEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnInputUp(InputEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSourceDetected(SourceStateEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSourceLost(SourceStateEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
