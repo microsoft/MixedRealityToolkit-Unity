4e100eeb699a65319ac30c7ceb6b4469d9fa7e43
Julian Hangstörfer <Julian.Hangstoerfer@generic.de>
2018-02-05 18:52:56 +0100

    Add first version of reference update

---
+++ Assets/HoloToolkit/UX/Scripts/Editor/AtlasReferenceUpdater.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+namespace HoloToolkit.UI.Keyboard
+{
+    public abstract class AtlasReferenceUpdater
+    {
+        protected abstract IList<string> PrefabPaths { get; }
+        protected abstract string AtlasPath { get; }
+
+        public void UpdateAtlasReferences()
+        {
+            var spriteAtlas = AssetDatabase.LoadAssetAtPath<SpriteAtlas>(AtlasPath);
+            var prefabs = LoadAssetsAtPaths<GameObject>(PrefabPaths);
+            if (spriteAtlas == null)
+            {
+                Debug.LogWarning("Sprite atlas could not be loaded for reference updating from path: " + AtlasPath);
+                return;
+            }
+            if (prefabs.Any(o => o == null))
+            {
+                Debug.LogWarning("One or more prefab references could not be loaded for reference updating from path: " + PrefabPaths.Aggregate((curr, next) => curr + "\n" + next));
+                return;
+            }
+
+
+            var uniqueSprites = GetUniqueSpritesInAssets(prefabs).ToList();
+            var serializedObject = new SerializedObject(spriteAtlas);
+            var packables = serializedObject.FindProperty("m_EditorData.packables");
+
+            InsertSprites(packables, uniqueSprites);
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private static void InsertSprites(SerializedProperty packables, IList<Sprite> uniqueSprites)
+        {
+            packables.arraySize = uniqueSprites.Count;
+            for (var i = 0; i < uniqueSprites.Count; i++)
+            {
+                packables.InsertArrayElementAtIndex(i);
+                packables.GetArrayElementAtIndex(i).objectReferenceValue = uniqueSprites[i];
+            }
+        }
+
+        private static IList<T> LoadAssetsAtPaths<T>(IList<string> paths) where T : Object
+        {
+            var assets = new List<T>();
+            foreach (var path in paths)
+            {
+                assets.Add(AssetDatabase.LoadAssetAtPath<T>(path));
+            }
+            return assets;
+        }
+
+        private static HashSet<Sprite> GetUniqueSpritesInAssets(IList<GameObject> prefabs)
+        {
+            var imageSet = new HashSet<Sprite>();
+            foreach (var prefab in prefabs)
+            {
+                foreach (var image in prefab.GetComponentsInChildren<Image>())
+                {
+                    if (!image.sprite) { continue; }
+                    imageSet.Add(image.sprite);
+                }
+            }
+
+            return imageSet;
+        }
+    }
+}
---
+++ Assets/HoloToolkit/UX/Scripts/Editor/KeyboardAtlasReferenceUpdater.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using HoloToolkit.UI.Keyboard;
+using UnityEditor;
+using UnityEditor.Build;
+
+public class KeyboardAtlasReferenceUpdater : AtlasReferenceUpdater, IPreprocessBuild
+{
+    protected override IList<string> PrefabPaths
+    {
+        get
+        {
+            return new List<string> { "Assets/HoloToolkit/UX/Prefabs/Keyboard.prefab" };
+        }
+    }
+
+    protected override string AtlasPath
+    {
+        get
+        {
+            return "Assets/HoloToolkit/UX/Textures/KeyboardAtlas.spriteatlas";
+        }
+    }
+
+    public int callbackOrder { get { return int.MaxValue; } }
+
+    public void OnPreprocessBuild(BuildTarget target, string path)
+    {
+        UpdateAtlasReferences();
+    }
+}
