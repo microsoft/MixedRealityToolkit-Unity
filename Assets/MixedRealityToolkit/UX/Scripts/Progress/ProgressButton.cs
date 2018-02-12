b8eda1a97c8afc7c9634affc002de7f505b1fb6d
johnppella <johnppella@live.com>
2018-02-12 14:09:07 -0800

    changed button event handler

--- Assets/MixedRealityToolkit/UX/Scripts/Progress/ProgressButton.cs
+++ Assets/MixedRealityToolkit/UX/Scripts/Progress/ProgressButton.cs
@@ -3,22 +3,27 @@ using UnityEngine.Events;
 using System.Collections;
 using System.Collections.Generic;
 using MixedRealityToolkit.InputModule;
+using MixedRealityToolkit.Examples.UX;
 
 #if UNITY_WSA || UNITY_STANDALONE_WIN
 using UnityEngine.Windows.Speech;
 #endif
 
 
-namespace MixedRealityToolkit.Examples.UX
+namespace MixedRealityToolkit.UX.Buttons
 {
-    public class ProgressButton : Interactive
+    public class ProgressButton : MonoBehaviour
     {
-        public override void OnInputClicked(MixedRealityToolkit.InputModule.EventData.InputClickedEventData eventData)
+        void OnEnable()
+        {
+            GetComponent<Button>().OnButtonClicked += OnButtonClicked;
+        }
+
+        private void OnButtonClicked(GameObject obj)
         {
             string na = this.gameObject.name;
             ProgressExamples examples = Object.FindObjectOfType<ProgressExamples>();
             examples.LaunchProgress(this.gameObject);
         }
-
     }
 }
