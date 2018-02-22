85bad6408af25b6799a78b62e401c1cd4c00962e
johnppella <johnppella@live.com>
2018-02-21 16:58:57 -0800

    change case of variable names

--- Assets/MixedRealityToolkit/UX/Scripts/Progress/ObjectDisplayScript.cs
+++ Assets/MixedRealityToolkit/UX/Scripts/Progress/ObjectDisplayScript.cs
@@ -5,27 +5,27 @@ using UnityEngine;
 public class ObjectDisplayScript : MonoBehaviour
 {
     [Header("How fast does object rotate?")]
-    public float rotationIncrement;
+    public float RotationIncrement;
 
     [Header("Start scale of the object?")]
-    public float minScale;
+    public float MinScale;
 
     [Header("Final scale of the object?")]
-    public float maxScale;
+    public float MaxScale;
 
     [Header("How fast does object grow?")]
-    public float scaleSpeed;
+    public float ScaleSpeed;
 
     [Header("Should object rotate after growing?")]
-    public bool rotationActive;
+    public bool RotationActive;
 
     [Header("Should object grow before rotating?")]
-    public bool growingActive;
+    public bool GrowingActive;
 
     [Header("Rotation occurs about which axes?")]
-    public bool xAxisRotation = false;
-    public bool yAxisRotation = true;
-    public bool zAxisRotation = false;
+    public bool XAxisRotation = false;
+    public bool YAxisRotation = true;
+    public bool ZAxisRotation = false;
 
     private float currentScale;
     private float elapsedTime;
@@ -38,7 +38,7 @@ public class ObjectDisplayScript : MonoBehaviour
     public void Reset()
     {
         elapsedTime = 0.0f;
-        currentScale = minScale;
+        currentScale = MinScale;
     }
 
     // Update is called once per frame
@@ -46,19 +46,19 @@ public class ObjectDisplayScript : MonoBehaviour
     {
         elapsedTime += Time.unscaledDeltaTime;
 
-        if (growingActive && currentScale < maxScale)
+        if (GrowingActive && currentScale < MaxScale)
         {
-            currentScale = minScale + (scaleSpeed * (maxScale * Mathf.Pow(elapsedTime, 2.0f)));
+            currentScale = MinScale + (ScaleSpeed * (MaxScale * Mathf.Pow(elapsedTime, 2.0f)));
         }
 
         transform.localScale = new Vector3(currentScale, currentScale, currentScale);
 
-        if (rotationActive)
+        if (RotationActive)
         {
-            float increment = Time.deltaTime * rotationIncrement;
-            float xRotation = xAxisRotation ? increment : 0;
-            float yRotation = yAxisRotation ? increment : 0;
-            float zRotation = zAxisRotation ? increment : 0;
+            float increment = Time.deltaTime * RotationIncrement;
+            float xRotation = XAxisRotation ? increment : 0;
+            float yRotation = YAxisRotation ? increment : 0;
+            float zRotation = ZAxisRotation ? increment : 0;
             transform.Rotate(xRotation, yRotation, zRotation);
         }
     }
