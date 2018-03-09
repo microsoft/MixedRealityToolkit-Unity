0615dcfad3f25a53079e668e8139c12690b53fa5
Lars Simkins <lars.simkins@gmail.com>
2018-03-08 17:55:13 -0800

    Added min/max velocity curve, updated visuals, added copy/paste to gradient attribute

--- Assets/MixedRealityToolkit/InputModule/Scripts/Pointers/ParabolicTeleportPointer.cs
+++ Assets/MixedRealityToolkit/InputModule/Scripts/Pointers/ParabolicTeleportPointer.cs
@@ -16,6 +16,12 @@ namespace MixedRealityToolkit.InputModule.Pointers
         [SerializeField]
         [Tooltip("Pointers that you want to disable while teleporting")]
         private BaseControllerPointer[] disableWhileActive;
+        [SerializeField]
+        private AnimationCurve parabolaVelocityCurve = AnimationCurve.Linear(0f, 1f, 1f, 1f);
+        [SerializeField]
+        private float minParabolaVelocity = 3f;
+        [SerializeField]
+        private float maxParabolaVelocity = 3f;
 
         [SerializeField]
         [DropDownComponent(true, true)]
@@ -34,6 +40,11 @@ namespace MixedRealityToolkit.InputModule.Pointers
             // NOTE: Parabola's custom line transform field should be set to a transform OTHER than its gameObject's transform
             physicsParabolaDataMain.Direction = transform.forward + Vector3.up;
             physicsParabolaDataMain.LineTransform.eulerAngles = Vector3.zero;
+            // Use our up angle and distance curve to determine the velocity
+            // This can be used to make the parabola point farther when aimed up
+            float angle = Mathf.Clamp01(Vector3.Angle(transform.forward, Vector3.up) / 180f);
+            float velocity = Mathf.Lerp(minParabolaVelocity, maxParabolaVelocity, angle);
+            physicsParabolaDataMain.Velocity = velocity;
         }
 
         public override void OnSelectPressed()
