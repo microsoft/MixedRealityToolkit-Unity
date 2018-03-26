e7f09de08ac9caac0867b4ac921c7a95a9f5a8bc
David Kline (ANALOG) <davidkl@microsoft.com>
2018-03-26 10:24:37 -0700

    pr feedback

--- Assets/MixedRealityToolkit-Examples/SpatialSound/Scripts/LoFiFilterSelection.cs
+++ Assets/MixedRealityToolkit-Examples/SpatialSound/Scripts/LoFiFilterSelection.cs
@@ -6,99 +6,114 @@ using MixedRealityToolkit.InputModule.EventData;
 using MixedRealityToolkit.InputModule.InputHandlers;
 using MixedRealityToolkit.SpatialSound.Effects;
 
-public class LoFiFilterSelection : MonoBehaviour, IInputClickHandler
+namespace MixedRealityToolkit.Examples.SpatialSound
 {
-    [Tooltip("Material used when the emitter is set to Narrow Band Telephony")]
-    [SerializeField]
-    private Material NarrowBandTelephony;
-
-    [Tooltip("Material used when the emitter is set to AM Radio")]
-    [SerializeField]
-    private Material AmRadio;
-
-    [Tooltip("Material used when the emitter is set to Full Range")]
-    [SerializeField]
-    private Material FullRange;
-
-    [Tooltip("Material used when the emitter is set to an unknown quality")]
-    [SerializeField]
-    private Material UnknownQuality;
-
-    private AudioLoFiEffect loFiEffect;
-
-    private void Start()
+    public class LoFiFilterSelection : MonoBehaviour, IInputClickHandler
     {
-        // Get the attached AudioLoFiEffect script.
-        loFiEffect = gameObject.GetComponent<AudioLoFiEffect>();
-        if (loFiEffect == null)
+        [Tooltip("Material used when the emitter is set to Narrow Band Telephony")]
+        [SerializeField]
+        private Material NarrowBandTelephony;
+
+        [Tooltip("Material used when the emitter is set to AM Radio")]
+        [SerializeField]
+        private Material AmRadio;
+
+        [Tooltip("Material used when the emitter is set to Full Range")]
+        [SerializeField]
+        private Material FullRange;
+
+        [Tooltip("Material used when the emitter is set to an unknown quality")]
+        [SerializeField]
+        private Material UnknownQuality;
+
+        private AudioLoFiEffect loFiEffect;
+
+        private Renderer emitterRenderer;
+
+        private void Start()
         {
-            Debug.LogError("LoFiFilterSelection requires an AudioLoFiEffect to be attached to the game object.");
+            // Cache the emitter game object's renderer.
+            emitterRenderer = gameObject.GetComponent<Renderer>();
+            if (emitterRenderer == null)
+            {
+                Debug.LogError("Failed to retrieve the renderer for this game object.");
+            }
+
+            // Get the attached AudioLoFiEffect script.
+            loFiEffect = gameObject.GetComponent<AudioLoFiEffect>();
+            if (loFiEffect == null)
+            {
+                Debug.LogError("LoFiFilterSelection requires an AudioLoFiEffect to be attached to the game object.");
+            }
+
+            // Set the material of the emitter object to match that of the
+            // initial AudioLoFiEffect.SourceQuality value.
+            SetEmitterMaterial(loFiEffect.SourceQuality);
         }
 
-        // Set the material of the emitter object to match that of the
-        // initial AudioLoFiEffect.SourceQuality value.
-        SetEmitterMaterial(loFiEffect.SourceQuality);
-    }
-
-    /// <summary>
-    /// Changes the AudioLoFiEffect source quality on click.
-    /// </summary>
-    public void OnInputClicked(InputClickedEventData data)
-    {
-        // Make sure we found an AudioLoFiEffect script.
-        if (loFiEffect == null) { return; }
-
-        // Get the current source quality setting.
-        AudioLoFiSourceQuality sourceQuality = loFiEffect.SourceQuality;
-
-        // Increment the source quality.
-        switch (sourceQuality)
+        /// <summary>
+        /// Changes the AudioLoFiEffect source quality on click.
+        /// </summary>
+        public void OnInputClicked(InputClickedEventData data)
         {
-            case AudioLoFiSourceQuality.NarrowBandTelephony:
-                sourceQuality = AudioLoFiSourceQuality.AmRadio;
-                break;
+            // Make sure we found an AudioLoFiEffect script.
+            if (loFiEffect == null) { return; }
 
-            case AudioLoFiSourceQuality.AmRadio:
-                sourceQuality = AudioLoFiSourceQuality.FullRange;
-                break;
+            // Get the current source quality setting.
+            AudioLoFiSourceQuality sourceQuality = loFiEffect.SourceQuality;
 
-            case AudioLoFiSourceQuality.FullRange:
-                sourceQuality = AudioLoFiSourceQuality.NarrowBandTelephony;
-                break;
+            // Increment the source quality.
+            switch (sourceQuality)
+            {
+                case AudioLoFiSourceQuality.NarrowBandTelephony:
+                    sourceQuality = AudioLoFiSourceQuality.AmRadio;
+                    break;
 
+                case AudioLoFiSourceQuality.AmRadio:
+                    sourceQuality = AudioLoFiSourceQuality.FullRange;
+                    break;
+
+                case AudioLoFiSourceQuality.FullRange:
+                    sourceQuality = AudioLoFiSourceQuality.NarrowBandTelephony;
+                    break;
+
+            }
+
+            // Update the emitter material to match the new source quality.
+            SetEmitterMaterial(sourceQuality);
+
+            // Update the source quality.
+            loFiEffect.SourceQuality = sourceQuality;
+
+            // Mark the event as used, so it doesn't fall through to other handlers.
+            data.Use();
         }
 
-        // Update the emitter material to match the new source quality.
-        SetEmitterMaterial(sourceQuality);
-
-        // Update the source quality.
-        loFiEffect.SourceQuality = sourceQuality;
-
-        // Mark the event as used, so it doesn't fall through to other handlers.
-        data.Use();
-    }
-
-    private void SetEmitterMaterial(AudioLoFiSourceQuality sourceQuality)
-    {
-        Material emitterMaterial = UnknownQuality;
-
-        // Determine the material for the emitter based on the source quality.
-        switch (sourceQuality)
+        private void SetEmitterMaterial(AudioLoFiSourceQuality sourceQuality)
         {
-            case AudioLoFiSourceQuality.NarrowBandTelephony:
-                emitterMaterial = NarrowBandTelephony;
-                break;
+            Material emitterMaterial = UnknownQuality;
 
-            case AudioLoFiSourceQuality.AmRadio:
-                emitterMaterial = AmRadio;
-                break;
+            // Determine the material for the emitter based on the source quality.
+            switch (sourceQuality)
+            {
+                case AudioLoFiSourceQuality.NarrowBandTelephony:
+                    emitterMaterial = NarrowBandTelephony;
+                    break;
 
-            case AudioLoFiSourceQuality.FullRange:
-                emitterMaterial = FullRange;
-                break;
+                case AudioLoFiSourceQuality.AmRadio:
+                    emitterMaterial = AmRadio;
+                    break;
+
+                case AudioLoFiSourceQuality.FullRange:
+                    emitterMaterial = FullRange;
+                    break;
+            }
+
+            // Set the material on the emitter.
+            if (emitterRenderer != null)
+            {
+                emitterRenderer.sharedMaterial = emitterMaterial;
+            }
         }
-
-        // Set the material on the emitter.
-        gameObject.GetComponent<Renderer>().sharedMaterial = emitterMaterial;
     }
 }
