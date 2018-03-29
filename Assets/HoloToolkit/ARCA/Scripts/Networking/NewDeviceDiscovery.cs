bcee36ebe17e666f3c6f13af7a10c1dfa78dad11
Jose Santano <jose.santano@microsoft.com>
2018-03-29 11:33:09 +0100

    Removed the license by mistake

--- Assets/HoloToolkit/ARCA/Scripts/Networking/NewDeviceDiscovery.cs
+++ Assets/HoloToolkit/ARCA/Scripts/Networking/NewDeviceDiscovery.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.using UnityEngine;
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using UnityEngine;
 using UnityEngine.Networking;
