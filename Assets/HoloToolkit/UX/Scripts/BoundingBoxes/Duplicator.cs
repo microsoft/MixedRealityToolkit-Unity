cae135148a7b529a34af361f38cd72f6c6216bd3
Dong Yoon Park <cre8ive@outlook.com>
2018-04-05 17:14:08 -0700

    Reverted Duplicator.cs to original since there is no need to change.

--- Assets/HoloToolkit/UX/Scripts/BoundingBoxes/Duplicator.cs
+++ Assets/HoloToolkit/UX/Scripts/BoundingBoxes/Duplicator.cs
@@ -1,5 +1,7 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿//
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
+//
 
 using System;
 using System.Collections;
