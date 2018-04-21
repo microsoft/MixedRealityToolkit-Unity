175c3b9179bf898bc693306b2f465ee2437becea
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2018-04-21 14:23:40 +0100

    Ported over the Build tools to vNext (as I was fed up of switching project) Might be a few too many things pulled over, but will clean up once we know what makes a project.

---
+++ Assets/MixedRealityToolkit/_Core/Utilities/Scripts/Editor/EnumFlagsPropertyDrawer.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#if UNITY_EDITOR
+
+using UnityEngine;
+using UnityEditor;
+using Microsoft.MixedReality.Toolkit.Internal.Attributes;
+
+namespace Microsoft.MixedReality.Toolkit.Internal.EditorScript
+{
+    [CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
+    public class EnumFlagsPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            // If we're using MRDL custom editors, let the draw override property handle it
+            if (MRTKEditor.ShowCustomEditors)
+                return;
+
+            // Otherwise draw a bitmask normally
+            base.OnGUI(position, property, label);
+        }
+    }
+}
+#endif
