b5b00afa10aff822e3120086da9a2f706e8f0a82
rigma <romain.failla@astrakhan.fr>
2018-04-25 17:34:48 +0200

    Add a new event data for the bounding box rigs

---
+++ Assets/MixedRealityToolkit/InputModule/Scripts/InputEventData/BoundingBoxEventData.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using MixedRealityToolkit.InputModule.InputSources;
+using UnityEngine.EventSystems;
+
+namespace MixedRealityToolkit.InputModule.EventData
+{
+    /// <summary>
+    /// Describes activity of a bounding box rig events.
+    /// </summary>
+    public class BoundingBoxEventData : BaseInputEventData
+    {
+        public BoundingBoxEventData(EventSystem eventSystem) : base(eventSystem) { }
+
+        public void Initialize(IInputSource source, uint sourceId, object tag)
+        {
+            BaseInitialize(source, SourceId, tag);
+        }
+    }
+}
