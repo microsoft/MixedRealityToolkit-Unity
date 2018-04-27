27a88c10427aa637b7ce7e882fac93696c60bf23
rigma <romain.failla@astrakhan.fr>
2018-04-27 09:35:59 +0200

    Adding an example of usage

---
+++ Assets/MixedRealityToolkit-Examples/UX/Scripts/BoundingBoxRigActivityExample.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using MixedRealityToolkit.InputModule.InputHandlers;
+using MixedRealityToolkit.InputModule;
+using UnityEngine;
+using MixedRealityToolkit.InputModule.EventData;
+
+namespace MixedRealityToolkit.Examples.UX
+{
+    public class BoundingBoxRigActivityExample : MonoBehaviour, IBoundingBoxRigActivityHandler
+    {
+        public void OnBoundingBoxRigActivated(BoundingBoxRigActivityEventData eventData)
+        {
+            Debug.LogFormat("{0}'s bounding box rig is activated", eventData.BoundingBoxRiggedObject.name);
+        }
+
+        public void OnBoundingBoxRigDeactivated(BoundingBoxRigActivityEventData eventData)
+        {
+            Debug.LogFormat("{0}'s bounding box rig is deactivated", eventData.BoundingBoxRiggedObject.name);
+        }
+
+        private void Start()
+        {
+            // We have to add a global listener in this case because the focused object is the rig's app bar
+            // So, it will be the app bar which will receive the event, not this listener
+            InputManager.Instance.AddGlobalListener(gameObject);
+        }
+
+        private void OnDestroy()
+        {
+            InputManager.Instance.RemoveGlobalListener(gameObject);
+        }
+    }
+}
