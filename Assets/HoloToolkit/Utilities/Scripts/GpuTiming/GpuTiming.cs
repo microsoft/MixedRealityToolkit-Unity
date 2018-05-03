2b962b104f8c9082714d79f9e3fce2c1c3111019
Kurtis Eveleigh <kurtie@microsoft.com>
2018-05-03 13:20:08 -0700

    Removing tabs and formatting

--- Assets/HoloToolkit/Utilities/Scripts/GpuTiming/GpuTiming.cs
+++ Assets/HoloToolkit/Utilities/Scripts/GpuTiming/GpuTiming.cs
@@ -8,81 +8,81 @@ using UnityEngine;
 
 namespace HoloToolkit.Unity
 {
-	/// <summary>
-	/// Encapsulates access to GPU timing methods.
-	/// </summary>
-	public static class GpuTiming
-	{
-		[DllImport("GpuTiming")]
-		private static extern IntPtr GetRenderEventFunc();
+    /// <summary>
+    /// Encapsulates access to GPU timing methods.
+    /// </summary>
+    public static class GpuTiming
+    {
+        [DllImport("GpuTiming")]
+        private static extern IntPtr GetRenderEventFunc();
 
-		[DllImport("GpuTiming")]
-		private static extern double GetLastFrameGPUTime(int eventId);
+        [DllImport("GpuTiming")]
+        private static extern double GetLastFrameGPUTime(int eventId);
 
-		private const int BaseBeginEventId = 1000;
-		private const int BaseEndEventId = 2000;
+        private const int BaseBeginEventId = 1000;
+        private const int BaseEndEventId = 2000;
 
-		private static int nextAvailableEventId = 0;
-		private static Stack<int> currentEventId = new Stack<int>();
-		private static Dictionary<string, int> eventIds = new Dictionary<string, int>();
-		/// <summary>
-		/// Gets the latest available sample time for the given event. 
-		/// </summary>
-		/// <param name="eventId">Name of the event.</param>
-		/// <returns>Time in milliseconds.</returns>
-		public static double GetTime(string eventId)
-		{
-			int eventValue;
-			if (eventIds.TryGetValue(eventId, out eventValue))
-			{
-				return GetLastFrameGPUTime(eventValue);
-			}
+        private static int nextAvailableEventId = 0;
+        private static Stack<int> currentEventId = new Stack<int>();
+        private static Dictionary<string, int> eventIds = new Dictionary<string, int>();
+        /// <summary>
+        /// Gets the latest available sample time for the given event. 
+        /// </summary>
+        /// <param name="eventId">Name of the event.</param>
+        /// <returns>Time in milliseconds.</returns>
+        public static double GetTime(string eventId)
+        {
+            int eventValue;
+            if (eventIds.TryGetValue(eventId, out eventValue))
+            {
+                return GetLastFrameGPUTime(eventValue);
+            }
 
-			return -1;
-		}
-		/// <summary>
-		/// Begins sampling GPU time. 
-		/// </summary>
-		/// <param name="eventId">Name of the event.</param>
-		/// <returns>Returns true if a BeginSample with the same event name was last added.</returns>
-		public static bool BeginSample(string eventId)
-		{
-			int eventValue;
-			if (!eventIds.TryGetValue(eventId, out eventValue))
-			{
-				if (nextAvailableEventId == BaseEndEventId)
-				{
-					return false;
-				}
+            return -1;
+        }
+        /// <summary>
+        /// Begins sampling GPU time. 
+        /// </summary>
+        /// <param name="eventId">Name of the event.</param>
+        /// <returns>Returns true if a BeginSample with the same event name was last added.</returns>
+        public static bool BeginSample(string eventId)
+        {
+            int eventValue;
+            if (!eventIds.TryGetValue(eventId, out eventValue))
+            {
+                if (nextAvailableEventId == BaseEndEventId)
+                {
+                    return false;
+                }
 
-				eventValue = nextAvailableEventId;
-				eventIds.Add(eventId, nextAvailableEventId++);
-			}
+                eventValue = nextAvailableEventId;
+                eventIds.Add(eventId, nextAvailableEventId++);
+            }
 
-			if (currentEventId.Contains(eventValue))
-			{
-				Debug.LogWarning("BeginSample() is being called without a corresponding EndSample() call.");
-				return false;
-			}
+            if (currentEventId.Contains(eventValue))
+            {
+                Debug.LogWarning("BeginSample() is being called without a corresponding EndSample() call.");
+                return false;
+            }
 
-			currentEventId.Push(eventValue);
+            currentEventId.Push(eventValue);
 
-			// Begin measuring GPU time
-			int eventFunctionId = eventValue + BaseBeginEventId;
-			GL.IssuePluginEvent(GetRenderEventFunc(), eventFunctionId);
-			return true;
-		}
-		/// <summary>
-		/// Ends the GPU sample currently in flight. 
-		/// </summary>
-		public static void EndSample()
-		{
-			if (currentEventId.Count > 0)
-			{
-				// End measuring GPU frame time
-				int eventId = currentEventId.Pop() + BaseEndEventId;
-				GL.IssuePluginEvent(GetRenderEventFunc(), eventId);
-			}
-		}
-	}
+            // Begin measuring GPU time
+            int eventFunctionId = eventValue + BaseBeginEventId;
+            GL.IssuePluginEvent(GetRenderEventFunc(), eventFunctionId);
+            return true;
+        }
+        /// <summary>
+        /// Ends the GPU sample currently in flight. 
+        /// </summary>
+        public static void EndSample()
+        {
+            if (currentEventId.Count > 0)
+            {
+                // End measuring GPU frame time
+                int eventId = currentEventId.Pop() + BaseEndEventId;
+                GL.IssuePluginEvent(GetRenderEventFunc(), eventId);
+            }
+        }
+    }
 }
--- Assets/HoloToolkit/Utilities/Scripts/Solvers/SolverSurfaceMagnetism.cs
+++ Assets/HoloToolkit/Utilities/Scripts/Solvers/SolverSurfaceMagnetism.cs
@@ -1,12 +1,10 @@
-//
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-//
+
 using UnityEngine;
 
 namespace HoloToolkit.Unity
 {
-
     /// <summary>
     ///   SurfaceMagnetism casts rays to Surfaces in the world align the object to the surface.
     /// </summary>
@@ -147,16 +145,16 @@ namespace HoloToolkit.Unity
                 return Vector3.zero;
             }
             return solverHandler.TransformTarget.position;
-		}
+        }
 
-		/// <summary>
-		///   Which point should the ray cast toward?  Not really the 'end' of the ray.  The ray may be cast along
-		///   the head facing direction, from the eye to the object, or to the solver's linked position (working from
-		///   the previous solvers)
-		/// </summary>
-		/// <returns>Vector3, a point on the ray besides the origin</returns>
-		Vector3 GetRaycastEndPoint()
-		{
+        /// <summary>
+        ///   Which point should the ray cast toward?  Not really the 'end' of the ray.  The ray may be cast along
+        ///   the head facing direction, from the eye to the object, or to the solver's linked position (working from
+        ///   the previous solvers)
+        /// </summary>
+        /// <returns>Vector3, a point on the ray besides the origin</returns>
+        Vector3 GetRaycastEndPoint()
+        {
             Vector3 ret = Vector3.forward;
             switch (raycastDirection)
             {
@@ -172,18 +170,18 @@ namespace HoloToolkit.Unity
                     ret = solverHandler.GoalPosition;
                     break;
             }
-			return ret;
-		}
+            return ret;
+        }
 
-		/// <summary>
-		///   Calculate the raycast direction based on the two ray points
-		/// </summary>
-		/// <returns>Vector3, the direction of the raycast</returns>
-		Vector3 GetRaycastDirection()
-		{
-			Vector3 ret = Vector3.forward;
-			if (raycastDirection == RaycastDirectionEnum.CameraFacing)
-			{
+        /// <summary>
+        ///   Calculate the raycast direction based on the two ray points
+        /// </summary>
+        /// <returns>Vector3, the direction of the raycast</returns>
+        Vector3 GetRaycastDirection()
+        {
+            Vector3 ret = Vector3.forward;
+            if (raycastDirection == RaycastDirectionEnum.CameraFacing)
+            {
 
                 if (solverHandler.TransformTarget)
                 {
@@ -234,45 +232,45 @@ namespace HoloToolkit.Unity
                 default:
                     return Quaternion.identity;
             }
-		}
+        }
 
-		/// <summary>
-		///   Checks if a normal is nearly vertical
-		/// </summary>
-		/// <param name="normal"></param>
-		/// <returns>bool</returns>
-		bool IsNormalVertical(Vector3 normal)
-		{
-			return 1f - Mathf.Abs(normal.y) < 0.01f;
-		}
+        /// <summary>
+        ///   Checks if a normal is nearly vertical
+        /// </summary>
+        /// <param name="normal"></param>
+        /// <returns>bool</returns>
+        bool IsNormalVertical(Vector3 normal)
+        {
+            return 1f - Mathf.Abs(normal.y) < 0.01f;
+        }
 
-		/// <summary>
-		///   A constant scale override may be specified for volumetric raycasts, oherwise uses the current value of the solver link's alt scale
-		/// </summary>
-		/// <returns>float</returns>
-		float GetScaleOverride()
-		{
-			if (UseLinkedAltScaleOverride)
-			{
-				return solverHandler.AltScale.Current.magnitude;
-			}
-			return VolumeCastSizeOverride;
-		}
+        /// <summary>
+        ///   A constant scale override may be specified for volumetric raycasts, oherwise uses the current value of the solver link's alt scale
+        /// </summary>
+        /// <returns>float</returns>
+        float GetScaleOverride()
+        {
+            if (UseLinkedAltScaleOverride)
+            {
+                return solverHandler.AltScale.Current.magnitude;
+            }
+            return VolumeCastSizeOverride;
+        }
 
-		public override void SolverUpdate()
-		{
-			// Pass-through by default
-			this.GoalPosition = WorkingPos;
-			this.GoalRotation = WorkingRot;
+        public override void SolverUpdate()
+        {
+            // Pass-through by default
+            this.GoalPosition = WorkingPos;
+            this.GoalRotation = WorkingRot;
 
-			// Determine raycast params
-			Ray ray = new Ray(GetRaycastOrigin(), GetRaycastDirection());
+            // Determine raycast params
+            Ray ray = new Ray(GetRaycastOrigin(), GetRaycastDirection());
 
-			// Skip if there's no valid direction
-			if (ray.direction == Vector3.zero)
-			{
-				return;
-			}
+            // Skip if there's no valid direction
+            if (ray.direction == Vector3.zero)
+            {
+                return;
+            }
 
             float ScaleOverride = GetScaleOverride();
             float len;
@@ -312,7 +310,7 @@ namespace HoloToolkit.Unity
                     break;
 
                 case RaycastModeEnum.Box:
-                    
+
                     Vector3 scale = transform.lossyScale;
                     if (ScaleOverride > 0)
                     {
@@ -387,210 +385,210 @@ namespace HoloToolkit.Unity
                     break;
             }
 
-			// Do frame to frame updates of transform, smoothly toward the goal, if desired
-			UpdateWorkingPosToGoal();
-			UpdateWorkingRotToGoal();
-		}
+            // Do frame to frame updates of transform, smoothly toward the goal, if desired
+            UpdateWorkingPosToGoal();
+            UpdateWorkingRotToGoal();
+        }
 
-		/// <summary>
-		///   Calculates a plane from all raycast hit locations upon which the object may align
-		/// </summary>
-		/// <param name="origin"></param>
-		/// <param name="direction"></param>
-		/// <param name="positions"></param>
-		/// <param name="normals"></param>
-		/// <param name="hits"></param>
-		/// <param name="assetWidth"></param>
-		/// <param name="maxNormalVariance"></param>
-		/// <param name="constrainVertical"></param>
-		/// <param name="bUseClosestDistance"></param>
-		/// <param name="plane"></param>
-		/// <param name="closestDistance"></param>
-		private static void FindPlacementPlane(Vector3 origin, Vector3 direction, Vector3[] positions, Vector3[] normals, bool[] hits, float assetWidth, float maxNormalVariance, bool constrainVertical, bool bUseClosestDistance, out Plane plane, out float closestDistance)
-		{
-			bool debugEnabled = RaycastHelper.DebugEnabled;
+        /// <summary>
+        ///   Calculates a plane from all raycast hit locations upon which the object may align
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="direction"></param>
+        /// <param name="positions"></param>
+        /// <param name="normals"></param>
+        /// <param name="hits"></param>
+        /// <param name="assetWidth"></param>
+        /// <param name="maxNormalVariance"></param>
+        /// <param name="constrainVertical"></param>
+        /// <param name="bUseClosestDistance"></param>
+        /// <param name="plane"></param>
+        /// <param name="closestDistance"></param>
+        private static void FindPlacementPlane(Vector3 origin, Vector3 direction, Vector3[] positions, Vector3[] normals, bool[] hits, float assetWidth, float maxNormalVariance, bool constrainVertical, bool bUseClosestDistance, out Plane plane, out float closestDistance)
+        {
+            bool debugEnabled = RaycastHelper.DebugEnabled;
 
-			int numRays = positions.Length;
+            int numRays = positions.Length;
 
-			Vector3 originalDirection = direction;
-			if (constrainVertical)
-			{
-				direction.y = 0.0f;
-				direction = direction.normalized;
-			}
+            Vector3 originalDirection = direction;
+            if (constrainVertical)
+            {
+                direction.y = 0.0f;
+                direction = direction.normalized;
+            }
 
-			// go through all the points and find the closest distance
-			int closestPoint = -1;
-			closestDistance = float.PositiveInfinity;
-			float farthestDistance = 0f;
-			int numHits = 0;
-			Vector3 averageNormal = Vector3.zero;
+            // go through all the points and find the closest distance
+            int closestPoint = -1;
+            closestDistance = float.PositiveInfinity;
+            float farthestDistance = 0f;
+            int numHits = 0;
+            Vector3 averageNormal = Vector3.zero;
 
-			for (int i = 0; i < numRays; i++)
-			{
-				if (hits[i] != false)
-				{
-					float dist = Vector3.Dot(direction, positions[i] - origin);
+            for (int i = 0; i < numRays; i++)
+            {
+                if (hits[i] != false)
+                {
+                    float dist = Vector3.Dot(direction, positions[i] - origin);
 
-					if (dist < closestDistance)
-					{
-						closestPoint = i;
-						closestDistance = dist;
-					}
-					if (dist > farthestDistance)
-					{
-						farthestDistance = dist;
-					}
+                    if (dist < closestDistance)
+                    {
+                        closestPoint = i;
+                        closestDistance = dist;
+                    }
+                    if (dist > farthestDistance)
+                    {
+                        farthestDistance = dist;
+                    }
 
-					averageNormal += normals[i];
-					++numHits;
-				}
-			}
-			averageNormal /= numHits;
+                    averageNormal += normals[i];
+                    ++numHits;
+                }
+            }
+            averageNormal /= numHits;
 
-			// Calculate variance of all normals
-			float variance = 0;
-			for (int i = 0; i < numRays; ++i)
-			{
-				if (hits[i] != false)
-				{
-					variance += (normals[i] - averageNormal).magnitude;
-				}
-			}
-			variance /= numHits;
+            // Calculate variance of all normals
+            float variance = 0;
+            for (int i = 0; i < numRays; ++i)
+            {
+                if (hits[i] != false)
+                {
+                    variance += (normals[i] - averageNormal).magnitude;
+                }
+            }
+            variance /= numHits;
 
-			// If variance is too high, I really don't want to deal with this surface
-			// And if we don't even have enough rays, I'm not confident about this at all
-			if (variance > maxNormalVariance || numHits < numRays / 4)
-			{
-				plane = new Plane(-direction, positions[closestPoint]);
-				return;
-			}
+            // If variance is too high, I really don't want to deal with this surface
+            // And if we don't even have enough rays, I'm not confident about this at all
+            if (variance > maxNormalVariance || numHits < numRays / 4)
+            {
+                plane = new Plane(-direction, positions[closestPoint]);
+                return;
+            }
 
-			// go through all the points and find the most orthagonal plane
-			float lowAngle = float.PositiveInfinity;
-			int lowIndex = -1;
-			float highAngle = float.NegativeInfinity;
-			int highIndex = -1;
+            // go through all the points and find the most orthagonal plane
+            float lowAngle = float.PositiveInfinity;
+            int lowIndex = -1;
+            float highAngle = float.NegativeInfinity;
+            int highIndex = -1;
 
-			for (int i = 0; i < numRays; i++)
-			{
-				if (hits[i] == false || i == closestPoint)
-				{
-					continue;
-				}
+            for (int i = 0; i < numRays; i++)
+            {
+                if (hits[i] == false || i == closestPoint)
+                {
+                    continue;
+                }
 
-				Vector3 diff = (positions[i] - positions[closestPoint]);
-				if (constrainVertical)
-				{
-					diff.y = 0.0f;
-					diff.Normalize();
+                Vector3 diff = (positions[i] - positions[closestPoint]);
+                if (constrainVertical)
+                {
+                    diff.y = 0.0f;
+                    diff.Normalize();
 
-					if (diff == Vector3.zero)
-					{
-						continue;
-					}
-				}
-				else
-				{
-					diff.Normalize();
-				}
+                    if (diff == Vector3.zero)
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    diff.Normalize();
+                }
 
-				float angle = Vector3.Dot(direction, diff);
+                float angle = Vector3.Dot(direction, diff);
 
-				if (angle < lowAngle)
-				{
-					lowAngle = angle;
-					lowIndex = i;
-				}
-			}
+                if (angle < lowAngle)
+                {
+                    lowAngle = angle;
+                    lowIndex = i;
+                }
+            }
 
-			if (!constrainVertical && lowIndex != -1)
-			{
-				for (int i = 0; i < numRays; i++)
-				{
-					if (hits[i] == false || i == closestPoint || i == lowIndex)
-					{
-						continue;
-					}
+            if (!constrainVertical && lowIndex != -1)
+            {
+                for (int i = 0; i < numRays; i++)
+                {
+                    if (hits[i] == false || i == closestPoint || i == lowIndex)
+                    {
+                        continue;
+                    }
 
-					float dot = Mathf.Abs(Vector3.Dot((positions[i] - positions[closestPoint]).normalized, (positions[lowIndex] - positions[closestPoint]).normalized));
-					if (dot > maxDot)
-					{
-						continue;
-					}
+                    float dot = Mathf.Abs(Vector3.Dot((positions[i] - positions[closestPoint]).normalized, (positions[lowIndex] - positions[closestPoint]).normalized));
+                    if (dot > maxDot)
+                    {
+                        continue;
+                    }
 
-					Vector3 normal = Vector3.Cross(positions[lowIndex] - positions[closestPoint], positions[i] - positions[closestPoint]).normalized;
+                    Vector3 normal = Vector3.Cross(positions[lowIndex] - positions[closestPoint], positions[i] - positions[closestPoint]).normalized;
 
-					float nextAngle = Mathf.Abs(Vector3.Dot(direction, normal));
+                    float nextAngle = Mathf.Abs(Vector3.Dot(direction, normal));
 
-					if (nextAngle > highAngle)
-					{
-						highAngle = nextAngle;
-						highIndex = i;
-					}
-				}
-			}
+                    if (nextAngle > highAngle)
+                    {
+                        highAngle = nextAngle;
+                        highIndex = i;
+                    }
+                }
+            }
 
-			Vector3 placementNormal;
-			if (lowIndex != -1)
-			{
-				if (debugEnabled)
-				{
-					Debug.DrawLine(positions[closestPoint], positions[lowIndex], Color.red);
-				}
+            Vector3 placementNormal;
+            if (lowIndex != -1)
+            {
+                if (debugEnabled)
+                {
+                    Debug.DrawLine(positions[closestPoint], positions[lowIndex], Color.red);
+                }
 
-				if (highIndex != -1)
-				{
-					if (debugEnabled)
-					{
-						Debug.DrawLine(positions[closestPoint], positions[highIndex], Color.green);
-					}
-					placementNormal = Vector3.Cross(positions[lowIndex] - positions[closestPoint], positions[highIndex] - positions[closestPoint]).normalized;
-				}
-				else
-				{
-					Vector3 planeUp = Vector3.Cross(positions[lowIndex] - positions[closestPoint], direction);
-					placementNormal = Vector3.Cross(positions[lowIndex] - positions[closestPoint], constrainVertical ? Vector3.up : planeUp).normalized;
-				}
+                if (highIndex != -1)
+                {
+                    if (debugEnabled)
+                    {
+                        Debug.DrawLine(positions[closestPoint], positions[highIndex], Color.green);
+                    }
+                    placementNormal = Vector3.Cross(positions[lowIndex] - positions[closestPoint], positions[highIndex] - positions[closestPoint]).normalized;
+                }
+                else
+                {
+                    Vector3 planeUp = Vector3.Cross(positions[lowIndex] - positions[closestPoint], direction);
+                    placementNormal = Vector3.Cross(positions[lowIndex] - positions[closestPoint], constrainVertical ? Vector3.up : planeUp).normalized;
+                }
 
-				if (debugEnabled)
-				{
-					Debug.DrawLine(positions[closestPoint], positions[closestPoint] + placementNormal, Color.blue);
-				}
-			}
-			else
-			{
-				placementNormal = direction * -1.0f;
-			}
+                if (debugEnabled)
+                {
+                    Debug.DrawLine(positions[closestPoint], positions[closestPoint] + placementNormal, Color.blue);
+                }
+            }
+            else
+            {
+                placementNormal = direction * -1.0f;
+            }
 
-			if (Vector3.Dot(placementNormal, direction) > 0.0f)
-			{
-				placementNormal *= -1.0f;
-			}
+            if (Vector3.Dot(placementNormal, direction) > 0.0f)
+            {
+                placementNormal *= -1.0f;
+            }
 
-			plane = new Plane(placementNormal, positions[closestPoint]);
+            plane = new Plane(placementNormal, positions[closestPoint]);
 
-			if (debugEnabled)
-			{
-				Debug.DrawRay(positions[closestPoint], placementNormal, Color.cyan);
-			}
+            if (debugEnabled)
+            {
+                Debug.DrawRay(positions[closestPoint], placementNormal, Color.cyan);
+            }
 
-			// Figure out how far the plane should be.
-			if (!bUseClosestDistance && closestPoint >= 0)
-			{
-				float centerPlaneDistance;
-				Ray centerPlaneRay = new Ray(origin, originalDirection);
-				if (plane.Raycast(centerPlaneRay, out centerPlaneDistance) || centerPlaneDistance != 0)
-				{
-					// When the plane is nearly parallel to the user, we need to clamp the distance to where the raycasts hit.
-					closestDistance = Mathf.Clamp(centerPlaneDistance, closestDistance, farthestDistance + assetWidth * 0.5f);
-				}
-				else
-				{
-					Debug.LogError("FindPlacementPlane: Not expected to have the center point not intersect the plane.");
-				}
-			}
-		}
-	}
+            // Figure out how far the plane should be.
+            if (!bUseClosestDistance && closestPoint >= 0)
+            {
+                float centerPlaneDistance;
+                Ray centerPlaneRay = new Ray(origin, originalDirection);
+                if (plane.Raycast(centerPlaneRay, out centerPlaneDistance) || centerPlaneDistance != 0)
+                {
+                    // When the plane is nearly parallel to the user, we need to clamp the distance to where the raycasts hit.
+                    closestDistance = Mathf.Clamp(centerPlaneDistance, closestDistance, farthestDistance + assetWidth * 0.5f);
+                }
+                else
+                {
+                    Debug.LogError("FindPlacementPlane: Not expected to have the center point not intersect the plane.");
+                }
+            }
+        }
+    }
 }
