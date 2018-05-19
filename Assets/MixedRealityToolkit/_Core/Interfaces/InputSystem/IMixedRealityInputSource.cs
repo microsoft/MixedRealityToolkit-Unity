e95f6cf4cb51a0db6d3be938ea95c5c42812d69e
Stephen Hodgson <hodgson.designs@gmail.com>
2018-05-19 13:18:58 -0700

    no need for generic

--- Assets/MixedRealityToolkit/_Core/Interfaces/InputSystem/IMixedRealityInputSource.cs
+++ Assets/MixedRealityToolkit/_Core/Interfaces/InputSystem/IMixedRealityInputSource.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System.Collections;
-using System.Collections.Generic;
 using Microsoft.MixedReality.Toolkit.Internal.Definitions;
 
 namespace Microsoft.MixedReality.Toolkit.Internal.Interfaces.InputSystem
