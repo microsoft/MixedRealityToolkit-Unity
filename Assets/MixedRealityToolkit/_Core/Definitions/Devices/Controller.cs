4ae3fad25bdc78fcda81981641e43908e22c0242
Stephen Hodgson <hodgson.designs@gmail.com>
2018-05-28 00:07:42 -0700

    Updated controller struct.

--- Assets/MixedRealityToolkit/_Core/Definitions/Devices/Controller.cs
+++ Assets/MixedRealityToolkit/_Core/Definitions/Devices/Controller.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using Microsoft.MixedReality.Toolkit.Internal.Definitions.InputSystem;
 using UnityEngine;
 
 namespace Microsoft.MixedReality.Toolkit.Internal.Definitions.Devices
@@ -11,34 +10,45 @@ namespace Microsoft.MixedReality.Toolkit.Internal.Definitions.Devices
     /// </summary>
     public struct Controller
     {
+        public Controller(uint id, Handedness handedness, Vector3 position, Quaternion rotation, ControllerState controllerState, InteractionDefinition[] interactions) : this()
+        {
+            Id = id;
+            Handedness = handedness;
+            Position = position;
+            Rotation = rotation;
+            ControllerState = controllerState;
+            Interactions = interactions;
+        }
+
+
         /// <summary>
-        /// The ID assigned to the Controller
+        /// The ID assigned to the Controller.
         /// </summary>
-        public string Id { get; set; }
+        public uint Id { get; }
 
         /// <summary>
         /// The designated hand that the controller is managing, as defined by the SDK / Unity.
         /// </summary>
-        public Handedness Handedness { get; set; }
+        public Handedness Handedness { get; }
 
         /// <summary>
         /// Outputs the current position of the controller, as defined by the SDK / Unity.
         /// </summary>
-        public Vector3 Position { get; set; }
+        public Vector3 Position { get; }
 
         /// <summary>
         /// Outputs the current rotation of the controller, as defined by the SDK / Unity.
         /// </summary>
-        public Quaternion Rotation { get; set; }
+        public Quaternion Rotation { get; }
 
         /// <summary>
         /// Outputs the current state of the controller, whether it is tracked or not. As defined by the SDK / Unity.
         /// </summary>
-        public ControllerState ControllerState { get; set; }
+        public ControllerState ControllerState { get; }
 
         /// <summary>
         /// Details the list of available buttons / interactions available from the controller.
         /// </summary>
-        public InteractionDefinition[] Interactions { get; set; }
+        public InteractionDefinition[] Interactions { get; }
     }
 }
