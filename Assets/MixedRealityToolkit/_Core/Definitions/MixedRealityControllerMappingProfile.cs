7e34db69c2ab17e2fd20a742ce804ad385d79d91
Simon (Darkside) Jackson <darkside@zenithmoon.com>
2018-05-29 16:24:17 +0100

    Initial Check-in, adding interfaces and profile scriptable object

---
+++ Assets/MixedRealityToolkit/_Core/Definitions/MixedRealityControllerMappingProfile.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Internal.Attributes;
+using Microsoft.MixedReality.Toolkit.Internal.Definitions.Devices;
+using Microsoft.MixedReality.Toolkit.Internal.Definitions.Utilities;
+using Microsoft.MixedReality.Toolkit.Internal.Interfaces.InputSystem;
+using Microsoft.MixedReality.Toolkit.Internal.Utilities;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Internal.Definitions
+{
+    /// <summary>
+    /// Configuration profile settings for the Mixed Reality Toolkit
+    /// </summary>
+    [CreateAssetMenu(menuName = "Mixed Reality Toolkit/Mixed Reality Controller Mapping Profile")]
+    public class MixedRealityControllerMappingProfile : ScriptableObject
+    {
+        //TODO - Does this also need to track Axis?
+
+        [SerializeField]
+        [Tooltip("Enable the Motion Controllers on Startup")]
+        private Handedness controllingHand;
+
+        public Handedness ControllingHand
+        {
+            get { return controllingHand; }
+            private set { controllingHand = value; }
+        }
+
+        [SerializeField]
+        [Tooltip("Controller type to instantiate at runtime.")]
+        [Implements(typeof(IMixedRealityController), TypeGrouping.None)]
+        private SystemType controllerType;
+
+        /// <summary>
+        /// Controller Type to instantiate at runtime.
+        /// </summary>
+        public SystemType ControllerType
+        {
+            get { return controllerType; }
+            private set { controllerType = value; }
+        }
+
+        [SerializeField]
+        [Tooltip("Input System Class to instantiate at runtime.")]
+        private InteractionMapping[] interactions = null;
+
+        public InteractionMapping[] Interactions => interactions ?? new InteractionMapping[0];
+
+        public InteractionMapping GetInteractionMapping(DeviceInputType inputType)
+        {
+            return default(InteractionMapping);
+        }
+    }
+}
---
+++ Assets/MixedRealityToolkit/_Core/Interfaces/IMixedRealityController.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Internal.Definitions;
+using Microsoft.MixedReality.Toolkit.Internal.Definitions.Devices;
+using System.Collections.Generic;
+
+namespace Microsoft.MixedReality.Toolkit.Internal.Interfaces.InputSystem
+{
+    /// <summary>
+    /// Mixed Reality Toolkit controller definition, used to manage a specific controller type
+    /// </summary>
+    public interface IMixedRealityController
+    {
+        /// <summary>
+        /// Outputs the current state of the Input Source, whether it is tracked or not. As defined by the SDK / Unity.
+        /// </summary>
+        ControllerState ControllerState { get; }
+
+        /// <summary>
+        /// The designated hand that the Input Source is managing, as defined by the SDK / Unity.
+        /// </summary>
+        Handedness ControllerHandedness { get; }
+
+        /// <summary>
+        /// The registered Input Source for this controller
+        /// </summary>
+        IMixedRealityInputSource InputSource { get; }
+
+        /// <summary>
+        /// Mapping definition for this controller, linking the Physical inputs to logical Input System Actions
+        /// </summary>
+        Dictionary<DeviceInputType, InteractionMapping> Interactions { get; }
+
+        /// <summary>
+        /// Inform the controller to setup and be ready when asked by it's registered device
+        /// </summary>
+        /// <typeparam name="T">Type of setup parameter (unique to certain SDK's)</typeparam>
+        /// <param name="state">The optional input state provided by the SDK/Platform that is required for setup</param>
+        void SetupInputSource<T>(IMixedRealityInputSystem inputSystem, T state);
+
+        /// <summary>
+        /// Update the controller with new state information / values
+        /// </summary>
+        /// <typeparam name="T">Type of setup parameter (unique to certain SDK's)</typeparam>
+        /// <param name="state">The optional input state provided by the SDK/Platform that is required for update</param>
+        void UpdateInputSource<T>(IMixedRealityInputSystem inputSystem, T state);
+    }
+}
