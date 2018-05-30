196263930483417cadfedf89e3ac5830bd405e34
Stephen Hodgson <hodgson.designs@gmail.com>
2018-05-30 01:56:23 -0700

    renamed SteamVR folder to OpenVR

--- Assets/MixedRealityToolkit/_Core/Devices/OpenVR/GenericOpenVRController.cs
+++ Assets/MixedRealityToolkit/_Core/Devices/OpenVR/GenericOpenVRController.cs
@@ -1,28 +1,42 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System.Collections.Generic;
 using Microsoft.MixedReality.Toolkit.Internal.Definitions.Devices;
 using Microsoft.MixedReality.Toolkit.Internal.Definitions.Utilities;
 using Microsoft.MixedReality.Toolkit.Internal.Interfaces.InputSystem;
+using System.Collections.Generic;
 
-namespace Microsoft.MixedReality.Toolkit.Internal.Devices.SteamVR
+namespace Microsoft.MixedReality.Toolkit.Internal.Devices.OpenVR
 {
-    public struct HTCViveController : IMixedRealityController
+    public struct GenericOpenVRController : IMixedRealityController
     {
-        public ControllerState ControllerState { get; }
-
-        public Handedness ControllerHandedness { get; }
-
-        public IMixedRealityInputSource InputSource { get; }
-
-        public Dictionary<DeviceInputType, InteractionMapping> Interactions { get; }
-
-        public void SetupInputSource<T>(IMixedRealityInputSystem inputSystem, T state)
+        public GenericOpenVRController(ControllerState controllerState, Handedness controllerHandedness, IMixedRealityInputSource inputSource, Dictionary<DeviceInputType, InteractionMapping> interactions) : this()
         {
-           // TODO
+            ControllerState = controllerState;
+            ControllerHandedness = controllerHandedness;
+            InputSource = inputSource;
+            Interactions = interactions;
         }
 
+        /// <inheritdoc />
+        public ControllerState ControllerState { get; }
+
+        /// <inheritdoc />
+        public Handedness ControllerHandedness { get; }
+
+        /// <inheritdoc />
+        public IMixedRealityInputSource InputSource { get; }
+
+        /// <inheritdoc />
+        public Dictionary<DeviceInputType, InteractionMapping> Interactions { get; }
+
+        /// <inheritdoc />
+        public void SetupInputSource<T>(IMixedRealityInputSystem inputSystem, T state)
+        {
+            // TODO
+        }
+
+        /// <inheritdoc />
         public void UpdateInputSource<T>(IMixedRealityInputSystem inputSystem, T state)
         {
             //TODO
