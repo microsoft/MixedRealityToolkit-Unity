9c738bd2b3b3c72527feaf32aff78933c4dfec64
JoseSantano-ms <jose.santano@microsoft.com>
2018-06-19 15:04:39 +0100

    Added Multilens support

---
+++ Assets/HoloToolkit-Examples/SpectatorView/Scripts/SpectatorViewNetworkManager.cs
@@ -0,0 +1,24 @@
+﻿using HoloToolkit.Unity.Preview.SpectatorView;
+using UnityEngine.Networking;
+
+/// <summary>
+/// Example NetworkManager implementation. It simply starts tge server if the HoloLens is running the app.
+/// This component represents the NetworkManager of an entire UNET multilens application.
+/// </summary>
+public class SpectatorViewNetworkManager : NetworkManager
+{
+    /// <summary>
+    /// Is the device a host or a client? (HoloLens or mobile?)
+    /// </summary>
+    private bool isHost;
+
+    // Use this for initialization
+    private void Start()
+    {
+        isHost = FindObjectOfType<PlatformSwitcher>().TargetPlatform == PlatformSwitcher.Platform.Hololens;
+        if (isHost)
+        {
+            StartHost();
+        }
+    }
+}
--- Assets/HoloToolkit-Preview/SpectatorView/Scripts/Networking/NewDeviceDiscovery.cs
+++ Assets/HoloToolkit-Preview/SpectatorView/Scripts/Networking/NewDeviceDiscovery.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -12,9 +13,11 @@ namespace HoloToolkit.Unity.Preview.SpectatorView
     public class NewDeviceDiscovery : NetworkDiscovery
     {
         /// <summary>
-        /// Is the device a host or a client? (HoloLens or mobile?)
+        /// Component that manages the main flow of spectator view
         /// </summary>
-        private bool isHost;
+        [SerializeField]
+        [Tooltip("Component that manages the main flow, events and the main contact point with UNET multilens")]
+        private SpectatorView spectatorView;
 
         /// <summary>
         /// Component used to detect a AR marker from the HoloLens
@@ -32,8 +35,24 @@ namespace HoloToolkit.Unity.Preview.SpectatorView
             set { markerDetectionHololens = value; }
         }
 
+        /// <summary>
+        /// Component that manages the main flow of spectator view
+        /// </summary>
+        public SpectatorView SpectatorView
+        {
+            get { return spectatorView; }
+            set { spectatorView = value; }
+        }
+
         private void Awake()
         {
+            string[] errors;
+            if (!DependenciesValid(out errors))
+            {
+                PrintValidationErrors(errors);
+                gameObject.SetActive(false);
+            }
+
 #if WINDOWS_UWP
             try
             {
@@ -46,11 +65,10 @@ namespace HoloToolkit.Unity.Preview.SpectatorView
                 return;
             }
 #endif
-            isHost = FindObjectOfType<PlatformSwitcher>().TargetPlatform == PlatformSwitcher.Platform.Hololens;
             // The client doesn't have to wait for the server to be started, but this works best if the component
             // waits for the remaining networking bits to have warmed up,
             // just give it a couple of seconds and then start it
-            if (!isHost)
+            if (!spectatorView.IsHost)
             {
                 Invoke("ManualStart", 4f);
             }
@@ -64,7 +82,7 @@ namespace HoloToolkit.Unity.Preview.SpectatorView
             Initialize();
 
             // In this case the host will be listening for a phone to ping it in order to switch on the camera
-            if (isHost)
+            if (spectatorView.IsHost)
             {
                 // As a host, we start listening for a ping to turn on the camera
                 if (MarkerDetectionHololens == null)
@@ -96,5 +114,59 @@ namespace HoloToolkit.Unity.Preview.SpectatorView
         }
 
         #endregion
+
+        #region Dependencies validation
+        private void OnValidate()
+        {
+            //Check if the object is in the scene. Otherwise it'll also check the prefabs and we don't want that
+            if (!gameObject.activeInHierarchy)
+            {
+                return;
+            }
+            string[] errors = null;
+            if (!DependenciesValid(out errors))
+            {
+                PrintValidationErrors(errors);
+            }
+        }
+
+        /// <summary>
+        /// Checks all the dependencies for the script.
+        /// </summary>
+        /// <param name="errors">Out variable that will hold an element for every error, if any</param>
+        /// <returns>Whether all the dependencies exist and are correctly linked up</returns>
+        private bool DependenciesValid(out string[] errors)
+        {
+            var dependenciesValid = true;
+            var errorsList = new List<string>();
+
+            if (markerDetectionHololens == null)
+            {
+                errorsList.Add("MarkerDetectionHololens reference is null on NewDeviceDiscovery.");
+                dependenciesValid = false;
+            }
+
+            if (spectatorView == null)
+            {
+                errorsList.Add("SpectatorView reference is null on NewDeviceDiscovery.");
+                dependenciesValid = false;
+            }
+
+            errors = errorsList.ToArray();
+            return dependenciesValid;
+        }
+
+        /// <summary>
+        /// Prints to the console an error for each
+        /// </summary>
+        /// <param name="errors"></param>
+        private void PrintValidationErrors(string[] errors)
+        {
+            for (var i = 0; i < errors.Length; i++)
+            {
+                Debug.LogError(errors[i]);
+            }
+        }
+        #endregion Dependencies validation
     }
 }
