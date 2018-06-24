3712e55f2bf260692248457fb4bd7069b2d04c24
Stephen Hodgson <hodgson.designs@gmail.com>
2018-06-24 02:53:44 -0700

    made sure that the input system cleans itself up properly.

---
+++ Assets/MixedRealityToolkit/_Core/Interfaces/InputSystem/ITeleportTarget.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Internal.Interfaces.InputSystem.Handlers;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Internal.Interfaces.InputSystem
+{
+    public interface ITeleportTarget : IMixedRealityFocusChangedHandler
+    {
+        Vector3 Position { get; }
+        Vector3 Normal { get; }
+        bool IsActive { get; }
+        bool OverrideTargetOrientation { get; }
+        float TargetOrientation { get; }
+    }
+}
