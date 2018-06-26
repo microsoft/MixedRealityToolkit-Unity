1baa59181a044341b6650c80b4c15deb2ce0a05d
Stephen Hodgson <hodgson.designs@gmail.com>
2018-06-26 01:07:46 -0700

    sorted usings

--- Assets/MixedRealityToolkit/InputSystem/MixedRealityInputManager.cs
+++ Assets/MixedRealityToolkit/InputSystem/MixedRealityInputManager.cs
@@ -9,13 +9,13 @@ using Microsoft.MixedReality.Toolkit.Internal.Definitions.InputSystem;
 using Microsoft.MixedReality.Toolkit.Internal.Definitions.Utilities;
 using Microsoft.MixedReality.Toolkit.Internal.EventDatum.Input;
 using Microsoft.MixedReality.Toolkit.Internal.Extensions;
+using Microsoft.MixedReality.Toolkit.Internal.Interfaces;
 using Microsoft.MixedReality.Toolkit.Internal.Interfaces.InputSystem;
 using Microsoft.MixedReality.Toolkit.Internal.Interfaces.InputSystem.Handlers;
 using Microsoft.MixedReality.Toolkit.Internal.Managers;
 using Microsoft.MixedReality.Toolkit.Internal.Utilities;
 using System;
 using System.Collections.Generic;
-using Microsoft.MixedReality.Toolkit.Internal.Interfaces;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
