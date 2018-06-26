7c961ae744f3bbd6e32ddd8801ec8131b175c7a9
JoseSantano-ms <jose.santano@microsoft.com>
2018-06-26 11:40:27 +0100

    Fix typo

--- Assets/HoloToolkit-Examples/SpectatorView/Scripts/SpectatorViewNetworkManager.cs
+++ Assets/HoloToolkit-Examples/SpectatorView/Scripts/SpectatorViewNetworkManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 /// <summary>
-/// Example NetworkManager implementation. It simply starts tge server if the HoloLens is running the app.
+/// Example NetworkManager implementation. It simply starts the server if the HoloLens is running the app.
 /// This component represents the NetworkManager of an entire UNET multilens application.
 /// </summary>
 public class SpectatorViewNetworkManager : NetworkManager
