b61eca8dc55d2b9b9f2cd2476b0c89c09d43822d
Stephen Hodgson <hodgson.designs@gmail.com>
2018-07-03 11:01:59 -0700

    bc why not?

--- Assets/MixedRealityToolkit/InputSystem/MixedRealityInputManager.cs
+++ Assets/MixedRealityToolkit/InputSystem/MixedRealityInputManager.cs
@@ -102,7 +102,6 @@ namespace Microsoft.MixedReality.Toolkit.InputSystem
             {
                 if (Application.isEditor)
                 {
-#if UNITY_EDITOR
                     bool addedComponents = false;
                     var eventSystems = UnityEngine.Object.FindObjectsOfType<EventSystem>();
                     var standaloneInputModules = UnityEngine.Object.FindObjectsOfType<StandaloneInputModule>();
@@ -121,6 +120,7 @@ namespace Microsoft.MixedReality.Toolkit.InputSystem
                         addedComponents = true;
                     }
 
+#if UNITY_EDITOR
                     if (addedComponents) { UnityEditor.EditorGUIUtility.PingObject(FocusProvider.UIRaycastCamera); }
 #endif
                 }
