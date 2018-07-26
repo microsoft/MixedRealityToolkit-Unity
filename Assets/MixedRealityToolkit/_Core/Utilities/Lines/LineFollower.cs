64513aedc468047b3ec2a1dd5b2e0ed6ac271a13
Stephen Hodgson <hodgson.designs@gmail.com>
2018-07-26 12:45:53 -0700

    cleaned up line follower a bit more and added icons

--- Assets/MixedRealityToolkit/_Core/Utilities/Lines/LineFollower.cs
+++ Assets/MixedRealityToolkit/_Core/Utilities/Lines/LineFollower.cs
@@ -7,34 +7,93 @@ using UnityEngine;
 namespace Microsoft.MixedReality.Toolkit.Internal.Utilities.Lines
 {
     [ExecuteInEditMode]
-    [RequireComponent(typeof(BaseMixedRealityLineDataProvider))]
     public class LineFollower : MonoBehaviour
     {
         [SerializeField]
         [Tooltip("The transform that will follow the point along the line.")]
         private Transform follower;
 
+        /// <summary>
+        /// The transform that will follow the point along the line.
+        /// </summary>
+        public Transform Follower
+        {
+            get
+            {
+                if (follower == null)
+                {
+                    follower = transform;
+                }
+
+                return follower;
+            }
+            set
+            {
+                follower = value == null ? transform : value;
+            }
+        }
+
         [Range(0f, 1f)]
         [SerializeField]
         [Tooltip("Gets a point along the line at the specified normalized length.")]
         private float normalizedLength = 0f;
 
+        public float NormalizedLength
+        {
+            get { return normalizedLength; }
+            set
+            {
+                if (value < 0f)
+                {
+                    normalizedLength = 0f;
+                }
+                else if (value > 1f)
+                {
+                    normalizedLength = 1f;
+                }
+                else
+                {
+                    normalizedLength = value;
+                }
+            }
+        }
+
         [SerializeField]
         [HideInInspector]
         private BaseMixedRealityLineDataProvider source = null;
 
-        private void OnValidate()
+        #region Monobeahviour Implementation
+
+        private void OnValidate() => EnsureSetup();
+
+        private void Awake() => EnsureSetup();
+
+        private void Update()
         {
+            if (source == null || follower == null) { return; }
+
+            Vector3 linePoint = source.GetPoint(normalizedLength);
+            follower.position = linePoint;
+        }
+
+        #endregion Monobeahviour Implementation
+
+        private void EnsureSetup()
+        {
+            if (follower == null)
+            {
+                follower = transform;
+            }
+
             if (source == null)
             {
                 source = GetComponent<BaseMixedRealityLineDataProvider>();
             }
-        }
 
-        private void Update()
-        {
-            Vector3 linePoint = source.GetPoint(normalizedLength);
-            follower.position = linePoint;
+            if (source == null)
+            {
+                Debug.LogError($"Missing a Mixed Reality Line Data Provider for Line Follower on {gameObject.name}");
+            }
         }
     }
 }
