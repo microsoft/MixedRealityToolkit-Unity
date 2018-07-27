853f3912cd4c5df2ff9848a485c2c50c221f7a7a
Shubham Arora <shubhamarora28@gmail.com>
2018-07-27 15:09:14 +0530

    Added TestCursorModifier to appropriate namespace

--- Assets/HoloToolkit-Examples/Input/Scripts/TestCursorModifier.cs
+++ Assets/HoloToolkit-Examples/Input/Scripts/TestCursorModifier.cs
@@ -3,51 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using HoloToolkit.Unity.InputModule;
 
-/// <summary>
-/// Component that can be added to any game object with a collider to modify 
-/// how a cursor reacts when on that collider.
-/// </summary>
-public class TestCursorModifier : MonoBehaviour, ICursorModifier
+namespace HoloToolkit.Unity.InputModule.Tests
 {
-    [SerializeField]
-    private GameObject cursorPrefab = null;
-    private GameObject cursor = null;
-
-    public bool GetCursorVisibility()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Vector3 GetModifiedPosition(ICursor cursor)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Quaternion GetModifiedRotation(ICursor cursor)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public Vector3 GetModifiedScale(ICursor cursor)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void GetModifiedTransform(ICursor cursor, out Vector3 position, out Quaternion rotation, out Vector3 scale)
-    {
-        throw new System.NotImplementedException();
-    }
-
     /// <summary>
-    /// Instantiates the cursor if it is null and returns it
+    /// Component that can be added to any game object with a collider to modify 
+    /// how a cursor reacts when on that collider.
     /// </summary>
-    /// <returns>Instantiated cursor game object</returns>
-    public GameObject GetCursor()
+    public class TestCursorModifier : MonoBehaviour, ICursorModifier
     {
-        if(cursor == null)
+        [SerializeField]
+        private GameObject cursorPrefab = null;
+        private GameObject cursor = null;
+
+        public bool GetCursorVisibility()
         {
-            cursor = Instantiate(cursorPrefab, Vector3.zero, Quaternion.identity);
+            throw new System.NotImplementedException();
         }
-        return cursor;
+
+        public Vector3 GetModifiedPosition(ICursor cursor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Quaternion GetModifiedRotation(ICursor cursor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Vector3 GetModifiedScale(ICursor cursor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void GetModifiedTransform(ICursor cursor, out Vector3 position, out Quaternion rotation, out Vector3 scale)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// Instantiates the cursor if it is null and returns it
+        /// </summary>
+        /// <returns>Instantiated cursor game object</returns>
+        public GameObject GetCursor()
+        {
+            if (cursor == null)
+            {
+                cursor = Instantiate(cursorPrefab, Vector3.zero, Quaternion.identity);
+            }
+            return cursor;
+        }
     }
 }
