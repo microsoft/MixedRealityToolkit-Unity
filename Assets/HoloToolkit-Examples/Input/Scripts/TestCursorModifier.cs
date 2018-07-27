4f2062048ff5a4868b37acb252506d2d0eedff6a
Shubham Arora <shubhamarora28@gmail.com>
2018-07-27 09:22:56 +0530

    Added custom cursor modifier example

---
+++ Assets/HoloToolkit-Examples/Input/Scripts/TestCursorModifier.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+/// <summary>
+/// Component that can be added to any game object with a collider to modify 
+/// how a cursor reacts when on that collider.
+/// </summary>
+public class TestCursorModifier : MonoBehaviour, ICursorModifier
+{
+    [SerializeField]
+    private GameObject cursorPrefab = null;
+    private GameObject cursor = null;
+
+    public bool GetCursorVisibility()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Vector3 GetModifiedPosition(ICursor cursor)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Quaternion GetModifiedRotation(ICursor cursor)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Vector3 GetModifiedScale(ICursor cursor)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void GetModifiedTransform(ICursor cursor, out Vector3 position, out Quaternion rotation, out Vector3 scale)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    /// <summary>
+    /// Instantiates the cursor if it is null and returns it
+    /// </summary>
+    /// <returns>Instantiated cursor game object</returns>
+    public GameObject GetCursor()
+    {
+        if(cursor == null)
+        {
+            cursor = Instantiate(cursorPrefab, Vector3.zero, Quaternion.identity);
+        }
+        return cursor;
+    }
+}
---
+++ Assets/HoloToolkit/Input/Scripts/Cursor/TestCursor.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HoloToolkit.Unity.InputModule.Tests
+{
+    /// <summary>
+    /// The test cursor can switch between different game objects based on its state.
+    /// It simply links the game object to set to active with its associated cursor state.
+    /// It also handles cursor modification with TestCursorModifier.
+    /// </summary>
+    public class TestCursor : Cursor
+    {
+        [System.Serializable]
+        public struct ObjectCursor
+        {
+            public CursorStateEnum CursorState;
+            public GameObject CursorObject;
+        }
+
+        [SerializeField]
+        public ObjectCursor[] Cursors;
+
+        private GameObject userDefinedCursor = null;
+
+        protected override void OnActiveModifier(ICursorModifier modifier)
+        {
+            if (modifier != null)
+            {
+                if (modifier is TestCursorModifier)
+                {
+                    TestCursorModifier cursorModifier = modifier as TestCursorModifier;
+                    userDefinedCursor = cursorModifier.GetCursor();
+                    userDefinedCursor.transform.SetParent(transform, false);
+                    HideDefaultCursors();
+                }
+                else if (modifier is CursorModifier)
+                {
+                    base.OnActiveModifier(modifier);
+                }
+            }
+            else
+            {
+                if (userDefinedCursor != null)
+                {
+                    Destroy(userDefinedCursor);
+                    userDefinedCursor = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Override OnCursorStateChange to set the correct cursor
+        /// state for the cursor
+        /// </summary>
+        /// <param name="state"></param>
+        public override void OnCursorStateChange(CursorStateEnum state)
+        {
+            base.OnCursorStateChange(state);
+
+            if(userDefinedCursor != null)
+            {
+                return;
+            }
+
+            if (Cursors != null)
+            {
+                for (int i = 0; i < Cursors.Length; i++)
+                {
+                    Cursors[i].CursorObject.SetActive(false);
+                }
+                for (int i = 0; i < Cursors.Length; i++)
+                {
+                    if (Cursors[i].CursorState == state)
+                    {
+                        Cursors[i].CursorObject.SetActive(true);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Hides the default cursors
+        /// </summary>
+        private void HideDefaultCursors()
+        {
+            if (Cursors != null)
+            {
+                for (int i = 0; i < Cursors.Length; i++)
+                {
+                    Cursors[i].CursorObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
