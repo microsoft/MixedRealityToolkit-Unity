75601c6028ad0d215ca736a3b4e9edd895a7e5a8
Stephen Hodgson <hodgson.designs@gmail.com>
2018-07-31 16:13:50 -0700

    sorted using

--- Assets/MixedRealityToolkit/_Core/Definitions/InputSystem/PointerOption.cs
+++ Assets/MixedRealityToolkit/_Core/Definitions/InputSystem/PointerOption.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.﻿
 
-using System;
 using Microsoft.MixedReality.Toolkit.Internal.Attributes;
 using Microsoft.MixedReality.Toolkit.Internal.Definitions.Utilities;
 using Microsoft.MixedReality.Toolkit.Internal.Interfaces;
+using System;
 using UnityEngine;
 
 namespace Microsoft.MixedReality.Toolkit.Internal.Definitions.InputSystem
