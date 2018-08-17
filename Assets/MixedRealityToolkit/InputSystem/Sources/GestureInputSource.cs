2564af577d624d7a10bae4d69cd7e7e25cb07b58
Stephen Hodgson <hodgson.designs@gmail.com>
2018-08-16 20:21:31 -0700

    removed unused collection

--- Assets/MixedRealityToolkit/InputSystem/Sources/GestureInputSource.cs
+++ Assets/MixedRealityToolkit/InputSystem/Sources/GestureInputSource.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System.Collections.Generic;
 using Microsoft.MixedReality.Toolkit.Internal.Definitions.InputSystem;
 using Microsoft.MixedReality.Toolkit.Internal.Definitions.Utilities;
 using UnityEngine.XR.WSA.Input;
