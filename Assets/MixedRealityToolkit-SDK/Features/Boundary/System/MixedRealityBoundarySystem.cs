c0232688e49212d4cd4855f99825f9ea1f4bf422
David Kline (ANALOG) <davidkl@microsoft.com>
2018-08-17 12:49:24 -0700

    pr feedback: move utility files

--- Assets/MixedRealityToolkit-SDK/Features/Boundary/System/MixedRealityBoundarySystem.cs
+++ Assets/MixedRealityToolkit-SDK/Features/Boundary/System/MixedRealityBoundarySystem.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using Microsoft.MixedReality.Toolkit.Internal.Definitions.BoundarySystem;
 using Microsoft.MixedReality.Toolkit.Internal.Definitions.Utilities;
 using Microsoft.MixedReality.Toolkit.Internal.EventDatum.Boundary;
 using Microsoft.MixedReality.Toolkit.Internal.Interfaces.BoundarySystem;
--- Assets/MixedRealityToolkit/_Core/Definitions/BoundarySystem/Edge.cs
+++ Assets/MixedRealityToolkit/_Core/Definitions/BoundarySystem/Edge.cs
@@ -3,7 +3,7 @@
 
 using UnityEngine;
 
-namespace Microsoft.MixedReality.Toolkit.Internal.Definitions.Utilities
+namespace Microsoft.MixedReality.Toolkit.Internal.Definitions.BoundarySystem
 {
     /// <summary>
     /// The BoundaryEdge structure defines the points of a line segment that are used to
