e4c7ca2a88c82b834a32a26c162ac00ebeffc864
Kurtis Eveleigh <kurtie@microsoft.com>
2018-08-21 14:10:20 -0700

    Update solvers with current master state

--- Assets/MixedRealityToolkit-SDK/Features/Utilities/Solvers/SolverHandler.cs
+++ Assets/MixedRealityToolkit-SDK/Features/Utilities/Solvers/SolverHandler.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using Microsoft.MixedReality.Toolkit.Core.Definitions.Utilities;
+using Microsoft.MixedReality.Toolkit.Core.Utilities;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,7 +15,53 @@ namespace Microsoft.MixedReality.Toolkit.SDK.Utilities.Solvers
     public class SolverHandler : MonoBehaviour
     {
         [SerializeField]
-        [Tooltip("The target transform that the solvers will act upon.")]
+        [Tooltip("Tracked object to calculate position and orientation from. If you want to manually override and use a scene object, use the TransformTarget field.")]
+        private TrackedObjectType trackedObjectToReference = TrackedObjectType.Head;
+
+        public TrackedObjectType TrackedObjectToReference
+        {
+            get { return trackedObjectToReference; }
+            set
+            {
+                if (trackedObjectToReference != value)
+                {
+                    trackedObjectToReference = value;
+                    TransformTarget = null;
+                    AttachToNewTrackedObject();
+                }
+            }
+        }
+
+        [SerializeField]
+        [Tooltip("Add an additional offset of the tracked object to base the solver on. Useful for tracking something like a halo position above your head or off the side of a controller.")]
+        private Vector3 additionalOffset;
+
+        public Vector3 AdditionalOffset
+        {
+            get { return additionalOffset; }
+            set
+            {
+                additionalOffset = value;
+                TransformTarget = MakeOffsetTransform(TransformTarget);
+            }
+        }
+
+        [SerializeField]
+        [Tooltip("Add an additional rotation on top of the tracked object. Useful for tracking what is essentially the up or right/left vectors.")]
+        private Vector3 additionalRotation;
+
+        public Vector3 AdditionalRotation
+        {
+            get { return additionalRotation; }
+            set
+            {
+                additionalRotation = value;
+                TransformTarget = MakeOffsetTransform(TransformTarget);
+            }
+        }
+
+        [SerializeField]
+        [Tooltip("Manual override for TrackedObjectToReference if you want to use a scene object. Leave empty if you want to use head or motion-tracked controllers.")]
         private Transform transformTarget;
 
         /// <summary>
@@ -25,34 +73,50 @@ namespace Microsoft.MixedReality.Toolkit.SDK.Utilities.Solvers
             set { transformTarget = value; }
         }
 
+
+        [SerializeField]
+        [Tooltip("Whether or not this SolverHandler calls SolverUpdate() every frame. Only one SolverHandler should manage SolverUpdate(). This setting does not affect whether the Target Transform of this SolverHandler gets updated or not.")]
+        private bool updateSolvers = true;
+
+        /// <summary>
+        /// Whether or not this SolverHandler calls SolverUpdate() every frame. Only one SolverHandler should manage SolverUpdate(). This setting does not affect whether the Target Transform of this SolverHandler gets updated or not.
+        /// </summary>
+        public bool UpdateSolvers
+        {
+            get { return updateSolvers; }
+            set { updateSolvers = value; }
+        }
+
         /// <summary>
         /// The position the solver is trying to move to.
         /// </summary>
-        public Vector3 GoalPosition { get; private set; }
+        public Vector3 GoalPosition { get; set; }
 
         /// <summary>
         /// The rotation the solver is trying to rotate to.
         /// </summary>
-        public Quaternion GoalRotation { get; private set; }
+        public Quaternion GoalRotation { get; set; }
 
         /// <summary>
         /// The scale the solver is trying to scale to.
         /// </summary>
-        public Vector3 GoalScale { get; private set; }
+        public Vector3 GoalScale { get; set; }
 
         /// <summary>
         /// Alternate scale.
         /// </summary>
-        public Vector3 AltScale { get; private set; }
+        public Vector3Smoothed AltScale { get; set; }
 
         /// <summary>
         /// The timestamp the solvers will use to calculate with.
         /// </summary>
-        public float DeltaTime { get; private set; }
+        public float DeltaTime { get; set; }
 
         private float lastUpdateTime;
 
-        private readonly List<Solver> solvers = new List<Solver>();
+        protected readonly List<Solver> solvers = new List<Solver>();
+
+        private GameObject transformWithOffset;
 
         #region MonoBehaviour Implementation
 
@@ -64,14 +128,15 @@ namespace Microsoft.MixedReality.Toolkit.SDK.Utilities.Solvers
 
         private void Awake()
         {
-            if (transformTarget == null)
-            {
-                transformTarget = transform;
-            }
-
             GoalScale = Vector3.one;
-            AltScale = Vector3.one;
+            AltScale = new Vector3Smoothed(Vector3.one, 0.1f);
             DeltaTime = 0.0f;
+
+            // TransformTarget overrides TrackedObjectToReference
+            if (!TransformTarget)
+            {
+                AttachToNewTrackedObject();
+            }
         }
 
         private void Update()
@@ -82,17 +147,63 @@ namespace Microsoft.MixedReality.Toolkit.SDK.Utilities.Solvers
 
         private void LateUpdate()
         {
-            for (int i = 0; i < solvers.Count; ++i)
+            if (UpdateSolvers)
             {
-                Solver solver = solvers[i];
-
-                if (solver.enabled)
+                for (int i = 0; i < solvers.Count; ++i)
                 {
-                    solver.SolverUpdate();
+                    Solver solver = solvers[i];
+
+                    if (solver.enabled)
+                    {
+                        solver.SolverUpdate();
+                    }
                 }
             }
         }
 
+        protected void OnDestroy()
+        {
+            if (transformWithOffset != null)
+            {
+                Destroy(transformWithOffset);
+            }
+        }
+
         #endregion MonoBehaviour Implementation
+
+        public virtual void AttachToNewTrackedObject()
+        {
+            switch (TrackedObjectToReference)
+            {
+                case TrackedObjectType.Head:
+                    TrackTransform(CameraCache.Main.transform);
+                    break;
+                // Other cases will come online as ControllerFinder is ported appropriately.
+            }
+        }
+
+        private void TrackTransform(Transform newTrackedTransform)
+        {
+            TransformTarget = RequiresOffset() ? MakeOffsetTransform(newTrackedTransform) : newTrackedTransform;
+        }
+
+        private bool RequiresOffset()
+        {
+            return AdditionalOffset.sqrMagnitude != 0 || AdditionalRotation.sqrMagnitude != 0;
+        }
+
+        private Transform MakeOffsetTransform(Transform parentTransform)
+        {
+            if (transformWithOffset == null)
+            {
+                transformWithOffset = new GameObject();
+                transformWithOffset.transform.parent = parentTransform;
+            }
+
+            transformWithOffset.transform.localPosition = AdditionalOffset;
+            transformWithOffset.transform.localRotation = Quaternion.Euler(AdditionalRotation);
+            transformWithOffset.name = string.Format("{0} on {1} with offset {2}, {3}", gameObject.name, TrackedObjectToReference.ToString(), AdditionalOffset, AdditionalRotation);
+            return transformWithOffset.transform;
+        }
     }
 }
