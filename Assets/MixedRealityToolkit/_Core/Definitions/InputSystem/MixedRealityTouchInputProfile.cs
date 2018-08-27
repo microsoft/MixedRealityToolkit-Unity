5007a244804afc450cbc7a50fc1dbbfddaad49f7
Stephen Hodgson <hodgson.designs@gmail.com>
2018-08-27 09:15:35 -0700

    Added Touch Screen Input Profile Inspector

--- Assets/MixedRealityToolkit/_Core/Definitions/InputSystem/MixedRealityTouchInputProfile.cs
+++ Assets/MixedRealityToolkit/_Core/Definitions/InputSystem/MixedRealityTouchInputProfile.cs
@@ -7,5 +7,24 @@ using UnityEngine;
 namespace Microsoft.MixedReality.Toolkit.Internal.Definitions.InputSystem
 {
     [CreateAssetMenu(menuName = "Mixed Reality Toolkit/Mixed Reality Touch Input Profile", fileName = "MixedRealityTouchInputProfile", order = (int)CreateProfileMenuItemIndices.TouchInput)]
-    public class MixedRealityTouchInputProfile : ScriptableObject { }
+    public class MixedRealityTouchInputProfile : ScriptableObject
+    {
+        [SerializeField]
+        [Tooltip("Action to use for pointer events.")]
+        private MixedRealityInputAction pointerAction;
+
+        /// <summary>
+        /// Action to use for pointer events.
+        /// </summary>
+        public MixedRealityInputAction PointerAction => pointerAction;
+
+        [SerializeField]
+        [Tooltip("Action to use for hold events.")]
+        private MixedRealityInputAction holdAction;
+
+        /// <summary>
+        /// Action to use for hold events.
+        /// </summary>
+        public MixedRealityInputAction HoldAction => holdAction;
+    }
 }
