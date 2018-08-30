014b01afa0599a686f70599f88de7dfeee587fb4
Stephen Hodgson <hodgson.designs@gmail.com>
2018-08-30 12:51:59 -0700

    sorted usings

--- Assets/MixedRealityToolkit-SDK/Features/Input/MixedRealityInputManager.cs
+++ Assets/MixedRealityToolkit-SDK/Features/Input/MixedRealityInputManager.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using Microsoft.MixedReality.Toolkit.InputSystem.Sources;
 using Microsoft.MixedReality.Toolkit.Core.Definitions.Devices;
 using Microsoft.MixedReality.Toolkit.Core.Definitions.InputSystem;
 using Microsoft.MixedReality.Toolkit.Core.Definitions.Utilities;
@@ -12,6 +11,7 @@ using Microsoft.MixedReality.Toolkit.Core.Interfaces.InputSystem;
 using Microsoft.MixedReality.Toolkit.Core.Interfaces.InputSystem.Handlers;
 using Microsoft.MixedReality.Toolkit.Core.Managers;
 using Microsoft.MixedReality.Toolkit.Core.Utilities;
+using Microsoft.MixedReality.Toolkit.InputSystem.Sources;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
