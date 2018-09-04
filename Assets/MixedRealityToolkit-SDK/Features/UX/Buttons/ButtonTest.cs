15c2548825e64f2c8fa8bdb4dfb7d56b8b03fd45
Todd Williams <toddwil@microsoft.com>
2018-09-04 16:32:02 -0700

    cleaned up code for Interactables

--- Assets/MixedRealityToolkit-SDK/Features/UX/Buttons/ButtonTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.MixedReality.Toolkit.SDK.Input.Handlers;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ButtonTest : BaseInputHandler {
-    
-
-}
