3baf7fdffe7bff05f3d08eaad8e05d861d8c29fd
David Kline (ANALOG) <davidkl@microsoft.com>
2018-09-07 14:13:04 -0700

    fix empty handler class

--- Assets/MixedRealityToolkit-SDK/Features/SpatialAwareness/System/Handlers/MixedRealitySpatialAwarenessSurfaceFindingHandler.cs
+++ Assets/MixedRealityToolkit-SDK/Features/SpatialAwareness/System/Handlers/MixedRealitySpatialAwarenessSurfaceFindingHandler.cs
@@ -1,5 +1,32 @@
-﻿
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 
-public class MixedRealitySpatialAwarenessSurfaceFindingHandler
+using Microsoft.MixedReality.Toolkit.Core.EventDatum.SpatialAwarenessSystem;
+using Microsoft.MixedReality.Toolkit.Core.Interfaces.SpatialAwarenessSystem.Handlers;
+
+namespace Microsoft.MixedReality.Toolkit.SDK.SpatialAwarenessSystem.Handlers
 {
+    /// <summary>
+    /// Class providing the default implementation of the <see cref="IMixedRealitySpatialAwarenessSurfaceFindingHandler"/> interface.
+    /// </summary>
+    public class MixedRealitySpatialAwarenessSurfaceFindingHandler : IMixedRealitySpatialAwarenessSurfaceFindingHandler
+    {
+        /// <inheritdoc />
+        public void OnSurfaceAdded(MixedRealitySpatialAwarenessSurfaceFindingEventData eventData)
+        {
+            // todo
+        }
+
+        /// <inheritdoc />
+        public void OnSurfaceUpdated(MixedRealitySpatialAwarenessSurfaceFindingEventData eventData)
+        {
+            // todo
+        }
+
+        /// <inheritdoc />
+        public void OnSurfaceRemoved(MixedRealitySpatialAwarenessSurfaceFindingEventData eventData)
+        {
+            // todo
+        }
+    }
 }
