30f793007e92135ca597e1caaf951cec2edaf339
Andrew Hall (METAL) <andrha@microsoft.com>
2018-09-11 15:38:08 -0700

    Add missing copyright

--- Assets/MixedRealityToolkit/_Core/Inspectors/Profiles/MixedRealityDiagnosticsSystemProfileInspector.cs
+++ Assets/MixedRealityToolkit/_Core/Inspectors/Profiles/MixedRealityDiagnosticsSystemProfileInspector.cs
@@ -1,7 +1,8 @@
-﻿using Microsoft.MixedReality.Toolkit.Core.Definitions.Diagnostics;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.﻿
+
+using Microsoft.MixedReality.Toolkit.Core.Definitions.Diagnostics;
 using Microsoft.MixedReality.Toolkit.Core.Managers;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
