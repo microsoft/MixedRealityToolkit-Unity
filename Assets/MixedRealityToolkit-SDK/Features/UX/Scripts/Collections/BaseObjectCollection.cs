c780ab8e3486559c1f0c137a9cd07f522ab3c758
Addison Linville <addison.linville@gmail.com>
2018-09-12 16:18:52 -0700

    MRTK SDK UX - ObjectCollection (#2606)
    * adding abstracted objectCollection -> baseCollection and gridCollection
    
    * fixing re: review feedbacK
    
    * Missed Dave's enum naming suggestions
    
    * pulling out newer changes to ObjectCollection from HTK-master. Various Fixes
    
    * missed the virtual -> abstract fix in BaseObjectCollection
    
    * updating to address feedback
    
    * adding lamdas and a few other fixes
    
    * addressing standards formatting
    
    * fixed a few small bugs when testing & added a demo scene
    
    * added a few fixes that were marked as outdated from a previous commit
    
    * moved scatter to a new collection script, removed type suffix from enum names
    
    * changing all enum suffixes from 'enum' to 'type'
    
    * small formatting fixes

---
+++ Assets/MixedRealityToolkit-SDK/Features/UX/Scripts/Collections/BaseObjectCollection.cs
@@ -0,0 +1,156 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Internal.Definitions.Utilities;
+
+namespace Microsoft.MixedReality.Toolkit.SDK.UX.Collections
+{
+    public abstract class BaseObjectCollection : MonoBehaviour
+    {
+        #region public members
+        protected Action<BaseObjectCollection> onCollectionUpdated;
+
+        /// <summary>
+        /// Action called when collection is updated
+        /// </summary>
+        public Action<BaseObjectCollection> OnCollectionUpdated
+        {
+            get { return onCollectionUpdated; }
+            set { onCollectionUpdated = value; }
+        }
+
+        protected List<ObjectCollectionNode> nodeList = new List<ObjectCollectionNode>();
+
+        /// <summary>
+        /// List of objects with generated data on the object.
+        /// </summary>
+        public List<ObjectCollectionNode> NodeList
+        {
+            get { return nodeList; }
+            set { nodeList = value; }
+        }
+
+        [Tooltip("Whether to include space for inactive transforms in the layout")]
+        [SerializeField]
+        protected bool ignoreInactiveTransforms = true;
+
+        /// <summary>
+        /// Whether to include space for inactive transforms in the layout
+        /// </summary>
+        public bool IgnoreInactiveTransforms
+        {
+            get { return ignoreInactiveTransforms; }
+            set { ignoreInactiveTransforms = value; }
+        }
+
+        [Tooltip("Type of sorting to use")]
+        [SerializeField]
+        protected CollationOrderType sortType = CollationOrderType.None;
+
+        /// <summary>
+        /// Type of sorting to use.
+        /// </summary>
+        public CollationOrderType SortType
+        {
+            get { return sortType; }
+            set { sortType = value; }
+        }
+        #endregion public members
+
+        /// <summary>
+        /// Rebuilds / updates the collection layout.
+        /// Update collection is called from the editor button on the inspector.
+        /// </summary>
+        public virtual void UpdateCollection()
+        {
+            // Check for empty nodes and remove them
+            List<ObjectCollectionNode> emptyNodes = new List<ObjectCollectionNode>();
+
+            for (int i = 0; i < NodeList.Count; i++)
+            {
+                if (NodeList[i].transform == null || (IgnoreInactiveTransforms && !NodeList[i].transform.gameObject.activeSelf) || NodeList[i].transform.parent == null || !(NodeList[i].transform.parent.gameObject == gameObject))
+                {
+                    emptyNodes.Add(NodeList[i]);
+                }
+            }
+
+            // Now delete the empty nodes
+            for (int i = 0; i < emptyNodes.Count; i++)
+            {
+                NodeList.Remove(emptyNodes[i]);
+            }
+
+            emptyNodes.Clear();
+
+            // Check when children change and adjust
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Transform child = transform.GetChild(i);
+
+                if (!ContainsNode(child) && (child.gameObject.activeSelf || !IgnoreInactiveTransforms))
+                {
+                    ObjectCollectionNode node = new ObjectCollectionNode();
+
+                    node.Name = child.name;
+                    node.transform = child;
+                    NodeList.Add(node);
+                }
+            }
+
+            switch (SortType)
+            {
+                case CollationOrderType.None:
+                default:
+                    break;
+
+                case CollationOrderType.ChildOrder:
+                    NodeList.Sort((c1, c2) => (c1.transform.GetSiblingIndex().CompareTo(c2.transform.GetSiblingIndex())));
+                    break;
+
+                case CollationOrderType.Alphabetical:
+                    NodeList.Sort((c1, c2) => (String.CompareOrdinal(c1.Name, c2.Name)) );
+                    break;
+
+                case CollationOrderType.AlphabeticalReversed:
+                    NodeList.Sort((c1, c2) => (String.CompareOrdinal(c1.Name, c2.Name)));
+                    NodeList.Reverse();
+                    break;
+
+                case CollationOrderType.ChildOrderReversed:
+                    NodeList.Sort((c1, c2) => (c1.transform.GetSiblingIndex().CompareTo(c2.transform.GetSiblingIndex())));
+                    NodeList.Reverse();
+                    break;
+            }
+
+            LayoutChildren();
+
+            if (OnCollectionUpdated != null)
+            {
+                OnCollectionUpdated.Invoke(this);
+            }
+        }
+
+        /// <summary>
+        /// Check if a node exists in the NodeList.
+        /// </summary>
+        protected bool ContainsNode(Transform node)
+        {
+            for (int i = 0; i < NodeList.Count; i++)
+            {
+                if (NodeList[i].transform == node)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Implement for laying out all children when UpdateCollection is called.
+        /// </summary>
+        protected abstract void LayoutChildren();
+    }
+}
---
+++ Assets/MixedRealityToolkit-SDK/Features/UX/Scripts/Collections/GridObjectCollection.cs
@@ -0,0 +1,375 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Internal.Definitions.Utilities;
+using Microsoft.MixedReality.Toolkit.Internal.Extensions;
+
+namespace Microsoft.MixedReality.Toolkit.SDK.UX.Collections
+{
+    /// <summary>
+    /// A Grid Object Collection is simply a set of child objects organized with some
+    /// layout parameters.  The collection can be used to quickly create 
+    /// control panels or sets of prefab/objects.
+    /// </summary>
+    public class GridObjectCollection : BaseObjectCollection
+    {
+        #region public accessors
+
+        [Tooltip("Type of surface to map the collection to")]
+        [SerializeField]
+        private ObjectOrientationSurfaceType surfaceType = ObjectOrientationSurfaceType.Plane;
+
+        /// <summary>
+        /// Type of surface to map the collection to.
+        /// </summary>
+        public ObjectOrientationSurfaceType SurfaceType
+        {
+            get { return surfaceType; }
+            set { surfaceType = value; }
+        }
+
+        [Tooltip("Should the objects in the collection be rotated / how should they be rotated")]
+        [SerializeField]
+        private OrientationType orientType = OrientationType.FaceOrigin;
+
+        /// <summary>
+        /// Should the objects in the collection face the origin of the collection
+        /// </summary>
+        public OrientationType OrientType
+        {
+            get { return orientType; }
+            set { orientType = value; }
+        }
+
+        [Tooltip("Whether to sort objects by row first or by column first")]
+        [SerializeField]
+        private LayoutOrderType layout = LayoutOrderType.ColumnThenRow;
+
+        /// <summary>
+        /// Whether to sort objects by row first or by column first
+        /// </summary>
+        public LayoutOrderType Layout
+        {
+            get { return layout; }
+            set { layout = value; }
+        }
+
+        [Range(0.05f, 100.0f)]
+        [Tooltip("Radius for the sphere or cylinder")]
+        [SerializeField]
+        private float radius = 2f;
+
+        /// <summary>
+        /// This is the radius of either the Cylinder or Sphere mapping and is ignored when using the plane mapping.
+        /// </summary>
+        public float Radius
+        {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        [SerializeField]
+        [Tooltip("Radial range for radial layout")]
+        [Range(5f, 360f)]
+        private float radialRange = 180f;
+
+        /// <summary>
+        /// This is the radial range for creating a radial fan layout.
+        /// </summary>
+        public float RadialRange
+        {
+            get { return radialRange; }
+            set { radialRange = value; }
+        }
+
+        [Tooltip("Number of rows per column")]
+        [SerializeField]
+        private int rows = 3;
+
+        /// <summary>
+        /// Number of rows per column, column number is automatically determined
+        /// </summary>
+        public int Rows
+        {
+            get { return rows; }
+            set { rows = value; }
+        }
+
+        [Tooltip("Width of cell per object")]
+        [SerializeField]
+        private float cellWidth = 0.5f;
+
+        /// <summary>
+        /// Width of the cell per object in the collection.
+        /// </summary>
+        public float CellWidth
+        {
+            get { return cellWidth; }
+            set { cellWidth = value; }
+        }
+
+        [Tooltip("Height of cell per object")]
+        [SerializeField]
+        private float cellHeight = 0.5f;
+
+        /// <summary>
+        /// Height of the cell per object in the collection.
+        /// </summary>
+        public float CellHeight
+        {
+            get { return cellHeight; }
+            set { cellHeight = value; }
+        }
+
+        /// <summary>
+        /// Total Width of collection
+        /// </summary>
+        public float Width
+        {
+            get
+            {
+                return columns * CellWidth;
+            }
+        }
+
+        /// <summary>
+        /// Total Height of collection
+        /// </summary>
+        public float Height
+        {
+            get
+            {
+                return rows * CellHeight;
+            }
+        }
+
+
+        private Mesh sphereMesh;
+
+        /// <summary>
+        /// Reference mesh to use for rendering the sphere layout
+        /// </summary>
+        public Mesh SphereMesh
+        {
+            get;
+            set;
+        }
+
+        private Mesh cylinderMesh;
+
+        /// <summary>
+        /// Reference mesh to use for rendering the cylinder layout
+        /// </summary>
+        public Mesh CylinderMesh
+        {
+            get;
+            set;
+        }
+
+        #endregion public accessors
+
+        #region private fields
+
+        protected int columns;
+        
+        protected Vector2 halfCell;
+
+        #endregion private fields
+
+        /// <summary>
+        /// Overriding base function for laying out all the children when UpdateCollection is called.
+        /// </summary>
+        protected override void LayoutChildren()
+        {
+            float startOffsetX;
+            float startOffsetY;
+            Vector3[] nodeGrid = new Vector3[NodeList.Count];
+            Vector3 newPos = Vector3.zero;
+
+            // Now lets lay out the grid
+            columns = Mathf.CeilToInt((float)NodeList.Count / rows);
+            startOffsetX = (columns * 0.5f) * CellWidth;
+            startOffsetY = (rows * 0.5f) * CellHeight;
+            halfCell = new Vector2(CellWidth * 0.5f, CellHeight * 0.5f);
+
+            // First start with a grid then project onto surface
+            ResolveGridLayout(nodeGrid, startOffsetX, startOffsetY, layout);
+
+            switch (SurfaceType)
+            {
+                case ObjectOrientationSurfaceType.Plane:
+                    for (int i = 0; i < NodeList.Count; i++)
+                    {
+                        ObjectCollectionNode node = NodeList[i];
+                        newPos = nodeGrid[i];
+                        //Debug.Log(newPos);
+                        node.transform.localPosition = newPos;
+                        UpdateNodeFacing(node);
+                        NodeList[i] = node;
+                    }
+                    break;
+
+                case ObjectOrientationSurfaceType.Cylinder:
+                    for (int i = 0; i < NodeList.Count; i++)
+                    {
+                        ObjectCollectionNode node = NodeList[i];
+                        newPos = VectorExtensions.CylindricalMapping(nodeGrid[i], radius);
+                        node.transform.localPosition = newPos;
+                        UpdateNodeFacing(node);
+                        NodeList[i] = node;
+                    }
+                    break;
+
+                case ObjectOrientationSurfaceType.Sphere:
+
+                    for (int i = 0; i < NodeList.Count; i++)
+                    {
+                        ObjectCollectionNode node = NodeList[i];
+                        newPos = VectorExtensions.SphericalMapping(nodeGrid[i], radius);
+                        node.transform.localPosition = newPos;
+                        UpdateNodeFacing(node);
+                        NodeList[i] = node;
+                    }
+                    break;
+
+                case ObjectOrientationSurfaceType.Radial:
+                    int curColumn = 0;
+                    int curRow = 1;
+
+                    for (int i = 0; i < NodeList.Count; i++)
+                    {
+                        ObjectCollectionNode node = NodeList[i];
+                        newPos = VectorExtensions.RadialMapping(nodeGrid[i], radialRange, radius, curRow, rows, curColumn, columns);
+
+                        if (curColumn == (columns - 1))
+                        {
+                            curColumn = 0;
+                            ++curRow;
+                        }
+                        else
+                        {
+                            ++curColumn;
+                        }
+
+                        node.transform.localPosition = newPos;
+                        UpdateNodeFacing(node);
+                        NodeList[i] = node;
+                    }
+                    break;
+            }
+        }
+
+        protected void ResolveGridLayout(Vector3[] grid, float offsetX, float offsetY, LayoutOrderType order)
+        {
+            int cellCounter = 0;
+            float iMax;
+            float jMax;
+
+            if (order == LayoutOrderType.RowThenColumn)
+            {
+                iMax = Rows;
+                jMax = columns;
+            }
+            else
+            {
+                iMax = columns;
+                jMax = Rows;
+            }
+
+            for (int i = 0; i < iMax; i++)
+            {
+                for (int j = 0; j < jMax; j++)
+                {
+                    if (cellCounter < NodeList.Count)
+                    {
+                        grid[cellCounter].Set(((i * CellWidth) - offsetX + halfCell.x) + NodeList[cellCounter].Offset.x,
+                                             (-(j * CellHeight) + offsetY - halfCell.y) + NodeList[cellCounter].Offset.y,
+                                             0.0f
+                                             );
+                    }
+                    cellCounter++;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Update the facing of a node given the nodes new position for facing orign with node and orientation type
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="orientType"></param>
+        /// <param name="newPos"></param>
+        protected void UpdateNodeFacing(ObjectCollectionNode node)
+        {
+            Vector3 centerAxis;
+            Vector3 pointOnAxisNearestNode;
+            switch (OrientType)
+            {
+                case OrientationType.FaceOrigin:
+                    node.transform.rotation = Quaternion.LookRotation(node.transform.position - transform.position, transform.up);
+                    break;
+
+                case OrientationType.FaceOriginReversed:
+                    node.transform.rotation = Quaternion.LookRotation(transform.position - node.transform.position, transform.up);
+                    break;
+
+                case OrientationType.FaceCenterAxis:
+                    centerAxis = Vector3.Project(node.transform.position - transform.position, transform.up);
+                    pointOnAxisNearestNode = transform.position + centerAxis;
+                    node.transform.rotation = Quaternion.LookRotation(node.transform.position - pointOnAxisNearestNode, transform.up);
+                    break;
+
+                case OrientationType.FaceCenterAxisReversed:
+                    centerAxis = Vector3.Project(node.transform.position - transform.position, transform.up);
+                    pointOnAxisNearestNode = transform.position + centerAxis;
+                    node.transform.rotation = Quaternion.LookRotation(pointOnAxisNearestNode - node.transform.position, transform.up);
+                    break;
+
+                case OrientationType.FaceParentFoward:
+                    node.transform.forward = transform.rotation * Vector3.forward;
+                    break;
+
+                case OrientationType.FaceParentForwardReversed:
+                    node.transform.forward = transform.rotation * Vector3.back;
+                    break;
+
+                case OrientationType.FaceParentUp:
+                    node.transform.forward = transform.rotation * Vector3.up;
+                    break;
+
+                case OrientationType.FaceParentDown:
+                    node.transform.forward = transform.rotation * Vector3.down;
+                    break;
+
+                case OrientationType.None:
+                    break;
+
+                default:
+                    Debug.LogWarning("OrientationType out of range");
+                    break;
+            }
+        }
+
+        // Gizmos to draw when the Collection is selected.
+        protected virtual void OnDrawGizmosSelected()
+        {
+            Vector3 scale = (2f * radius) * Vector3.one;
+            switch (surfaceType)
+            {
+                case ObjectOrientationSurfaceType.Plane:
+                    break;
+                case ObjectOrientationSurfaceType.Cylinder:
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawWireMesh(cylinderMesh, transform.position, transform.rotation, scale);
+                    break;
+                case ObjectOrientationSurfaceType.Sphere:
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawWireMesh(sphereMesh, transform.position, transform.rotation, scale);
+                    break;
+            }
+        }
+    }
+}
