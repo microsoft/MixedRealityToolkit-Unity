839f43d7555066972c5345ec8d3e27b29fa952f2
Stephen Hodgson <hodgson.designs@gmail.com>
2018-09-13 09:58:25 -0700

    Updated pointers to properly raise events from device/controller classes _ONLY_

--- Assets/MixedRealityToolkit/_Core/Devices/MixedReality/WindowsMixedRealityController.cs
+++ Assets/MixedRealityToolkit/_Core/Devices/MixedReality/WindowsMixedRealityController.cs
@@ -132,7 +132,7 @@ namespace Microsoft.MixedReality.Toolkit.Core.Devices.WindowsMixedReality
                         UpdateMenuData(interactionSourceState, Interactions[i]);
                         break;
                     default:
-                        Debug.LogError($"Input [{Interactions[i].InputType}] is not handled for this controller [WindowsMixedRealityController]");
+                        Debug.LogError($"Input [{Interactions[i].InputType}] is not handled for this controller [WindowsMixedRealityController] with Action [{Interactions[i].Description}]");
                         Enabled = false;
                         break;
                 }
@@ -418,9 +418,24 @@ namespace Microsoft.MixedReality.Toolkit.Core.Devices.WindowsMixedReality
                             if (interactionSourceState.selectPressed)
                             {
                                 InputSystem?.RaiseOnInputDown(InputSource, ControllerHandedness, interactionMapping.MixedRealityInputAction);
+
+                                for (int i = 0; i < InputSource.Pointers.Length; i++)
+                                {
+                                    InputSystem?.RaisePointerDown(InputSource.Pointers[i], ControllerHandedness, interactionMapping.MixedRealityInputAction);
+                                }
                             }
                             else
                             {
+                                for (int i = 0; i < InputSource.Pointers.Length; i++)
+                                {
+                                    InputSystem?.RaisePointerClicked(InputSource.Pointers[i], ControllerHandedness, interactionMapping.MixedRealityInputAction, 1);
+                                }
+
+                                for (int i = 0; i < InputSource.Pointers.Length; i++)
+                                {
+                                    InputSystem?.RaisePointerUp(InputSource.Pointers[i], ControllerHandedness, interactionMapping.MixedRealityInputAction);
+                                }
+
                                 InputSystem?.RaiseOnInputUp(InputSource, ControllerHandedness, interactionMapping.MixedRealityInputAction);
                             }
                         }
