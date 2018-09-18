8e5861ec2a618815145b5408b71a248a9df193c2
Stephen Hodgson <hodgson.designs@gmail.com>
2018-09-17 18:38:06 -0700

    removed unused using statement

--- Assets/MixedRealityToolkit/_Core/Definitions/Devices/MixedRealityControllerVisualizationProfile.cs
+++ Assets/MixedRealityToolkit/_Core/Definitions/Devices/MixedRealityControllerVisualizationProfile.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using Microsoft.MixedReality.Toolkit.Core.Definitions.Utilities;
-using Microsoft.MixedReality.Toolkit.Core.Devices.WindowsMixedReality;
 using System;
 using UnityEngine;
 
