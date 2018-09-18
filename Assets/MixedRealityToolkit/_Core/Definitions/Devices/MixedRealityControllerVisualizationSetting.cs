a7d2327360aa64ce7cf83f7f52451ea4b32d263f
Stephen Hodgson <hodgson.designs@gmail.com>
2018-09-17 18:21:04 -0700

    sorted usings

--- Assets/MixedRealityToolkit/_Core/Definitions/Devices/MixedRealityControllerVisualizationSetting.cs
+++ Assets/MixedRealityToolkit/_Core/Definitions/Devices/MixedRealityControllerVisualizationSetting.cs
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information. 
 
-using System;
 using Microsoft.MixedReality.Toolkit.Core.Attributes;
 using Microsoft.MixedReality.Toolkit.Core.Definitions.Utilities;
 using Microsoft.MixedReality.Toolkit.Core.Interfaces.Devices;
+using System;
 using UnityEngine;
 
 namespace Microsoft.MixedReality.Toolkit.Core.Definitions.Devices
