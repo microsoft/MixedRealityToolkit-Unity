8872b273b070daf099c49961a2f742b2893be3eb
Andrew Hall (METAL) <andrha@microsoft.com>
2018-09-19 10:32:23 -0700

    Add missing copyright

--- Assets/MixedRealityToolkit-SDK/Features/Diagnostics/CpuUseTracker.cs
+++ Assets/MixedRealityToolkit-SDK/Features/Diagnostics/CpuUseTracker.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
 using System.Linq;
-using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics;
-using UnityEngine;
 
 namespace Microsoft.MixedReality.Toolkit.SDK.DiagnosticsSystem
 {
--- Assets/MixedRealityToolkit-SDK/Features/Diagnostics/FpsUseTracker.cs
+++ Assets/MixedRealityToolkit-SDK/Features/Diagnostics/FpsUseTracker.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Linq;
 using UnityEngine;
 
 public class FpsUseTracker
