8578d80fd0a81299065b6dcaff23d9c081264762
David Kline (ANALOG) <david.kline@microsoft.com>
2018-10-17 15:05:21 -0700

    add demo script to move the observer volume

---
+++ Assets/MixedRealityToolkit-Examples/Demos/SpatialAwareness/Scripts/SpatialObserverMover.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.MixedReality.Toolkit.Core.Interfaces.SpatialAwarenessSystem;
+using Microsoft.MixedReality.Toolkit.Core.Managers;
+using Microsoft.MixedReality.Toolkit.Core.Utilities;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Examples.Demos.SpatialAwareness
+{
+    /// <summary>
+    /// Moves the spatial observer's origin to match the camera's position.
+    /// </summary>
+    public class SpatialObserverMover : MonoBehaviour
+    {
+        private IMixedRealitySpatialAwarenessSystem spatialAwarenessSystem = null;
+
+        /// <summary>
+        /// The active <see cref="IMixedRealitySpatialAwarenessSystem"/> implementation.
+        /// </summary>
+        private IMixedRealitySpatialAwarenessSystem SpatialAwarenessSystem => spatialAwarenessSystem ?? (spatialAwarenessSystem = MixedRealityManager.SpatialAwarenessSystem);
+
+        void Update()
+        {
+            if (SpatialAwarenessSystem == null) { return; }
+
+            // Set the origin of the observer to the current camera position.
+            SpatialAwarenessSystem.ObserverOrigin = CameraCache.Main.transform.position;
+        }
+    }
+}
