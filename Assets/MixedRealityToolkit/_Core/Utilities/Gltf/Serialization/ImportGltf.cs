80704a33ec16c5a6bd4be61bec868cd4001886d5
Stephen Hodgson <hodgson.designs@gmail.com>
2018-11-12 07:14:18 -0800

    sorted usings

--- Assets/MixedRealityToolkit/_Core/Utilities/Gltf/Serialization/ImportGltf.cs
+++ Assets/MixedRealityToolkit/_Core/Utilities/Gltf/Serialization/ImportGltf.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System;
 using Microsoft.MixedReality.Toolkit.Core.Utilities.Async;
 using Microsoft.MixedReality.Toolkit.Core.Utilities.Async.AwaitYieldInstructions;
 using Microsoft.MixedReality.Toolkit.Core.Utilities.Gltf.Schema;
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using UnityEngine;
