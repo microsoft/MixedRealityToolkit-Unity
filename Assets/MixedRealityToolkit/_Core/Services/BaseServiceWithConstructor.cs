d297737ed7e2ead650ea6ad5ab2c00201f140163
Stephen Hodgson <hodgson.designs@gmail.com>
2018-11-20 12:29:17 -0500

    Added IMixedRealityDataProvider interface and base abstract type. Updated SystemType to filter out abstract types from being assigned in the dropdown inspector. Added assert to the base profile inspector to ensure that the Base.OnEnable is being called correctly from derived types. Updated public MixedRealityComponents to be a readonly list and renamed it.

---
+++ Assets/MixedRealityToolkit/_Core/Services/BaseServiceWithConstructor.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Core.Interfaces;
+
+namespace Microsoft.MixedReality.Toolkit.Core.Services
+{
+    /// <summary>
+    /// Base <see cref="IMixedRealityService"/> with a constructor override.
+    /// </summary>
+    public abstract class BaseServiceWithConstructor : BaseService
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="priority"></param>
+        public BaseServiceWithConstructor(string name, uint priority)
+        {
+            this.name = name;
+            this.priority = priority;
+        }
+
+        private readonly string name;
+
+        /// <inheritdoc />
+        public override string Name => name;
+
+        private readonly uint priority;
+
+        /// <inheritdoc />
+        public override uint Priority => priority;
+    }
+}
