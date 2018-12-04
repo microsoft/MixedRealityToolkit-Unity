6e3bc8804ce6a981d9f2cccf3a4922b0cb7b3168
Lars Simkins <lars.simkins@gmail.com>
2018-12-03 16:17:36 -0800

    Demo - User Space Alignment

---
+++ Assets/MixedRealityToolkit-Examples/Demos/Sharing/UserSpaceAlignment/SimulatedAnchorService.cs
@@ -0,0 +1,52 @@
+﻿using Pixie.AnchorControl;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pixie.Demos
+{
+    public class SimulatedAnchorService : MonoBehaviour, IAnchorDefinitions, IAnchorMatrixSource
+    {
+        [SerializeField]
+        private Transform[] anchorDefinitions;
+
+        public bool Ready
+        {
+            get { return true; }
+        }
+
+        public IEnumerable<AnchorDefinition> Definitions
+        {
+            get
+            {
+                foreach (Transform anchorTransform in anchorDefinitions)
+                {
+                    AnchorDefinition anchorDefinition = new AnchorDefinition();
+                    anchorDefinition.ID = anchorTransform.name;
+                    anchorDefinition.Position = anchorTransform.position;
+                    anchorDefinition.Rotation = anchorTransform.eulerAngles;
+
+                    yield return anchorDefinition;
+                }
+            }
+        }
+
+        public void FetchDefinitions()
+        {
+            return;
+        }
+
+        public bool GetAnchorMatrix(string anchorID, out Matrix4x4 matrix)
+        {
+            matrix = Matrix4x4.identity;
+            foreach (Transform anchorTransform in anchorDefinitions)
+            {
+                if (anchorTransform.name == anchorID)
+                {
+                    matrix = anchorTransform.localToWorldMatrix;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
