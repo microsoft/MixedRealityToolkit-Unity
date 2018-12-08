6ecbcb67904228a720541be1ed7c24a542d30a26
Stephen Hodgson <hodgson.designs@gmail.com>
2018-12-07 16:46:16 -0800

    added a namespace that was forgotten

--- Assets/MixedRealityToolkit/_Core/EventDatum/Networking/BaseNetworkingEventData.cs
+++ Assets/MixedRealityToolkit/_Core/EventDatum/Networking/BaseNetworkingEventData.cs
@@ -1,28 +1,34 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using Microsoft.MixedReality.Toolkit.Core.EventDatum;
 using UnityEngine.EventSystems;
 
-public class BaseNetworkingEventData<T> : GenericBaseEventData
+namespace Microsoft.MixedReality.Toolkit.Core.EventDatum.Networking
 {
     /// <summary>
-    /// The data of the network event.
+    /// The base networking event data for networked messages and events.
     /// </summary>
-    public T Data { get; private set; }
-
-    /// <summary>
-    /// Constructor.
-    /// </summary>
-    /// <param name="eventSystem"></param>
-    public BaseNetworkingEventData(EventSystem eventSystem) : base(eventSystem) { }
-
-    /// <summary>
-    /// Used to initialize/reset the event and populate the data.
-    /// </summary>
-    public void Initialize(T data)
+    /// <typeparam name="T"></typeparam>
+    public class BaseNetworkingEventData<T> : GenericBaseEventData
     {
-        BaseInitialize(null);
-        Data = data;
+        /// <summary>
+        /// The data of the network event.
+        /// </summary>
+        public T Data { get; private set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="eventSystem"></param>
+        public BaseNetworkingEventData(EventSystem eventSystem) : base(eventSystem) { }
+
+        /// <summary>
+        /// Used to initialize/reset the event and populate the data.
+        /// </summary>
+        public void Initialize(T data)
+        {
+            BaseInitialize(null);
+            Data = data;
+        }
     }
 }
--- Assets/MixedRealityToolkit/_Core/Interfaces/NetworkingSystem/Handlers/IMixedRealityNetworkingHandler.cs
+++ Assets/MixedRealityToolkit/_Core/Interfaces/NetworkingSystem/Handlers/IMixedRealityNetworkingHandler.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using Microsoft.MixedReality.Toolkit.Core.EventDatum.Networking;
 using UnityEngine.EventSystems;
 
 namespace Microsoft.MixedReality.Toolkit.Core.Interfaces.NetworkingSystem.Handlers
