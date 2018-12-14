1e454f9bae1d59b8911e9ed8beac6acb73802db1
Lars Simkins <lars.simkins@gmail.com>
2018-12-13 16:50:56 -0800

    Initial commit for simple state sharing

---
+++ Assets/MixedRealityToolkit/_Core/Interfaces/Sharing/ISimpleState.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Microsoft.MixedReality.Toolkit.Core.Interfaces.Sharing
+{
+    /// <summary>
+    /// Simplest possible non-automatic state sharing.
+    /// Will typically be networked but could also plug into a file system or local database.
+    /// </summary>
+    public interface ISimpleState : IMixedRealityService
+    {
+        Task<bool> HasKey(string key);
+        Task SetState<T>(string key, T state);
+        Task<T> GetState<T>(string key);
+    }
+}
