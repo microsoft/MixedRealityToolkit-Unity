c8c81689af69d4de27c4bad4e3410c6d4e1fedd0
Cameron Micka <thmicka@microsoft.com>
2018-12-28 17:45:14 -0800

    Improving the per-pixel primitive clipping feature within the MRTK/Standard shader

---
+++ Assets/MixedRealityToolkit/_Core/Inspectors/ClippingBoxInspector.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.MixedReality.Toolkit.Core.Utilities;
+using UnityEditor;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Core.Inspectors
+{
+    [CustomEditor(typeof(ClippingBox))]
+    public class ClippingBoxEditor : Editor
+    {
+        private bool HasFrameBounds() { return true; }
+
+        private Bounds OnGetFrameBounds()
+        {
+            var primitive = target as ClippingBox;
+            Debug.Assert(primitive != null);
+            return new Bounds(primitive.transform.position, primitive.transform.lossyScale * 0.5f);
+        }
+    }
+}
--- Assets/MixedRealityToolkit/_Core/Inspectors/HoverLightInspector.cs
+++ Assets/MixedRealityToolkit/_Core/Inspectors/HoverLightInspector.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.﻿
 
-using Microsoft.MixedReality.Toolkit.SDK.UX.Utilities;
+using Microsoft.MixedReality.Toolkit.Core.Utilities;
 using UnityEditor;
 using UnityEngine;
 
-namespace Microsoft.MixedReality.Toolkit.SDK.Inspectors.UX.Utilities
+namespace Microsoft.MixedReality.Toolkit.Core.Inspectors
 {
     [CustomEditor(typeof(HoverLight))]
     public class HoverLightInspector : Editor
