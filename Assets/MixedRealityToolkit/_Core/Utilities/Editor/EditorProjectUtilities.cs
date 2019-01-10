473e331eece4af1707a3a38c0b5de92567c132fd
David Kline (ANALOG) <david.kline@microsoft.com>
2019-01-10 15:22:17 -0800

    fix missing namespace and license header

--- Assets/MixedRealityToolkit/_Core/Utilities/Editor/EditorProjectUtilities.cs
+++ Assets/MixedRealityToolkit/_Core/Utilities/Editor/EditorProjectUtilities.cs
@@ -1,43 +1,49 @@
-﻿using UnityEditor;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEditor;
 
 #if UNITY_EDITOR
 
-[InitializeOnLoad]
-internal class EditorProjectUtilities
+namespace Microsoft.MixedReality.Toolkit.Core.Utilities.Editor
 {
-    /// <summary>
-    /// Static constructor that allows for executing code on project load.
-    /// </summary>
-    static EditorProjectUtilities()
+    [InitializeOnLoad]
+    internal class EditorProjectUtilities
     {
-        CheckMinimumEditorVersion();
-    }
+        /// <summary>
+        /// Static constructor that allows for executing code on project load.
+        /// </summary>
+        static EditorProjectUtilities()
+        {
+            CheckMinimumEditorVersion();
+        }
 
-    /// <summary>
-    /// Checks that a supported version of Unity is being used with this project.
-    /// </summary>
-    /// <remarks>
-    /// This method displays a message to the user allowing them to continue or to exit the editor.
-    /// </remarks>
-    public static void CheckMinimumEditorVersion()
-    {
+        /// <summary>
+        /// Checks that a supported version of Unity is being used with this project.
+        /// </summary>
+        /// <remarks>
+        /// This method displays a message to the user allowing them to continue or to exit the editor.
+        /// </remarks>
+        public static void CheckMinimumEditorVersion()
+        {
 #if !UNITY_2018_3_OR_NEWER
         DisplayIncorrectEditorVersionDialog();
 #endif
-    }
+        }
 
-    /// <summary>
-    /// Displays a message indicating that a project was loaded in an unsupported version of Unity and allows the user
-    /// to continu or exit.
-    /// </summary>
-    private static void DisplayIncorrectEditorVersionDialog()
-    {
-        if (!EditorUtility.DisplayDialog(
-            "Mixed Reality Toolkit", 
-            "The Mixed Reality Toolkit requires Unity 2018.3 or newer.\n\nUsing an older version of Unity may result in compile errors or incorrect behavior.", 
-            "Continue", "Close Editor"))
+        /// <summary>
+        /// Displays a message indicating that a project was loaded in an unsupported version of Unity and allows the user
+        /// to continu or exit.
+        /// </summary>
+        private static void DisplayIncorrectEditorVersionDialog()
         {
-            EditorApplication.Exit(0);
+            if (!EditorUtility.DisplayDialog(
+                "Mixed Reality Toolkit",
+                "The Mixed Reality Toolkit requires Unity 2018.3 or newer.\n\nUsing an older version of Unity may result in compile errors or incorrect behavior.",
+                "Continue", "Close Editor"))
+            {
+                EditorApplication.Exit(0);
+            }
         }
     }
 }
