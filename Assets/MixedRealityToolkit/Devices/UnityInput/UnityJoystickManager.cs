b3f55be7157dfddb268204e2cc94f0c127e0c639
Kurtis <kurtie@microsoft.com>
2019-01-24 14:32:14 -0800

    Update Assets/MixedRealityToolkit/Devices/UnityInput/UnityJoystickManager.cs
    Co-Authored-By: davidkline-ms <davidkl@microsoft.com>

--- Assets/MixedRealityToolkit/Devices/UnityInput/UnityJoystickManager.cs
+++ Assets/MixedRealityToolkit/Devices/UnityInput/UnityJoystickManager.cs
@@ -24,7 +24,7 @@ namespace Microsoft.MixedReality.Toolkit.Core.Devices.UnityInput
         /// </summary>
         /// <param name="name">Friendly name of the service.</param>
         /// <param name="priority">Service priority. Used to determine order of instantiation.</param>
-        /// <param name="profile">The service's con</param>
+        /// <param name="profile">The service's configuration profile.</param>
         public UnityJoystickManager(string name, uint priority, BaseMixedRealityProfile profile) : base(name, priority, profile) { }
 
         private const float DeviceRefreshInterval = 3.0f;
