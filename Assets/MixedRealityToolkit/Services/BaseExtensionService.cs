63b880fe26f110e148ea4918fb20498f958e1d22
Kurtis <kurtie@microsoft.com>
2019-01-24 14:33:03 -0800

    Update Assets/MixedRealityToolkit/Services/BaseExtensionService.cs
    Co-Authored-By: davidkline-ms <davidkl@microsoft.com>

--- Assets/MixedRealityToolkit/Services/BaseExtensionService.cs
+++ Assets/MixedRealityToolkit/Services/BaseExtensionService.cs
@@ -25,7 +25,7 @@ namespace Microsoft.MixedReality.Toolkit.Core.Services
         /// </summary>
         /// <param name="name">Friendly name of the service.</param>
         /// <param name="priority">Service priority. Used to determine order of instantiation.</param>
-        /// <param name="profile">The service's con</param>
+        /// <param name="profile">The service's configuration profile.</param>
         public BaseExtensionService(string name, uint priority, BaseMixedRealityProfile profile) : base(name, priority)
         {
             ConfigurationProfile = profile;
