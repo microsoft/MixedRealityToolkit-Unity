38063ecc7dcca76fb6ef2f2ad3d0f62b214bee9e
Kurtis <kurtie@microsoft.com>
2019-01-24 14:32:36 -0800

    Update Assets/MixedRealityToolkit/Devices/WindowsMixedReality/WindowsMixedRealityDeviceManager.cs
    Co-Authored-By: davidkline-ms <davidkl@microsoft.com>

--- Assets/MixedRealityToolkit/Devices/WindowsMixedReality/WindowsMixedRealityDeviceManager.cs
+++ Assets/MixedRealityToolkit/Devices/WindowsMixedReality/WindowsMixedRealityDeviceManager.cs
@@ -27,7 +27,7 @@ namespace Microsoft.MixedReality.Toolkit.Core.Devices.WindowsMixedReality
         /// </summary>
         /// <param name="name">Friendly name of the service.</param>
         /// <param name="priority">Service priority. Used to determine order of instantiation.</param>
-        /// <param name="profile">The service's con</param>
+        /// <param name="profile">The service's configuration profile.</param>
         public WindowsMixedRealityDeviceManager(string name, uint priority, BaseMixedRealityProfile profile) : base(name, priority, profile) { }
 
 #if UNITY_WSA
