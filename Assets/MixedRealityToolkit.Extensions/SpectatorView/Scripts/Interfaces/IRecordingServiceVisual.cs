0ccfc3e017bb896f63f7131ea9590debc8090d25
chrisfromwork <c.m.barth@outlook.com>
2019-02-06 16:48:01 -0800

    add some recording ui and interfaces for said ui

---
+++ Assets/MixedRealityToolkit.Extensions/SpectatorView/Scripts/Interfaces/IRecordingServiceVisual.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Microsoft.MixedReality.Tookit.Extension.SpectatorView.Interfaces
+{
+    public delegate void RecordingStateChangedHandler(bool recording); 
+
+    public interface IRecordingServiceVisual
+    {
+        event RecordingStateChangedHandler RecordingStateChanged;
+    }
+}
