54f7c11ef1245826b0e1cd160c16d43aea8b87ab
Kurtis <kurtie@microsoft.com>
2019-02-14 16:06:19 -0800

    Expose RadialView's settings as public properties

---
+++ Assets/MixedRealityToolkit/Definitions/Utilities/RadialViewReferenceDirection.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Microsoft.MixedReality.Toolkit.Core.Definitions.Utilities
+{
+    /// <summary>
+    /// Which direction to orient the radial view object.
+    /// </summary>
+    public enum RadialViewReferenceDirection
+    {
+        /// <summary>
+        /// Orient towards the target including roll, pitch and yaw
+        /// </summary>
+        ObjectOriented,
+        /// <summary>
+        /// Orient toward the target but ignore roll
+        /// </summary>
+        FacingWorldUp,
+        /// <summary>
+        /// Orient towards the target but remain vertical or gravity aligned
+        /// </summary>
+        GravityAligned
+    }
+}
