c253de87c90bd1c87d83ea51691ce548cbe29dcd
Alexander Seeck <as@hololux.de>
2019-03-06 15:01:30 +0100

    Added a simple demo scene for the Oculus Go to the MixedRealityToolkit.Examples Demos path

---
+++ Assets/MixedRealityToolkit.Examples/Demos/Providers/OculusGo/Scripts/InputTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Core.EventDatum.Input;
+using Microsoft.MixedReality.Toolkit.Core.Interfaces.InputSystem.Handlers;
+using Microsoft.MixedReality.Toolkit.Services.InputSystem;
+using Microsoft.MixedReality.Toolkit.Core.Definitions.InputSystem;
+
+public class InputTest : InputSystemGlobalListener, IMixedRealityInputHandler, IMixedRealityInputHandler<Vector2>
+{
+    public void OnInputChanged(InputEventData<Vector2> eventData)
+    {
+        Debug.Log($"{ToString(eventData.MixedRealityInputAction)} , Input: {eventData.InputData}");
+    }
+
+    public void OnInputDown(InputEventData eventData)
+    {
+        Debug.Log(ToString(eventData.MixedRealityInputAction));
+    }
+
+    public void OnInputPressed(InputEventData<float> eventData)
+    {
+        Debug.Log(ToString(eventData.MixedRealityInputAction));
+    }
+
+    public void OnInputUp(InputEventData eventData){ }
+
+    public void OnPositionInputChanged(InputEventData<Vector2> eventData) { }
+
+    private string ToString(MixedRealityInputAction action)
+    {
+        return $"Description: {action.Description}, ID: {action.Id}, Constraint: {action.AxisConstraint}";
+    }
+}
