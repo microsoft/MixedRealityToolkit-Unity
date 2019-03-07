007187adfabe7c548055986c0f5bee1ba9a8e035
Alexander Seeck <as@hololux.de>
2019-03-07 15:45:24 +0100

    Revert "fixed the LineRenderer always be drawn correctly along with the controller gizmo"
    This reverts commit ede453f0ed2252b761e7f75bd900d64b5cb558a9.

--- Assets/MixedRealityToolkit/Utilities/Lines/Renderers/MixedRealityLineRenderer.cs
+++ Assets/MixedRealityToolkit/Utilities/Lines/Renderers/MixedRealityLineRenderer.cs
@@ -71,7 +71,7 @@ namespace Microsoft.MixedReality.Toolkit.Core.Utilities.Lines.Renderers
             lineRenderer.enabled = false;
         }
 
-        private void LateUpdate()
+        private void Update()
         {
             if (LineDataSource == null)
             {
