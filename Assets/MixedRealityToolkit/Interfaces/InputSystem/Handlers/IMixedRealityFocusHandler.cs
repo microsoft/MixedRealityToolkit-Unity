b674f09f6a783929730ca75e9390f67c6cf15df9
Alexander Seeck <as@hololux.de>
2019-03-12 22:21:30 +0100

    removed last traces of obsolete methods

--- Assets/MixedRealityToolkit/Interfaces/InputSystem/Handlers/IMixedRealityFocusHandler.cs
+++ Assets/MixedRealityToolkit/Interfaces/InputSystem/Handlers/IMixedRealityFocusHandler.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using System;
 using Microsoft.MixedReality.Toolkit.Core.EventDatum.Input;
 using UnityEngine.EventSystems;
 
@@ -12,12 +11,6 @@ namespace Microsoft.MixedReality.Toolkit.Core.Interfaces.InputSystem.Handlers
     /// </summary>
     public interface IMixedRealityFocusHandler : IEventSystemHandler
     {
-        [Obsolete("Use IMixedRealityFocusChangedHandler instead.")]
-        void OnBeforeFocusChange(FocusEventData eventData);
-
-        [Obsolete("Use IMixedRealityFocusChangedHandler instead.")]
-        void OnFocusChanged(FocusEventData eventData);
-
         /// <summary>
         /// The Focus Enter event is raised on this <see cref="UnityEngine.GameObject"/> whenever a <see cref="Microsoft.MixedReality.Toolkit.Core.Interfaces.InputSystem.IMixedRealityPointer"/>'s focus enters this <see cref="UnityEngine.GameObject"/>'s <see cref="UnityEngine.Collider"/>.
         /// </summary>
