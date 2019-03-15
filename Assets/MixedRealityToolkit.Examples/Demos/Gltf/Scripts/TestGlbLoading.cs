b5efc3d35e2e4a6ade735346b9a7b12eba024422
Stephen Hodgson <hodgson.designs@gmail.com>
2019-03-15 13:03:55 -0700

    Updated gltf demo scene to load the asset from the streaming assets folder

--- Assets/MixedRealityToolkit.Examples/Demos/Gltf/Scripts/TestGlbLoading.cs
+++ Assets/MixedRealityToolkit.Examples/Demos/Gltf/Scripts/TestGlbLoading.cs
@@ -8,6 +8,9 @@ using UnityEngine;
 
 namespace Microsoft.MixedReality.Toolkit.Examples.Demos.Gltf
 {
+    /// <summary>
+    /// glb loading test script that attempts to download the asset from a local or external resource via web request.
+    /// </summary>
     public class TestGlbLoading : MonoBehaviour
     {
         [SerializeField]
