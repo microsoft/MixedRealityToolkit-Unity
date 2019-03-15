551e014004e3fe54ec3ee8e212568118ad3e14cc
Chris Barth <chriba@microsoft.com>
2019-03-15 14:02:57 -0700

    remove accidental typo

--- Assets/MixedRealityToolkit/Utilities/Gltf/Serialization/ConstructGltf.cs
+++ Assets/MixedRealityToolkit/Utilities/Gltf/Serialization/ConstructGltf.cs
@@ -1,4 +1,4 @@
-﻿update // Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using Microsoft.MixedReality.Toolkit.Core.Utilities.Async;
