7822a8c90be2e22f88014da71c8c458410a4d1fe
MenelvagorMilsom <nimilsom@microsoft.com>
2019-03-20 11:20:29 +0000

    Docs for commit 93f77833b (added support for multiple doc versions)

--- styles/main.js
+++ styles/main.js
@@ -1,1 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+
+var imported = document.createElement('script');
+imported.src = '/web/version.js';
+document.head.appendChild(imported);
