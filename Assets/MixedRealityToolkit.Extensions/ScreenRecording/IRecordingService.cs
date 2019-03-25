1f96b504ba0d737f1966da3789aed1e3b263f8bf
Chris Barth <chriba@microsoft.com>
2019-03-25 14:36:24 -0700

    add summaries to screen recording files

--- Assets/MixedRealityToolkit.Extensions/ScreenRecording/IRecordingService.cs
+++ Assets/MixedRealityToolkit.Extensions/ScreenRecording/IRecordingService.cs
@@ -5,13 +5,42 @@ using System;
 
 namespace Microsoft.MixedReality.Toolkit.Extensions.ScreenRecording
 {
+    /// <summary>
+    /// An interface implemented by classes that perform screen recording
+    /// </summary>
     public interface IRecordingService : IDisposable
     {
+        /// <summary>
+        /// Initializes the screen recording service
+        /// </summary>
         void Initialize();
+
+        /// <summary>
+        /// True if the screen recording service has completed initialization and is ready for use
+        /// </summary>
+        /// <returns></returns>
         bool IsInitialized();
+
+        /// <summary>
+        /// Starts screen recording
+        /// </summary>
+        /// <returns></returns>
         bool StartRecording();
+
+        /// <summary>
+        /// Stops screen recording
+        /// </summary>
         void StopRecording();
+
+        /// <summary>
+        /// True if a screen recording has been taken during the current application session
+        /// </summary>
+        /// <returns></returns>
         bool IsRecordingAvailable();
+
+        /// <summary>
+        /// Shows the last captured screen recording from the current application session
+        /// </summary>
         void ShowRecording();
     }
 }
