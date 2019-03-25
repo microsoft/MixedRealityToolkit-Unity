2382b8224b87c9f3d02c2b5f1a105f2eed6b83d2
Chris Barth <chriba@microsoft.com>
2019-03-25 13:23:17 -0700

    add marker detection comments

--- Assets/MixedRealityToolkit.Extensions/MarkerDetection/IMarkerVisual.cs
+++ Assets/MixedRealityToolkit.Extensions/MarkerDetection/IMarkerVisual.cs
@@ -3,10 +3,26 @@
 
 namespace Microsoft.MixedReality.Toolkit.Extensions.MarkerDetection
 {
+    /// <summary>
+    /// Interface that should be implemented by any class that displays markers for detection
+    /// </summary>
     public interface IMarkerVisual
     {
+        /// <summary>
+        /// Shows the specified marker
+        /// </summary>
+        /// <param name="id">Id of the marker to show</param>
         void ShowMarker(int id);
+
+        /// <summary>
+        /// Hides any shown markers
+        /// </summary>
         void HideMarker();
+
+        /// <summary>
+        /// Set the physical size for displaying markers
+        /// </summary>
+        /// <param name="size">The physical size (in meters) that markers should be when shown</param>
         void SetMarkerSize(float size);
     }
 }
