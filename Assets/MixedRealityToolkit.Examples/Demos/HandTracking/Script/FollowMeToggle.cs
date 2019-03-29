cd96224bf4b77f2cdaa09d3eaa42c63707b4fc57
Kurtis <kurtie@microsoft.com>
2019-03-28 19:37:09 -0700

    Add missing license headers

--- Assets/MixedRealityToolkit.Examples/Demos/HandTracking/Script/FollowMeToggle.cs
+++ Assets/MixedRealityToolkit.Examples/Demos/HandTracking/Script/FollowMeToggle.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
 using UnityEngine;
 
 public class FollowMeToggle : MonoBehaviour
--- Assets/MixedRealityToolkit.SDK/Features/UX/Interactable/Scripts/TypeResolution/InteractableType.cs
+++ Assets/MixedRealityToolkit.SDK/Features/UX/Interactable/Scripts/TypeResolution/InteractableType.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.MixedReality.Toolkit.Utilities;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Utilities;
 using System;
 
 namespace Microsoft.MixedReality.Toolkit.UI
