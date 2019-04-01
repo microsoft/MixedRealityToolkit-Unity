a83298116aac9bf1e8d7301bd45e2e78f98e0875
Lars Simkins <lars.simkins@gmail.com>
2019-04-01 09:56:33 -0700

    Boilerplate

--- Assets/MixedRealityToolkit/Inspectors/ExtensionTemplates/ExtensionServiceCreator.cs
+++ Assets/MixedRealityToolkit/Inspectors/ExtensionTemplates/ExtensionServiceCreator.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.MixedReality.Toolkit.Utilities;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
