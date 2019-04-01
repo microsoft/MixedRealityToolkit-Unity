d5e987c21a541e429a1937c7a9f1763e0a218bfb
William Tian <witian@microsoft.com>
2019-04-01 16:53:33 +0100

    Added some useful attributes from Longbow.

---
+++ Assets/MixedRealityToolkit/Attributes/TagPropertyAttribute.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+using UnityEngine;
+using System;
+
+namespace Microsoft.MixedReality.Toolkit
+{
+    /// <summary>
+    /// A PropertyAttribute for Unity tags (a string field).
+    /// </summary>
+    /// <seealso cref="UnityEngine.PropertyAttribute" />
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class TagPropertyAttribute : PropertyAttribute
+    {
+        // Do nothing
+    }
+}
