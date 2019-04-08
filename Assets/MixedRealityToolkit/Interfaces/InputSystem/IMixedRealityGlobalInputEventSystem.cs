26e6a902e2a7d340a555806f51b0e2368723d30e
Eric Provencher <eric.provencher@cae.com>
2019-04-08 13:02:16 -0400

    Removed unused Usings from IMixedRealityGlobalInputEventSystem

--- Assets/MixedRealityToolkit/Interfaces/InputSystem/IMixedRealityGlobalInputEventSystem.cs
+++ Assets/MixedRealityToolkit/Interfaces/InputSystem/IMixedRealityGlobalInputEventSystem.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Microsoft.MixedReality.Toolkit.Input
+﻿namespace Microsoft.MixedReality.Toolkit.Input
 {
     public interface IMixedRealityGlobalInputEventSystem : IMixedRealityInputEventSystem
     {
