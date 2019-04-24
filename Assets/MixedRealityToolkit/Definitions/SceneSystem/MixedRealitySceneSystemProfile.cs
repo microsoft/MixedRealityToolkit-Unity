8cb9724218d9b9a519004ec000c4d35b63bc5483
Lars Simkins <lars.simkins@gmail.com>
2019-04-24 13:52:39 -0700

    Created scene asset reference tools

--- Assets/MixedRealityToolkit/Definitions/SceneSystem/MixedRealitySceneSystemProfile.cs
+++ Assets/MixedRealityToolkit/Definitions/SceneSystem/MixedRealitySceneSystemProfile.cs
@@ -2,18 +2,24 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using Microsoft.MixedReality.Toolkit.Utilities;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Microsoft.MixedReality.Toolkit.SceneSystem
 {
     /// <summary>
-    /// Configuration profile settings for setting up diagnostics.
+    /// Configuration profile settings for setting up scene system.
     /// </summary>
     [CreateAssetMenu(menuName = "Mixed Reality Toolkit/Mixed Reality Scene System Profile", fileName = "MixedRealitySceneSystemProfile", order = (int)CreateProfileMenuItemIndices.SceneSystem)]
     [MixedRealityServiceProfile(typeof(IMixedRealitySceneSystem))]
     public class MixedRealitySceneSystemProfile : BaseMixedRealityProfile
     {
-       
+        [SerializeField]
+#if (UNITY_EDITOR)
+        [SceneAssetReference]
+#endif
+        private UnityEngine.Object managerSceneObject;
     }
 }
