4a5287ad4077a737bf1cca4706aadb737fd0c3eb
Lars Simkins <lars.simkins@gmail.com>
2019-04-24 14:59:46 -0700

    Created scene system profile inspector

---
+++ Assets/MixedRealityToolkit/Inspectors/Profiles/MixedRealitySceneSystemProfileInspector.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information. 
+
+using Microsoft.MixedReality.Toolkit.SceneSystem;
+using Microsoft.MixedReality.Toolkit.Utilities.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Editor
+{
+    [CustomEditor(typeof(MixedRealitySceneSystemProfile))]
+    public class MixedRealitySceneSystemProfileInspector : BaseMixedRealityToolkitConfigurationProfileInspector
+    {
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            if (!MixedRealityInspectorUtility.CheckMixedRealityConfigured(false))
+            {
+                return;
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            RenderMixedRealityToolkitLogo();
+            if (!MixedRealityInspectorUtility.CheckMixedRealityConfigured())
+            {
+                return;
+            }
+
+            if (DrawBacktrackProfileButton("Back to Configuration Profile", MixedRealityToolkit.Instance.ActiveProfile))
+            {
+                return;
+            }
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("Scene System Profile", EditorStyles.boldLabel);
+            EditorGUILayout.HelpBox("The Scene System Profile helps configure your scene settings.", MessageType.Info);
+
+            CheckProfileLock(target);
+
+            serializedObject.Update();
+
+            EditorGUILayout.Space();
+
+            base.OnInspectorGUI();           
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
