03658b734cbd3fc21a57f3b8b4ee249c21f5ea93
Lukas Tönne <lutnne@microsoft.com>
2019-04-25 14:23:20 +0100

    GUI for recording and timeline control buttons.

--- Assets/MixedRealityToolkit.Services/InputSimulation/Editor/InputTestRecordingWindow.cs
+++ Assets/MixedRealityToolkit.Services/InputSimulation/Editor/InputTestRecordingWindow.cs
@@ -2,7 +2,9 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Utilities.Editor;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEditor;
 using System;
 
@@ -38,25 +40,146 @@ namespace Microsoft.MixedReality.Toolkit.Input
     /// </summary>
     public class InputTestRecordingWindow : EditorWindow
     {
-        private InputAnimationAsset inputAnimation;
+        private InputAnimationAsset _inputAnimation;
+        private InputAnimationAsset inputAnimation
+        {
+            get
+            {
+                return _inputAnimation;
+            }
+            set
+            {
+                if (_inputAnimation != value)
+                {
+                    _inputAnimation = value;
+
+                    if (director != null)
+                    {
+                        director.playableAsset = inputAnimation;
+                    }
+                }
+            }
+        }
 
         public enum RecordingMode
         {
-            InputAnimation,
-            TestValues,
+            /// <summary>
+            /// Record input animation and store in the asset.
+            /// </summary>
+            Recording,
+            /// <summary>
+            /// Play back input animation as simulated input.
+            /// </summary>
+            Playback,
         }
 
-        private RecordingMode mode = RecordingMode.InputAnimation;
+        private RecordingMode mode;
+        public RecordingMode Mode => mode;
 
+        private GameObject ownerObject = null;
+        private PlayableDirector director = null;
         private InputAnimationRecorder recorder = null;
+
+        private Texture2D iconPlay = null;
+        private Texture2D iconPause = null;
+        private Texture2D iconRecord = null;
+        private Texture2D iconRecordActive = null;
+        private Texture2D iconStop = null;
+        private Texture2D iconStepFwd = null;
+        private Texture2D iconStepBack = null;
+        private Texture2D iconJumpFwd = null;
+        private Texture2D iconJumpBack = null;
 
         [MenuItem("Mixed Reality Toolkit/Utilities/Input Test Recording")]
         private static void ShowWindow()
         {
             InputTestRecordingWindow window = GetWindow<InputTestRecordingWindow>();
             window.titleContent = new GUIContent("Input Test Recording");
-            window.minSize = new Vector2(380.0f, 680.0f);
+            window.minSize = new Vector2(420.0f, 300.0f);
             window.Show();
+        }
+
+        public void Awake()
+        {
+            string assetPath = "StandardAssets/Textures";
+
+            if (iconPlay == null)
+            {
+                iconPlay = (Texture2D)AssetDatabase.LoadAssetAtPath(MixedRealityToolkitFiles.MapRelativeFilePath($"{assetPath}/MRTK_TimelinePlay.png"), typeof(Texture2D));
+            }
+            if (iconPause == null)
+            {
+                iconPause = (Texture2D)AssetDatabase.LoadAssetAtPath(MixedRealityToolkitFiles.MapRelativeFilePath($"{assetPath}/MRTK_TimelinePause.png"), typeof(Texture2D));
+            }
+            if (iconRecord == null)
+            {
+                iconRecord = (Texture2D)AssetDatabase.LoadAssetAtPath(MixedRealityToolkitFiles.MapRelativeFilePath($"{assetPath}/MRTK_TimelineRecord.png"), typeof(Texture2D));
+            }
+            if (iconRecordActive == null)
+            {
+                iconRecordActive = (Texture2D)AssetDatabase.LoadAssetAtPath(MixedRealityToolkitFiles.MapRelativeFilePath($"{assetPath}/MRTK_TimelineRecordActive.png"), typeof(Texture2D));
+            }
+            if (iconStop == null)
+            {
+                iconStop = (Texture2D)AssetDatabase.LoadAssetAtPath(MixedRealityToolkitFiles.MapRelativeFilePath($"{assetPath}/MRTK_TimelineStop.png"), typeof(Texture2D));
+            }
+            if (iconStepFwd == null)
+            {
+                iconStepFwd = (Texture2D)AssetDatabase.LoadAssetAtPath(MixedRealityToolkitFiles.MapRelativeFilePath($"{assetPath}/MRTK_TimelineStepFwd.png"), typeof(Texture2D));
+            }
+            if (iconStepBack == null)
+            {
+                iconStepBack = (Texture2D)AssetDatabase.LoadAssetAtPath(MixedRealityToolkitFiles.MapRelativeFilePath($"{assetPath}/MRTK_TimelineStepBack.png"), typeof(Texture2D));
+            }
+            if (iconJumpFwd == null)
+            {
+                iconJumpFwd = (Texture2D)AssetDatabase.LoadAssetAtPath(MixedRealityToolkitFiles.MapRelativeFilePath($"{assetPath}/MRTK_TimelineJumpFwd.png"), typeof(Texture2D));
+            }
+            if (iconJumpBack == null)
+            {
+                iconJumpBack = (Texture2D)AssetDatabase.LoadAssetAtPath(MixedRealityToolkitFiles.MapRelativeFilePath($"{assetPath}/MRTK_TimelineJumpBack.png"), typeof(Texture2D));
+            }
+
+            ownerObject = new GameObject();
+            ownerObject.hideFlags = HideFlags.HideAndDontSave;
+
+            SetMode(RecordingMode.Recording);
+        }
+
+        public void OnDestroy()
+        {
+            Destroy(ownerObject);
+        }
+
+        public void SetMode(RecordingMode _mode)
+        {
+            if (_mode != mode)
+            {
+                mode = _mode;
+
+                switch (mode)
+                {
+                    case RecordingMode.Recording:
+                        if (director != null)
+                        {
+                            DestroyImmediate(director);
+                        }
+                        break;
+
+                    case RecordingMode.Playback:
+                        if (recorder != null)
+                        {
+                            DestroyImmediate(recorder);
+                        }
+
+                        director = ownerObject.AddComponent<PlayableDirector>();
+                        director.playOnAwake = false;
+                        director.playableAsset = inputAnimation;
+                        director.Play();
+                        director.Pause();
+                        break;
+                }
+            }
         }
 
         private void OnGUI()
@@ -69,39 +192,108 @@ namespace Microsoft.MixedReality.Toolkit.Input
                 return;
             }
 
+            string[] modeStrings = { "Record", "Playback" };
+            RecordingMode newMode = (RecordingMode)GUILayout.SelectionGrid((int)mode, modeStrings, modeStrings.Length);
+            if (newMode != mode)
+            {
+                SetMode(newMode);
+            }
+
+            switch (mode)
+            {
+                case RecordingMode.Recording:
+                    DrawRecordingGUI();
+                    break;
+                case RecordingMode.Playback:
+                    DrawPlaybackGUI();
+                    break;
+            }
+        }
+
+        private void DrawRecordingGUI()
+        {
+            bool isGUIEnabled = GUI.enabled;
             if (!Application.isPlaying)
             {
-                EditorGUILayout.HelpBox("Input test recording is only available in play mode", MessageType.Warning);
+                EditorGUILayout.HelpBox("Input test recording is only available in play mode", MessageType.Info);
 
                 isGUIEnabled = false;
                 GUI.enabled = false;
             }
 
-            string[] recordingModeNames = { "Record Input Animation", "Record Test Values" };
-            GUILayout.SelectionGrid((int)mode, recordingModeNames, recordingModeNames.Length);
+            bool wasRecording = (recorder != null);
+            bool record = GUILayout.Toggle(wasRecording, wasRecording ? new GUIContent(iconRecordActive, "Stop recording input animation") : new GUIContent(iconRecord, "Record new input animation"), "Button");
 
-            switch (mode)
+            if (record != wasRecording)
             {
-                case RecordingMode.InputAnimation:
-                    bool wasRecording = (recorder != null);
-                    bool record = GUILayout.Toggle(wasRecording, wasRecording ? "Stop Recording" : "Start Recording", "Button");
+                if (record)
+                {
+                    StartRecording();
+                }
+                else
+                {
+                    StopRecording();
+                    EditorUtility.SetDirty(inputAnimation);
+                }
+            }
+        }
 
-                    if (record != wasRecording)
-                    {
-                        if (record)
-                        {
-                            StartRecording();
-                        }
-                        else
-                        {
-                            StopRecording();
-                            EditorUtility.SetDirty(inputAnimation);
-                        }
-                    }
-                    break;
+        private void DrawPlaybackGUI()
+        {
+            Debug.Assert(director != null);
 
-                case RecordingMode.TestValues:
-                    break;
+            bool isGUIEnabled = GUI.enabled;
+            if (!Application.isPlaying)
+            {
+                EditorGUILayout.HelpBox("Input test playback is only available in play mode", MessageType.Info);
+
+                isGUIEnabled = false;
+                GUI.enabled = false;
+            }
+
+            bool wasPlaying = (director.state == PlayState.Playing);
+            bool wasPaused = (director.state == PlayState.Paused);
+
+            EditorGUILayout.BeginHorizontal();
+            bool jumpBack = GUILayout.Button(new GUIContent(iconJumpBack, "Reset input animation"), "Button");
+            bool play = GUILayout.Toggle(wasPlaying, wasPlaying ? new GUIContent(iconPlay, "Stop playing input animation") : new GUIContent(iconPlay, "Play back input animation"), "Button");
+            bool stepFwd = GUILayout.Button(new GUIContent(iconStepFwd, "Step forward one frame"), "Button");
+            EditorGUILayout.EndHorizontal();
+
+            float time = (float)director.time;
+            float newTime = GUILayout.HorizontalSlider(time, 0.0f, (float)inputAnimation.duration);
+
+            if (play != wasPlaying)
+            {
+                if (play)
+                {
+                    director.Play();
+                }
+                else
+                {
+                    director.Pause();
+                }
+            }
+            if (jumpBack)
+            {
+                director.time = 0.0f;
+                director.Evaluate();
+            }
+            if (stepFwd)
+            {
+                director.time += Time.deltaTime;
+                director.Evaluate();
+            }
+            if (newTime != time)
+            {
+                director.time = newTime;
+                director.Evaluate();
+            }
+
+            // Repaint while playing to update the timeline
+            if (director.state == PlayState.Playing)
+            {
+                Repaint();
             }
         }
 
@@ -119,9 +311,7 @@ namespace Microsoft.MixedReality.Toolkit.Input
                 return false;
             }
 
-            var recorderObject = new GameObject();
-            recorderObject.hideFlags = HideFlags.HideAndDontSave;
-            recorder = recorderObject.AddComponent<InputAnimationRecorder>();
+            recorder = ownerObject.AddComponent<InputAnimationRecorder>();
             recorder.settings = profile.RecordingSettings;
 
             return true;
@@ -129,10 +319,10 @@ namespace Microsoft.MixedReality.Toolkit.Input
 
         private void StopRecording()
         {
-            if (recorder != null)
+            if (inputAnimation != null && recorder != null)
             {
                 inputAnimation.InputAnimation = recorder.InputAnimation;
-                Destroy(recorder.gameObject);
+                Destroy(recorder);
             }
         }
     }
