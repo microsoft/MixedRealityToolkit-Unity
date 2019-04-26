34c8045d6d75b4d25d06c0562a1be7828b698e36
Lars Simkins <lars.simkins@gmail.com>
2019-04-26 12:28:57 -0700

    Update IMixedRealityCameraSystem.cs

--- Assets/MixedRealityToolkit/Interfaces/CameraSystem/IMixedRealityCameraSystem.cs
+++ Assets/MixedRealityToolkit/Interfaces/CameraSystem/IMixedRealityCameraSystem.cs
@@ -5,7 +5,7 @@ namespace Microsoft.MixedReality.Toolkit.CameraSystem
 {
     /// <summary>
     /// Manager interface for a camera system in the Mixed Reality Toolkit.
-    /// The camera system is expected to manage render settings on the main camera.
+    /// The camera system is expected to manage settings on the main camera.
     /// It should update the camera's clear settings, render mask, etc based on platform.
     /// </summary>
     public interface IMixedRealityCameraSystem : IMixedRealityEventSystem, IMixedRealityEventSource, IMixedRealityDataProvider
