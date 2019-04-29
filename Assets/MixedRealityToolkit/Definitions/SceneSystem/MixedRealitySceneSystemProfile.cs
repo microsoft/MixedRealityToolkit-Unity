5bafbdd0de258bbab978aa8172ee5f4636103ccc
Lars Simkins <lars.simkins@gmail.com>
2019-04-29 13:34:21 -0700

    Automatically copy lighting settings across all scenes, manage active scene setting.

--- Assets/MixedRealityToolkit/Definitions/SceneSystem/MixedRealitySceneSystemProfile.cs
+++ Assets/MixedRealityToolkit/Definitions/SceneSystem/MixedRealitySceneSystemProfile.cs
@@ -16,9 +16,42 @@ namespace Microsoft.MixedReality.Toolkit.SceneSystem
     [MixedRealityServiceProfile(typeof(IMixedRealitySceneSystem))]
     public class MixedRealitySceneSystemProfile : BaseMixedRealityProfile
     {
-        public bool UseManagerScene => useManagerScene;
+        /// <summary>
+        /// The active scene determines which lighting settings are used and where objects are instantiated.
+        /// If a lighting scene is used, that will be the active scene. Otherwise the manager scene will be used.
+        /// If neither are used, active scene management will be left up to Unity.
+        /// </summary>
+        public bool ManageActiveScene
+        {
+            get
+            {
+                return manageActiveScene && (UseLightingScene || UseManagerScene);
+            }
+        }
+
+        public UnityEngine.Object ActiveSceneObject
+        {
+            get
+            {
+                if (UseLightingScene)
+                {
+                    return LightingSceneObject;
+                }
+                if (UseManagerScene)
+                {
+                    return managerSceneObject;
+                }
+                return null;
+            }
+        }
+
+        public bool UseManagerScene { get { return useManagerScene && managerSceneObject != null; } }
+
+        public bool UseLightingScene { get { return useLightingScene && lightingSceneObject != null; } }
 
         public UnityEngine.Object ManagerSceneObject => managerSceneObject;
+
+        public UnityEngine.Object LightingSceneObject => lightingSceneObject;
 
         [SerializeField]
         [Tooltip("Using a manager scene ensures a MixedRealityToolkit instance will always be loaded first in your application. It also ensures that this scene and instance will never be unloaded.")]
@@ -28,6 +61,19 @@ namespace Microsoft.MixedReality.Toolkit.SceneSystem
 #if (UNITY_EDITOR)
         [SceneAssetReference]
 #endif
-        private UnityEngine.Object managerSceneObject;
+        private UnityEngine.Object managerSceneObject = null;
+
+        [SerializeField]
+        [Tooltip("Using a lighting scene ensures that the same lighting settings will be enforced across all loaded scenes.")]
+        private bool useLightingScene = true;
+
+        [SerializeField]
+#if (UNITY_EDITOR)
+        [SceneAssetReference]
+#endif
+        private UnityEngine.Object lightingSceneObject = null;
+
+        [SerializeField]
+        private bool manageActiveScene = true;
     }
 }
