11fb9a9b47a3d0400125da0706cb22f822778ea6
Kurtis <kurtie@microsoft.com>
2019-05-08 08:29:43 -0700

    Update Assets/MixedRealityToolkit/Interfaces/InputSystem/IMixedRealityEyeGazeProvider.cs
    Co-Authored-By: wiwei <wiwei@microsoft.com>

--- Assets/MixedRealityToolkit/Interfaces/InputSystem/IMixedRealityEyeGazeProvider.cs
+++ Assets/MixedRealityToolkit/Interfaces/InputSystem/IMixedRealityEyeGazeProvider.cs
@@ -21,7 +21,7 @@ namespace Microsoft.MixedReality.Toolkit.Input
         /// </summary>
         /// <remarks>
         /// The usage of eye-based tracking depends on having the Gaze Input permission set
-        /// and user approved, along with proper device eye calibration. This willback to head-based
+        /// and user approved, along with proper device eye calibration. This will fallback to head-based
         /// gaze when eye-based tracking is not available.
         /// </remarks>
         bool UseEyeTracking { get; set; }
