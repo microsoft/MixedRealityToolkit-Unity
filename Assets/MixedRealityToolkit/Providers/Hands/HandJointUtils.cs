a19472d87b7639110035ee5df12b74f63795b5e4
Lukas Tönne <lutnne@microsoft.com>
2019-05-10 17:06:41 +0100

    Improved hand sim poses (#4211)
    * Use full pos+rot poses for simulated hand joint interpolation.
    
    * Finish json data files for joint poses.
    
    * Revert unwanted changes.
    
    * Document ArticulatedHandPose class and remove TransitionTo function.
    
    * Include customized profiles for the hand recording demo scene to make speech commands work.
    
    * Expose property for default file name and add timestamps for hand recording.
    
    * Remove spurious meta file.
    
    See https://github.com/microsoft/MixedRealityToolkit-Unity/issues/4237
    
    * Handle error case when reading unknown joint name from JSON.

--- Assets/MixedRealityToolkit/Providers/Hands/HandJointUtils.cs
+++ Assets/MixedRealityToolkit/Providers/Hands/HandJointUtils.cs
@@ -15,7 +15,15 @@ namespace Microsoft.MixedReality.Toolkit.Input
         /// <param name="pose">The output pose data</param>
         public static bool TryGetJointPose(TrackedHandJoint joint, Handedness handedness, out MixedRealityPose pose)
         {
-            IMixedRealityHand hand = FindHand(handedness);
+            return TryGetJointPose<IMixedRealityHand>(joint, handedness, out pose);
+        }
+
+        /// <summary>
+        /// Try to find the first matching hand controller of the given type and return the pose of the requested joint for that hand.
+        /// </summary>
+        public static bool TryGetJointPose<T>(TrackedHandJoint joint, Handedness handedness, out MixedRealityPose pose) where T : class, IMixedRealityHand
+        {
+            T hand = FindHand<T>(handedness);
             if (hand != null)
             {
                 return hand.TryGetJoint(joint, out pose);
@@ -33,9 +41,17 @@ namespace Microsoft.MixedReality.Toolkit.Input
         /// </remarks>
         public static IMixedRealityHand FindHand(Handedness handedness)
         {
+            return FindHand<IMixedRealityHand>(handedness);
+        }
+
+        /// <summary>
+        /// Find the first detected hand controller of the given type with matching handedness.
+        /// </summary>
+        public static T FindHand<T>(Handedness handedness) where T : class, IMixedRealityHand
+        {
             foreach (var detectedController in MixedRealityToolkit.InputSystem.DetectedControllers)
             {
-                var hand = detectedController as IMixedRealityHand;
+                var hand = detectedController as T;
                 if (hand != null)
                 {
                     if (detectedController.ControllerHandedness == handedness)
