be73ff6548a7a3882bf23daf7c6397f59c02ffd1
Lars Simkins <lars.simkins@gmail.com>
2019-05-14 10:56:53 -0700

    Adds playspace in ensure requirements

--- Assets/MixedRealityToolkit/Services/MixedRealityToolkit.cs
+++ Assets/MixedRealityToolkit/Services/MixedRealityToolkit.cs
@@ -488,6 +488,11 @@ namespace Microsoft.MixedReality.Toolkit
             // We'll enforce that here, then tracking can update it to the appropriate position later.
             CameraCache.Main.transform.position = Vector3.zero;
 
+            // This will create the playspace
+            Transform playspace = MixedRealityPlayspace.Transform;
+            // Parent the camera under the playspace
+            CameraCache.Main.transform.parent = playspace;
+
             bool addedComponents = false;
             if (!Application.isPlaying)
             {
