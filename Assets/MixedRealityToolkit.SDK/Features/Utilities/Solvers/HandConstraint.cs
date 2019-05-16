d72e02ddb9aa76471bc388ced084eb2d99f5b601
Cameron Micka <cameron.micka@microsoft.com>
2019-05-16 16:37:51 -0700

    WIP hand constraint

---
+++ Assets/MixedRealityToolkit.SDK/Features/Utilities/Solvers/HandConstraint.cs
@@ -0,0 +1,150 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Utilities.Solvers
+{
+    /// <summary>
+    /// Provides a solver that constrains to hands with behaviors common to hand constrained UI.
+    /// </summary>
+    public class HandConstraint : Solver, IMixedRealitySourceStateHandler
+    {
+        [SerializeField]
+        [Tooltip("TODO")]
+        private bool billboard = true;
+
+        [SerializeField]
+        [Tooltip("TODO")]
+        private bool transitionBetweenHands = true;
+
+        [SerializeField]
+        [Tooltip("TODO")]
+        private bool displayOnPalmUp = true;
+
+        private InputSystemGlobalListener.Implementation inputSystemGlobalListener = new InputSystemGlobalListener.Implementation();
+        private List<IMixedRealityHand> activeHands = new List<IMixedRealityHand>();
+
+        public override void SolverUpdate()
+        {
+            if (transitionBetweenHands)
+            {
+                TransitionBetweenHands();
+            }
+
+            GoalPosition = SolverHandler.TransformTarget.position;
+            GoalRotation = billboard ? CameraCache.Main.transform.rotation : SolverHandler.TransformTarget.rotation;
+
+            UpdateWorkingPositionToGoal();
+            UpdateWorkingRotationToGoal();
+        }
+
+        private void TransitionBetweenHands()
+        {
+            foreach (var hand in activeHands)
+            {
+                TrackedObjectType trackedObjectType;
+
+                if (HandIsValid(hand) && HandednessToTrackedObjectType(hand.ControllerHandedness, out trackedObjectType))
+                {
+                    ChangeTrackedObjectType(trackedObjectType);
+                    return;
+                }
+            }
+
+            ChangeTrackedObjectType(null);
+        }
+
+        private bool HandIsValid(IMixedRealityHand hand)
+        {
+            if (displayOnPalmUp)
+            {
+                MixedRealityPose pose;
+
+                if (hand.TryGetJoint(TrackedHandJoint.Palm, out pose))
+                {
+                    const float palmFacingThreshold = 0.3f;
+                    return Vector3.Dot(pose.Up, CameraCache.Main.transform.forward) > palmFacingThreshold;
+                }
+            }
+
+            return true;
+        }
+
+        private void ChangeTrackedObjectType(TrackedObjectType? trackedObjectType)
+        {
+            // TODO show/hide
+            if (trackedObjectType != null)
+            {
+                SolverHandler.TrackedObjectToReference = trackedObjectType.Value;
+            }
+            else
+            {
+
+            }
+        }
+
+        private static bool HandednessToTrackedObjectType(Handedness handedness, out TrackedObjectType trackedObjectType)
+        {
+            switch (handedness)
+            {
+                case Handedness.Left:
+                    trackedObjectType = TrackedObjectType.HandJointLeft;
+                    return true;
+                case Handedness.Right:
+                    trackedObjectType = TrackedObjectType.HandJointRight;
+                    return true;
+            }
+
+            trackedObjectType = default(TrackedObjectType);
+            return false;
+        }
+
+        #region MonoBehaviour Implementation
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            inputSystemGlobalListener.OnEnable(gameObject);
+        }
+
+        protected virtual async void Start()
+        {
+            await inputSystemGlobalListener.Start(gameObject);
+        }
+
+        protected virtual void OnDisable()
+        {
+            inputSystemGlobalListener.OnDisable(gameObject);
+        }
+
+        #endregion MonoBehaviour Implementation
+
+        #region IMixedRealitySourceStateHandler Implementation
+
+        public void OnSourceDetected(SourceStateEventData eventData)
+        {
+            var hand = eventData.Controller as IMixedRealityHand;
+
+            if (hand != null)
+            {
+                activeHands.Add(hand);
+            }
+        }
+
+        public void OnSourceLost(SourceStateEventData eventData)
+        {
+            var hand = eventData.Controller as IMixedRealityHand;
+
+            if (hand != null)
+            {
+                activeHands.Remove(hand);
+            }
+        }
+
+        #endregion IMixedRealitySourceStateHandler Implementation
+    }
+}
--- Assets/MixedRealityToolkit.Services/InputSystem/InputSystemGlobalListener.cs
+++ Assets/MixedRealityToolkit.Services/InputSystem/InputSystemGlobalListener.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using Microsoft.MixedReality.Toolkit.Utilities;
-using System;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -13,9 +12,91 @@ namespace Microsoft.MixedReality.Toolkit.Input
     /// </summary>
     public class InputSystemGlobalListener : MonoBehaviour
     {
-        private bool lateInitialize = true;
+        private Implementation implementation = new Implementation();
 
-        private IMixedRealityInputSystem inputSystem = null;
+        /// <summary>
+        /// This class handles InputSystemGlobalListenerImplementation registration. Classes which cannot derive from InputSystemGlobalListener 
+        /// can use this implementation class to aide in registration. 
+        /// </summary>
+        public class Implementation
+        {
+            private bool lateInitialize = true;
+            private IMixedRealityInputSystem inputSystem = null;
+
+            /// <summary>
+            /// The active instance of the input system.
+            /// </summary>
+            public IMixedRealityInputSystem InputSystem
+            {
+                get
+                {
+                    if (inputSystem == null)
+                    {
+                        MixedRealityServiceRegistry.TryGetService<IMixedRealityInputSystem>(out inputSystem);
+                    }
+
+                    return inputSystem;
+                }
+            }
+
+            /// <summary>
+            /// Registers a GameObject as a global listener.
+            /// </summary>
+            /// <param name="gameObject">GameObject to register.</param>
+            public void OnEnable(GameObject gameObject)
+            {
+                if (MixedRealityToolkit.IsInitialized && InputSystem != null && !lateInitialize)
+                {
+                    InputSystem.Register(gameObject);
+                }
+            }
+
+            /// <summary>
+            /// Waits until the input system is valid to registers a GameObject as a global listener.
+            /// </summary>
+            /// <param name="gameObject">GameObject to register.</param>
+            /// <returns></returns>
+            public async Task Start(GameObject gameObject)
+            {
+                if (lateInitialize)
+                {
+                    await EnsureInputSystemValid();
+
+                    // We've been destroyed during the await.
+                    if (this == null)
+                    {
+                        return;
+                    }
+
+                    lateInitialize = false;
+                    InputSystem.Register(gameObject);
+                }
+            }
+
+            /// <summary>
+            /// Unregisters a GameObject as a global listener.
+            /// </summary>
+            /// <param name="gameObject">GameObject to unregister.</param>
+            public virtual void OnDisable(GameObject gameObject)
+            {
+                InputSystem?.Unregister(gameObject);
+            }
+
+            /// <summary>
+            /// A task that will only complete when the input system has in a valid state.
+            /// </summary>
+            /// <remarks>
+            /// It's possible for this object to have been destroyed after the await, which
+            /// implies that callers should check that this != null after awaiting this task.
+            /// </remarks>
+            public async Task EnsureInputSystemValid()
+            {
+                if (InputSystem == null)
+                {
+                    await new WaitUntil(() => InputSystem != null);
+                }
+            }
+        }
 
         /// <summary>
         /// The active instance of the input system.
@@ -24,57 +105,28 @@ namespace Microsoft.MixedReality.Toolkit.Input
         {
             get
             {
-                if (inputSystem == null)
-                {
-                    MixedRealityServiceRegistry.TryGetService<IMixedRealityInputSystem>(out inputSystem);
-                }
-                return inputSystem;
+                return implementation.InputSystem;
             }
         }
 
         protected virtual void OnEnable()
         {
-            if (MixedRealityToolkit.IsInitialized && InputSystem != null && !lateInitialize)
-            {
-                InputSystem.Register(gameObject);
-            }
+            implementation.OnEnable(gameObject);
         }
 
         protected virtual async void Start()
         {
-            if (lateInitialize)
-            {
-                await EnsureInputSystemValid();
-
-                // We've been destroyed during the await.
-                if (this == null)
-                {
-                    return;
-                }
-
-                lateInitialize = false;
-                InputSystem.Register(gameObject);
-            }
+            await implementation.Start(gameObject);
         }
 
         protected virtual void OnDisable()
         {
-            InputSystem?.Unregister(gameObject);
+            implementation.OnDisable(gameObject);
         }
 
-        /// <summary>
-        /// A task that will only complete when the input system has in a valid state.
-        /// </summary>
-        /// <remarks>
-        /// It's possible for this object to have been destroyed after the await, which
-        /// implies that callers should check that this != null after awaiting this task.
-        /// </remarks>
         protected async Task EnsureInputSystemValid()
         {
-            if (InputSystem == null)
-            {
-                await new WaitUntil(() => InputSystem != null);
-            }
+            await implementation.EnsureInputSystemValid();
         }
     }
 }
