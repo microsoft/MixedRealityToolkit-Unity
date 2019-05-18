0e9d59f7c5cdb8acbf37d1bfcf1721acf97f3341
davidkline-ms <david.kline@microsoft.com>
2019-05-17 23:31:48 -0700

    add missing file headers

--- Assets/MixedRealityToolkit/Interfaces/InputSystem/IMixedRealityMouseDeviceManager.cs
+++ Assets/MixedRealityToolkit/Interfaces/InputSystem/IMixedRealityMouseDeviceManager.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.MixedReality.Toolkit.Input;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Input;
 
 public interface IMixedRealityMouseDeviceManager : IMixedRealityInputDeviceManager
 {
