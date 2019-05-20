365e53d81b490d6c487ba85fe2a3e0c2b63a6af9
Luis Valverde <luval@microsoft.com>
2019-05-20 14:37:48 +0100

    Input actions example

---
+++ Assets/MixedRealityToolkit.Examples/Demos/Input/Scenes/InputActions/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Silly script that applies a rotation about the up axis on demand.
+public class Rotator : MonoBehaviour
+{
+    public float angle = 45f;
+
+    public void Rotate()
+    {
+        transform.Rotate(0, angle, 0);
+    }
+}
