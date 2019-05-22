325b0a932438ccb32c5dad2b51564ad6609ade58
Andrei Borodin <anborod@microsoft.com>
2019-05-22 09:02:11 -0700

    Adding missed files

---
+++ Assets/MixedRealityToolkit.Extensions/Sharing/SpatialAlignment/AzureSpatialAnchors/SpatialAnchorsCoordinate.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.Azure.SpatialAnchors;
+using Microsoft.MixedReality.Experimental.SpatialAlignment.Common;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Experimental.SpatialAlignment.AzureSpatialAnchors
+{
+    internal class SpatialAnchorsCoordinate : SpatialCoordinateUnityBase<string>
+    {
+        private readonly GameObject anchorGO;
+
+        private readonly Quaternion anchorRotation;
+        private readonly Quaternion invertedAnchorRotation;
+
+        public CloudSpatialAnchor CloudSpatialAnchor { get; }
+
+        public override LocatedState State => base.State;
+
+        public SpatialAnchorsCoordinate(CloudSpatialAnchor cloudSpatialAnchor, GameObject anchorGO)
+            : base(cloudSpatialAnchor.Identifier)
+        {
+            this.CloudSpatialAnchor = cloudSpatialAnchor;
+            this.anchorGO = anchorGO;
+            anchorRotation = anchorGO.transform.rotation;
+            invertedAnchorRotation = Quaternion.Inverse(anchorRotation);
+        }
+
+        protected override Vector3 CoordinateToWorldSpace(Vector3 vector)
+        {
+            return anchorGO.transform.TransformPoint(vector);
+        }
+
+        protected override Quaternion CoordinateToWorldSpace(Quaternion quaternion)
+        {
+            return quaternion * invertedAnchorRotation;
+        }
+
+        protected override Vector3 WorldToCoordinateSpace(Vector3 vector)
+        {
+            return anchorGO.transform.InverseTransformPoint(vector);
+        }
+
+        protected override Quaternion WorldToCoordinateSpace(Quaternion quaternion)
+        {
+            return quaternion * anchorRotation;
+        }
+
+        protected override void OnManagedDispose()
+        {
+            base.OnManagedDispose();
+
+            UnityEngine.Object.Destroy(anchorGO);
+        }
+    }
+}
---
+++ Assets/MixedRealityToolkit.Extensions/Sharing/SpatialAlignment/AzureSpatialAnchors/SpatialAnchorsUWPCoordinateService.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Assets.MRTK.MixedRealityToolkit.Extensions.Sharing.SpatialAlignment.AzureSpatialAnchors;
+using Microsoft.Azure.SpatialAnchors;
+using System.Threading.Tasks;
+
+namespace Microsoft.MixedReality.Experimental.SpatialAlignment.AzureSpatialAnchors
+{
+    internal class SpatialAnchorsUWPCoordinateService : SpatialAnchorsCoordinateService
+    {
+        public SpatialAnchorsUWPCoordinateService(SpatialAnchorsConfiguration spatialAnchorsConfiguration) 
+            : base(spatialAnchorsConfiguration)
+        {
+        }
+
+        protected override Task OnInitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected override void OnConfigureSession(CloudSpatialAnchorSession session)
+        {
+            // TODO anborod: This is a copy from DemoWrapper (it was commented), figure out what should be done here
+
+            // AAD user token scenario to get an authentication token
+            //cloudSpatialAnchorSession.TokenRequired += async (object sender, SpatialServices.TokenRequiredEventArgs args) =>
+            //{
+            //    CloudSpatialAnchorSessionDeferral deferral = args.GetDeferral();
+            //    // AAD user token scenario to get an authentication token
+            //    args.AuthenticationToken = await AuthenticationHelper.GetAuthenticationTokenAsync();
+            //    deferral.Complete();
+            //};
+        }
+    }
+}
