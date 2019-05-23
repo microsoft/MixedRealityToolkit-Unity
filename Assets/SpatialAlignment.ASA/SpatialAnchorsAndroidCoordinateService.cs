650060640c641b0d381706f3d2182698a2dcab60
Andrei Borodin <anborod@microsoft.com>
2019-05-23 09:58:23 -0700

    Moved SpatialAlignment back to correct location

---
+++ Assets/SpatialAlignment.ASA/SpatialAnchorsAndroidCoordinateService.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#if UNITY_ANDROID
+using Microsoft.Azure.SpatialAnchors;
+using Microsoft.Azure.SpatialAnchors.Unity.Android;
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Experimental.SpatialAlignment.AzureSpatialAnchors
+{
+    internal class SpatialAnchorsAndroidCoordinateService : SpatialAnchorsCoordinateService
+    {
+        private long lastFrameProcessedTimeStamp;
+
+        public SpatialAnchorsAndroidCoordinateService(SpatialAnchorsConfiguration spatialAnchorsConfiguration)
+            : base(spatialAnchorsConfiguration)
+        {
+        }
+
+        protected override Task OnInitializeAsync()
+        {
+            TaskCompletionSource<object> taskCompletionSource = new TaskCompletionSource<object>();
+
+            UnityAndroidHelper.Instance.DispatchUiThread(unityActivity =>
+            {
+                try
+                {
+                    // We should only run the java initialization once
+                    using (AndroidJavaClass cloudServices = new AndroidJavaClass("com.microsoft.CloudServices"))
+                    {
+                        cloudServices.CallStatic("initialize", unityActivity);
+                        taskCompletionSource.SetResult(null);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    taskCompletionSource.SetException(ex);
+                }
+            });
+
+            return taskCompletionSource.Task;
+        }
+
+        protected override void OnConfigureSession(CloudSpatialAnchorSession session)
+        {
+            session.Session = GoogleARCoreInternal.ARCoreAndroidLifecycleManager.Instance.NativeSession.SessionHandle;
+        }
+
+        //TODO this needs pumping
+        private void ProcessLatestFrame()
+        {
+            if (!IsTracking)
+            {
+                return;
+            }
+
+            if (session == null)
+            {
+                throw new InvalidOperationException("Cloud spatial anchor session is not available.");
+            }
+
+            GoogleARCoreInternal.NativeSession nativeSession = GoogleARCoreInternal.ARCoreAndroidLifecycleManager.Instance.NativeSession;
+
+            if (nativeSession.FrameHandle == IntPtr.Zero)
+            {
+                return;
+            }
+
+            long latestFrameTimeStamp = nativeSession.FrameApi.GetTimestamp();
+
+            bool newFrameToProcess = latestFrameTimeStamp > lastFrameProcessedTimeStamp;
+
+            if (newFrameToProcess)
+            {
+                session.ProcessFrame(nativeSession.FrameHandle);
+                lastFrameProcessedTimeStamp = latestFrameTimeStamp;
+            }
+        }
+    }
+}
+#endif
---
+++ Assets/SpatialAlignment.ASA/SpatialAnchorsIOSCoordinateService.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#if UNITY_WSA
+using Assets.MRTK.MixedRealityToolkit.Extensions.Sharing.SpatialAlignment.AzureSpatialAnchors;
+using Microsoft.Azure.SpatialAnchors;
+using System.Threading.Tasks;
+
+namespace Microsoft.MixedReality.Experimental.SpatialAlignment.AzureSpatialAnchors
+{
+    internal class SpatialAnchorsIOSCoordinateService : SpatialAnchorsCoordinateService
+    {
+        public SpatialAnchorsIOSCoordinateService(SpatialAnchorsConfiguration spatialAnchorsConfiguration)
+            : base(spatialAnchorsConfiguration)
+        {
+        }
+
+        protected override Task OnInitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        protected override void OnConfigureSession(CloudSpatialAnchorSession session)
+        {
+            // TODO anborod: This is a copy from DemoWrapper (it was commented), figure out what should be done here
+
+            // AAD user token scenario to get an authentication token
+            //cloudSpatialAnchorSession.TokenRequired += async (object sender, SpatialServices.TokenRequiredEventArgs args) =>
+            //{
+            //    CloudSpatialAnchorSessionDeferral deferral = args.GetDeferral();
+            //    // AAD user token scenario to get an authentication token
+            //    args.AuthenticationToken = await AuthenticationHelper.GetAuthenticationTokenAsync();
+            //    deferral.Complete();
+            //};
+        }
+    }
+}
+#endif
