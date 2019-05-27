0abd1c136594545bc7e7eb8b5ae2a68dc5b63bef
Luis Valverde <luval@microsoft.com>
2019-05-27 13:55:42 +0100

    Documentation fix

--- Assets/MixedRealityToolkit/Interfaces/InputSystem/Handlers/IMixedRealityBaseInputHandler.cs
+++ Assets/MixedRealityToolkit/Interfaces/InputSystem/Handlers/IMixedRealityBaseInputHandler.cs
@@ -8,7 +8,7 @@ using UnityEngine.EventSystems;
 namespace Microsoft.MixedReality.Toolkit.Input
 {
     /// <summary>
-    /// Base interface for all input handlers. This allows us to use ExecuteEvents.ExecuteHierarchy<IMixedRealityBaseInputHandler>
+    /// Base interface for all input handlers. This allows us to use ExecuteEvents.ExecuteHierarchy&lt;IMixedRealityBaseInputHandler&gt;
     /// to send an event to all input handling interfaces.
     /// </summary>
     public interface IMixedRealityBaseInputHandler : IEventSystemHandler {}
