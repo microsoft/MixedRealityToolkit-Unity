fe29c3c10ca63f8e1157fbb197221f21d1f1b2ca
Luis Valverde <luval@microsoft.com>
2019-05-28 15:12:09 +0100

    Created an example scene to show how to use the pointer result property. Added a script for handling pointer events as Unity Events. Updated pointer documentation.

---
+++ Assets/MixedRealityToolkit.Examples/Demos/Input/Scenes/PointerResult/SpawnOnPointerEvent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine;
+
+public class SpawnOnPointerEvent : MonoBehaviour
+{
+    public GameObject PrefabToSpawn;
+
+    public void Spawn(MixedRealityPointerEventData eventData)
+    {
+        if (PrefabToSpawn != null)
+        {
+            var result = eventData.Pointer.Result;
+            Instantiate(PrefabToSpawn, result.Details.Point, Quaternion.LookRotation(result.Details.Normal));
+        }
+    }
+}
