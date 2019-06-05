036099977e45e3e558c5965754a63724ae11414e
Addison Linville <adlinv@microsoft.com>
2019-06-05 09:27:23 -0700

    Created a generic Axes Orientation enum

---
+++ Assets/MixedRealityToolkit/Definitions/Utilities/AxisOrientationType.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Microsoft.MixedReality.Toolkit.Utilities
+{
+    /// <summary>
+    /// AxisOrientationType identifies orientation in relation to cartesian axes.
+    /// </summary>
+    public enum AxisOrientationType
+    {
+        /// <summary>
+        /// No Specified orientation
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// X+, right
+        /// </summary>
+        PositiveX,
+        /// <summary>
+        /// X-, left
+        /// </summary>
+        NegativeX,
+        /// <summary>
+        /// Y+, up
+        /// </summary>
+        PositiveY,
+        /// <summary>
+        /// Y-, down
+        /// </summary>
+        NegativeY,
+        /// <summary>
+        /// Z+, forward
+        /// </summary>
+        PositiveZ,
+        /// <summary>
+        /// Z-, backward
+        /// </summary>
+        NegativeZ
+    }
+}
