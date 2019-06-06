8de0e8cc59dbd03840338fc48b3b843384c43c5f
davidkline-ms <david.kline@microsoft.com>
2019-06-06 16:23:49 -0700

    add input and spatial awareness managers (still need testing)

---
+++ Assets/MixedRealityToolkit.SDK/Features/Input/Scripts/InputSystemManager.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Utilities;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Input
+{
+    public class InputSystemManager : BaseServiceManager
+    {
+        // todo: tooltips, etc
+        [SerializeField]
+        [Implements(typeof(IMixedRealityInputSystem), TypeGrouping.ByNamespaceFlat)]
+        private SystemType InputSystemType = null;
+
+        // todo: tooltips, etc
+        [SerializeField]
+        private MixedRealityInputSystemProfile profile = null;
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        protected override void OnDestroy()
+        {
+            Uninitialize();
+            base.OnDestroy();
+        }
+
+        /// <summary>
+        ///  Initialize the manager.
+        /// </summary>
+        private void Initialize()
+        {
+            // The input system class takes arguments for:
+            // * The registrar
+            // * The input system profile
+            object[] args = { this, profile };
+            Initialize<IMixedRealityInputSystem>(InputSystemType.Type, args: args);
+
+            // The input system uses the focus provider specified in the profile.
+            // The args for the focus provider are:
+            // * The registrar
+            // * The input system profile
+            args = new object[] { this, profile };
+            Initialize<IMixedRealityFocusProvider>(profile.FocusProviderType.Type, args: args);
+        }
+
+        /// <summary>
+        ///  Uninitialize the manager.
+        /// </summary>
+        private void Uninitialize()
+        {
+            Uninitialize<IMixedRealityInputSystem>();
+        }
+    }
+}
---
+++ Assets/MixedRealityToolkit.SDK/Features/SpatialAwareness/Scripts/SpatialAwarenessSystemManager.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Utilities;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.SpatialAwareness
+{
+    public class SpatialAwarenessSystemManager : BaseServiceManager
+    {
+        // todo: tooltips, etc
+        [SerializeField]
+        [Implements(typeof(IMixedRealitySpatialAwarenessSystem), TypeGrouping.ByNamespaceFlat)]
+        private SystemType SpatialAwarenessSystemType = null;
+
+        // todo: tooltips, etc
+        [SerializeField]
+        private MixedRealitySpatialAwarenessSystemProfile profile = null;
+
+        #region MonoBehaviour implementation
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        protected override void OnDestroy()
+        {
+            Uninitialize();
+            base.OnDestroy();
+        }
+
+        #endregion MonoBehaviour implementation
+
+        /// <summary>
+        ///  Initialize the manager.
+        /// </summary>
+        private void Initialize()
+        {
+            // The spatial awareness system class takes arguments for:
+            // * The registrar
+            // * The spatial awareness system profile
+            object[] args = { this, profile };
+
+            Initialize<IMixedRealitySpatialAwarenessSystem>(SpatialAwarenessSystemType.Type, args: args);
+        }
+
+        /// <summary>
+        ///  Uninitialize the manager.
+        /// </summary>
+        private void Uninitialize()
+        {
+            Uninitialize<IMixedRealitySpatialAwarenessSystem>();
+        }
+    }
+}
