ee4ab6c0ff8bf7f1bd88363a1cc44ed62fe790c0
davidkline-ms <david.kline@microsoft.com>
2019-06-06 14:15:15 -0700

    add camera and diagnostic managers

---
+++ Assets/MixedRealityToolkit.SDK/Features/Camera/Scripts/CameraSystemManager.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Utilities;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.CameraSystem
+{
+    public class CameraSystemManager : BaseServiceManager
+    {
+        // todo: tooltips, etc
+        [SerializeField]
+        [Implements(typeof(IMixedRealityCameraSystem), TypeGrouping.ByNamespaceFlat)]
+        private SystemType CameraSystemType = null;
+
+        // todo: tooltips, etc
+        [SerializeField]
+        private MixedRealityCameraProfile profile = null;
+
+        /// <summary>
+        /// Instance of the camera system being managed.
+        /// </summary>
+        private IMixedRealityCameraSystem CameraSystem => service as IMixedRealityCameraSystem;
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        protected override void OnDestroy()
+        {
+            Uninitialize();
+            base.OnDestroy();
+        }
+
+        /// <summary>
+        ///  Initialize the manager.
+        /// </summary>
+        private void Initialize()
+        {
+            // The camera system class takes arguments for:
+            // * The registrar
+            // * The camera system profile
+            object[] args = { this, profile };
+
+            Initialize<IMixedRealityCameraSystem>(CameraSystemType.Type, args: args);
+        }
+
+        /// <summary>
+        ///  Uninitialize the manager.
+        /// </summary>
+        private void Uninitialize()
+        {
+            Uninitialize<IMixedRealityCameraSystem>();
+        }
+    }
+}
---
+++ Assets/MixedRealityToolkit.SDK/Features/Diagnostics/Scripts/DiagnosticsSystemManager.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Utilities;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Diagnostics
+{
+    public class DiagnosticsSystemManager : BaseServiceManager
+    {
+        // todo: tooltips, etc
+        [SerializeField]
+        [Implements(typeof(IMixedRealityDiagnosticsSystem), TypeGrouping.ByNamespaceFlat)]
+        private SystemType DiagnosticsSystemType = null;
+
+        // todo: tooltips, etc
+        [SerializeField]
+        private MixedRealityDiagnosticsProfile profile = null;
+
+        /// <summary>
+        /// Instance of the diagnostics system being managed.
+        /// </summary>
+        private IMixedRealityDiagnosticsSystem DiagnosticsSystem => service as IMixedRealityDiagnosticsSystem;
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        protected override void OnDestroy()
+        {
+            Uninitialize();
+            base.OnDestroy();
+        }
+
+        /// <summary>
+        ///  Initialize the manager.
+        /// </summary>
+        private void Initialize()
+        {
+            // The Diagnostics system class takes arguments for:
+            // * The registrar
+            // * The diagnostics system profile
+            object[] args = { this, profile };
+
+            Initialize<IMixedRealityDiagnosticsSystem>(DiagnosticsSystemType.Type, args: args);
+        }
+
+        /// <summary>
+        ///  Uninitialize the manager.
+        /// </summary>
+        private void Uninitialize()
+        {
+            Uninitialize<IMixedRealityDiagnosticsSystem>();
+        }
+    }
+}
