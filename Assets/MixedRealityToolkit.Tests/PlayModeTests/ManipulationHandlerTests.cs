5358b9e1650446b0bbbf41ebcca547478c2d98be
Bernadette Thalhammer <bethalha@microsoft.com>
2019-06-12 19:23:35 +0100

    added playmode test for translating and force end of manipulation handler

--- Assets/MixedRealityToolkit.Tests/PlayModeTests/ManipulationHandlerTests.cs
+++ Assets/MixedRealityToolkit.Tests/PlayModeTests/ManipulationHandlerTests.cs
@@ -9,11 +9,11 @@
 using Microsoft.MixedReality.Toolkit.UI;
 using NUnit.Framework;
 using System.Collections;
-using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
-using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.Input;
+
 namespace Microsoft.MixedReality.Toolkit.Tests
 {
     public class ManipulationHandlerTests
@@ -97,6 +97,76 @@ namespace Microsoft.MixedReality.Toolkit.Tests
 
             // Wait for a frame to give Unity a chance to actually destroy the object
             yield return null;
+        }
+
+        /// <summary>
+        /// Test creates an object with manipulationhandler verifies translation with articulated hand
+        /// as well as forcefully ending of the manipulation
+        /// from gaze provider.
+        /// </summary>
+        /// <returns></returns>
+        [UnityTest]
+        public IEnumerator ManipulationHandlerForceRelease()
+        {
+            TestUtilities.InitializeMixedRealityToolkitAndCreateScenes(true);
+            TestUtilities.InitializePlayspace();
+
+            // set up cube with manipulation handler
+            var testObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            testObject.transform.localScale = Vector3.one * 0.2f;
+            Vector3 initialObjectPosition = new Vector3(0f, 0f, 1f);
+            testObject.transform.position = initialObjectPosition;
+            var manipHandler = testObject.AddComponent<ManipulationHandler>();
+            manipHandler.HostTransform = testObject.transform;
+            manipHandler.SmoothingActive = false;
+
+            // add near interaction grabbable to be able to grab the cube with the siulated articulated hand
+            testObject.AddComponent<NearInteractionGrabbable>();
+
+            yield return new WaitForFixedUpdate();
+            yield return null;
+
+            // grab the cube - move it to the right 
+            var inputSimulationService = PlayModeTestUtilities.GetInputSimulationService();
+            int numSteps = 30;
+            
+            Vector3 handOffset = new Vector3(0, 0, 0.1f);
+            Vector3 initialHandPosition = new Vector3(0, 0, 0.5f);
+            Vector3 rightPosition = new Vector3(1f, 0f, 1f);
+
+            yield return PlayModeTestUtilities.ShowHand(Handedness.Right, inputSimulationService);
+            yield return PlayModeTestUtilities.MoveHandFromTo(initialHandPosition, initialObjectPosition, numSteps, ArticulatedHandPose.GestureId.Open, Handedness.Right, inputSimulationService);
+            yield return PlayModeTestUtilities.MoveHandFromTo(initialObjectPosition, rightPosition, numSteps, ArticulatedHandPose.GestureId.Pinch, Handedness.Right, inputSimulationService);
+
+            yield return null;
+
+            // test if the object was properly translated
+            float maxError = 0.05f;
+            Vector3 posDiff = testObject.transform.position - rightPosition;
+            Assert.IsTrue(posDiff.magnitude <= maxError, "ManipulationHandler translate failed");
+
+            // forcefully end manipulation and drag with hand back to original position - object shouldn't move with hand
+            manipHandler.ForceEndManipulation();
+            yield return PlayModeTestUtilities.MoveHandFromTo(rightPosition, initialObjectPosition, numSteps, ArticulatedHandPose.GestureId.Pinch, Handedness.Right, inputSimulationService);
+
+            posDiff = testObject.transform.position - initialObjectPosition;
+            Assert.IsTrue(posDiff.magnitude > maxError, "Manipulationhandler modified objects even though manipulation was forcefully ended.");
+            posDiff = testObject.transform.position - rightPosition;
+            Assert.IsTrue(posDiff.magnitude <= maxError, "Manipulated object didn't remain in place after forcefully ending manipulation");
+
+            // move hand back to object
+            yield return PlayModeTestUtilities.MoveHandFromTo(initialObjectPosition, rightPosition, numSteps, ArticulatedHandPose.GestureId.Open, Handedness.Right, inputSimulationService);
+
+            // grab object again and move to original position
+            yield return PlayModeTestUtilities.MoveHandFromTo(rightPosition, initialObjectPosition, numSteps, ArticulatedHandPose.GestureId.Pinch, Handedness.Right, inputSimulationService);
+
+            // test if object was moved by manipulationhandler
+            posDiff = testObject.transform.position - initialObjectPosition;
+            Assert.IsTrue(posDiff.magnitude <= maxError, "ManipulationHandler translate failed on valid manipulation after ForceEndManipulation");
+
+            yield return PlayModeTestUtilities.HideHand(Handedness.Right, inputSimulationService);
+
+            Object.Destroy(testObject);
         }
 
     }
