9b673b00828f56ef5158a5e52c0a4e83b64fe091
Lars Simkins <lars.simkins@gmail.com>
2019-06-12 09:41:24 -0700

    Made camera cache logic clearer, removed get service call

--- Assets/MixedRealityToolkit/Utilities/CameraCache.cs
+++ Assets/MixedRealityToolkit/Utilities/CameraCache.cs
@@ -14,7 +14,6 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
     public static class CameraCache
     {
         private static Camera cachedCamera;
-        private static IMixedRealityCameraSystem cameraSystem;
 
         /// <summary>
         /// Returns a cached reference to the main camera. Uses the camera system's main camera, if available. Otherwise finds or creates a main camera.
@@ -23,39 +22,49 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
         {
             get
             {
+                // If the camera system is enabled, use the main camera.
+                // This allows developers to control exactly which camera will be returned by the cache.
                 if (MixedRealityToolkit.IsCameraSystemEnabled)
                 {
-                    if (cameraSystem == null)
-                    {
-                        cameraSystem = MixedRealityToolkit.Instance.GetService<IMixedRealityCameraSystem>();
-                    }
-                    return cameraSystem.Main;
+                    return MixedRealityToolkit.CameraSystem.Main;
                 }
 
-                if (cachedCamera != null)
-                {
-                    if (cachedCamera.gameObject.activeInHierarchy)
-                    {   // If the cached camera is active, return it
-                        // Otherwise, our playspace may have been disabled
-                        // We'll have to search for the next available
-                        return cachedCamera;
-                    }
-                }
-
-                // If the cached camera is null, search for main
-                var mainCamera = Camera.main;
-
-                if (mainCamera == null)
-                {   // If no main camera was found, create it now
-                    Debug.LogWarning("No main camera found. The Mixed Reality Toolkit requires at least one camera in the scene. One will be generated now.");
-                    mainCamera = new GameObject("Main Camera", typeof(Camera)) { tag = "MainCamera" }.GetComponent<Camera>();
-                }
-
-                // Cache the main camera
-                cachedCamera = mainCamera;
-
-                return cachedCamera;
+                // Otherwise, reutrn a default camera
+                return FindOrCreateDefaultMainCamera();
             }
+        }
+
+        /// <summary>
+        /// Creates a default main camera if our cached camera is null or disabled.
+        /// </summary>
+        /// <returns></returns>
+        private static Camera FindOrCreateDefaultMainCamera()
+        {
+            if (cachedCamera != null)
+            {
+                if (cachedCamera.gameObject.activeInHierarchy)
+                {   // If the cached camera is active, return it
+                    // Otherwise, our playspace may have been disabled
+                    // We'll have to search for the next available
+                    return cachedCamera;
+                }
+            }
+
+            // If the cached camera is null, search for main
+            var mainCamera = Camera.main;
+
+            if (mainCamera == null)
+            {   // If no main camera was found, create it now
+                Debug.LogWarning("No main camera found. The Mixed Reality Toolkit requires at least one camera in the scene. One will be generated now.");
+                mainCamera = new GameObject("Main Camera", typeof(Camera)) { tag = "MainCamera" }.GetComponent<Camera>();
+            }
+
+            // Cache the main camera
+            cachedCamera = mainCamera;
+            // Parent the camera under our play space
+            cachedCamera.transform.SetParent(MixedRealityPlayspace.Transform);
+
+            return cachedCamera;
         }
     }
 }
