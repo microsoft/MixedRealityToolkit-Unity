7262a1c3472e1b17273ff889fe022d896db8a866
Julia Schwarz <jschwarz@microsoft.com>
2019-06-25 00:12:07 -0700

    Bounding box transform tests

--- Assets/MixedRealityToolkit.Tests/PlayModeTests/BoundingBoxTests.cs
+++ Assets/MixedRealityToolkit.Tests/PlayModeTests/BoundingBoxTests.cs
@@ -11,6 +11,7 @@ using NUnit.Framework;
 using System.Collections;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 using Microsoft.MixedReality.Toolkit;
@@ -20,6 +21,7 @@ using System.Linq;
 using System;
 using UnityEditor;
 using System.Collections.Generic;
+using Assert = UnityEngine.Assertions.Assert;
 
 namespace Microsoft.MixedReality.Toolkit.Tests
 {
@@ -97,12 +99,7 @@ namespace Microsoft.MixedReality.Toolkit.Tests
             // Get the dimensions of the corners
             List<GameObject> corners = FindDescendantsContainingName(bbox.gameObject, "corner_");
 
-            Bounds b = new Bounds();
-            b.center = corners[0].transform.position;
-            foreach (var c in corners.Skip(1))
-            {
-                b.Encapsulate(c.transform.position);
-            }
+            Bounds b = GetBoundingBoxRigBounds(bbox);
 
             Debug.Assert(b.center == bc.center, $"bounds center should be {bc.center} but they are {b.center}");
             Debug.Assert(b.size == bc.size, $"bounds size should be {bc.size} but they are {b.size}");
@@ -195,6 +192,53 @@ namespace Microsoft.MixedReality.Toolkit.Tests
             iss.InputSimulationProfile = oldIsp;
         }
 
+        /// <summary>
+        /// Test that changing the transform of the bounding box target (rotation, scale, translation)
+        /// updates the rig bounds
+        /// </summary>
+        /// <returns></returns>
+        [UnityTest]
+        public IEnumerator UpdateTransformUpdatesBounds()
+        {
+            var bbox = InstantiateSceneAndDefaultBbox();
+            bbox.HideElementsInInspector = false;
+            yield return null;
+
+            var startBounds = GetBoundingBoxRigBounds(bbox);
+            var startCenter = new Vector3(0, 0, 1.5f);
+            var startSize = new Vector3(.5f, .5f, .5f);
+            TestUtilities.AssertAboutEqual(startBounds.center, startCenter, "bbox incorrect center at start");
+            TestUtilities.AssertAboutEqual(startBounds.size, startSize, "bbox incorrect size at start");
+
+            bbox.gameObject.transform.localScale *= 2;
+            yield return null;
+
+            var afterScaleBounds = GetBoundingBoxRigBounds(bbox);
+            var scaledSize = startSize * 2;
+            TestUtilities.AssertAboutEqual(afterScaleBounds.center, startCenter, "bbox incorrect center after scale");
+            TestUtilities.AssertAboutEqual(afterScaleBounds.size, scaledSize, "bbox incorrect size after scale");
+
+            bbox.gameObject.transform.position += Vector3.one;
+            yield return null;
+            var afterTranslateBounds = GetBoundingBoxRigBounds(bbox);
+            var afterTranslateCenter = Vector3.one + startCenter;
+
+            TestUtilities.AssertAboutEqual(afterTranslateBounds.center, afterTranslateCenter, "bbox incorrect center after translate");
+            TestUtilities.AssertAboutEqual(afterTranslateBounds.size, scaledSize, "bbox incorrect size after translate");
+
+            List<GameObject> corners = FindDescendantsContainingName(bbox.gameObject, "corner_");
+            var c0 = corners[0];
+            var bboxBottomCenter = afterTranslateBounds.center - Vector3.up * afterTranslateBounds.extents.y;
+            Vector3 cc0 = c0.transform.position - bboxBottomCenter;
+            float rotateAmount = 30;
+            bbox.gameObject.transform.Rotate(new Vector3(0, rotateAmount, 0));
+            yield return null;
+            Vector3 cc0_rotated = c0.transform.position - bboxBottomCenter;
+            Assert.AreApproximatelyEqual(Vector3.Angle(cc0, cc0_rotated), 30, $"rotated angle is not correct. expected {rotateAmount} but got {Vector3.Angle(cc0, cc0_rotated)}");
+
+            GameObject.Destroy(bbox.gameObject);
+        }
+
         private List<GameObject> FindDescendantsContainingName(GameObject rigRoot, string v)
         {
             Queue<Transform> toExplore = new Queue<Transform>();
@@ -213,6 +257,25 @@ namespace Microsoft.MixedReality.Toolkit.Tests
                 }
             }
             return result;
+        }
+
+        /// <summary>
+        /// Returns the AABB of the bounding box rig (corners, edges)
+        /// that make up the bounding box by using the positions of the corners
+        /// </summary>
+        /// <param name="bbox"></param>
+        /// <returns></returns>
+        private Bounds GetBoundingBoxRigBounds(BoundingBox bbox)
+        {
+            List<GameObject> corners = FindDescendantsContainingName(bbox.gameObject, "corner_");
+
+            Bounds b = new Bounds();
+            b.center = corners[0].transform.position;
+            foreach (var c in corners.Skip(1))
+            {
+                b.Encapsulate(c.transform.position);
+            }
+            return b;
         }
     }
 }
