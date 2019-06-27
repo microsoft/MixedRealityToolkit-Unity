4bcaf2b1f93589989e525bc0787f434c876023ed
Will <wiwei@microsoft.com>
2019-06-27 13:45:11 -0700

    Update PrimaryPointerHandlerExample.cs

--- Assets/MixedRealityToolkit.Examples/Demos/Input/Scenes/PrimaryPointer/PrimaryPointerHandlerExample.cs
+++ Assets/MixedRealityToolkit.Examples/Demos/Input/Scenes/PrimaryPointer/PrimaryPointerHandlerExample.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.MixedReality.Toolkit;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit;
 using Microsoft.MixedReality.Toolkit.Input;
 using UnityEngine;
 
