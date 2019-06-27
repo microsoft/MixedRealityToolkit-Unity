cd941dd7f1eeb6e0fb5cf26c932142b06f2aeecd
Will Wei <wiwei@microsoft.com>
2019-06-27 13:21:45 -0700

    Fix the broken .NET-C# projects build.
    This is an oldie but goldie. There's an issue that arises only when building on the .NET backend with C# projects enabled (this is a deprecated scenario but still one that is really useful for fast iteration).
    
    The problem is that even if an assembly is marked as a "test assembly" (as the play mode tests are), they will still be included in player builds for .NET-C# project builds. This is what the old comment was alluding to (but wasn't very specific about)
    
    We also explored doing things like marking the play mode tests as editor only (i.e. matching what the edit mode test asmdef looks like) but this had the side effect of removing all tests from the test runner (bad, tests are gone). Also tried making things editor + Win32 (standalone) + Win64 (standalone) only as well, though this still had the same problem even when building UWP .NET
    
    I updated the comment to be a little clearer about what's going on here and also updated the test guidance as well.
    
    Lastly, got rid of some unused using statements while I was in there.

--- Assets/MixedRealityToolkit.Tests/PlayModeTests/TestHand.cs
+++ Assets/MixedRealityToolkit.Tests/PlayModeTests/TestHand.cs
@@ -1,6 +1,15 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+#if !WINDOWS_UWP
+// When the .NET scripting backend is enabled and C# projects are built
+// The assembly that this file is part of is still built for the player,
+// even though the assembly itself is marked as a test assembly (this is not
+// expected because test assemblies should not be included in player builds).
+// Because the .NET backend is deprecated in 2018 and removed in 2019 and this
+// issue will likely persist for 2018, this issue is worked around by wrapping all
+// play mode tests in this check.
+
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Utilities;
 using System.Collections;
@@ -47,3 +56,4 @@ namespace Microsoft.MixedReality.Toolkit.Tests
         }
     }
 }
+#endif
