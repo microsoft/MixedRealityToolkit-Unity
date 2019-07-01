73cc801ea67542bd67d12c6050bee33f03bfbb1c
Lukas Tönne <lutnne@microsoft.com>
2019-07-01 14:17:55 +0100

    Revert "Input recording and playback feature (#4775)"
    This reverts commit bafe982831d9281cd54efb2b73289fc4764b85ca.

--- Assets/MixedRealityToolkit.Tests/TestUtilities.cs
+++ Assets/MixedRealityToolkit.Tests/TestUtilities.cs
@@ -1,15 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using Microsoft.MixedReality.Toolkit.Input;
-using Microsoft.MixedReality.Toolkit.UI;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 using Microsoft.MixedReality.Toolkit.Utilities;
 
 #if UNITY_EDITOR
