5bffe459c5fd36841ae234c3193da5f9f1c63c69
Lars Simkins <lars.simkins@gmail.com>
2019-07-10 12:49:37 -0700

    Added UWP preprocessor check

--- Assets/MixedRealityToolkit.Tests/PlayModeTests/DemoSceneTests.cs
+++ Assets/MixedRealityToolkit.Tests/PlayModeTests/DemoSceneTests.cs
@@ -1,4 +1,16 @@
-﻿using System.Collections;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#if !WINDOWS_UWP
+// When the .NET scripting backend is enabled and C# projects are built
+// The assembly that this file is part of is still built for the player,
+// even though the assembly itself is marked as a test assembly (this is not
+// expected because test assemblies should not be included in player builds).
+// Because the .NET backend is deprecated in 2018 and removed in 2019 and this
+// issue will likely persist for 2018, this issue is worked around by wrapping all
+// play mode tests in this check.
+
+using System.Collections;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 using UnityEngine;
@@ -43,3 +55,4 @@ namespace Microsoft.MixedReality.Toolkit.Tests.SanityTests
         }
     }
 }
+#endif
