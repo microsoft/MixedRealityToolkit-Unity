300160d1897c455b7af996c580a38cc352edc634
alexees <as@holospaces.com>
2019-07-15 14:04:41 +0200

    Updates on Start too

--- Assets/MixedRealityToolkit.SDK/Features/UX/Interactable/Scripts/Interactable.cs
+++ Assets/MixedRealityToolkit.SDK/Features/UX/Interactable/Scripts/Interactable.cs
@@ -480,7 +480,17 @@ namespace Microsoft.MixedReality.Toolkit.UI
             }
         }
 
+        protected virtual void Start()
+        {
+            InternalUpdate();
+        }
+
         protected virtual void Update()
+        {
+            InternalUpdate();
+        }
+
+        private void InternalUpdate()
         {
             if (rollOffTimer < rollOffTime && HasPress)
             {
