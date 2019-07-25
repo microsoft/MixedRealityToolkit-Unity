61ee2c8158d9294b33beebc26527707c10f1b51b
davidkline-ms <david.kline@microsoft.com>
2019-07-25 16:59:59 -0700

    add missing mic stream method, start demo scene

---
+++ Assets/MixedRealityToolkit.Examples/Experimental/Audio/Scripts/AmbientSoundAmplitude.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.MixedReality.Toolkit.Experimental.Audio;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Microsoft.MixedReality.Toolkit.Examples.Experimental
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AmbientSoundAmplitude : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("The text field in which to display the ambient sound amplitude.")]
+        private Text amplitudeDisplay = null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private WindowsMicrophoneStream micStream = null;
+
+        private void Awake()
+        {
+            micStream = new WindowsMicrophoneStream();
+
+            WindowsMicrophoneStreamErrorCode result = micStream.Initialize(WindowsMicrophoneStreamType.RoomCapture);
+            if (result != WindowsMicrophoneStreamErrorCode.Success)
+            {
+                // todo
+            }
+
+            micStream.Gain = 1.0f;
+        }
+
+        private void OnDestroy()
+        {
+            micStream.Uninitialize();
+            micStream = null;
+        }
+
+        private void Update()
+        {
+            // todo
+        }
+
+        private void OnAudioFilterRead(float[] buffer, int numChannels)
+        {
+            WindowsMicrophoneStreamErrorCode result = micStream.ReadAudioFrame(buffer, numChannels);
+            
+            if (result != WindowsMicrophoneStreamErrorCode.Success)
+            {
+                // todo
+            }
+
+            float sumOfValues = 0;
+
+            // Calculate this frame's average amplitude.
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                if (float.IsNaN(buffer[i]))
+                {
+                    buffer[i] = 0;
+                }
+
+                buffer[i] = Mathf.Clamp(buffer[i], -1.0f, 1.0f);
+                sumOfValues += Mathf.Clamp01(Mathf.Abs(buffer[i]));
+            }
+
+            // todo consider how often we want to update this data in the UX
+            // averageAmplitude = sumOfValues / buffer.Length;
+        }
+    }
+}
---
+++ Assets/MixedRealityToolkit.Examples/Experimental/Audio/Scripts/ThrowingYourVoice.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowingYourVoice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
