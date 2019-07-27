9d16173412845a1c501402b027513882a1282a36
davidkline-ms <david.kline@microsoft.com>
2019-07-26 17:25:10 -0700

    fix spatial mesh intensity pulse

--- Assets/MixedRealityToolkit.Examples/Experimental/Audio/Scripts/AmbientSoundAmplitude.cs
+++ Assets/MixedRealityToolkit.Examples/Experimental/Audio/Scripts/AmbientSoundAmplitude.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.MixedReality.Toolkit.Experimental.Audio;
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,6 +15,45 @@ namespace Microsoft.MixedReality.Toolkit.Examples.Experimental
 
         private float averageAmplitude = 0.0f;
 
+        private IMixedRealitySpatialAwarenessSystem spatialAwarenessSystem = null;
+        private IMixedRealitySpatialAwarenessSystem SpatialAwarenessSystem
+        {
+            get
+            {
+                if (spatialAwarenessSystem == null)
+                {
+                    MixedRealityServiceRegistry.TryGetService<IMixedRealitySpatialAwarenessSystem>(out spatialAwarenessSystem);
+                }
+                return spatialAwarenessSystem;
+            }
+        }
+
+        private IMixedRealitySpatialAwarenessMeshObserver spatialMeshObserver = null;
+        private IMixedRealitySpatialAwarenessMeshObserver SpatialMeshObserver
+        {
+            get
+            {
+                if (spatialMeshObserver == null)
+                {
+                    spatialMeshObserver = (SpatialAwarenessSystem as IMixedRealityDataProviderAccess)?.GetDataProvider<IMixedRealitySpatialAwarenessMeshObserver>();
+                }
+                return spatialMeshObserver;
+            }
+        }
+
+        private Material visibleMaterial = null;
+        private Material VisibleMaterial
+        {
+            get
+            {
+                if (visibleMaterial == null)
+                {
+                    visibleMaterial = SpatialMeshObserver?.VisibleMaterial;
+                }
+                return visibleMaterial;
+            }
+        }
+
         /// <summary>
         /// 
         /// </summary>
@@ -21,70 +61,81 @@ namespace Microsoft.MixedReality.Toolkit.Examples.Experimental
 
         private void Awake()
         {
-            micStream = new WindowsMicrophoneStream();
+            // We do not wish to play the ambient room sound from the audio source.
+            gameObject.GetComponent<AudioSource>().volume = 0.0f;
 
+            micStream = new WindowsMicrophoneStream();
+            micStream.Gain = 1.0f;
+
+            // Initialize the microphone stream.
             WindowsMicrophoneStreamErrorCode result = micStream.Initialize(WindowsMicrophoneStreamType.RoomCapture);
             if (result != WindowsMicrophoneStreamErrorCode.Success)
             {
                 Debug.Log($"Failed to initialize the microphone stream. {result}");
-                amplitudeDisplay.text = $"Failed to initialize the microphone stream. {result}";
             }
 
-            micStream.Gain = 1.0f;
-
+            // Start the microphone stream.
+            // Do not keep the data and do not preview.
             result = micStream.StartStream(false, false);
             if (result != WindowsMicrophoneStreamErrorCode.Success)
             {
                 Debug.Log($"Failed to start the microphone stream. {result}");
-                amplitudeDisplay.text = $"Failed to start the microphone stream. {result}";
             }
-
-            // We do not wish to play the ambient room sound from the audio source.
-            gameObject.GetComponent<AudioSource>().volume = 0.0f;
         }
 
         private void OnDestroy()
         {
+            // Stop the microphone stream.
             WindowsMicrophoneStreamErrorCode result = micStream.StopStream();
             if (result != WindowsMicrophoneStreamErrorCode.Success)
             {
                 Debug.Log($"Failed to stop the microphone stream. {result}");
-                amplitudeDisplay.text = $"Failed to stop the microphone stream. {result}";
             }
 
+            // Uninitialize the microphone stream.
             micStream.Uninitialize();
             micStream = null;
         }
 
         private void OnDisable()
         {
+            // Pause the microphone stream.
             WindowsMicrophoneStreamErrorCode result = micStream.Pause();
             if (result != WindowsMicrophoneStreamErrorCode.Success)
             {
                 Debug.Log($"Failed to pause the microphone stream. {result}");
-                amplitudeDisplay.text = $"Failed to pause the microphone stream. {result}";
             }
         }
 
         private void OnEnable()
         {
+            // Resume the microphone stream.
             WindowsMicrophoneStreamErrorCode result = micStream.Resume();
             if (result != WindowsMicrophoneStreamErrorCode.Success)
             {
                 Debug.Log($"Failed to resume the microphone stream. {result}");
-                amplitudeDisplay.text = $"Failed to resume the microphone stream. {result}";
             }
         }
 
         private void Update()
         {
-            amplitudeDisplay.text = averageAmplitude.ToString();
+            amplitudeDisplay.text = averageAmplitude.ToString("N4");
+
+            if (VisibleMaterial != null)
+            {
+                // todo: this modifies the shader in the editor.... consider a different approach (ex: make a copy in the observer)
+                VisibleMaterial.SetColor("_WireColor", Color.blue);
+
+                int wireThickness = (int)(averageAmplitude * 10 * 700);
+                wireThickness = Mathf.Clamp(wireThickness, 0, 700);
+                VisibleMaterial.SetInt("_WireThickness", wireThickness);
+            }
         }
 
         private void OnAudioFilterRead(float[] buffer, int numChannels)
         {
+            // Read the microphone stream data.
             WindowsMicrophoneStreamErrorCode result = micStream.ReadAudioFrame(buffer, numChannels);
-            
             if (result != WindowsMicrophoneStreamErrorCode.Success)
             {
                 Debug.Log($"Failed to read the microphone stream data. {result}");
