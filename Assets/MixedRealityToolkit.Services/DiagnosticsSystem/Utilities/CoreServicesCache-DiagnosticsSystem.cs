16c4f07b19350a09d03d29ceb0e3053c0255bd02
Troy Ferrell <trferrel@microsoft.com>
2019-07-29 15:40:11 -0700

    Add partial class of core services cache

---
+++ Assets/MixedRealityToolkit.Services/DiagnosticsSystem/Utilities/CoreServicesCache-DiagnosticsSystem.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Diagnostics;
+
+namespace Microsoft.MixedReality.Toolkit.Utilities
+{
+    public partial class CoreServices : BaseCoreServices
+    {
+        /// <summary>
+        /// Cached reference to the active instance of the diagnostics system.
+        /// If system is destroyed, reference will be invalid. Please use ResetCacheReferences() 
+        /// </summary>
+        public static IMixedRealityDiagnosticsSystem DiagnosticsSystem
+        {
+            get
+            {
+                return GetService<IMixedRealityDiagnosticsSystem>();
+            }
+        }
+    }
+}
