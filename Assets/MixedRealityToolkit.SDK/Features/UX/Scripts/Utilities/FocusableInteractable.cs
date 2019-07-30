52680ca8764d8cf4d9330157a26c1bb453540558
Troy Ferrell <trferrel@microsoft.com>
2019-07-30 14:18:47 -0700

    Update interactable

---
+++ Assets/MixedRealityToolkit.SDK/Features/UX/Scripts/Utilities/FocusableInteractable.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.UI
+{
+    /// <summary>
+    /// Adds or removes materials to target renderer for highlighting Focused <see href="https://docs.unity3d.com/ScriptReference/GameObject.html">GameObject</see>s.
+    /// </summary>
+    /// <remarks>Useful with focusable <see href="https://docs.unity3d.com/ScriptReference/GameObject.html">GameObject</see>s</remarks>
+    public class FocusableInteractable : BaseFocusHandler
+    {
+        /// <summary>
+        /// List of profiles can match themes with gameObjects
+        /// </summary>
+        [SerializeField]
+        private List<InteractableProfileItem> Profiles = new List<InteractableProfileItem>();
+
+        public InteractableProfileItem test;
+
+        private List<InteractableThemeBase> themes = new List<InteractableThemeBase>();
+
+        public void Awake()
+        {
+            foreach (var profile in this.Profiles)
+            {
+                foreach (var theme in profile.Themes)
+                {
+                    foreach (var setting in theme.Settings)
+                    {
+                        // NOTE: GetTheme actually is important as it initializes the setting with the associated gameobject
+                        themes.Add(InteractableProfileItem.GetTheme(setting, profile.Target));
+                    }
+                }
+            }
+        }
+
+        public virtual void OnDisable()
+        {
+            //Refresh();
+        }
+
+        private void UpdateThemes(bool isFocused)
+        {
+            var state = isFocused ? InteractableStates.InteractableStateEnum.Focus : InteractableStates.InteractableStateEnum.Default;
+
+            foreach(var theme in this.themes)
+            { 
+                theme.OnUpdate((int)state, null, true);
+            }
+        }
+
+        #region IMixedRealityFocusHandler Implementation
+
+        /// <inheritdoc />
+        public override void OnFocusEnter(FocusEventData eventData)
+        {
+            base.OnFocusEnter(eventData);
+
+            UpdateThemes(HasFocus);
+        }
+
+        /// <inheritdoc />
+        public override void OnFocusExit(FocusEventData eventData)
+        {
+            base.OnFocusExit(eventData);
+
+            UpdateThemes(HasFocus);
+        }
+
+        #endregion IMixedRealityFocusHandler Implementation
+    }
+}
---
+++ Assets/MixedRealityToolkit.SDK/Inspectors/UX/Interactable/ProfileItemInspector.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.UI
+{
+    /// <summary>
+    /// Inspector for themes, and used by Interactable
+    /// </summary>
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(InteractableProfileItem))]
+    public class ProfileItemInspector : UnityEditor.Editor
+    {
+        protected SerializedProperty settings;
+
+        protected static InteractableTypesContainer themeOptions;
+        protected static string[] shaderOptions;
+        protected static State[] themeStates;
+
+        protected GUIStyle boxStyle;
+        protected bool layoutComplete = false;
+        private const float ThemeStateFontScale = 1.2f;
+
+        protected virtual void OnEnable()
+        {
+            //settings = serializedObject.FindProperty("Settings");
+            //SetupThemeOptions();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.LabelField("TesT!!!!");
+            base.OnInspectorGUI();
+        }
+
+    }
+#endif
+}
