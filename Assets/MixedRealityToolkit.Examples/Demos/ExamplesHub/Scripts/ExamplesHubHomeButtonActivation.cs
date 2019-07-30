c12624c0f4d070fd31976561dd8b3147bb9513ca
Yoon Park <cre8ive.park@gmail.com>
2019-07-30 15:09:27 -0700

    Adding a script to enable the Hub Home button only when the scene is in Scene System.

---
+++ Assets/MixedRealityToolkit.Examples/Demos/ExamplesHub/Scripts/ExamplesHubHomeButtonActivation.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExamplesHubHomeButtonActivation : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Home button which brings the user back to examples hub")]
+    private GameObject buttonHubHome = null;
+
+    void Start()
+    {
+        if (MixedRealityToolkit.IsSceneSystemEnabled == true)
+        {
+            if (buttonHubHome != null)
+            {
+                buttonHubHome.SetActive(true);
+            }
+        }
+    }
+}
