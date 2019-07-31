a5f6e38495717c8fe87e6ab8473e32b4abb99030
Joost van Schaik <joostvanschaik@outlook.com>
2019-07-31 11:31:52 +0200

    ExtensionServiceCreator ignores custom namespace, always takes default. Fixing that required also required adaption of templates

--- Assets/MixedRealityToolkit.Tools/ExtensionServiceCreator/ExtensionServiceCreator.cs
+++ Assets/MixedRealityToolkit.Tools/ExtensionServiceCreator/ExtensionServiceCreator.cs
@@ -517,10 +517,10 @@ namespace Microsoft.MixedReality.Toolkit.Editor
 
             try
             {
-                ScriptableObject profileInstance = ScriptableObject.CreateInstance(DefaultExtensionNamespace + "." + ProfileName);
+                ScriptableObject profileInstance = ScriptableObject.CreateInstance(Namespace + "." + ProfileName);
                 if (profileInstance == null)
                 {
-                    creationLog.Add("Couldn't create instance of profile class " + DefaultExtensionNamespace + "." + ProfileName + " - aborting");
+                    creationLog.Add("Couldn't create instance of profile class " + Namespace + "." + ProfileName + " - aborting");
                     Result = CreateResult.Error;
                     return;
                 }
@@ -587,7 +587,7 @@ namespace Microsoft.MixedReality.Toolkit.Editor
             scriptContents = scriptContents.Replace(InterfaceNameSearchString, InterfaceName);
             scriptContents = scriptContents.Replace(ProfileNameSearchString, ProfileName);
             scriptContents = scriptContents.Replace(ProfileFieldNameSearchString, ProfileFieldName);
-            scriptContents = scriptContents.Replace(ExtensionNamespaceSearchString, DefaultExtensionNamespace);
+            scriptContents = scriptContents.Replace(ExtensionNamespaceSearchString, Namespace);
 
             List<string> platformValues = new List<string>();
             foreach (SupportedPlatforms platform in Enum.GetValues(typeof(SupportedPlatforms)))
