ab2e04a87c3cb150ba4742d4dfd1d18a18e1ef6a
Andrei Borodin <andrei.borodin@microsoft.com>
2019-08-06 16:21:17 -0700

    Undoing change.

--- Assets/MixedRealityToolkit.SDK/Inspectors/Utilities/Solvers/SolverInspector.cs
+++ Assets/MixedRealityToolkit.SDK/Inspectors/Utilities/Solvers/SolverInspector.cs
@@ -1,6 +1,7 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.﻿
 
+using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
 using UnityEditor;
 
 namespace Microsoft.MixedReality.Toolkit.Utilities.Editor.Solvers
