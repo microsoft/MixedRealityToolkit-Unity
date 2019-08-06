89dce10fa1aea60cb92ba7ec5a94cfc9a86c3a76
Cameron Micka <cameron.micka@microsoft.com>
2019-08-06 15:27:30 -0700

    Commenting pass and fixing mesh resource leak.

--- Assets/MixedRealityToolkit/Utilities/StandardShader/MeshOutline.cs
+++ Assets/MixedRealityToolkit/Utilities/StandardShader/MeshOutline.cs
@@ -3,18 +3,25 @@
 
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Microsoft.MixedReality.Toolkit.Utilities
 {
     /// <summary>
-    /// TODO
+    /// Component which can be used to render an outline around a mesh renderer. Enabling this component introduces an additional render pass 
+    /// of the object being outlined, but is designed to run performantly on mobile Mixed Reality devices and does not utilize any post processes.
+    /// This behavior is designed to be used in conjunction with the MRTK/Standard shader. Limitations of this effect include it not working well 
+    /// on objects which are non-uniformly scaled and depth sorting issues on overlapping objects.
     /// </summary>
-    [RequireComponent(typeof(MeshFilter))]
     [RequireComponent(typeof(MeshRenderer))]
     public class MeshOutline : MonoBehaviour
     {
+        private const string smoothNormalsKeyword = "_SMOOTH_NORMALS";
+        private const string vertexExtrusionValueName = "_VertexExtrusionValue";
+
         /// <summary>
-        /// TODO Make sure material has "Depth Write" set to Off. And "Vertex Extrusion" enabled.
+        /// The material used to render the outline. Outline materials should normal have "Depth Write" set to Off and "Vertex Extrusion" enabled.
+        /// Most MRTK/Standard features should work as an outline material, but it is recommended to use a simple unlit material.
         /// </summary>
         public Material OutlineMaterial
         {
@@ -26,12 +33,12 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
             }
         }
 
-        [Tooltip("TODO")]
+        [Tooltip("The material used to render the outline. Outline materials should normal have \"Depth Write\" set to Off and \"Vertex Extrusion\" enabled.")]
         [SerializeField]
         private Material outlineMaterial = null;
 
         /// <summary>
-        /// TODO
+        /// How thick (in meters) should the outline be. Overrides the "Extrusion Value" in the MRTK/Standard material.
         /// </summary>
         public float OutlineWidth
         {
@@ -43,37 +50,58 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
             }
         }
 
-        [Tooltip("TODO")]
+        [Tooltip("How thick (in meters) should the outline be. Overrides the \"Extrusion Value\" in the MRTK/Standard material.")]
         [SerializeField]
         [Range(0.001f, 1.0f)]
         private float outlineWidth = 0.01f;
 
-        private MaterialPropertyBlock propertyBlock = null;
         private MeshRenderer meshRenderer = null;
+        private MaterialPropertyBlock propertyBlock = null;
+        private int vertexExtrusionValueID = 0;
         private Material[] defaultMaterials = null;
 
+        #region MonoBehaviour Implementation
+
+        /// <summary>
+        /// Used to update the outline width when modifying the property in the inspector.
+        /// </summary>
         private void OnValidate()
         {
             ApplyOutlineMaterial();
         }
 
+        /// <summary>
+        /// Gathers initial render state.
+        /// </summary>
         private void Awake()
         {
-            propertyBlock = new MaterialPropertyBlock();
             meshRenderer = GetComponent<MeshRenderer>();
+            propertyBlock = new MaterialPropertyBlock();
+            vertexExtrusionValueID = Shader.PropertyToID(vertexExtrusionValueName);
             defaultMaterials = meshRenderer.sharedMaterials;
         }
 
+        /// <summary>
+        /// Enables the outline.
+        /// </summary>
         private void OnEnable()
         {
             ApplyOutlineMaterial();
         }
 
+        /// <summary>
+        /// Resets the renderer materials to the default settings.
+        /// </summary>
         private void OnDisable()
         {
             meshRenderer.materials = defaultMaterials;
         }
 
+        #endregion MonoBehaviour Implementation
+
+        /// <summary>
+        /// Prepares and applies the current outline material to the renderer.
+        /// </summary>
         private void ApplyOutlineMaterial()
         {
             if (outlineMaterial != null && meshRenderer != null)
@@ -82,7 +110,7 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
                 outlineMaterial.renderQueue = GetMinRenderQueue(defaultMaterials) - 1;
 
                 // If smooth normals are requested, make sure the mesh has smooth normals.
-                if (outlineMaterial.IsKeywordEnabled("_SMOOTH_NORMALS"))
+                if (outlineMaterial.IsKeywordEnabled(smoothNormalsKeyword))
                 {
                     gameObject.EnsureComponent<MeshSmoother>().SmoothNormalsAsync();
                 }
@@ -96,16 +124,24 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
             }
         }
 
+        /// <summary>
+        /// Updates the current vertex extrusion value used by the shader. 
+        /// </summary>
         private void ApplyOutlineWidth()
         {
             if (meshRenderer != null && propertyBlock != null)
             {
                 meshRenderer.GetPropertyBlock(propertyBlock);
-                propertyBlock.SetFloat("_VertexExtrusionValue", outlineWidth);
+                propertyBlock.SetFloat(vertexExtrusionValueName, outlineWidth);
                 meshRenderer.SetPropertyBlock(propertyBlock);
             }
         }
 
+        /// <summary>
+        /// Searches for the minimum render queue value in a list of materials.
+        /// </summary>
+        /// <param name="materials">The list of materials to search.</param>
+        /// <returns>The minimum render queue value.</returns>
         private static int GetMinRenderQueue(Material[] materials)
         {
             var min = int.MaxValue;
@@ -113,6 +149,11 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
             foreach (var material in materials)
             {
                 min = Mathf.Min(min, material.renderQueue);
+            }
+
+            if (min == int.MaxValue)
+            {
+                min = (int)RenderQueue.Background;
             }
 
             return min;
--- Assets/MixedRealityToolkit/Utilities/StandardShader/MeshSmoother.cs
+++ Assets/MixedRealityToolkit/Utilities/StandardShader/MeshSmoother.cs
@@ -8,20 +8,104 @@ using UnityEngine;
 namespace Microsoft.MixedReality.Toolkit.Utilities
 {
     /// <summary>
-    /// TODO
+    /// Component which can be used to automatically generate smoothed normals on a mesh and pack 
+    /// those normals into an UV set. Smoothed normals can be used for a variety of effects including 
+    /// extruding disjoint meshes along a vertex normal. This behavior is designed to be used in conjunction 
+    /// with the MRTK/Standard shader which assumes smoothed normals are packed into the 3rd UV set.
     /// </summary>
     [RequireComponent(typeof(MeshFilter))]
     public class MeshSmoother : MonoBehaviour
     {
         private const int smoothNormalUVChannel = 2;
 
-        [Tooltip("TODO")]
+        [Tooltip("Should this component automatically smooth normals on awake?")]
         [SerializeField]
         private bool smoothNormalsOnAwake = false;
 
         private MeshFilter meshFilter = null;
-        private static Dictionary<Mesh, Mesh> processedMeshes = new Dictionary<Mesh, Mesh>();
 
+        /// <summary>
+        /// Helper class to track mesh references.
+        /// </summary>
+        private class MeshReference
+        {
+            public Mesh Mesh;
+            private int referenceCount;
+
+            public MeshReference(Mesh mesh)
+            {
+                Mesh = mesh;
+                referenceCount = 1;
+            }
+
+            public void Increment()
+            {
+                ++referenceCount;
+            }
+
+            public void Decrement()
+            {
+                --referenceCount;
+            }
+
+            public bool IsReferenced()
+            {
+                return referenceCount > 0;
+            }
+        }
+
+        private static Dictionary<Mesh, MeshReference> processedMeshes = new Dictionary<Mesh, MeshReference>();
+
+        /// <summary>
+        /// Performs normal smoothing on the current mesh filter associated with this component synchronously.
+        /// This method will not try and re-smooth meshes which have already been smoothed.
+        /// </summary>
+        public void SmoothNormals()
+        {
+            Mesh mesh;
+
+            // No need to do any smoothing if this mesh has already been processed.
+            if (AcquirePreprocessedMesh(out mesh))
+            {
+                return;
+            }
+
+            var result = CalculateSmoothNormals(mesh.vertices, mesh.normals);
+            mesh.SetUVs(smoothNormalUVChannel, result);
+        }
+
+        /// <summary>
+        /// Performs normal smoothing on the current mesh filter associated with this component asynchronously.
+        /// This method will not try and re-smooth meshes which have already been smoothed.
+        /// </summary>
+        /// <returns>A task which will complete once normal smoothing is finished.</returns>
+        public Task SmoothNormalsAsync()
+        {
+            Mesh mesh;
+
+            // No need to do any smoothing if this mesh has already been processed.
+            if (AcquirePreprocessedMesh(out mesh))
+            {
+                return Task.CompletedTask;
+            }
+
+            // Create a copy of the vertices and normals and apply the smoothing in an async task.
+            var vertices = mesh.vertices;
+            var normals = mesh.normals;
+            var asyncTask = Task.Run(() => CalculateSmoothNormals(vertices, normals));
+
+            // Once the async task is complete, apply the smoothed normals to the mesh on the main thread.
+            return asyncTask.ContinueWith((i) =>
+            {
+                mesh.SetUVs(smoothNormalUVChannel, i.Result);
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        #region MonoBehaviour Implementation
+
+        /// <summary>
+        /// Applies smoothing asynchronously if specified by the inspector property.
+        /// </summary>
         private void Awake()
         {
             meshFilter = GetComponent<MeshFilter>();
@@ -32,68 +116,77 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
             }
         }
 
-        public void SmoothNormals()
+        /// <summary>
+        /// Clean up any meshes which were created if no longer referenced.
+        /// </summary>
+        private void OnDestroy()
         {
-            Mesh mesh;
+            var sharedMesh = meshFilter.sharedMesh;
+            MeshReference meshReference;
 
-            if (AcquirePreprocessedMesh(out mesh))
+            if (processedMeshes.TryGetValue(sharedMesh, out meshReference))
             {
-                return;
-            }
+                meshReference.Decrement();
 
-            var result = CalculateSmoothNormals(mesh.vertices, mesh.normals);
-            mesh.SetUVs(smoothNormalUVChannel, result);
+                if (!meshReference.IsReferenced())
+                {
+                    Destroy(meshReference.Mesh);
+                    processedMeshes.Remove(sharedMesh);
+                }
+            }
         }
 
-        public Task SmoothNormalsAsync()
-        {
-            Mesh mesh;
+        #endregion MonoBehaviour Implementation
 
-            if (AcquirePreprocessedMesh(out mesh))
-            {
-                return Task.CompletedTask;
-            }
-
-            var vertices = mesh.vertices;
-            var normals = mesh.normals;
-            var asyncTask = Task.Run(() => CalculateSmoothNormals(vertices, normals));
-
-            return asyncTask.ContinueWith((i) =>
-            {
-                mesh.SetUVs(smoothNormalUVChannel, i.Result);
-            }, TaskScheduler.FromCurrentSynchronizationContext());
-        }
-
+        /// <summary>
+        /// Safely acquires a mesh for processing. Checks for meshes which have already been processed and increments reference counts.
+        /// </summary>
+        /// <param name="mesh">A reference to the mesh which was already processed or is ready to be processed.</param>
+        /// <returns>True if the mesh was already processed, false otherwise.</returns>
         private bool AcquirePreprocessedMesh(out Mesh mesh)
         {
-            // Check if a mesh has already been processed, and if so simply assign that mesh to this filter.
             var sharedMesh = meshFilter.sharedMesh;
 
-            if (processedMeshes.TryGetValue(sharedMesh, out mesh))
+            MeshReference meshReference;
+
+            // If this mesh has already been processed, apply the preprocessed mesh and increment the reference count.
+            if (processedMeshes.TryGetValue(sharedMesh, out meshReference))
             {
+                meshReference.Increment();
+                mesh = meshReference.Mesh;
                 meshFilter.mesh = mesh;
 
                 return true;
             }
 
-            // Else, clone the current mesh and add shared mesh and cloned mesh to the table of previously smoothed meshes.
+            // Clone the mesh, and create a mesh reference which can be keyed off either the original mesh or cloned mesh.
             mesh = meshFilter.mesh;
-            processedMeshes[sharedMesh] = mesh;
-            processedMeshes[mesh] = mesh;
+            meshReference = new MeshReference(mesh);
+            processedMeshes[sharedMesh] = meshReference;
+            processedMeshes[mesh] = meshReference;
 
             return false;
         }
 
-        private static List<Vector3> CalculateSmoothNormals(Vector3[] verticies, Vector3[] normals)
+        /// <summary>
+        /// This method groups vertices in a mesh that share the same location in space then averages the normals of those vertices.
+        /// For example, if you imagine the 3 vertices that make up one corner of a cube. Normally there will be 3 normals facing in the direction 
+        /// of each face that touches that corner. This method will take those 3 normals and average them into a normal that points in the 
+        /// direction from the center of the cube to the corner of the cube.
+        /// </summary>
+        /// <param name="vertices">A list of vertices that represent a mesh.</param>
+        /// <param name="normals">A list of normals that correspond to each vertex passed in via the vertices param.</param>
+        /// <returns>A list of normals which are smoothed, or averaged, based on share vertex position.</returns>
+        private static List<Vector3> CalculateSmoothNormals(Vector3[] vertices, Vector3[] normals)
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
 
             // Group all vertices that share the same location in space.
             var groupedVerticies = new Dictionary<Vector3, List<KeyValuePair<int, Vector3>>>();
 
-            for (int i = 0; i < verticies.Length; ++i)
+            for (int i = 0; i < vertices.Length; ++i)
             {
-                var vertex = verticies[i];
+                var vertex = vertices[i];
                 List<KeyValuePair<int, Vector3>> group;
 
                 if (!groupedVerticies.TryGetValue(vertex, out group))
@@ -108,7 +201,7 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
             var smoothNormals = new List<Vector3>(normals);
 
             // If we don't hit the degenerate case of each vertex is it's own group (no vertices shared a location), average the normals of each group.
-            if (groupedVerticies.Count != verticies.Length)
+            if (groupedVerticies.Count != vertices.Length)
             {
                 foreach (var group in groupedVerticies)
                 {
@@ -134,7 +227,7 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
                 }
             }
 
-            Debug.LogFormat("CalculateSmoothNormals took {0} ms on {1} vertices.", watch.ElapsedMilliseconds, verticies.Length);
+            Debug.LogFormat("CalculateSmoothNormals took {0} ms on {1} vertices.", watch.ElapsedMilliseconds, vertices.Length);
 
             return smoothNormals;
         }
