2f235bc7034ac555cd1d85e66a91380c4064c712
davidkline-ms <david.kline@microsoft.com>
2019-08-13 12:56:17 -0700

    replace boo with system.collections.generic

--- Assets/MixedRealityToolkit/Utilities/Editor/Setup/MixedRealityEditorSettings.cs
+++ Assets/MixedRealityToolkit/Utilities/Editor/Setup/MixedRealityEditorSettings.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using Boo.Lang;
 using Microsoft.MixedReality.Toolkit.Editor;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEditor;
