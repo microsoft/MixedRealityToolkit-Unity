64fa38bfe927bc534ef59df73183fcfd9bdf0761
Yoon Park <cre8ive@outlook.com>
2019-08-14 18:57:10 -0700

    Adding MRTK Example Hub. Normalized the example scene content scale. Updated eye tracking example scenes from previous PR.

---
+++ Assets/MixedRealityToolkit.SDK/Features/UX/Scripts/Utilities/LoadContentScene.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.SceneSystem;
+#if SCENE_TRANSITIONS_ENABLED
+using Microsoft.MixedReality.Toolkit.Extensions.SceneTransitions;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Microsoft.MixedReality.Toolkit.UI
+{
+    /// <summary>
+    /// Utility class to load scenes through MRTK Scene System. If Scene Transition Service is enabled, it uses it.
+    /// Otherwise, it uses Scene System's LoadContent()
+    /// </summary>
+    public class LoadContentScene : MonoBehaviour
+	{
+        [SerializeField]
+        private LoadSceneMode loadSceneMode = LoadSceneMode.Single;
+        [SerializeField]
+        private SceneInfo contentScene = SceneInfo.Empty;
+        [SerializeField]
+        private bool loadOnStartup = false;
+
+        private void Start()
+        {
+            if (loadOnStartup)
+            {
+                LoadContent();
+            }
+        }
+
+        public void LoadContent()
+		{
+#if SCENE_TRANSITIONS_ENABLED
+			ISceneTransitionService transitions = MixedRealityToolkit.Instance.GetService<ISceneTransitionService>();
+			if (transitions.TransitionInProgress)
+				return;
+
+			transitions.DoSceneTransition(() => CoreServices.SceneSystem.LoadContent(contentScene.Name, loadSceneMode));
+#else
+            CoreServices.SceneSystem.LoadContent(contentScene.Name, loadSceneMode);
+            #endif
+        }
+	}
+}
