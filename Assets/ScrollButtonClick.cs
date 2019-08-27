8daf5294ed95b0abcb99452ad555bae93d746f49
Addison Linville <adlinv@microsoft.com>
2019-08-27 12:51:37 -0700

    temp files for testing

---
+++ Assets/ScrollButtonClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+
+public class ScrollButtonClick : MonoBehaviour
+{
+    public TextMeshPro clickTextbox;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void DisplayClick(GameObject sender)
+    {
+        clickTextbox.text = "Click happened with" + sender;
+    }
+
+
+}
