29b83777544d19f67e29a95d1188a2b8db977778
Troy Ferrell <trferrel@microsoft.com>
2019-09-05 11:01:08 -0700

    Faster types

--- Assets/MixedRealityToolkit/Extensions/TypeExtensions.cs
+++ Assets/MixedRealityToolkit/Extensions/TypeExtensions.cs
@@ -3,8 +3,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace Microsoft.MixedReality.Toolkit
 {
@@ -14,29 +14,27 @@ namespace Microsoft.MixedReality.Toolkit
         /// Returns a list of types for all classes that extend from the current type and are not abstract
         /// </summary>
         /// <param name="rootType">The class type from which to search for inherited classes</param>
+        /// <param name="searchAssemblies">List of assemblies to search through for types. If null, default is to grab all assemblies in current app domain</param>
         /// <returns>Null if rootType is not a class, otherwise returns list of types for sub-classes of rootType</returns>
-        public static List<Type> GetAllSubClassesOf(this Type rootType, Assembly[] assemblies = null)
+        public static List<Type> GetAllSubClassesOf(this Type rootType, Assembly[] searchAssemblies = null)
         {
             if (!rootType.IsClass) return null;
 
-            if (assemblies == null) { assemblies = AppDomain.CurrentDomain.GetAssemblies(); }
+            if (searchAssemblies == null) { searchAssemblies = AppDomain.CurrentDomain.GetAssemblies(); }
 
             var results = new List<Type>();
-            Stopwatch st = new Stopwatch();
-            st.Start();
-            //Parallel.ForEach(
-            foreach (var assembly in assemblies)
+
+            Parallel.ForEach(searchAssemblies, (assembly) =>
             {
-                foreach (var type in assembly.GetTypes())
+                Parallel.ForEach(assembly.GetTypes(), (type) =>
                 {
                     if (type.IsClass && !type.IsAbstract && type.IsSubclassOf(rootType))
                     {
                         results.Add(type);
                     }
-                }
-            }
-            st.Stop();
-            UnityEngine.Debug.Log("Time:" + st.ElapsedMilliseconds);
+                });
+            });
+
             return results;
         }
     }
