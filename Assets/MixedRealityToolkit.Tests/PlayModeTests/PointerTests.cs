01cfd63919204e34447587a47b57590b80228736
Catherine Diaz <cadia@microsoft.com>
2019-09-11 14:44:41 -0700

    Added comment before class and .NET scripting comment block

--- Assets/MixedRealityToolkit.Tests/PlayModeTests/PointerTests.cs
+++ Assets/MixedRealityToolkit.Tests/PlayModeTests/PointerTests.cs
@@ -2,6 +2,14 @@
 // Licensed under the MIT License.
 
 #if !WINDOWS_UWP
+// When the .NET scripting backend is enabled and C# projects are built
+// The assembly that this file is part of is still built for the player,
+// even though the assembly itself is marked as a test assembly (this is not
+// expected because test assemblies should not be included in player builds).
+// Because the .NET backend is deprecated in 2018 and removed in 2019 and this
+// issue will likely persist for 2018, this issue is worked around by wrapping all
+// play mode tests in this check.
+
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Utilities;
 using NUnit.Framework;
@@ -12,6 +20,7 @@ using UnityEngine.TestTools;
 
 namespace Microsoft.MixedReality.Toolkit.Tests
 {
+    // Tests to verify pointer state and pointer direction
     class PointerTests 
     {
         [SetUp]
