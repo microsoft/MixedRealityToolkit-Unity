76959419975cc511d6d438a5fcd0e6369cf666ac
davidkline-ms <david.kline@microsoft.com>
2019-09-12 13:00:49 -0700

    add simple methods to acquire physics ray for a pointer

--- Assets/MixedRealityToolkit.SDK/Features/Utilities/PointerUtils.cs
+++ Assets/MixedRealityToolkit.SDK/Features/Utilities/PointerUtils.cs
@@ -144,6 +144,157 @@ namespace Microsoft.MixedReality.Toolkit.Input
                 }
             }
         }
-    }
 
+        /// <summary>
+        /// Gets the physics ray based on the interaction enabled pointer assosiciated with the head gaze.
+        /// </summary>
+        /// <param name="ray">The physics ray for the pointer</param>
+        /// <returns>
+        /// True if the ray contains valid data, false otherwise.
+        /// </returns>
+        public static bool TryGetHeadGazeRay(out Ray ray)
+        {
+            return TryGetRay(InputSourceType.Head, Handedness.None, out ray);
+        }
+
+        /// <summary>
+        /// Gets the physics ray based on the interaction enabled pointer assosiciated with the eye gaze.
+        /// </summary>
+        /// <param name="ray">The physics ray for the pointer</param>
+        /// <returns>
+        /// True if the ray contains valid data, false otherwise.
+        /// </returns>
+        public static bool TryGetEyeGazeRay(out Ray ray)
+        {
+            return TryGetRay(InputSourceType.Eyes, Handedness.None, out ray);
+        }
+
+        /// <summary>
+        /// Gets the physics ray based on the interaction enabled pointer assosiciated with the user's gaze
+        /// and reports the type of input (head or eyes) of the active gaze.
+        /// </summary>
+        /// <param name="ray">The physics ray for the pointer</param>
+        /// <param name="sourceType">The type of input source</param>
+        /// <returns>
+        /// True if the ray contains valid data, false otherwise.
+        /// </returns>
+        public static bool TryGetGazeRay(out Ray ray, out InputSourceType sourceType)
+        {
+            ray = new Ray();
+            sourceType = InputSourceType.Other;
+
+            // First, try to get the eye gaze ray
+            if (TryGetEyeGazeRay(out ray))
+            {
+                sourceType = InputSourceType.Eyes;
+                return true;
+            }
+
+            // Then try to get the head gaze ray
+            if (TryGetHeadGazeRay(out ray))
+            {
+                sourceType = InputSourceType.Head;
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the physics ray based on the interaction enabled pointer assosiciated with the user's hand.
+        /// </summary>
+        /// <param name="hand">The <see cref="Handedness"/> of the hand</param>
+        /// <param name="ray">The physics ray for the pointer</param>
+        /// <returns>
+        /// True if the ray contains valid data, false otherwise.
+        /// </returns>
+        public static bool TryGetHandRay(Handedness hand, out Ray ray)
+        {
+            return TryGetRay(InputSourceType.Hand, hand, out ray);
+        }
+
+        /// <summary>
+        /// Gets the physics ray based on the interaction enabled pointer assosiciated with the
+        /// motion controller.
+        /// </summary>
+        /// <param name="hand">The <see cref="Handedness"/> of the motion controller</param>
+        /// <param name="ray">The physics ray for the pointer</param>
+        /// <returns>
+        /// True if the ray contains valid data, false otherwise.
+        /// </returns>
+        public static bool TryGetMotionControllerRay(Handedness hand, out Ray ray)
+
+        {
+            return TryGetRay(InputSourceType.Controller, hand, out ray);
+        }
+
+        /// <summary>
+        /// Gets the physics ray based on the interaction enabled pointer assosiciated with the user's hand
+        /// or a motion controller.
+        /// </summary>
+        /// <param name="hand">The <see cref="Handedness"/> of the user's hand or motion controller</param>
+        /// <param name="ray">The physics ray for the pointer</param>
+        /// <param name="sourceType">The type of input source</param>
+        /// <returns></returns>
+        public static bool TryGetHandOrControllerRay(Handedness hand, out Ray ray, out InputSourceType sourceType)
+        {
+            ray = new Ray();
+            sourceType = InputSourceType.Other;
+
+            // First, try to get the hand ray
+            if (TryGetHandRay(hand, out ray))
+            {
+                sourceType = InputSourceType.Hand;
+                return true;
+            }
+
+            // Then try to get the controller ray
+            if (TryGetMotionControllerRay(hand, out ray))
+            {
+                sourceType = InputSourceType.Controller;
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the physics ray based on the interaction enabled pointer assosiciated with the
+        /// desired input source type.
+        /// </summary>
+        /// <param name="sourceType">The type of input source</param>
+        /// <param name="hand">The <see cref="Handedness"/> of the input source</param>
+        /// <param name="ray">The physics ray for the pointer</param>
+        /// <returns>
+        /// True if the ray contains valid data, false otherwise.
+        /// </returns>
+        public static bool TryGetRay(InputSourceType sourceType, Handedness hand, out Ray ray)
+        {
+            ray = new Ray();
+
+            foreach (IMixedRealityPointer pointer in PointerUtils.GetPointers<IMixedRealityPointer>(hand, sourceType))
+            {
+                if (pointer.IsInteractionEnabled)
+                {
+                    ray.origin = pointer.Position;
+                    ray.direction = GetDirection(pointer.Rotation);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Calculates the direction vector from a rotation.
+        /// </summary>
+        /// <param name="rotation">Quaternion representing the rotation of the object.</param>
+        /// <returns>
+        /// Normalized Vector3 representing the direction vector.
+        /// </returns>
+        public static Vector3 GetDirection(Quaternion rotation)
+        {
+            return (rotation * Vector3.forward).normalized;
+        }
+    }
 }
