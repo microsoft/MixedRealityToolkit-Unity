97bd5fb2bff9facd33a1fdd978f64de96035a69c
Addison Linville <adlinv@microsoft.com>
2019-09-16 11:43:25 -0700

    adding namespace to buttonClick script

---
+++ Assets/MixedRealityToolkit.Examples/Demos/UX/Collections/Scripts/ScrollButtonClick.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using TMPro;
+using UnityEngine;
+
+
+namespace Microsoft.MixedReality.Toolkit.Examples.Demos
+{
+    /// <summary>
+    /// Simple click handler to display button clicks from a <see cref="Microsoft.MixedReality.Toolkit.Experimental.Utilities.ScrollingObjectCollection"/>
+    /// </summary>
+    public class ScrollButtonClick : MonoBehaviour
+    {
+        public TextMeshPro clickTextbox;
+        public void DisplayClick(GameObject sender)
+        {
+            clickTextbox.text = "Click happened with" + sender;
+        }
+
+
+    }
+}
