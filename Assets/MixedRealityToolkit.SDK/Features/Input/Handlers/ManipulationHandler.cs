11c137b486c0f67afb8e55bc3dfc34f29813b929
Will Wei <wiwei@microsoft.com>
2019-09-17 15:03:11 -0700

    Fix the namespace of AxisPreference

--- Assets/MixedRealityToolkit.SDK/Features/Input/Handlers/ManipulationHandler.cs
+++ Assets/MixedRealityToolkit.SDK/Features/Input/Handlers/ManipulationHandler.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-using Assets.MixedRealityToolkit.Definitions;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Physics;
 using Microsoft.MixedReality.Toolkit.Utilities;
--- Assets/MixedRealityToolkit/Definitions/Utilities/AxisPreference.cs
+++ Assets/MixedRealityToolkit/Definitions/Utilities/AxisPreference.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-namespace Assets.MixedRealityToolkit.Definitions
+namespace Microsoft.MixedReality.Toolkit.Utilities
 {
     /// <summary>
     /// Defines the X, Y, and Z Axes as flag-based preferences.
