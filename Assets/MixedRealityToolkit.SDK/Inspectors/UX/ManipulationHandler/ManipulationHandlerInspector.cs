709c197cf2770298e02fad7113cbc4b55bfc2f84
Niall Milsom <nimilsom@microsoft.com>
2019-09-18 16:31:40 +0100

    Move migration to more public location
    Also provided some skeleton code for a more general solution for migration. I expect this to change in future.

--- Assets/MixedRealityToolkit.SDK/Inspectors/UX/ManipulationHandler/ManipulationHandlerInspector.cs
+++ Assets/MixedRealityToolkit.SDK/Inspectors/UX/ManipulationHandler/ManipulationHandlerInspector.cs
@@ -7,6 +7,7 @@ using Microsoft.MixedReality.Toolkit.UI;
 using Microsoft.MixedReality.Toolkit.Experimental.UI;
 using UnityEditor;
 using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Experimental.Utilities;
 
 namespace Microsoft.MixedReality.Toolkit.Editor
 {
@@ -74,7 +75,9 @@ namespace Microsoft.MixedReality.Toolkit.Editor
             EditorGUILayout.HelpBox("Manipulation Handler will soon be removed, please upgrade to Object Manipulator", MessageType.Warning);
             if (GUILayout.Button("Upgrade to Object Manipulator"))
             {
-                Migrate();
+                var migrationHandler = new ObjectManipulatorMigrationHandler();
+                var manipulationHandler = target as ManipulationHandler;
+                migrationHandler.Migrate(manipulationHandler.gameObject);
                 return;
             }
 
@@ -171,73 +174,6 @@ namespace Microsoft.MixedReality.Toolkit.Editor
             style.fontStyle = previousStyle;
 
             serializedObject.ApplyModifiedProperties();
-        }
-
-        private void Migrate()
-        {
-            var mh1 = target as ManipulationHandler;
-            var mh2 = mh1.gameObject.AddComponent<ObjectManipulator>();
-
-            mh2.HostTransform = mh1.HostTransform;
-
-            switch (mh1.ManipulationType)
-            {
-                case ManipulationHandler.HandMovementType.OneHandedOnly:
-                    mh2.ManipulationType = ObjectManipulator.HandMovementType.OneHanded;
-                    break;
-                case ManipulationHandler.HandMovementType.TwoHandedOnly:
-                    mh2.ManipulationType = ObjectManipulator.HandMovementType.TwoHanded;
-                    break;
-                case ManipulationHandler.HandMovementType.OneAndTwoHanded:
-                    mh2.ManipulationType = ObjectManipulator.HandMovementType.OneHanded | 
-                        ObjectManipulator.HandMovementType.TwoHanded;
-                    break;
-            }
-
-            mh2.AllowFarManipulation = mh1.AllowFarManipulation;
-            mh2.OneHandRotationModeNear = (ObjectManipulator.RotateInOneHandType)mh1.OneHandRotationModeNear;
-            mh2.OneHandRotationModeFar = (ObjectManipulator.RotateInOneHandType)mh1.OneHandRotationModeFar;
-
-            switch (mh1.TwoHandedManipulationType)
-            {
-                case ManipulationHandler.TwoHandedManipulation.Scale:
-                    mh2.TwoHandedManipulationType = ObjectManipulator.TwoHandedManipulation.Scale;
-                    break;
-                case ManipulationHandler.TwoHandedManipulation.Rotate:
-                    mh2.TwoHandedManipulationType = ObjectManipulator.TwoHandedManipulation.Rotate;
-                    break;
-                case ManipulationHandler.TwoHandedManipulation.MoveScale:
-                    mh2.TwoHandedManipulationType = ObjectManipulator.TwoHandedManipulation.Move |
-                        ObjectManipulator.TwoHandedManipulation.Scale;
-                    break;
-                case ManipulationHandler.TwoHandedManipulation.MoveRotate:
-                    mh2.TwoHandedManipulationType = ObjectManipulator.TwoHandedManipulation.Move |
-                        ObjectManipulator.TwoHandedManipulation.Rotate;
-                    break;
-                case ManipulationHandler.TwoHandedManipulation.RotateScale:
-                    mh2.TwoHandedManipulationType = ObjectManipulator.TwoHandedManipulation.Rotate |
-                        ObjectManipulator.TwoHandedManipulation.Scale;
-                    break;
-                case ManipulationHandler.TwoHandedManipulation.MoveRotateScale:
-                    mh2.TwoHandedManipulationType = ObjectManipulator.TwoHandedManipulation.Move |
-                        ObjectManipulator.TwoHandedManipulation.Rotate |
-                        ObjectManipulator.TwoHandedManipulation.Scale;
-                    break;
-            }
-
-            mh2.ReleaseBehavior = (ObjectManipulator.ReleaseBehaviorType)mh1.ReleaseBehavior;
-            mh2.ConstraintOnRotation = mh1.ConstraintOnRotation;
-            mh2.ConstraintOnMovement = mh1.ConstraintOnMovement;
-            mh2.SmoothingActive = mh1.SmoothingActive;
-            mh2.MoveLerpTime = mh1.SmoothingAmoutOneHandManip;
-            mh2.RotateLerpTime = mh1.SmoothingAmoutOneHandManip;
-            mh2.ScaleLerpTime = mh1.SmoothingAmoutOneHandManip;
-            mh2.OnManipulationStarted = mh1.OnManipulationStarted;
-            mh2.OnManipulationEnded = mh1.OnManipulationEnded;
-            mh2.OnHoverEntered = mh1.OnHoverEntered;
-            mh2.OnHoverExited = mh1.OnHoverExited;
-
-            DestroyImmediate(mh1);
         }
     }
 }
