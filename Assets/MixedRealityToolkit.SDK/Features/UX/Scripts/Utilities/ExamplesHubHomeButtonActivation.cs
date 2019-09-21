5b14b9c7173277072f076f213baec0987c61c81c
Yoon Park <cre8ive.park@gmail.com>
2019-09-20 23:05:34 -0700

    Adding comments

--- Assets/MixedRealityToolkit.SDK/Features/UX/Scripts/Utilities/ExamplesHubHomeButtonActivation.cs
+++ Assets/MixedRealityToolkit.SDK/Features/UX/Scripts/Utilities/ExamplesHubHomeButtonActivation.cs
@@ -7,8 +7,14 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace Microsoft.MixedReality.Toolkit.UI
 {
+    /// <summary>
+    /// Shows the home button in the ToggleFeaturesPanel if the Scene System is enabled
+    /// </summary>
     public class ExamplesHubHomeButtonActivation : MonoBehaviour
     {
+        /// <summary>
+        /// Home button which brings the user back to examples hub
+        /// </summary>
         [SerializeField]
         [Tooltip("Home button which brings the user back to examples hub")]
         private GameObject buttonHubHome = null;
