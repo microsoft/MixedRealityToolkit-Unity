1acd85eb2468274366eff0ae20ad6d74f57ee8ab
Julia Schwarz <jschwarz@microsoft.com>
2019-09-26 12:27:53 -0700

    Update prefabs

--- Assets/MixedRealityToolkit.SDK/Features/UX/Scripts/Utilities/PointerBehaviorControls.cs
+++ Assets/MixedRealityToolkit.SDK/Features/UX/Scripts/Utilities/PointerBehaviorControls.cs
@@ -15,6 +15,13 @@ namespace Microsoft.MixedReality.Toolkit.UI
     /// </summary>
     public class PointerBehaviorControls : MonoBehaviour
     {
+        /// <summary>
+        /// Toggles a pointer's "enabled" behavior. If a pointer's <see cref="PointerBehavior"/> is Default or AlwaysOn,
+        /// set it to AlwaysOff. Otherwise, set the pointer's behavior to Default.
+        /// Will set this state for all matching pointers.
+        /// </summary>
+        /// <typeparam name="T">Type of pointer to set</typeparam>
+        /// <param name="inputType">Input type of pointer to set</param>
         public void TogglePointerEnabled<T>(InputSourceType inputType) where T : class, IMixedRealityPointer
         {
             PointerBehavior oldBehavior = PointerUtils.GetPointerBehavior<T>(Handedness.Any, inputType);
@@ -30,65 +37,102 @@ namespace Microsoft.MixedReality.Toolkit.UI
             PointerUtils.SetPointerBehavior<T>(newBehavior, inputType);
         }
 
+        /// <summary>
+        /// If hand ray <see cref="PointerBehavior"/> is AlwaysOn or Default, set it to off.
+        /// Otherwise, set behavior to default
+        /// </summary>
         public void ToggleHandRayEnabled()
         {
             TogglePointerEnabled<ShellHandRayPointer>(InputSourceType.Hand);
         }
 
+        /// <summary>
+        /// If controller ray <see cref="PointerBehavior"/> is AlwaysOn or Default, set it to off.
+        /// Otherwise, set behavior to default
+        /// </summary>
         public void ToggleControllerRayEnabled()
         {
             TogglePointerEnabled<ShellHandRayPointer>(InputSourceType.Controller);
         }
 
+        /// <summary>
+        /// If hand grab pointer <see cref="PointerBehavior"/> is AlwaysOn or Default, set it to off.
+        /// Otherwise, set behavior to default
+        /// </summary>
         public void ToggleHandGrabEnabled()
         {
             TogglePointerEnabled<SpherePointer>(InputSourceType.Hand);
         }
 
+        /// <summary>
+        /// If finger poke pointer <see cref="PointerBehavior"/> is AlwaysOn or Default, set it to off.
+        /// Otherwise, set behavior to default
+        /// </summary>
         public void ToggleHandPokeEnabled()
         {
             TogglePointerEnabled<PokePointer>(InputSourceType.Hand);
         }
 
+        /// <summary>
+        /// Sets the <see cref="PointerBehavior"/> for all hand rays.
+        /// <param name="isEnabled">If true, behavior will be set to Default.
+        /// Otherwise it will be set to AlwaysOff</param>
+        /// </summary>
         public void SetHandRayEnabled(bool isEnabled)
         {
             PointerUtils.SetHandRayPointerBehavior(isEnabled ? PointerBehavior.Default : PointerBehavior.AlwaysOff,
                 Handedness.Any);
         }
 
-        public void SetRightHandRayEnabled(bool isEnabled)
-        {
-            PointerUtils.SetHandRayPointerBehavior(isEnabled ? PointerBehavior.Default : PointerBehavior.AlwaysOff,
-                Handedness.Right);
-        }
-
-        public void SetLeftHandRayEnabled(bool isEnabled)
-        {
-            PointerUtils.SetHandRayPointerBehavior(isEnabled ? PointerBehavior.Default : PointerBehavior.AlwaysOff,
-                Handedness.Left);
-        }
-
+        /// <summary>
+        /// Sets the <see cref="PointerBehavior"/> for all controller rays to be AlwaysOff
+        /// <param name="isEnabled">If true, behavior will be set to Default.
+        /// Otherwise it will be set to AlwaysOff</param>
+        /// </summary>
         public void SetControllerRayEnabled(bool isEnabled)
         {
             PointerUtils.SetMotionControllerRayPointerBehavior(isEnabled ? PointerBehavior.Default : PointerBehavior.AlwaysOff,
                 Handedness.Any);
         }
 
+        /// <summary>
+        /// Sets the <see cref="PointerBehavior"/> for the gaze pointer to be AlwaysOff
+        /// <param name="isEnabled">If true, behavior will be set to Default.
+        /// Otherwise it will be set to AlwaysOff</param>
+        /// </summary>
         public void SetGazeEnabled(bool isEnabled)
         {
             PointerUtils.SetGazePointerBehavior(isEnabled ? PointerBehavior.Default : PointerBehavior.AlwaysOff);
         }
 
+        /// <summary>
+        /// Sets the <see cref="PointerBehavior"/> for the grab pointer to be AlwaysOff
+        /// <param name="isEnabled">If true, behavior will be set to Default.
+        /// Otherwise it will be set to AlwaysOff</param>
+        /// </summary>
         public void SetGrabEnabled(bool isEnabled)
         {
             PointerUtils.SetHandGrabPointerBehavior(isEnabled ? PointerBehavior.Default : PointerBehavior.AlwaysOff, Handedness.Any);
         }
 
+        /// <summary>
+        /// Sets the <see cref="PointerBehavior"/> for the poke pointer to be AlwaysOff
+        /// <param name="isEnabled">If true, behavior will be set to Default.
+        /// Otherwise it will be set to AlwaysOff</param>
+        /// </summary>
         public void SetPokeEnabled(bool isEnabled)
         {
             PointerUtils.SetHandPokePointerBehavior(isEnabled ? PointerBehavior.Default : PointerBehavior.AlwaysOff, Handedness.Any);
         }
 
+        /// <summary>
+        /// Sets pointer states to mimic traditional vr behavior.
+        /// PokePointer will be off
+        /// GrabPointer will be off
+        /// HandRayPointer will be off
+        /// MotionControllerRayPointer will be Default
+        /// GazePointef will be off
+        /// </summary>
         public void SetVR()
         {
             PointerUtils.SetHandPokePointerBehavior(PointerBehavior.AlwaysOff, Handedness.Any);
@@ -98,6 +142,9 @@ namespace Microsoft.MixedReality.Toolkit.UI
             PointerUtils.SetGazePointerBehavior(PointerBehavior.AlwaysOff);
         }
 
+        /// <summary>
+        /// Sets pointer states to turn off all but the poke pointer
+        /// </summary>
         public void SetFingerOnly()
         {
             PointerUtils.SetHandPokePointerBehavior(PointerBehavior.Default, Handedness.Any);
@@ -107,6 +154,15 @@ namespace Microsoft.MixedReality.Toolkit.UI
             PointerUtils.SetGazePointerBehavior(PointerBehavior.AlwaysOff);
         }
 
+        /// <summary>
+        /// Sets pointer behavior to mimic HoloLens 1 interactions, useful
+        /// for using Hololens 1 interactions on HoloLens 2.
+        /// PokePointer will be off
+        /// GrabPointer will be off
+        /// HandRayPointer will be off
+        /// MotionControllerRayPointer will be off
+        /// GazePointer will be Default
+        /// </summary>
         public void SetHoloLens1()
         {
             PointerUtils.SetHandPokePointerBehavior(PointerBehavior.AlwaysOff, Handedness.Any);
@@ -116,6 +172,14 @@ namespace Microsoft.MixedReality.Toolkit.UI
             PointerUtils.SetGazePointerBehavior(PointerBehavior.Default);
         }
 
+        /// <summary>
+        /// Sets pointer behavior to mimic HoloLens 2
+        /// PokePointer will be Default
+        /// GrabPointer will be Default
+        /// HandRayPointer will be Default
+        /// MotionControllerRayPointer will be off
+        /// GazePointer will be Off
+        /// </summary>
         public void SetHoloLens2()
         {
             PointerUtils.SetHandPokePointerBehavior(PointerBehavior.Default, Handedness.Any);
