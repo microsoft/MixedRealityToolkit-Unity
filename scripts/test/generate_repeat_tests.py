e0710550ca9f321dac0138213593347c0b1ad9aa
Julia Schwarz <julenka.schwarz@gmail.com>
2019-09-26 18:14:03 -0700

    Update scripts/test/generate_repeat_tests.py

--- scripts/test/generate_repeat_tests.py
+++ scripts/test/generate_repeat_tests.py
@@ -2,7 +2,7 @@ def test_str(j, s, i):
     return "[UnityTest]\npublic IEnumerator A%i%s%i(){ yield return %s();}" % ( i, s, j, s)
 
 def print_tests(testnames, repeat):
-    for i in range(1,repeat):
+    for i in range(1, repeat):
         for j, name in enumerate(testnames):
             print(test_str(j, name,  i))
 
