a29855adc730b10aba880c58ce521678bb164254
Ritika Jain <ritikaj@microsoft.com>
2019-10-01 15:28:57 -0700

    Fix serialized property name in the inspector that was broken due to property renaming

--- Assets/MixedRealityToolkit.SDK/Inspectors/UX/Interactable/InteractableInspector.cs
+++ Assets/MixedRealityToolkit.SDK/Inspectors/UX/Interactable/InteractableInspector.cs
@@ -58,7 +58,7 @@ namespace Microsoft.MixedReality.Toolkit.UI.Editor
             isGlobal = serializedObject.FindProperty("isGlobal");
             canSelect = serializedObject.FindProperty("CanSelect");
             canDeselect = serializedObject.FindProperty("CanDeselect");
-            startDimensionIndex = serializedObject.FindProperty("StartDimensionIndex");
+            startDimensionIndex = serializedObject.FindProperty("startDimensionIndex");
             dimensionIndex = serializedObject.FindProperty("dimensionIndex");
             dimensions = serializedObject.FindProperty("dimensions");
 
