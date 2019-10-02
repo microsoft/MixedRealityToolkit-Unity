7127e0eb1132455260567af685d416351ca19647
Adam Mitchell <adammit@microsoft.com>
2019-10-02 13:15:16 -0700

    Add Documentation button on PressableButton

--- Assets/MixedRealityToolkit.SDK/Inspectors/UX/Interactable/PressableButtonInspector.cs
+++ Assets/MixedRealityToolkit.SDK/Inspectors/UX/Interactable/PressableButtonInspector.cs
@@ -4,6 +4,7 @@
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.UI;
 using Microsoft.MixedReality.Toolkit.Utilities.Editor;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
@@ -258,6 +259,15 @@ namespace Microsoft.MixedReality.Toolkit.Editor
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
+
+            if (target != null)
+            {
+                var helpURL = target.GetType().GetCustomAttribute<HelpURLAttribute>();
+                if (helpURL != null)
+                {
+                    InspectorUIUtility.RenderDocumentationButton(helpURL.URL);
+                }
+            }
 
             // Ensure there is a touchable.
             if (touchable == null)
