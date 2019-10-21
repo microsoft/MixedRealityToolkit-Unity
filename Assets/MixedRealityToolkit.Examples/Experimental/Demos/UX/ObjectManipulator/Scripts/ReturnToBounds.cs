491af1b706ce576fa17c60f09d67d9ea264e5215
Niall Milsom <nimilsom@microsoft.com>
2019-10-21 12:49:45 +0100

    Add sample scene

---
+++ Assets/MixedRealityToolkit.Examples/Experimental/Demos/UX/ObjectManipulator/Scripts/ReturnToBounds.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToBounds : MonoBehaviour
+{
+    [SerializeField]
+    Transform frontBounds = null;
+
+    [SerializeField]
+    Transform backBounds = null;
+
+    [SerializeField]
+    Transform leftBounds = null;
+
+    [SerializeField]
+    Transform rightBounds = null;
+
+    [SerializeField]
+    Transform bottomBounds = null;
+
+    [SerializeField]
+    Transform topBounds = null;
+
+    Vector3 positionAtStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionAtStart = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > rightBounds.position.x || transform.position.x < leftBounds.position.x ||
+            transform.position.y > topBounds.position.y || transform.position.y < bottomBounds.position.y ||
+            transform.position.z > backBounds.position.z || transform.position.z < frontBounds.position.z)
+        {
+            transform.position = positionAtStart;
+        }
+    }
+}
