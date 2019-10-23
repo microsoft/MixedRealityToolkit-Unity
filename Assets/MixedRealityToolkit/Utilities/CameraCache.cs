3a271d2f6a03d7c7dee15122a50375537dd41269
davidkline-ms <david.kline@microsoft.com>
2019-10-23 09:55:08 -0700

    fix playmode tests, add camera provider to profile

--- Assets/MixedRealityToolkit/Utilities/CameraCache.cs
+++ Assets/MixedRealityToolkit/Utilities/CameraCache.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 namespace Microsoft.MixedReality.Toolkit.Utilities
@@ -35,8 +36,28 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
 
                 if (mainCamera == null)
                 {
-                    // If no main camera was found, alert the developer.
-                    Debug.LogError("No main camera found. The Mixed Reality Toolkit requires one camera in the scene to be tagged as \"MainCamera\".");
+                    Debug.LogWarning("No main camera found. The Mixed Reality Toolkit requires one camera in the scene to be tagged as \"MainCamera\".");
+                    Debug.Log("Searching for cameras in the scene.");
+
+                    // If no main camera was found, try to determine one.
+                    Camera[] cameras = GameObject.FindObjectsOfType<Camera>();
+                    switch (cameras.Length)
+                    {
+                        case 0:
+                            Debug.LogWarning("No cameras found. Creating a \"MainCamera\".");
+                            mainCamera = new GameObject("Main Camera", typeof(Camera), typeof(AudioListener)) { tag = "MainCamera" }.GetComponent<Camera>();
+                            break;
+
+                        case 1:
+                            Debug.LogWarning("Camera found. Tagging the camera as \"MainCamera\".");
+                            cameras[0].tag = "MainCamera";
+                            mainCamera = cameras[0];
+                            break;
+
+                        default:
+                            Debug.LogError("More than one camera found. Please tag one of the cameras as \"MainCamera\".");
+                            break;
+                    }
                 }
 
                 // Cache the main camera
