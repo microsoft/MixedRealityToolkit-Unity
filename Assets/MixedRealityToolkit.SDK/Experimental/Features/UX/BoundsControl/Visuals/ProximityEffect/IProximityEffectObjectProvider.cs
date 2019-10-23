926cf8ea2f51e090d56ae04a0b85fa90cd29d8a0
Niall Milsom <nimilsom@microsoft.com>
2019-10-23 17:15:31 +0100

    Code validation fix

--- Assets/MixedRealityToolkit.SDK/Experimental/Features/UX/BoundsControl/Visuals/ProximityEffect/IProximityEffectObjectProvider.cs
+++ Assets/MixedRealityToolkit.SDK/Experimental/Features/UX/BoundsControl/Visuals/ProximityEffect/IProximityEffectObjectProvider.cs
@@ -12,31 +12,26 @@ namespace Microsoft.MixedReality.Toolkit.UI.Experimental
         /// <summary>
         /// Returns true if the provider has any visible objects
         /// </summary>
-        /// <returns></returns>
         bool IsActive();
 
         /// <summary>
         /// Base Material is applied to any proximity scaled object whenever in medium or far/no proximity mode
         /// </summary>
-        /// <returns></returns>
         Material GetBaseMaterial();
 
         /// <summary>
         /// Provides the highlighted material that gets applied to any proximity scaled object in close proximity mode
         /// </summary>
-        /// <returns></returns>
         Material GetHighlightedMaterial();
 
         /// <summary>
         /// Returns the original object size of the provided proximity scaled objects
         /// </summary>
-        /// <returns></returns>
         float GetObjectSize();
 
         /// <summary>
         /// This method allows iterating over the proximity scaled visuals. It should return the gameobject the scaling should be applied to.
         /// </summary>
-        /// <param name="action"></param>
         void ForEachProximityObject(Action<Transform> action);
     }
 }
