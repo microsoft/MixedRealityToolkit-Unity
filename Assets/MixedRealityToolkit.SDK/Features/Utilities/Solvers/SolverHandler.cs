4fc07e13717b8b6c7edafa0cac614fe811be50f3
Troy Ferrell <trferrel@microsoft.com>
2019-10-30 12:52:24 -0700

    Update Assets/MixedRealityToolkit.SDK/Features/Utilities/Solvers/SolverHandler.cs
    Co-Authored-By: Kurtis <kurtie@microsoft.com>

--- Assets/MixedRealityToolkit.SDK/Features/Utilities/Solvers/SolverHandler.cs
+++ Assets/MixedRealityToolkit.SDK/Features/Utilities/Solvers/SolverHandler.cs
@@ -489,7 +489,7 @@ namespace Microsoft.MixedReality.Toolkit.Utilities.Solvers
             // If we were tracking a particular hand, check that our transform is still valid
             // The HandJointService does not destroy it's own hand joint tracked GameObjects even when a hand is no longer tracked
             // Those HandJointService's GameObjects though are the parents of our tracked transform and thus will not be null/destroyed
-            if (TrackedTargetType == TrackedObjectType.HandJoint && currentTrackedHandedness != Handedness.None)
+            if (TrackedTargetType == TrackedObjectType.HandJoint && !currentTrackedHandedness.IsNone())
             {
                 bool trackingLeft = HandJointService.IsHandTracked(Handedness.Left);
                 bool trackingRight = HandJointService.IsHandTracked(Handedness.Right);
