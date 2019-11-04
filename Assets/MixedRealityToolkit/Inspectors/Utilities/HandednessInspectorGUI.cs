9110e805f8c3f24c3b93b1a6469fc715cd58e458
alexees <as@holospaces.com>
2019-11-04 09:28:34 +0100

    Added namespace, added method summary

--- Assets/MixedRealityToolkit/Inspectors/Utilities/HandednessInspectorGUI.cs
+++ Assets/MixedRealityToolkit/Inspectors/Utilities/HandednessInspectorGUI.cs
@@ -1,29 +1,35 @@
-﻿
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
-public static class HandednessInspectorGUI
+namespace Microsoft.MixedReality.Toolkit.Utilities.Editor
 {
-    private static readonly GUIContent[] HandednessSelections =
+    public static class HandednessInspectorGUI
     {
+        private static readonly GUIContent[] HandednessSelections =
+        {
         new GUIContent("Left Hand"),
         new GUIContent("Right Hand"),
         new GUIContent("Both Hands"),
     };
 
-    public static void DrawControllerHandednessDropdown(SerializedProperty controllerHandedness)
-    {
-        var handednessValue = controllerHandedness.intValue - 1;
-
-        // Reset in case it was set to something other than left, right or both.
-        if (handednessValue < 0 || handednessValue > 2) { handednessValue = 0; }
-
-        EditorGUI.BeginChangeCheck();
-        handednessValue = EditorGUILayout.IntPopup(new GUIContent(controllerHandedness.displayName, controllerHandedness.tooltip), handednessValue, HandednessSelections, null);
-
-        if (EditorGUI.EndChangeCheck())
+        /// <summary>
+        /// Responsible for drawing the ControllerHandedness as a dropdown. Resets to None in case it was set to something other than left, right or both
+        /// </summary>
+        /// <param name="controllerHandedness">The SerializedProperty for the the controllerHandedness</param>
+        public static void DrawControllerHandednessDropdown(SerializedProperty controllerHandedness)
         {
-            controllerHandedness.intValue = handednessValue + 1;
+            var handednessValue = controllerHandedness.intValue - 1;
+
+            // Reset in case it was set to something other than left, right or both.
+            if (handednessValue < 0 || handednessValue > 2) { handednessValue = 0; }
+
+            EditorGUI.BeginChangeCheck();
+            handednessValue = EditorGUILayout.IntPopup(new GUIContent(controllerHandedness.displayName, controllerHandedness.tooltip), handednessValue, HandednessSelections, null);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                controllerHandedness.intValue = handednessValue + 1;
+            }
         }
     }
 }
