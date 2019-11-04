3641856595498f22f4d4dfb00097c0c5e3d2101d
gilbdev <gboulard@amiltone.com>
2019-11-04 10:35:28 +0100

    scale theme now relative to gameobject initial scale, like in other themes

--- Assets/MixedRealityToolkit.SDK/Features/UX/Scripts/VisualThemes/ThemeEngines/InteractableScaleTheme.cs
+++ Assets/MixedRealityToolkit.SDK/Features/UX/Scripts/VisualThemes/ThemeEngines/InteractableScaleTheme.cs
@@ -12,6 +12,7 @@ namespace Microsoft.MixedReality.Toolkit.UI
     /// </summary>
     public class InteractableScaleTheme : InteractableThemeBase
     {
+        protected Vector3 startScale;
         private Transform hostTransform;
 
         public InteractableScaleTheme()
@@ -46,6 +47,7 @@ namespace Microsoft.MixedReality.Toolkit.UI
             base.Init(host, settings);
 
             hostTransform = Host.transform;
+            startScale = hostTransform.localScale;
         }
 
         /// <inheritdoc />
@@ -59,7 +61,7 @@ namespace Microsoft.MixedReality.Toolkit.UI
         /// <inheritdoc />
         public override void SetValue(ThemeStateProperty property, int index, float percentage)
         {
-            hostTransform.localScale = Vector3.Lerp(property.StartValue.Vector3, property.Values[index].Vector3, percentage);
+            hostTransform.localScale = Vector3.Lerp(property.StartValue.Vector3, Vector3.Scale(startScale, property.Values[index].Vector3), percentage);
         }
     }
 }
