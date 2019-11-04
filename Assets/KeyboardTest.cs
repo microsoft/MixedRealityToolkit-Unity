44e8bfb31c082a03ad4ad236650a3b5d384d9688
alexees <as@holospaces.com>
2019-11-04 09:12:00 +0100

    import and scripts adjustments

---
+++ Assets/KeyboardTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.MixedReality.Toolkit.Experimental.UI;
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class KeyboardTest : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private Keyboard keyboard = null;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        keyboard.PresentKeyboard();
+
+        keyboard.OnClosed += DisableKeyboard;
+        keyboard.OnTextSubmitted += DisableKeyboard;
+        keyboard.OnTextUpdated += UpdateText;
+    }
+
+    private void UpdateText(string text)
+    {
+        GetComponent<TMP_InputField>().text = text;
+    }
+
+    private void DisableKeyboard(object sender, EventArgs e)
+    {
+        keyboard.Close();
+
+        keyboard.OnTextUpdated -= UpdateText;
+        keyboard.OnClosed -= DisableKeyboard;
+        keyboard.OnTextSubmitted -= DisableKeyboard;
+    }
+}
