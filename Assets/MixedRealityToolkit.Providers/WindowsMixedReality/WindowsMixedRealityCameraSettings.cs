0f6cbc7c3d4aa6be3a91b32e853138a9663e6812
Kurtis <kurtie@microsoft.com>
2019-11-05 14:05:25 -0800

    Add WMR camera settings

---
+++ Assets/MixedRealityToolkit.Providers/WindowsMixedReality/WindowsMixedRealityCameraSettings.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.MixedReality.Toolkit.CameraSystem;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using UnityEngine;
+using UnityEngine.XR.WSA;
+
+namespace Microsoft.MixedReality.Toolkit.WindowsMixedReality
+{
+    /// <summary>
+    /// Camera settings provider for use with the Unity AR Foundation system.
+    /// </summary>
+    [MixedRealityDataProvider(
+        typeof(IMixedRealityCameraSystem),
+        SupportedPlatforms.WindowsUniversal,
+        "Windows Mixed Reality Camera Settings")]
+    public class WindowsMixedRealityCameraSettings : BaseDataProvider, IMixedRealityCameraSettingsProvider
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="cameraSystem">The instance of the camera system which is managing this provider.</param>
+        /// <param name="name">Friendly name of the provider.</param>
+        /// <param name="priority">Provider priority. Used to determine order of instantiation.</param>
+        /// <param name="profile">The provider's configuration profile.</param>
+        public WindowsMixedRealityCameraSettings(
+            IMixedRealityCameraSystem cameraSystem,
+            string name = null,
+            uint priority = DefaultPriority,
+            BaseCameraSettingsProfile profile = null) : base(cameraSystem, name, priority, profile)
+        { }
+
+        #region IMixedRealityCameraSettings
+
+        /// <inheritdoc/>
+        public bool IsOpaque => HolographicSettings.IsDisplayOpaque;
+
+        /// <inheritdoc/>
+        public void ApplyDisplaySettings()
+        {
+            MixedRealityCameraProfile cameraProfile = (Service as IMixedRealityCameraSystem)?.CameraProfile;
+            if (cameraProfile == null) { return; }
+
+            if (IsOpaque)
+            {
+                CameraCache.Main.clearFlags = cameraProfile.CameraClearFlagsOpaqueDisplay;
+                CameraCache.Main.nearClipPlane = cameraProfile.NearClipPlaneOpaqueDisplay;
+                CameraCache.Main.farClipPlane = cameraProfile.FarClipPlaneOpaqueDisplay;
+                CameraCache.Main.backgroundColor = cameraProfile.BackgroundColorOpaqueDisplay;
+                QualitySettings.SetQualityLevel(cameraProfile.OpaqueQualityLevel, false);
+            }
+            else
+            {
+                CameraCache.Main.clearFlags = cameraProfile.CameraClearFlagsTransparentDisplay;
+                CameraCache.Main.backgroundColor = cameraProfile.BackgroundColorTransparentDisplay;
+                CameraCache.Main.nearClipPlane = cameraProfile.NearClipPlaneTransparentDisplay;
+                CameraCache.Main.farClipPlane = cameraProfile.FarClipPlaneTransparentDisplay;
+                QualitySettings.SetQualityLevel(cameraProfile.TransparentQualityLevel, false);
+            }
+        }
+
+        #endregion IMixedRealityCameraSettings
+    }
+}
