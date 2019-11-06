2b8c58632185383b2a6d002b842ad6de3237c766
Alex Gough <algough@microsoft.com>
2019-11-06 14:29:43 -0800

    new hand menu example layouts scene
    - additional hand menu layout prefabs with unique hand constraint tunings
    - added toggle menu to switch between layouts
    - customized slider prefab and script to one of the hand menu layouts
    - edited scene description text
    - polished toggle panel in original Hand Menu Example scene

---
+++ Assets/MixedRealityToolkit.Examples/Demos/HandTracking/Script/UpdateSliderTrackLine.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+
+public class UpdateSliderTrackLine : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject activeLine = null;
+    private float roundedValue;
+
+    public void OnSliderUpdated(SliderEventData eventData)
+    {
+            roundedValue = Mathf.Round(eventData.NewValue * 100f) / 100f;
+            activeLine.transform.localScale = new Vector3 (transform.localScale.x, roundedValue, transform.localScale.z);
+    }
+}
