1aabe46ebe6cb47a4efab6f878630f245a9e682c
Kurtis <kurtie@microsoft.com>
2019-11-14 17:04:37 -0800

    Add documentation

--- Assets/MixedRealityToolkit/Definitions/BoundarySystem/BoundaryType.cs
+++ Assets/MixedRealityToolkit/Definitions/BoundarySystem/BoundaryType.cs
@@ -1,11 +1,20 @@
-﻿// Copyright (c) Microsoft Corporation.
+// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
 namespace Microsoft.MixedReality.Toolkit.Boundary
 {
+    /// <summary>
+    /// Defines different types of boundaries that can be requested.
+    /// </summary>
     public enum BoundaryType
     {
+        /// <summary>
+        /// A rectangular area calculated as the largest rectangle within the tracked area, good for placing content near the user.
+        /// </summary>
         PlayArea,
+        /// <summary>
+        /// The full tracked boundary, typically manually drawn by a user while setting up their device.
+        /// </summary>
         TrackedArea
     }
 }
