0e163c42ce00cfdae2b5f7712ed657aa3e649a06
davidkline-ms <david.kline@microsoft.com>
2019-11-18 14:18:56 -0800

    add assemblyinfo for each asmdef in core and examples

--- Assets/MixedRealityToolkit.Examples/Demos/StandardShader/Scripts/Editor/AssemblyInfo.cs
+++ Assets/MixedRealityToolkit.Examples/Demos/StandardShader/Scripts/Editor/AssemblyInfo.cs
@@ -2,12 +2,9 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("2.2.0.0")]
 [assembly: AssemblyFileVersion("2.2.0.0")]
 
-[assembly: AssemblyProduct("Microsoft® Mixed Reality Toolkit")]
+[assembly: AssemblyProduct("Microsoft® Mixed Reality Toolkit Examples")]
 [assembly: AssemblyCopyright("Copyright © Microsoft Corporation")]
-
-[assembly: InternalsVisibleTo("Microsoft.MixedReality.Toolkit.Gltf.Importers")]
--- Assets/MixedRealityToolkit.Extensions/LostTrackingService/AssemblyInfo.cs
+++ Assets/MixedRealityToolkit.Extensions/LostTrackingService/AssemblyInfo.cs
@@ -2,12 +2,11 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("2.2.0.0")]
 [assembly: AssemblyFileVersion("2.2.0.0")]
 
 [assembly: AssemblyProduct("Microsoft® Mixed Reality Toolkit")]
-[assembly: AssemblyCopyright("Copyright © Microsoft Corporation")]
+[assembly: AssemblyDescription("Microsoft® Mixed Reality Toolkit tracking lost service")]
 
-[assembly: InternalsVisibleTo("Microsoft.MixedReality.Toolkit.Gltf.Importers")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation")]
