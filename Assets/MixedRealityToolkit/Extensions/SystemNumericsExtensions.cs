f0b287f483012a7a68f963e93913480bc68d9ea3
Kurtis <kurtie@microsoft.com>
2019-11-19 17:26:53 -0800

    Create SystemNumericsExtensions

---
+++ Assets/MixedRealityToolkit/Extensions/SystemNumericsExtensions.cs
@@ -0,0 +1,18 @@
+﻿// Copyright(c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.MixedReality.Toolkit
+{
+    public static class SystemNumericsExtensions
+    {
+        public static UnityEngine.Vector3 ToUnityVector3(this System.Numerics.Vector3 vector)
+        {
+            return new UnityEngine.Vector3(vector.X, vector.Y, -vector.Z);
+        }
+
+        public static UnityEngine.Quaternion ToUnityQuaternion(this System.Numerics.Quaternion quaternion)
+        {
+            return new UnityEngine.Quaternion(-quaternion.X, -quaternion.Y, quaternion.Z, quaternion.W);
+        }
+    }
+}
