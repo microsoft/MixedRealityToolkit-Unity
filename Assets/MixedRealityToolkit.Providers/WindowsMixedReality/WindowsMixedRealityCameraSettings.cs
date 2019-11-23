15860faf710f246115d6ae15e5407c5da7148157
Kurtis <kurtie@microsoft.com>
2019-11-22 18:27:13 -0800

    Update WindowsMixedRealityCameraSettings.cs

--- Assets/MixedRealityToolkit.Providers/WindowsMixedReality/WindowsMixedRealityCameraSettings.cs
+++ Assets/MixedRealityToolkit.Providers/WindowsMixedReality/WindowsMixedRealityCameraSettings.cs
@@ -3,13 +3,16 @@
 
 using Microsoft.MixedReality.Toolkit.CameraSystem;
 using Microsoft.MixedReality.Toolkit.Utilities;
+using System.Collections.Generic;
 
 #if UNITY_WSA
 using UnityEngine.XR.WSA;
 #endif // UNITY_WSA
 
 #if WINDOWS_UWP
-using Windows.Graphics.Holographic;
+using Windows.Foundation.Metadata;
+#elif UNITY_WSA && DOTNETWINRT_PRESENT
+using Microsoft.Windows.Foundation.Metadata;
 #endif // WINDOWS_UWP
 
 namespace Microsoft.MixedReality.Toolkit.WindowsMixedReality
@@ -43,6 +46,16 @@ namespace Microsoft.MixedReality.Toolkit.WindowsMixedReality
 
         private WindowsMixedRealityCameraSettingsProfile Profile => ConfigurationProfile as WindowsMixedRealityCameraSettingsProfile;
 
+#if UNITY_WSA && DOTNETWINRT_PRESENT
+        private readonly Dictionary<uint, bool> cameraIdToSupportsAutoPlanar = new Dictionary<uint, bool>();
+
+        private static readonly bool isDepthReprojectionModeSupported = ApiInformation.IsPropertyPresent("Windows.Graphics.Holographic.HolographicCameraRenderingParameters", "DepthReprojectionMethod");
+#endif // (UNITY_WSA && DOTNETWINRT_PRESENT) || WINDOWS_UWP
+
+#if WINDOWS_UWP
+        private static readonly bool isTryGetViewConfigurationSupported = ApiInformation.IsMethodPresent("Windows.Graphics.Holographic.HolographicDisplay", "TryGetViewConfiguration");
+#endif // WINDOWS_UWP
+
         /// <inheritdoc/>
         public override bool IsOpaque =>
 #if UNITY_WSA
@@ -51,24 +64,61 @@ namespace Microsoft.MixedReality.Toolkit.WindowsMixedReality
             false;
 #endif
 
-#if WINDOWS_UWP
         public override void ApplyConfiguration()
         {
             base.ApplyConfiguration();
 
-            if (Profile != null &&
-                Profile.RenderFromPVCameraForMixedRealityCapture &&
-                global::Windows.Foundation.Metadata.ApiInformation.IsMethodPresent("Windows.Graphics.Holographic.HolographicDisplay", "TryGetViewConfiguration"))
+#if WINDOWS_UWP
+            if (Profile != null
+                && Profile.RenderFromPVCameraForMixedRealityCapture
+                && isTryGetViewConfigurationSupported)
             {
                 // If the default display has configuration for a PhotoVideoCamera, we want to enable it
-                HolographicViewConfiguration viewConfiguration = HolographicDisplay.GetDefault()?.TryGetViewConfiguration(HolographicViewConfigurationKind.PhotoVideoCamera);
+                global::Windows.Graphics.Holographic.HolographicViewConfiguration viewConfiguration = global::Windows.Graphics.Holographic.HolographicDisplay.GetDefault()?.TryGetViewConfiguration(global::Windows.Graphics.Holographic.HolographicViewConfigurationKind.PhotoVideoCamera);
                 if (viewConfiguration != null)
                 {
                     viewConfiguration.IsEnabled = true;
                 }
             }
-        }
 #endif // WINDOWS_UWP
+
+#if UNITY_WSA && DOTNETWINRT_PRESENT
+            if (Profile != null
+                && Profile.ReprojectionMethod == HolographicDepthReprojectionMethod.AutoPlanar
+                && isDepthReprojectionModeSupported)
+            {
+                Microsoft.Windows.Graphics.Holographic.HolographicFrame frame = Input.WindowsMixedRealityUtilities.CurrentHolographicFrame;
+                foreach (var cameraPose in frame?.CurrentPrediction.CameraPoses)
+                {
+                    if (CameraSupportsAutoPlanar(cameraPose.HolographicCamera))
+                    {
+                        Microsoft.Windows.Graphics.Holographic.HolographicCameraRenderingParameters renderingParams = frame.GetRenderingParameters(cameraPose);
+                        renderingParams.DepthReprojectionMethod = Microsoft.Windows.Graphics.Holographic.HolographicDepthReprojectionMethod.AutoPlanar;
+                    }
+                }
+            }
+#endif // UNITY_WSA && DOTNETWINRT_PRESENT
+        }
+
+#if UNITY_WSA && DOTNETWINRT_PRESENT
+        private bool CameraSupportsAutoPlanar(Microsoft.Windows.Graphics.Holographic.HolographicCamera camera)
+        {
+            bool supportsAutoPlanar;
+            if (!cameraIdToSupportsAutoPlanar.TryGetValue(camera.Id, out supportsAutoPlanar))
+            {
+                foreach (var method in camera.ViewConfiguration.SupportedDepthReprojectionMethods)
+                {
+                    if (method == Microsoft.Windows.Graphics.Holographic.HolographicDepthReprojectionMethod.AutoPlanar)
+                    {
+                        supportsAutoPlanar = true;
+                        break;
+                    }
+                }
+                cameraIdToSupportsAutoPlanar.Add(camera.Id, supportsAutoPlanar);
+            }
+            return supportsAutoPlanar;
+        }
+#endif // UNITY_WSA && DOTNETWINRT_PRESENT
 
         #endregion IMixedRealityCameraSettings
     }
