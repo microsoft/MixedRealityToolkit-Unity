505a43f8ec4e876549028448c96c663380d74bc7
davidkline-ms <david.kline@microsoft.com>
2019-12-13 16:13:08 -0800

    replace asmdef swapping with modification

--- Assets/MixedRealityToolkit.Staging/UnityAR/Editor/ConfigurationChecker/UnityARConfigurationChecker.cs
+++ Assets/MixedRealityToolkit.Staging/UnityAR/Editor/ConfigurationChecker/UnityARConfigurationChecker.cs
@@ -5,6 +5,7 @@ using Microsoft.MixedReality.Toolkit.Utilities.Editor;
 using System.IO;
 using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
 
 namespace Microsoft.MixedReality.Toolkit.Experimental.UnityAR
 {
@@ -16,32 +17,7 @@ namespace Microsoft.MixedReality.Toolkit.Experimental.UnityAR
     {
         static UnityARConfigurationChecker()
         {
-            string asmDefFileName = "Microsoft.MixedReality.Toolkit.Providers.UnityAR.asmdef";
-            FileInfo[] asmDefFiles = FileUtilities.FindFilesInAssets(asmDefFileName);
-
-            if (EnsureArFoundationDefine())
-            {
-               if (asmDefFiles.Length > 1)
-               {
-                   Debug.LogWarning($"More than one copy of {asmDefFileName} found in the project. The first instance will be updated.");
-               }
-
-               // Ensure we have the correct asmdef files for the current version of Unity.
-               FileInfo newAsmDefFile = GetNewAsmDefFile(asmDefFiles[0]);
-               if (newAsmDefFile != null)
-               {
-                   File.Copy(newAsmDefFile.FullName, asmDefFiles[0].FullName, true);
-               }
-
-            }
-            else
-            {
-               // Remove the assembly definition files, if present.
-               if ( (asmDefFiles.Length != 0) && asmDefFiles[0].Exists)
-               {
-                   File.Copy(asmDefFiles[0].FullName, $"{asmDefFiles[0]}.norefs");
-               }
-            }
+            UpdateAsmDef(EnsureArFoundationDefine());
         }
 
         /// <summary>
@@ -69,43 +45,120 @@ namespace Microsoft.MixedReality.Toolkit.Experimental.UnityAR
         }
 
         /// <summary>
-        /// Determines if a file update is needed and returns the replacement.
+        /// Updates the assembly definition to contain the appropriate references based on the Unity
+        /// version and if the project contains the AR Foundation package.
         /// </summary>
-        /// <param name="file">The reference file used to determine the new file.</param>
-        /// <returns>FileInfo object representing the file, or null if a new file is not needed.</returns>
-        private static FileInfo GetNewAsmDefFile(FileInfo file)
+        private static void UpdateAsmDef(bool arFoundationPresent)
         {
-            // Read the file.
-            string fileContents = string.Empty;
-            using (FileStream fs = new FileStream(file.FullName, FileMode.Open, FileAccess.Read))
+            string asmDefFileName = "Microsoft.MixedReality.Toolkit.Providers.UnityAR.asmdef";
+            FileInfo[] asmDefFiles = FileUtilities.FindFilesInAssets(asmDefFileName);
+
+            if (asmDefFiles.Length == 0)
             {
-                using (StreamReader reader = new StreamReader(fs))
+                Debug.LogWarning($"Unable to locate file: {asmDefFileName}");
+                return;
+            }
+
+            AssemblyDefinition asmDef = AssemblyDefinition.Load(asmDefFiles[0].FullName);
+            if (asmDef == null)
+            {
+                Debug.LogWarning($"Unable to load file: {asmDefFileName}");
+                return;
+            }
+
+            List<string> references = new List<string>();
+            if (asmDef.References != null)
+            {
+                references.AddRange(asmDef.References);
+            }
+
+            // Assemblies required by Unity 2018 and / or 2019 (and newer)
+            const string arFoundationReference = "Unity.XR.ARFoundation";
+            const string spatialTrackingReference = "UnityEngine.SpatialTracking";
+            bool changed = false;
+
+            if (arFoundationPresent)
+            {
+#if UNITY_2018 || UNITY_2019_1_OR_NEWER
+                if (!references.Contains(arFoundationReference))
                 {
-                    fileContents = reader.ReadToEnd();
+                    references.Add(arFoundationReference);
+                    changed = true; 
+                }
+#endif
+#if UNITY_2019_1_OR_NEWER
+                if (!references.Contains(spatialTrackingReference))
+                {
+                    references.Add(spatialTrackingReference);
+                    changed = true;
+                }
+#elif UNITY_2018
+                if (references.Contains(spatialTrackingReference))
+                {
+                    references.Remove(spatialTrackingReference);
+                    changed = true;
+                }
+#endif
+            }
+            else
+            {
+                if (references.Contains(arFoundationReference))
+                {
+                    references.Remove(arFoundationReference);
+                    changed = true;
+                }
+                if (references.Contains(spatialTrackingReference))
+                {
+                    references.Remove(spatialTrackingReference);
+                    changed = true;
                 }
             }
 
-            // Key values used to determine if a replacement is required.
-            const string arFoundationReference = "Unity.XR.ARFoundation";
-            const string spatialTrackingReference = "UnityEngine.SpatialTracking";
-
-            string updateTo = string.Empty;
-#if UNITY_2018
-            if (!fileContents.Contains(arFoundationReference) ||
-                fileContents.Contains(spatialTrackingReference))
+            if (changed)
             {
-                updateTo = "2018";
+                asmDef.References = references.ToArray();
+                asmDef.Save(asmDefFiles[0].FullName);
             }
-#elif UNITY_2019_2_OR_NEWER
-            if (!fileContents.Contains(arFoundationReference) ||
-                !fileContents.Contains(spatialTrackingReference))
-            {
-                updateTo = "2019";
-            }
-#endif
-
-            FileInfo fi = new FileInfo(Path.Combine(file.DirectoryName, $"{file.Name}.{updateTo}"));
-            return fi.Exists ? fi : null;
         }
+
+//        /// <summary>
+//        /// Determines if a file update is needed and returns the replacement.
+//        /// </summary>
+//        /// <param name="file">The reference file used to determine the new file.</param>
+//        /// <returns>FileInfo object representing the file, or null if a new file is not needed.</returns>
+//        private static FileInfo GetNewAsmDefFile(FileInfo file)
+//        {
+//            // Read the file.
+//            string fileContents = string.Empty;
+//            using (FileStream fs = new FileStream(file.FullName, FileMode.Open, FileAccess.Read))
+//            {
+//                using (StreamReader reader = new StreamReader(fs))
+//                {
+//                    fileContents = reader.ReadToEnd();
+//                }
+//            }
+
+//            // Key values used to determine if a replacement is required.
+//            const string arFoundationReference = "Unity.XR.ARFoundation";
+//            const string spatialTrackingReference = "UnityEngine.SpatialTracking";
+
+//            string updateTo = string.Empty;
+//#if UNITY_2018
+//            if (!fileContents.Contains(arFoundationReference) ||
+//                fileContents.Contains(spatialTrackingReference))
+//            {
+//                updateTo = "2018";
+//            }
+//#elif UNITY_2019_2_OR_NEWER
+//            if (!fileContents.Contains(arFoundationReference) ||
+//                !fileContents.Contains(spatialTrackingReference))
+//            {
+//                updateTo = "2019";
+//            }
+//#endif
+
+//            FileInfo fi = new FileInfo(Path.Combine(file.DirectoryName, $"{file.Name}.{updateTo}"));
+//            return fi.Exists ? fi : null;
+//        }
     }
 }
