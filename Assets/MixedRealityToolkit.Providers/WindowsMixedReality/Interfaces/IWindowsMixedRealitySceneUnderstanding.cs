940814b2435bc74cc245567fd9230cb360be5636
Pink Bonnstetter <pinkb@microsoft.com>
2019-12-16 10:50:36 -0800

    first commit!

---
+++ Assets/MixedRealityToolkit.Providers/WindowsMixedReality/Interfaces/IWindowsMixedRealitySceneUnderstanding.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using UnityEngine;
+
+public interface IWindowsMixedRealitySceneUnderstanding
+{
+    bool TryGetPlaneValidationMask(SpatialAwarenessSceneObject.Quad quad, ushort textureWidth, ushort textureHeight, out byte[] mask);
+    bool TryGetBestPlacementPosition(SpatialAwarenessSceneObject.Quad quad, Vector2 objExtents, out Vector2 placementPosOnPlane);
+}
