4949865764ec0f9918a339d35e97e6fd36dfb483
Francesco Clasadonte <klasaf@outlook.com>
2019-12-18 14:38:05 +0100

    Moved the DialogButtonType enum into its own file.

---
+++ Assets/MixedRealityToolkit.SDK/Experimental/Dialog/Scripts/DialogButtonType.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Experimental.Dialog
+{
+    /// <summary>
+    /// Enum describing the style (caption) of button on a Dialog.
+    /// </summary>
+    public enum DialogButtonType
+    {
+        None = 0,
+        Close = 1,
+        Confirm = 2,
+        Cancel = 4,
+        Accept = 8,
+        Yes = 16,
+        No = 32,
+        OK = 64
+    }
+}
