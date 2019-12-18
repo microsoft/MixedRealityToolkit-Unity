6e9602dda10cd398bea4259d4953f9582bb0b6bf
Francesco Clasadonte <klasaf@outlook.com>
2019-12-18 14:38:22 +0100

    Moved the DialogState enum into its own file.

---
+++ Assets/MixedRealityToolkit.SDK/Experimental/Dialog/Scripts/DialogState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Experimental.Dialog
+{
+    /// <summary>
+    /// Enum that describes the current state of a Dialog.
+    /// </summary>
+    public enum DialogState
+    {
+        Uninitialized = 0,
+        Opening,
+        WaitingForInput,
+        InputReceived,
+        Closing,
+        Closed,
+    }
+}
