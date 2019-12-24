dfe0d48c3fbadc23e9de176a756f33d58b7d62e1
Troy Ferrell <trferrel@microsoft.com>
2019-12-24 12:23:22 -0700

    Update Assets/MixedRealityToolkit/Interfaces/InputSystem/IMixedRealityPointer.cs
    Co-Authored-By: Julia Schwarz <julenka.schwarz@gmail.com>

--- Assets/MixedRealityToolkit/Interfaces/InputSystem/IMixedRealityPointer.cs
+++ Assets/MixedRealityToolkit/Interfaces/InputSystem/IMixedRealityPointer.cs
@@ -127,7 +127,7 @@ namespace Microsoft.MixedReality.Toolkit.Input
         void OnPreCurrentPointerTargetChange();
 
         /// <summary>
-        /// Resets pointer to initial state. After invoked pointer should be functional and ready for re-use
+        /// Resets pointer to initial state. After invoked pointer should be functional and ready for re-use.
         /// </summary>
         /// <remarks>
         /// Useful for caching and recycling of pointers
