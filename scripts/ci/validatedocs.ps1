880f085653ffa3b6e28ad197f755e4f0dd8af9e2
Will Wei <wiwei@microsoft.com>
2020-01-09 09:22:50 -0800

    Clean up some of the code to match poshstyle, and also add some tracking TODOs for powershell module imports

--- scripts/ci/validatedocs.ps1
+++ scripts/ci/validatedocs.ps1
@@ -9,83 +9,92 @@
       and are instead relative links. Relative links can be resolved and validated by docfx, and fully resolved links cannot.
 
     Returns 0 if there are no issues, non-zero if there are.
-.PARAMETER Directory
-    The directory containing the docs to validate. This is the fallback if
-    ChangesFile doesn't exist or isn't valid, and as a result this is
-    always required.
-.PARAMETER ChangesFile
-    The filename containing the list of files to scope the code validation
-    to. This is useful in pull request validation when there isn't a need
-    to check every single file in the repo for changes (i.e. only the list
-    of changed files)
-
-    If ChangesFile doesn't exist (i.e. not specified, null, is specified but
-    the actual file doesn't exist), then this defaults to checking for everything
-    in the repo.
-.PARAMETER RepoRoot
-    The directory containing the repo root. Used in conjunction with ChangesFile
 .EXAMPLE
     .\validatedocs.ps1 -Directory c:\path\to\MRTK\Documentation
 #>
 param(
+    # The directory containing the docs to validate. This is the fallback if
+    # ChangesFile doesn't exist or isn't valid, and as a result this is
+    # always required.
     [Parameter(Mandatory=$true)]
     [string]$Directory,
+
+    # The filename containing the list of files to scope the code validation
+    # to. This is useful in pull request validation when there isn't a need
+    # to check every single file in the repo for changes (i.e. only the list
+    # of changed files)
+    #
+    # If ChangesFile doesn't exist (i.e. not specified, null, is specified but
+    # the actual file doesn't exist), then this defaults to checking for everything
+    # in the repo.
     [string]$ChangesFile,
+
+    # The directory containing the repo root. Used in conjunction with ChangesFile
     [string]$RepoRoot
 )
 
-function CheckDocLinks(
-    [string]$FileName,
-    [string[]]$FileContent,
-    [int]$LineNumber
-) {
-    <#
-    .SYNOPSIS
-        Checks if the given file at the given line contains a fully resolved documentation
-        link. Returns true if found.
-    #>
-    if ($FileContent[$LineNumber] -match "https://microsoft.github.io/MixedRealityToolkit-Unity/Documentation") {
-        Write-Debug "An non-relative doc link was found in $FileName at line $LineNumber "
-        Write-Debug "Avoid doc links containing https://microsoft.github.io/MixedRealityToolkit-Unity/Documentation "
-        Write-Debug "and use relative links instead."
-        return $true;
-    }
-    return $false
-}
-
-function CheckDocument(
-    [string]$FileName
-) {
-    # Each line of each script is checked by all of the validators above - this ensures that in
-    # a single pass, we'll get all of the issues highlighted all at once, rather than
-    # repeatedly running this script, discovering a single issue, fixing it, and then
-    # re-running the script
-    $containsIssue = $false
-    $fileContent = Get-Content $FileName
-    for ($i = 0; $i -lt $fileContent.Length; $i++) {
-        if (CheckDocLinks $FileName $fileContent $i) {
-            $containsIssue = $true
+<#
+.SYNOPSIS
+    Checks if the given file at the given line contains a fully resolved documentation
+    link. Returns true if found.
+#>
+function CheckDocLinks {
+    [CmdletBinding()]
+    param(
+        [string]$FileName,
+        [string[]]$FileContent,
+        [int]$LineNumber
+    )
+    process {
+        if ($FileContent[$LineNumber] -match "https://microsoft.github.io/MixedRealityToolkit-Unity/Documentation") {
+            Write-Host "An non-relative doc link was found in $FileName at line $LineNumber "
+            Write-Host "Avoid doc links containing https://microsoft.github.io/MixedRealityToolkit-Unity/Documentation "
+            Write-Host "and use relative links instead."
+            $true;
         }
+        $false
     }
-    return $containsIssue
 }
 
-if ($ChangesFile -and Test-Path $Output -PathType leaf) {
+function CheckDocument {
+    [CmdletBinding()]
+    param(
+        [string]$FileName
+    )
+    process {
+        # Each line of each script is checked by all of the validators above - this ensures that in
+        # a single pass, we'll get all of the issues highlighted all at once, rather than
+        # repeatedly running this script, discovering a single issue, fixing it, and then
+        # re-running the script
+        $containsIssue = $false
+        $fileContent = Get-Content $FileName
+        for ($i = 0; $i -lt $fileContent.Length; $i++) {
+            if (CheckDocLinks $FileName $fileContent $i) {
+                $containsIssue = $true
+            }
+        }
+        $containsIssue
+    }
+}
 
+# If the file containing the list of changes was provided and actually exists,
+# this validation should scope to only those changed files.
+if ($ChangesFile -and Test-Path $Output -PathType leaf) {
+    # TODO(https://github.com/microsoft/MixedRealityToolkit-Unity/issues/7022)
+    # There may be ways to configure common modules so that paths like this aren't required
     Import-Module (Resolve-Path("$RepoRoot\scripts\ci\common.psm1"))
 
-    Write-Debug "Checking only changed files for doc issues:"
+    Write-Host "Checking only changed files for doc issues:"
     $changedFiles = GetChangedFiles -Filename $ChangesFile -RepoRoot $RepoRoot
 
     foreach ($changedFile in $changedFiles) {
-        Write-Debug "Checking $changedFile"
-        if (IsMarkdown -Filename $changedFile -and CheckDocument $changedFile) {
-            $containsIssue= true;
+        if ((IsMarkdown -Filename $changedFile) -and (CheckDocument $changedFile)) {
+            $containsIssue = true;
         }
     }
 }
 else {
-    Write-Debug "Checking $Directory for common doc issues"
+    Write-Host "Checking $Directory for common doc issues"
 
     $docFiles = Get-ChildItem $Directory *.md -Recurse | Select-Object FullName
     $containsIssue = $false
@@ -97,10 +106,10 @@ else {
 }
 
 if ($containsIssue) {
-    Write-Debug "Issues found, please see above for details"
+    Write-Output "Issues found, please see above for details"
     exit 1;
 }
 else {
-    Write-Debug "No issues found"
+    Write-Output "No issues found"
     exit 0;
 }
