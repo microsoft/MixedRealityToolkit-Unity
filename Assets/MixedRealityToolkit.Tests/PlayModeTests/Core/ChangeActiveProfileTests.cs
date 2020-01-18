7d07d254a092eba9166705db7e343f858f6a9293
davidkline-ms <david.kline@microsoft.com>
2020-01-17 16:49:41 -0800

    fix some small bugs in boundary, new test (and cleanup) for profile change

--- Assets/MixedRealityToolkit.Tests/PlayModeTests/Core/ChangeActiveProfileTests.cs
+++ Assets/MixedRealityToolkit.Tests/PlayModeTests/Core/ChangeActiveProfileTests.cs
@@ -10,10 +10,13 @@
 // issue will likely persist for 2018, this issue is worked around by wrapping all
 // play mode tests in this check.
 
+using Microsoft.MixedReality.Toolkit.Boundary;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Utilities;
 using NUnit.Framework;
 using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -24,15 +27,6 @@ namespace Microsoft.MixedReality.Toolkit.Tests
     // the MRTK being correctly reconfigured.
     public class ChangeActiveProfileTests
     {
-        //[SetUp]
-        //public void SetUp()
-        //{
-        //    TestUtilities.InitializeMixedRealityToolkit(true);
-        //    TestUtilities.PlayspaceToOriginLookingForward();
-
-        //    // todo - load scene
-        //}
-
         [TearDown]
         public void TearDown()
         {
@@ -42,13 +36,12 @@ namespace Microsoft.MixedReality.Toolkit.Tests
         }
 
         private const string CameraInputDiagsProfile = "Assets/MixedRealityToolkit.Tests/PlayModeTests/Core/TestProfiles/CameraInputDiags.asset";
+        private const string BoundaryOnlyProfile = "Assets/MixedRealityToolkit.Tests/PlayModeTests/Core/TestProfiles/BoundaryOnly.asset";
 
         [UnityTest]
-        public IEnumerator VerifyServiceCount_ToFewer()
+        public IEnumerator VerifyServiceCount()
         {
-            
             MixedRealityToolkitConfigurationProfile profile1 = TestUtilities.GetDefaultMixedRealityProfile<MixedRealityToolkitConfigurationProfile>();
-            MixedRealityToolkitConfigurationProfile profile2 = LoadTestProfile(CameraInputDiagsProfile);
 
             yield return null;
 
@@ -56,40 +49,79 @@ namespace Microsoft.MixedReality.Toolkit.Tests
             InitializeTest(profile1);
 
             // Get count of registered services
-            int count1 = MixedRealityServiceRegistry.GetAllServices().Count;
+            IReadOnlyList<IMixedRealityService> services = MixedRealityServiceRegistry.GetAllServices();
+            int count1 = services.Count;
+            Debug.Log($"{profile1.name} loaded {count1} services.");
+            foreach (IMixedRealityService s in services)
+            {
+                Debug.Log(s.Name);
+            }
+
+            yield return null;
 
             // Switch to profile 2
+            MixedRealityToolkitConfigurationProfile profile2 = LoadTestProfile(CameraInputDiagsProfile);
             ChangeProfile(profile2);
 
-            // Get count of registered services
-            int count2 = MixedRealityServiceRegistry.GetAllServices().Count;
+            yield return null;
 
-            // There should be fewer services registered
-            Assert.IsTrue(count1 > count2);
+            // Get count of registered services
+            services = MixedRealityServiceRegistry.GetAllServices();
+            int count2 = services.Count;
+            Debug.Log($"{profile2.name} loaded {count2} services.");
+            foreach (IMixedRealityService s in services)
+            {
+                Debug.Log(s.Name);
+            }
+
+            // Compare the service counts
+            Assert.IsTrue(count1 != count2);
         }
 
         [UnityTest]
-        public IEnumerator VerifyServiceCount_ToMore()
+        public IEnumerator VerifyServiceState()
         {
-            MixedRealityToolkitConfigurationProfile profile1 = LoadTestProfile(CameraInputDiagsProfile);
-            MixedRealityToolkitConfigurationProfile profile2 = TestUtilities.GetDefaultMixedRealityProfile<MixedRealityToolkitConfigurationProfile>();
+            MixedRealityToolkitConfigurationProfile profile1 = TestUtilities.GetDefaultMixedRealityProfile<MixedRealityToolkitConfigurationProfile>();
 
             yield return null;
 
             // Initialize the test case with profile 1
             InitializeTest(profile1);
 
-            // Get count of registered services
-            int count1 = MixedRealityServiceRegistry.GetAllServices().Count;
+            // Cache the interesting settings read from the initial boundary system instance
+            IMixedRealityBoundarySystem boundarySystem1 = null;
+            MixedRealityServiceRegistry.TryGetService<IMixedRealityBoundarySystem>(out boundarySystem1);
+
+            yield return null;
+
+            MixedRealityToolkitConfigurationProfile profile2 = LoadTestProfile(BoundaryOnlyProfile);
 
             // Switch to profile 2
             ChangeProfile(profile2);
 
-            // Get count of registered services
-            int count2 = MixedRealityServiceRegistry.GetAllServices().Count;
+            for (int i = 0; i < 300; i++)
+            {
+                yield return null;
+            }
 
-            // There should be more services registered
-            Assert.IsTrue(count1 < count2);
+            // The custom boundary profile has been configured to match the default with the following fields being different
+            // * Boundary height
+            // * Floor plane physics layer
+            // * Show tracked area
+            // * Show boundary ceiling
+            IMixedRealityBoundarySystem boundarySystem2 = null;
+            MixedRealityServiceRegistry.TryGetService<IMixedRealityBoundarySystem>(out boundarySystem2);
+
+            Debug.Log($"Boundary height: 1) {boundarySystem1.BoundaryHeight} 2) {boundarySystem2.BoundaryHeight}");
+            Debug.Log($"Floor layer: 1) {boundarySystem1.FloorPhysicsLayer} 2) {boundarySystem2.FloorPhysicsLayer}");
+            Debug.Log($"Show tracked area: 1) {boundarySystem1.ShowTrackedArea} 2) {boundarySystem2.ShowTrackedArea}");
+            Debug.Log($"Show Ceiling: 1) {boundarySystem1.ShowBoundaryCeiling} 2) {boundarySystem2.ShowBoundaryCeiling}");
+
+            // Check service settings to ensure it has properly reset
+            Assert.IsTrue(boundarySystem1.BoundaryHeight != boundarySystem2.BoundaryHeight);
+            Assert.IsTrue(boundarySystem1.FloorPhysicsLayer != boundarySystem2.FloorPhysicsLayer);
+            Assert.IsTrue(boundarySystem1.ShowTrackedArea != boundarySystem2.ShowTrackedArea);
+            Assert.IsTrue(boundarySystem1.ShowBoundaryCeiling != boundarySystem2.ShowBoundaryCeiling);
         }
 
         private void InitializeTest(MixedRealityToolkitConfigurationProfile profile)
