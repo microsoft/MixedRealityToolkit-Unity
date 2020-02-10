766e255f9ed86308fa2fe95f47f584b260841c02
Lars Simkins <lars.simkins@gmail.com>
2020-02-10 10:20:39 -0800

    Updated comments

--- Assets/MixedRealityToolkit.SDK/Features/UX/Scripts/Buttons/ButtonConfigHelper.cs
+++ Assets/MixedRealityToolkit.SDK/Features/UX/Scripts/Buttons/ButtonConfigHelper.cs
@@ -174,7 +174,7 @@ namespace Microsoft.MixedReality.Toolkit.UI
         /// Searches the icon set for a character matching newIconCharName.
         /// If no icon set is avaiable, or if no texture with that name is found, no action is take.
         /// </summary>
-        /// <param name="newIconTextureName"></param>
+        /// <param name="newIconCharName">Name of the new icon character as defined in the IconSet.</param>
         public void SetCharIconByName(string newIconCharName)
         {
             if (string.IsNullOrEmpty(newIconCharName))
@@ -203,7 +203,7 @@ namespace Microsoft.MixedReality.Toolkit.UI
         /// Searches the icon set for a texture matching newIconTextureName.
         /// If no icon set is avaiable, or if no texture with that name is found, no action is take.
         /// </summary>
-        /// <param name="newIconTextureName"></param>
+        /// <param name="newIconTextureName">Name of the new icon texture asset.</param>
         public void SetQuadIconByName(string newIconTextureName)
         {
             if (string.IsNullOrEmpty(newIconTextureName))
@@ -232,7 +232,7 @@ namespace Microsoft.MixedReality.Toolkit.UI
         /// Searches the icon set for a texture matching newIconSpriteName.
         /// If no icon set is avaiable, or if no texture with that name is found, no action is take.
         /// </summary>
-        /// <param name="newIconTextureName"></param>
+        /// <param name="newIconTextureName">Name of the new icon texture asset.</param>
         public void SetSpriteIconByName(string newIconSpriteName)
         {
             if (string.IsNullOrEmpty(newIconSpriteName))
