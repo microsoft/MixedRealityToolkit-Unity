73b113253e580d37f764c7b190583245c00316e5
davidkline-ms <david.kline@microsoft.com>
2020-02-24 06:35:29 -0800

    mark WindowsApiChecker (and members) as obsolete

--- Assets/MixedRealityToolkit/Utilities/WindowsApiChecker.cs
+++ Assets/MixedRealityToolkit/Utilities/WindowsApiChecker.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
 using UnityEngine;
 
 #if WINDOWS_UWP
@@ -14,6 +15,7 @@ namespace Microsoft.MixedReality.Toolkit.Windows.Utilities
     /// </summary>
     /// <remarks> See https://docs.microsoft.com/uwp/extension-sdks/windows-universal-sdk
     /// for a full list of contracts.</remarks>
+    [Obsolete("The WindowsApiChecker class is obsolete and will be removed from a future version of MRTK. Please use ApiInformation.IsMethodPresent().")]
     public static class WindowsApiChecker
     {
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
@@ -35,35 +37,41 @@ namespace Microsoft.MixedReality.Toolkit.Windows.Utilities
             UniversalApiContractV3_IsAvailable = false;
 #endif
         }
-		
+
         /// <summary>
         /// Is the Universal API Contract v8.0 Available?
         /// </summary>
+        [Obsolete("The UniversalApiContractV8_IsAvailable property is obsolete and will be removed from a future version of MRTK. Please use ApiInformation.IsMethodPresent().")]
         public static bool UniversalApiContractV8_IsAvailable { get; private set; }
-        
+
         /// <summary>
         /// Is the Universal API Contract v7.0 Available?
         /// </summary>
+        [Obsolete("The UniversalApiContractV7_IsAvailable property is obsolete and will be removed from a future version of MRTK. Please use ApiInformation.IsMethodPresent().")]
         public static bool UniversalApiContractV7_IsAvailable { get; private set; }
 
         /// <summary>
         /// Is the Universal API Contract v6.0 Available?
         /// </summary>
+        [Obsolete("The UniversalApiContractV6_IsAvailable property is obsolete and will be removed from a future version of MRTK. Please use ApiInformation.IsMethodPresent().")]
         public static bool UniversalApiContractV6_IsAvailable { get; private set; }
 
         /// <summary>
         /// Is the Universal API Contract v5.0 Available?
         /// </summary>
+        [Obsolete("The UniversalApiContractV5_IsAvailable property is obsolete and will be removed from a future version of MRTK. Please use ApiInformation.IsMethodPresent().")]
         public static bool UniversalApiContractV5_IsAvailable { get; private set; }
 
         /// <summary>
         /// Is the Universal API Contract v4.0 Available?
         /// </summary>
+        [Obsolete("The UniversalApiContractV4_IsAvailable property is obsolete and will be removed from a future version of MRTK. Please use ApiInformation.IsMethodPresent().")]
         public static bool UniversalApiContractV4_IsAvailable { get; private set; }
 
         /// <summary>
         /// Is the Universal API Contract v3.0 Available?
         /// </summary>
+        [Obsolete("The UniversalApiContractV3_IsAvailable property is obsolete and will be removed from a future version of MRTK. Please use ApiInformation.IsMethodPresent().")]
         public static bool UniversalApiContractV3_IsAvailable { get; private set; }
     }
 }
