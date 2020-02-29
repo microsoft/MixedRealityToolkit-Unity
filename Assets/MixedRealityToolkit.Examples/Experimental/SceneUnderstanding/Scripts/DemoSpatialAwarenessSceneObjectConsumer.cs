f4536526ba05222542e2ba6f8a50a890dc50c523
Yoon Park <cre8ive@outlook.com>
2020-02-28 17:36:07 -0800

    Adding namesapce.

--- Assets/MixedRealityToolkit.Examples/Experimental/SceneUnderstanding/Scripts/DemoSpatialAwarenessSceneObjectConsumer.cs
+++ Assets/MixedRealityToolkit.Examples/Experimental/SceneUnderstanding/Scripts/DemoSpatialAwarenessSceneObjectConsumer.cs
@@ -5,39 +5,42 @@ using Microsoft.MixedReality.Toolkit.Physics;
 using Microsoft.MixedReality.Toolkit.SpatialAwareness;
 using UnityEngine;
 
-public class DemoSpatialAwarenessSceneObjectConsumer : MonoBehaviour, ISpatialAwarenessSceneObjectConsumer
+namespace Microsoft.MixedReality.Toolkit.Experimental.Examples
 {
-    [PhysicsLayer]
-    public int DefaultLayer;
-    public string DefaultTag;
-
-    [PhysicsLayer]
-    public int WallLayer;
-    public string WallTag;
-
-    public void OnSpatialAwarenessSceneObjectCreated(SpatialAwarenessSceneObject sceneObject)
+    public class DemoSpatialAwarenessSceneObjectConsumer : MonoBehaviour, ISpatialAwarenessSceneObjectConsumer
     {
-        switch (sceneObject.SurfaceType)
-        {
-            case SpatialAwarenessSurfaceTypes.Wall:
-                gameObject.tag = WallTag;
-                gameObject.layer = WallLayer;
-                break;
-            default:
-                gameObject.tag = DefaultTag;
-                gameObject.layer = DefaultLayer;
-                break;
-        }
+        [PhysicsLayer]
+        public int DefaultLayer;
+        public string DefaultTag;
 
-        if (sceneObject.Quads.Count > 0)
+        [PhysicsLayer]
+        public int WallLayer;
+        public string WallTag;
+
+        public void OnSpatialAwarenessSceneObjectCreated(SpatialAwarenessSceneObject sceneObject)
         {
-            float sx = sceneObject.Quads[0].extents.x;
-            float sy = sceneObject.Quads[0].extents.y;
-            transform.localScale = new Vector3(sx, sy, .1f);
-        }
-        else
-        {
-            Debug.LogWarning("No quad data found - did you request it from the observer?");
+            switch (sceneObject.SurfaceType)
+            {
+                case SpatialAwarenessSurfaceTypes.Wall:
+                    gameObject.tag = WallTag;
+                    gameObject.layer = WallLayer;
+                    break;
+                default:
+                    gameObject.tag = DefaultTag;
+                    gameObject.layer = DefaultLayer;
+                    break;
+            }
+
+            if (sceneObject.Quads.Count > 0)
+            {
+                float sx = sceneObject.Quads[0].extents.x;
+                float sy = sceneObject.Quads[0].extents.y;
+                transform.localScale = new Vector3(sx, sy, .1f);
+            }
+            else
+            {
+                Debug.LogWarning("No quad data found - did you request it from the observer?");
+            }
         }
     }
 }
