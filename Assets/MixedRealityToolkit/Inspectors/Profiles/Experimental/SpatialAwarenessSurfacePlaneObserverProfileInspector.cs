67ef1fc9022c24e96493fce1a8e3376f840ba94a
Srinjoy Majumdar <srmajumd@microsoft.com>
2020-03-02 15:15:17 -0800

    initial work on bringing in plane finding as an observer into MRTK, very much WIP

---
+++ Assets/MixedRealityToolkit/Inspectors/Profiles/Experimental/SpatialAwarenessSurfacePlaneObserverProfileInspector.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.﻿
+
+using Microsoft.MixedReality.Toolkit.Utilities.Editor;
+using System.Linq;
+using UnityEditor;
+
+namespace Microsoft.MixedReality.Toolkit.Editor.SpatialAwareness
+{
+    [CustomEditor(typeof(Experimental.SpatialAwareness.SpatialAwarenessSurfacePlaneObserverProfile))]
+    public class MixedRealitySpatialAwarenessSurfacePlaneObserverProfileInspector : BaseMixedRealityToolkitConfigurationProfileInspector
+    {
+       
+        private SerializedProperty physicsLayer;
+
+        private const string ProfileTitle = "Surface Plane Observer Settings";
+        private const string ProfileDescription = "Settings for plane finder used on Hololens 1";
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+
+            physicsLayer = serializedObject.FindProperty("physicsLayer");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            RenderProfileHeader(ProfileTitle, ProfileDescription, target, true, BackProfileType.SpatialAwareness);
+
+            //using (new GUIEnabledWrapper(!IsProfileLock((BaseMixedRealityProfile)target)))
+            using (new GUIEnabledWrapper())
+            {
+                serializedObject.Update();
+
+                EditorGUILayout.PropertyField(physicsLayer);
+
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+
+        protected override bool IsProfileInActiveInstance()
+        {
+            var profile = target as BaseMixedRealityProfile;
+
+            return MixedRealityToolkit.IsInitialized && profile != null &&
+                   MixedRealityToolkit.Instance.HasActiveProfile &&
+                   MixedRealityToolkit.Instance.ActiveProfile.SpatialAwarenessSystemProfile != null &&
+                   MixedRealityToolkit.Instance.ActiveProfile.SpatialAwarenessSystemProfile.ObserverConfigurations != null &&
+                   MixedRealityToolkit.Instance.ActiveProfile.SpatialAwarenessSystemProfile.ObserverConfigurations.Any(s => s.ObserverProfile == profile);
+        }
+    }
+}
