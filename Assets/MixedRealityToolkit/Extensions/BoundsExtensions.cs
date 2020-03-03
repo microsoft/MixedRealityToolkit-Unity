58b4b366dbcc7c950b0e5475c936e7865fe0ee21
Cameron <thmicka@microsoft.com>
2020-03-03 08:40:32 -0800

    Update Assets/MixedRealityToolkit/Extensions/BoundsExtensions.cs
    Co-Authored-By: Bernadette Thalhammer <36998103+thalbern@users.noreply.github.com>

--- Assets/MixedRealityToolkit/Extensions/BoundsExtensions.cs
+++ Assets/MixedRealityToolkit/Extensions/BoundsExtensions.cs
@@ -481,7 +481,7 @@ namespace Microsoft.MixedReality.Toolkit
         /// </summary>
         /// <param name="target">GameObject to generate the bounds around.</param>
         /// <param name="bounds">An AABB in world space around all the renderers in a gameObject hierarchy.</param>
-        /// <param name="ignoreLayers">A LayerMask to restrict the colliers selected.</param>
+        /// <param name="ignoreLayers">A LayerMask to restrict the colliders selected.</param>
         /// <returns>True if bounds were calculated, if zero renderers are present bounds will not be calculated.</returns>
         public static bool GetRenderBounds(GameObject target, out Bounds bounds, LayerMask ignoreLayers)
         {
