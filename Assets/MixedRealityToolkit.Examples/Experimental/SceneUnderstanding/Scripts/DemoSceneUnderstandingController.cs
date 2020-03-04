0d498712986b947fbc64c1b8606b2662207f9ea1
Pink Bonnstetter <pinkb@microsoft.com>
2020-03-04 10:12:51 -0800

    added license

--- Assets/MixedRealityToolkit.Examples/Experimental/SceneUnderstanding/Scripts/DemoSceneUnderstandingController.cs
+++ Assets/MixedRealityToolkit.Examples/Experimental/SceneUnderstanding/Scripts/DemoSceneUnderstandingController.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
 
 using Microsoft.MixedReality.Toolkit.SpatialAwareness;
 using Microsoft.MixedReality.Toolkit.Experimental.SpatialAwareness;
--- Assets/MixedRealityToolkit.Examples/Experimental/SceneUnderstanding/Scripts/DemoSceneUnderstandingSceneObjectConsumer.cs
+++ Assets/MixedRealityToolkit.Examples/Experimental/SceneUnderstanding/Scripts/DemoSceneUnderstandingSceneObjectConsumer.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
 using Microsoft.MixedReality.Toolkit.Experimental.SpatialAwareness;
 using Microsoft.MixedReality.Toolkit.Physics;
 using Microsoft.MixedReality.Toolkit.SpatialAwareness;
