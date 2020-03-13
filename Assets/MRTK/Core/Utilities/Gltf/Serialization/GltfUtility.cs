f05d0ed13a00402f6151c9adca3839a8a441ddac
Jesse Vander Does <jvanderdoes@gmail.com>
2020-03-13 10:58:24 -0500

    Makes custom gltf attribtues available

--- Assets/MRTK/Core/Utilities/Gltf/Serialization/GltfUtility.cs
+++ Assets/MRTK/Core/Utilities/Gltf/Serialization/GltfUtility.cs
@@ -199,7 +199,7 @@ namespace Microsoft.MixedReality.Toolkit.Utilities.Gltf.Serialization
             {
                 for (int j = 0; j < gltfObject.meshes[i].primitives.Length; j++)
                 {
-                    gltfObject.meshes[i].primitives[j].Attributes = JsonUtility.FromJson<GltfMeshPrimitiveAttributes>(meshPrimitiveAttributes[primitiveIndex]);
+                    gltfObject.meshes[i].primitives[j].Attributes = new GltfMeshPrimitiveAttributes(StringIntDictionaryFromJson(meshPrimitiveAttributes[primitiveIndex]));
                     primitiveIndex++;
                 }
             }
@@ -394,5 +394,62 @@ namespace Microsoft.MixedReality.Toolkit.Utilities.Gltf.Serialization
             jsonString = jsonString.Substring(0, jsonString.IndexOf("\"", StringComparison.Ordinal));
             return jsonString;
         }
+
+        /// <summary>
+        /// A utility function to work around the JsonUtility inability to deserialize to a dictionary.
+        /// </summary>
+        /// <param name="json">JSON string</param>
+        /// <returns>A dictionary with the key value pairs found in the json</returns>
+        private static Dictionary<string, int> StringIntDictionaryFromJson(string json)
+        {
+            string reformatted = JsonDictionaryToArray(json);
+            StringIntKeyValueArray loadedData = JsonUtility.FromJson<StringIntKeyValueArray>(reformatted);
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            for (int i = 0; i < loadedData.items.Length; i++)
+            {
+                dictionary.Add(loadedData.items[i].key, loadedData.items[i].value);
+            }
+            return dictionary;
+        }
+
+        /// <summary>
+        /// Takes a json object string with key value pairs, and returns a json string
+        /// in the format of `{"items": [{"key": $key_name, "value": $value}]}`.
+        /// This format can be handled by JsonUtility and support an aribtrary number
+        /// of key/value pairs
+        /// </summary>
+        /// <param name="json">JSON string in the format `{"key": $value}`</param>
+        /// <returns>Returns a reformatted JSON string</returns>
+        private static string JsonDictionaryToArray(string json)
+        {
+            string reformatted = "{\"items\": [";
+            string pattern = @"""(\w+)"":\s?(""?\w+""?)";
+            RegexOptions options = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(json, pattern, options))
+            {
+                string key = m.Groups[1].Value;
+                string value = m.Groups[2].Value;
+
+                reformatted += $"{{\"key\":\"{key}\", \"value\":{value}}},";
+            }
+            reformatted = reformatted.TrimEnd(',');
+            reformatted += "]}";
+            return reformatted;
+        }
+
+        [System.Serializable]
+        private class StringKeyValue
+        {
+            public string key = string.Empty;
+            public int value = 0;
+        }
+
+        [System.Serializable]
+        private class StringIntKeyValueArray
+        {
+            public StringKeyValue[] items = Array.Empty<StringKeyValue>();
+        }
+
     }
 }
