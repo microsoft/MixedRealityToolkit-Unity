3b888e94a4f8187192e1190540ae7bf442d683c8
Srinjoy Majumdar <srmajumd@microsoft.com>
2020-03-17 11:42:13 -0700

    Add new SurfaceMeshToPlanes script for easier porting
    Working on updating script to run on background thread

---
+++ Assets/MixedRealityToolkit/Interfaces/SpatialAwareness/Observers/Experimental/IMixedRealitySpatialAwarenessSurfacePlaneObserver.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Experimental.SpatialAwareness;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.SpatialAwareness
+{
+    public interface IMixedRealitySpatialAwarenessSurfacePlaneObserver: IMixedRealitySpatialAwarenessObserver
+    {
+        SpatialAwarenessSurfaceTypes SurfaceTypes { get; set; }
+
+        int PhysicsLayer { get; set; }
+    }
+}
---
+++ Assets/MixedRealityToolkit/Providers/Experimental/BaseSurfacePlaneObserver.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+namespace Microsoft.MixedReality.Toolkit.Experimental.SpatialAwareness
+{
+    public abstract class BaseSurfacePlaneObserver : BaseSpatialObserver, IMixedRealitySpatialAwarenessSurfacePlaneObserver
+    {
+        public BaseSurfacePlaneObserver(
+            IMixedRealitySpatialAwarenessSystem spatialAwarenessSystem,
+            string name = null,
+            uint priority = 10,
+            BaseMixedRealityProfile profile = null) : base(spatialAwarenessSystem, name, priority, profile)
+        { }
+
+        public SpatialAwarenessSurfaceTypes SurfaceTypes { get; set; }
+        public int PhysicsLayer { get; set; }
+
+        protected virtual void ReadProfile()
+        {
+            if (ConfigurationProfile == null)
+            {
+                Debug.LogError($"{Name} requires a configuration profile to run properly.");
+                return;
+            }
+
+            SpatialAwarenessSurfacePlaneObserverProfile profile = ConfigurationProfile as SpatialAwarenessSurfacePlaneObserverProfile;
+            if (profile == null)
+            {
+                Debug.LogError($"{Name}'s configuration profile must be a MixedRealitySpatialAwarenessMeshObserverProfile.");
+                return;
+            }
+
+            //SurfaceTypes = profile.SurfaceTypes;
+            PhysicsLayer = profile.PhysicsLayer;
+        }
+    }
+}
