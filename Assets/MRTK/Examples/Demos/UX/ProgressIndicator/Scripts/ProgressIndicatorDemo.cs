c4d9800d620f869b7259200ce1f23df1bdbc35d7
Lars Simkins <lars.simkins@gmail.com>
2020-03-19 13:57:49 -0700

    Copyright

--- Assets/MRTK/Examples/Demos/UX/ProgressIndicator/Scripts/ProgressIndicatorDemo.cs
+++ Assets/MRTK/Examples/Demos/UX/ProgressIndicator/Scripts/ProgressIndicatorDemo.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.MixedReality.Toolkit.UI;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.UI;
 using System.Threading.Tasks;
 using UnityEngine;
 
