54e12b8ab18ae80e184a69bfe1eed9c9cdbd8c35
Yoon Park <cre8ive@outlook.com>
2020-03-18 22:32:04 -0700

    Temporary change of the slider to control the Palm Up script threshold

---
+++ Assets/MRTK/Examples/Demos/UX/Slider/Scripts/SliderChangePalmThresholdValue.cs
@@ -0,0 +1,21 @@
+﻿//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
+
+namespace Microsoft.MixedReality.Toolkit.Examples.Demos
+{
+    public class SliderChangePalmThresholdValue : MonoBehaviour
+    {
+
+        public void ChangeFacingCameraTrackingThreshold(SliderEventData eventData)
+        {
+            GetComponent<HandConstraintPalmUp>().FacingCameraTrackingThreshold = eventData.NewValue * 90.0f;
+        }
+    }
+}
