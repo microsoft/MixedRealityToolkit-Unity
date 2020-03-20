16b1283d2bfd6d62d0d4fd9b7b3e9bdf7e5913df
Troy Ferrell <Troy.Ferrell@outlook.com>
2020-03-19 17:41:13 -0700

    Update Assets/MRTK/Tests/EditModeTests/Editor/AssemblyInfo.cs
    Co-Authored-By: Kurtis <kurtie@microsoft.com>

--- Assets/MRTK/Tests/EditModeTests/Editor/AssemblyInfo.cs
+++ Assets/MRTK/Tests/EditModeTests/Editor/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Reflection;
 [assembly: AssemblyVersion("2.4.0.0")]
 [assembly: AssemblyFileVersion("2.4.0.0")]
 
-[assembly: AssemblyProduct("Microsoft® Mixed Reality Toolkit Services")]
+[assembly: AssemblyProduct("Microsoft® Mixed Reality Toolkit Tests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft Corporation")]
