90908c69e8ad88a1a20858beff45a430ec088171
Julia Schwarz <jschwarz@microsoft.com>
2020-03-23 13:49:38 -0700

    Comment MRKeyboardBase

--- Assets/MRTK/SDK/Experimental/MixedRealityKeyboard/MixedRealityKeyboardBase.cs
+++ Assets/MRTK/SDK/Experimental/MixedRealityKeyboard/MixedRealityKeyboardBase.cs
@@ -12,6 +12,14 @@ using System.Collections;
 
 namespace Microsoft.MixedReality.Toolkit.Experimental.UI
 {
+    /// <summary>
+    /// Base class for objects that wish to launch and hide a system keyboard specifically for Windows Mixed Reality
+    /// devices (HoloLens 2, Windows Mixed Reality).
+    /// 
+    /// Implements a workaround for UWP TouchScreenKeyboard bug which prevents
+    /// UWP keyboard from showing up again after it is closed.
+    /// Unity bug tracking the issue https://fogbugz.unity3d.com/default.asp?1137074_rttdnt8t1lccmtd3
+    /// </summary>
     public abstract class MixedRealityKeyboardBase : MonoBehaviour
     {
         #region Properties
