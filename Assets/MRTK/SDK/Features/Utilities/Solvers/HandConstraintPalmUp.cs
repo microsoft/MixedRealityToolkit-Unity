0baab1d15de8d3d496221ca9e50e773b25752a60
Ken Jakubzak <kjakubz@microsoft.com>
2020-03-24 15:59:38 -0700

    Abstracted the palm upwards and gaze checks out

--- Assets/MRTK/SDK/Features/Utilities/Solvers/HandConstraintPalmUp.cs
+++ Assets/MRTK/SDK/Features/Utilities/Solvers/HandConstraintPalmUp.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using Microsoft.MixedReality.Toolkit.Input;
-using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.Assertions.Must;
 using UnityEngine.Serialization;
@@ -119,7 +118,19 @@ namespace Microsoft.MixedReality.Toolkit.Utilities.Solvers
             set => gazeProximityThreshold = value;
         }
 
+        private bool targetWorldLocked = false;
+
+        /// <summary>
+        /// Is the current solver object world-locked? If so, don't compute whether it's a valid controller.
+        /// </summary>
+        public bool TargetWorldLocked
+        {
+            get => targetWorldLocked;
+            set => targetWorldLocked = value;
+        }
+
         private bool eyeGazeActivationAlreadyTriggered = false;
+
         /// <summary>
         /// Determines if a controller meets the requirements for use with constraining the tracked object and determines if the 
         /// palm is currently facing the user.
@@ -144,27 +155,7 @@ namespace Microsoft.MixedReality.Toolkit.Utilities.Solvers
                 {
                     float palmCameraAngle = Vector3.Angle(palmPose.Up, CameraCache.Main.transform.forward);
 
-                    if (requireFlatHand)
-                    {
-                        // Check if the triangle's normal formed from the palm, to index, to ring finger tip roughly matches the palm normal.
-                        MixedRealityPose indexTipPose, ringTipPose;
-
-                        if (jointedHand.TryGetJoint(TrackedHandJoint.IndexTip, out indexTipPose) &&
-                            jointedHand.TryGetJoint(TrackedHandJoint.RingTip, out ringTipPose))
-                        {
-                            var handNormal = Vector3.Cross(indexTipPose.Position - palmPose.Position,
-                                                           ringTipPose.Position - indexTipPose.Position).normalized;
-                            handNormal *= (jointedHand.ControllerHandedness == Handedness.Right) ? 1.0f : -1.0f;
-
-                            if (Vector3.Angle(palmPose.Up, handNormal) > flatHandThreshold)
-                            {
-                                return false;
-                            }
-                        }
-                    }
-
-                    // Check if the palm angle meets the prescribed threshold
-                    palmFacingThresholdMet = palmCameraAngle < facingCameraTrackingThreshold;
+                    palmFacingThresholdMet = IsPalmFacingUpwards(jointedHand, palmPose, palmCameraAngle);
 
                     // If using hybrid hand rotation, we proceed with additional checks
                     if (palmFacingThresholdMet)
@@ -185,45 +176,9 @@ namespace Microsoft.MixedReality.Toolkit.Utilities.Solvers
                             }
                         }
 
-                        if (useGazeActivation && !eyeGazeActivationAlreadyTriggered)
+                        if (useGazeActivation && (!eyeGazeActivationAlreadyTriggered || targetWorldLocked))
                         {
-                            var gazeProvider = (GazeProvider) CoreServices.InputSystem.EyeGazeProvider;
-
-                            if (gazeProvider != null)
-                            { 
-                                Ray eyeRay = new Ray(gazeProvider.GazeOrigin, gazeProvider.GazeDirection);
-
-                                // Generate the hand plane that we're using to generate a distance value.
-                                // This is done by using the index knuckle, pinky knuckle, and wrist
-                                MixedRealityPose indexKnuckle;
-                                MixedRealityPose pinkyKnuckle;
-                                MixedRealityPose wrist;
-
-                                if (jointedHand.TryGetJoint(TrackedHandJoint.IndexKnuckle, out indexKnuckle) &&
-                                    jointedHand.TryGetJoint(TrackedHandJoint.PinkyKnuckle, out pinkyKnuckle) &&
-                                    jointedHand.TryGetJoint(TrackedHandJoint.Wrist, out wrist))
-                                {
-                                    Plane handPlane = new Plane(indexKnuckle.Position, pinkyKnuckle.Position, wrist.Position);
-                                    float distanceToHandPlane;
-
-                                    if (handPlane.Raycast(eyeRay, out distanceToHandPlane))
-                                    {
-                                        // Define the activation point as a vector between the wrist and pinky knuckle; then cast it against the plane to get a smooth location
-                                        Vector3 activationPoint = Vector3.Lerp(pinkyKnuckle.Position, wrist.Position, .5f);
-
-                                        // Now that we know the dist to the plane, create a vector at that point
-                                        Vector3 gazePosOnPlane = eyeRay.origin + eyeRay.direction.normalized * distanceToHandPlane;
-                                        Vector3 PlanePos = handPlane.ClosestPointOnPlane(gazePosOnPlane);
-                                        Vector3 activationPointPlanePos = handPlane.ClosestPointOnPlane(activationPoint);
-
-                                        float gazePosDistToActivationPosition = (activationPointPlanePos - PlanePos).sqrMagnitude;
-
-                                        eyeGazeActivationAlreadyTriggered = (gazePosDistToActivationPosition < gazeProximityThreshold);
-                                        
-                                        return eyeGazeActivationAlreadyTriggered;
-                                    }
-                                }
-                            }
+                            return IsUserGazingAtActivationPoint(jointedHand);
                         }
                     }
 
@@ -235,12 +190,98 @@ namespace Microsoft.MixedReality.Toolkit.Utilities.Solvers
                 {
                     Debug.LogError("HandConstraintPalmUp requires controllers of type IMixedRealityHand to perform hand activation tests.");
                 }
-                
+
                 eyeGazeActivationAlreadyTriggered = palmFacingThresholdMet ? eyeGazeActivationAlreadyTriggered : false;
                 return palmFacingThresholdMet;
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Checks to see if the palm is currently facing the user
+        /// </summary>
+        /// <param name="jointedHand"></param>
+        /// <param name="palmPose"></param>
+        /// <param name="palmCameraAngle"></param>
+        /// <returns></returns>
+        private bool IsPalmFacingUpwards(IMixedRealityHand jointedHand, MixedRealityPose palmPose, float palmCameraAngle)
+        {
+            if (requireFlatHand)
+            {
+                // Check if the triangle's normal formed from the palm, to index, to ring finger tip roughly matches the palm normal.
+                MixedRealityPose indexTipPose, ringTipPose;
+
+                if (jointedHand.TryGetJoint(TrackedHandJoint.IndexTip, out indexTipPose) &&
+                    jointedHand.TryGetJoint(TrackedHandJoint.RingTip, out ringTipPose))
+                {
+                    var handNormal = Vector3.Cross(indexTipPose.Position - palmPose.Position,
+                                                   ringTipPose.Position - indexTipPose.Position).normalized;
+                    handNormal *= (jointedHand.ControllerHandedness == Handedness.Right) ? 1.0f : -1.0f;
+
+                    if (Vector3.Angle(palmPose.Up, handNormal) > flatHandThreshold)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // Check if the palm angle meets the prescribed threshold
+            return palmCameraAngle < facingCameraTrackingThreshold;
+        }
+
+        /// <summary>
+        /// Checks to see if the user is currently gazing at the activation point
+        /// </summary>
+        /// <param name="jointedHand"></param>
+        /// <returns></returns>
+        private bool IsUserGazingAtActivationPoint(IMixedRealityHand jointedHand)
+        {
+            var gazeProvider = (GazeProvider)CoreServices.InputSystem.EyeGazeProvider;
+
+            if (gazeProvider != null)
+            {
+                Ray eyeRay = new Ray(gazeProvider.GazeOrigin, gazeProvider.GazeDirection);
+
+                // Generate the hand plane that we're using to generate a distance value.
+                // This is done by using the index knuckle, pinky knuckle, and wrist
+                MixedRealityPose indexKnuckle;
+                MixedRealityPose pinkyKnuckle;
+                MixedRealityPose wrist;
+
+                if (jointedHand.TryGetJoint(TrackedHandJoint.IndexKnuckle, out indexKnuckle) &&
+                    jointedHand.TryGetJoint(TrackedHandJoint.PinkyKnuckle, out pinkyKnuckle) &&
+                    jointedHand.TryGetJoint(TrackedHandJoint.Wrist, out wrist))
+                {
+                    Plane handPlane = new Plane(indexKnuckle.Position, pinkyKnuckle.Position, wrist.Position);
+                    float distanceToHandPlane;
+
+                    if (handPlane.Raycast(eyeRay, out distanceToHandPlane))
+                    {
+                        // Define the activation point as a vector between the wrist and pinky knuckle; then cast it against the plane to get a smooth location
+                        Vector3 activationPoint = Vector3.Lerp(pinkyKnuckle.Position, wrist.Position, .5f);
+
+                        // Now that we know the dist to the plane, create a vector at that point
+                        Vector3 gazePosOnPlane = eyeRay.origin + eyeRay.direction.normalized * distanceToHandPlane;
+                        Vector3 PlanePos = handPlane.ClosestPointOnPlane(gazePosOnPlane);
+                        Vector3 activationPointPlanePos = handPlane.ClosestPointOnPlane(activationPoint);
+
+                        float gazePosDistToActivationPosition = (activationPointPlanePos - PlanePos).sqrMagnitude;
+
+                        var gazeActivated = eyeGazeActivationAlreadyTriggered = (gazePosDistToActivationPosition < gazeProximityThreshold);
+
+                        if (targetWorldLocked)
+                        {
+                            eyeGazeActivationAlreadyTriggered = false;
+                            targetWorldLocked = false;
+                        }
+
+                        return gazeActivated;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
