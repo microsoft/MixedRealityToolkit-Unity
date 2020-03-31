588e2da136ec04c6369807b31f3a5304ab1cb9ce
Ken Jakubzak <kjakubz@microsoft.com>
2020-03-31 14:58:53 -0700

    Had flipped radial and ulnar

--- Assets/MRTK/SDK/Features/Utilities/Solvers/HandConstraintPalmUp.cs
+++ Assets/MRTK/SDK/Features/Utilities/Solvers/HandConstraintPalmUp.cs
@@ -332,16 +332,16 @@ namespace Microsoft.MixedReality.Toolkit.Utilities.Solvers
                     return jointedHand.TryGetJoint(TrackedHandJoint.Wrist, out referenceJoint1) ? referenceJoint1.Position : Vector3.zero;
 
                 case SolverSafeZone.RadialSide:
-                    if (!jointedHand.TryGetJoint(TrackedHandJoint.Wrist, out referenceJoint1) ||
-                        !jointedHand.TryGetJoint(TrackedHandJoint.PinkyKnuckle, out referenceJoint2))
+                    if (!jointedHand.TryGetJoint(TrackedHandJoint.IndexKnuckle, out referenceJoint1) ||
+                        !jointedHand.TryGetJoint(TrackedHandJoint.ThumbProximalJoint, out referenceJoint2))
                     {
                         return Vector3.zero;
                     }
                     break;
                 case SolverSafeZone.UlnarSide:
                 default:
-                    if (!jointedHand.TryGetJoint(TrackedHandJoint.IndexKnuckle, out referenceJoint1) ||
-                        !jointedHand.TryGetJoint(TrackedHandJoint.ThumbProximalJoint, out referenceJoint2))
+                    if (!jointedHand.TryGetJoint(TrackedHandJoint.Wrist, out referenceJoint1) ||
+                        !jointedHand.TryGetJoint(TrackedHandJoint.PinkyKnuckle, out referenceJoint2))
                     {
                         return Vector3.zero;
                     }
