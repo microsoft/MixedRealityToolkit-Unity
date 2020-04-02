c92759f3ae2436a62986b2847e0e5907733d94cc
Kurtis <kurtie@microsoft.com>
2020-04-02 11:28:01 -0700

    Update flags

--- Assets/MRTK/SDK/Experimental/Dialog/Scripts/DialogButtonType.cs
+++ Assets/MRTK/SDK/Experimental/Dialog/Scripts/DialogButtonType.cs
@@ -1,7 +1,5 @@
-﻿//
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
-//
 
 using System;
 using System.Collections;
@@ -16,13 +14,13 @@ namespace Microsoft.MixedReality.Toolkit.Experimental.Dialog
     [Flags]
     public enum DialogButtonType
     {
-        None = 0,
-        Close = 1,
-        Confirm = 2,
-        Cancel = 4,
-        Accept = 8,
-        Yes = 16,
-        No = 32,
-        OK = 64
+        None = 0 << 0,
+        Close = 1 << 0,
+        Confirm = 1 << 1,
+        Cancel = 1 << 2,
+        Accept = 1 << 3,
+        Yes = 1 << 4,
+        No = 1 << 5,
+        OK = 1 << 6
     }
 }
