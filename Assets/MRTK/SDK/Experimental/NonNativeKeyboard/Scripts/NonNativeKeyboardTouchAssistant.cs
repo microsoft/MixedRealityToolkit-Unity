199ae59f4b2a161d7071bd4a3a1b90d45b23ad7f
Joost van Schaik <joostvanschaik@outlook.com>
2020-04-03 20:09:26 +0200

    Made NonNativeKeyboard touchable

---
+++ Assets/MRTK/SDK/Experimental/NonNativeKeyboard/Scripts/NonNativeKeyboardTouchAssistant.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Microsoft.MixedReality.Toolkit.Experimental.UI
+{
+    public class NonNativeKeyboardTouchAssistant : MonoBehaviour
+    {
+#pragma warning disable 0649
+        [SerializeField]
+        private AudioClip _clickSound;
+#pragma warning restore 0649
+
+
+        private AudioSource _clickSoundPlayer;
+
+        private void Start()
+        {
+            var capabilityChecker = CoreServices.InputSystem as IMixedRealityCapabilityCheck;
+
+            if(capabilityChecker != null && capabilityChecker.CheckCapability(MixedRealityCapability.ArticulatedHand))
+            {
+                EnableTouch();
+            }
+        }
+
+        private void EnableTouch()
+        {
+            _clickSoundPlayer = gameObject.AddComponent<AudioSource>();
+            _clickSoundPlayer.playOnAwake = false;
+            _clickSoundPlayer.spatialize = true;
+            _clickSoundPlayer.clip = _clickSound;
+            var buttons = GetComponentsInChildren<Button>();
+            foreach (var button in buttons)
+            {
+                var ni = button.gameObject.AddComponent<NearInteractionTouchableUnityUI>();
+                ni.EventsToReceive = TouchableEventType.Pointer;
+                button.onClick.AddListener(PlayClick);
+            }
+        }
+
+        private void PlayClick()
+        {
+            if (_clickSound != null)
+            {
+                _clickSoundPlayer.Play();
+            }
+        }
+    }
+}
