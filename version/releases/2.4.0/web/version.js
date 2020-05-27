a78018d44826638e82ef23cb1a8818138d6b8a15
mrtk-bld <mrtk-bld@microsoft.com>
2020-05-27 16:47:02 +0000

    Docs for commit c4f0a80b8 (Update the version dropdowns in preparation for 2.4.0 release.)

--- version/releases/2.4.0/web/version.js
+++ version/releases/2.4.0/web/version.js
@@ -2,7 +2,15 @@ function createDropdown()
 {
 	// configurable values:
 	var defaultTitle = "mrtk_development"; // title in the dropdown for the root version of the docs
-	var versionArray = ["releases/2.0.0", "releases/2.1.0", "releases/2.2.0", "releases/2.3.0", "prerelease/2.4.0_stabilization"]; // list of all versions in the version folder
+
+	// list of all versions in the version folder
+	var versionArray = [
+		"releases/2.0.0",
+		"releases/2.1.0",
+		"releases/2.2.0",
+		"releases/2.3.0",
+		"releases/2.4.0"
+	];
 	
 	//--------------------------------------
 	
