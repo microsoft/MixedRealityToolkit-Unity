89c3b912b3899876d3e1569ab206c9150b0980ce
Finn Sinclair <finnnorth@gmail.com>
2020-05-28 15:20:47 -0800

    Scene changes
    Drawstring proof of concept working, without carousel display yet
    
    Work on drawstring + diorama functional concept
    
    Work on elastic system refactor
    
    Further cleanup of elastic system
    
    Further cleanup
    
    Polish + refinement
    
    Cleaning, pruning, prepping for PR

---
+++ Assets/MRTK/SDK/Experimental/Drawstring/Scripts/ElasticHandleLogic.cs
@@ -0,0 +1,111 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Physics
+{
+
+    /// <summary>
+    /// Implements a two-handle elastic "stretch" logic, which allows for
+    /// either one or two pointers to stretch along a particular axis.
+    /// 
+    /// Built around the differential equations for a damped harmonic oscillator.
+    /// Intended for use with critically and over-damped oscillators.
+    /// 
+    /// Usage:
+    /// When a manipulation starts, call Setup.
+    /// Call Update any time to update the move logic and get a new rotation for the object.
+    /// </summary>
+    internal class ElasticHandleLogic
+    {
+        private Vector3 leftInitialPosition;
+        private Vector3 rightInitialPosition;
+
+        private LinearElasticSystem elasticSystem;
+
+        private bool isSetup;
+
+        /// <summary>
+        /// Initialize system with source info from controllers/hands.
+        /// Left and right handles must both be specified, but this does not
+        /// preclude single-hand interaction when Update() is called.
+        /// </summary>
+        /// <param name="leftHandleStart">World position of "left" handle point.</param>
+        /// <param name="rightHandleStart">World position of "right" handle point.</param>
+        /// <param name="extentInfo">Properties of the linear 1-D elastic extent to be manipulated</param>
+        /// <param name="elasticProperties">Properties of the elastic material/spring.</param>
+        /// <param name="leftHandleVelocity">Optional, initial velocity in 1-dimensional stretch space</param>
+        /// <param name="rightHandleVelocity">Optional, initial velocity in 1-dimensional stretch space</param>
+        public virtual void Setup(Vector3 leftHandleStart, Vector3 rightHandleStart,
+                                    ElasticExtentProperties<float> extentInfo, ElasticProperties elasticProperties,
+                                    float leftHandleVelocity = 0.0f, float rightHandleVelocity = 0.0f)
+        {
+            isSetup = true;
+            leftInitialPosition = leftHandleStart;
+            rightInitialPosition = rightHandleStart;
+            elasticSystem = new LinearElasticSystem((leftHandleStart - rightHandleStart).magnitude, 0.0f, extentInfo, elasticProperties);
+        }
+
+        /// <summary>
+        /// Update the internal state of the damped harmonic oscillator, given the left and right pointer positions.
+        /// Note, both left and right input positions are nullable; set the pointer position to null
+        /// if only one of the handles is being interacted with. Returns the calculated distance between the
+        /// handles.
+        /// 
+        /// </summary>
+        /// <param name="leftPointer">World position of the pointer manipulating the left handle.</param>
+        /// <param name="rightPointer">World position of the pointer manipulating the right handle.</param>
+        /// <param name="deltaTime">Amount of time to simulate this step.</param>
+        /// <param name="normalizedAxis">World axis of the two-handle system.</param>
+        public virtual float Update(Vector3? leftPointer, Vector3? rightPointer, float deltaTime, Vector3? normalizedAxis = null)
+        {
+            // If we have not been Setup() yet
+            if (!isSetup) { return 0.1f; }
+
+            var handleAxis = normalizedAxis ?? (rightInitialPosition - leftInitialPosition).normalized;
+
+            var handDistance = elasticSystem.GetCurrentValue();
+            if (leftPointer.HasValue && rightPointer.HasValue)
+            {
+                // If we have both pointers, we simply project the vector between the
+                // two hands onto the handle axis vector.
+                handDistance = Vector3.Dot(rightPointer.Value - leftPointer.Value, handleAxis);
+            } else if (!leftPointer.HasValue && rightPointer.HasValue)
+            {
+                // If we only have a right pointer, calculate the hand distance
+                // as twice the distance of the right pointer from the center.
+                handDistance = 2.0f * Vector3.Dot(rightPointer.Value - (leftInitialPosition + rightInitialPosition) / 2.0f, handleAxis);
+            } else if (leftPointer.HasValue && !rightPointer.HasValue)
+            {
+                // If we only have a left pointer, calculate the hand distance
+                // as twice the distance of the left pointer from the center.
+                handDistance = 2.0f * -Vector3.Dot(leftPointer.Value - (leftInitialPosition + rightInitialPosition) / 2.0f, handleAxis);
+            }
+
+            return elasticSystem.ComputeIteration(handDistance, deltaTime);
+        }
+
+        public virtual Vector3 GetCenterPosition(Vector3? leftPointer, Vector3? rightPointer)
+        {
+            if (leftPointer.HasValue && rightPointer.HasValue)
+            {
+                return (leftPointer.Value + rightPointer.Value) / 2.0f;
+            }
+            else if (!leftPointer.HasValue && rightPointer.HasValue)
+            {
+                return (rightPointer.Value + leftInitialPosition) / 2.0f;
+            }
+            else if (leftPointer.HasValue && !rightPointer.HasValue)
+            {
+                return (leftPointer.Value + rightInitialPosition) / 2.0f;
+            } else
+            {
+                return Vector3.zero; // Failure case.
+            }
+        }
+    }
+}
---
+++ Assets/MRTK/SDK/Experimental/Drawstring/Scripts/ElasticSystem.cs
@@ -0,0 +1,196 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Physics
+{
+
+    /// <summary>
+    /// Properties of the extent in which a damped
+    /// harmonic oscillator is free to move.
+    /// </summary>
+    [Serializable]
+    public struct ElasticExtentProperties<T>
+    {
+        /// <value>
+        /// Represents the lower bound of the extent,
+        /// specified as the norm of the n-dimensional extent
+        /// </value>
+        [SerializeField]
+        public float minStretch;
+
+        /// <value>
+        /// Represents the upper bound of the extent,
+        /// specified as the norm of the n-dimensional extent
+        /// </value>
+        [SerializeField]
+        public float maxStretch;
+
+        /// <value>
+        /// Whether the system, when approaching the upper bound,
+        /// will treat the upper bound as a snap point.
+        /// </value>
+        [SerializeField]
+        public bool snapToMax;
+
+        /// <value>
+        /// Points inside the extent to which the system will snap.
+        /// </value>
+        [SerializeField]
+        public T[] snapPoints;
+    }
+
+    /// <summary>
+    /// Properties of the damped harmonic oscillator differential system.
+    /// </summary>
+    [Serializable]
+    public struct ElasticProperties
+    {
+        /// <value>
+        /// Mass of the simulated oscillator element
+        /// </value>
+        [SerializeField]
+        public float mass;
+        /// <value>
+        /// Hand spring constant
+        /// </value>
+        [SerializeField]
+        public float hand_k;
+        /// <value>
+        /// End cap spring constant
+        /// </value>
+        [SerializeField]
+        public float end_k;
+        /// <value>
+        /// Snap point spring constant
+        /// </value>
+        [SerializeField]
+        public float snap_k;
+        /// <value>
+        /// Extent at which snap points begin forcing the spring.
+        /// </value>
+        [SerializeField]
+        public float snap_radius;
+        /// <value>
+        /// Drag/damper factor, proportional to velocity.
+        /// </value>
+        [SerializeField]
+        public float drag;
+    }
+
+    /// <summary>
+    /// Represents a damped harmonic oscillator over an
+    /// N-dimensional vector space, specified by generic type T.
+    /// 
+    /// This extensibility allows not just for 1, 2, and 3-D springs, but
+    /// allows for 4-dimensional quaternion springs.
+    /// </summary>
+    internal abstract class ElasticSystem<T>
+    {
+        protected ElasticExtentProperties<T> extentInfo;
+        protected ElasticProperties elasticProperties;
+
+        protected T currentValue;
+        protected T currentVelocity;
+
+        public ElasticSystem(T initialValue, T initialVelocity,
+                                ElasticExtentProperties<T> extentInfo, ElasticProperties elasticProperties)
+        {
+            this.extentInfo = extentInfo;
+            this.elasticProperties = elasticProperties;
+            currentValue = initialValue;
+            currentVelocity = initialVelocity;
+        }
+
+        /// <summary>
+        /// Update the internal state of the damped harmonic oscillator, given the forcing/desired value.
+        /// </summary>
+        /// <param name="forcingValue">Input value, for example, a desired manipulation position.</param>
+        /// <param name="deltaTime">Amount of time that has passed since the last update.</param>
+        public abstract T ComputeIteration(T forcingValue, float deltaTime);
+
+        /// <summary>
+        /// Query the elastic system for the current instantaneous value
+        /// </summary>
+        /// <returns>Current value of the elastic system</returns>
+        public T GetCurrentValue() => currentValue;
+    }
+
+    internal class LinearElasticSystem : ElasticSystem<float>
+    {
+        public LinearElasticSystem(float initialValue, float initialVelocity,
+                                ElasticExtentProperties<float> extentInfo, ElasticProperties elasticProperties)
+        : base(initialValue, initialVelocity, extentInfo, elasticProperties) { }
+
+
+        /// <summary>
+        /// Update the internal state of the damped harmonic oscillator, given the forcing/desired value.
+        /// </summary>
+        /// <param name="forcingValue">Input value, for example, a desired manipulation position.</param>
+        /// <param name="deltaTime">Amount of time that has passed since the last update.</param>
+        public override float ComputeIteration(float forcingValue, float deltaTime)
+        {
+            // F = -kx - (drag * v)
+            var force = (forcingValue - currentValue) * elasticProperties.hand_k - elasticProperties.drag * currentVelocity;
+
+            // Distance that the current stretch value is from the end limit.
+            float distFromEnd = extentInfo.maxStretch - currentValue;
+
+            // If we are extended beyond the end cap,
+            // add one-sided force back to the center.
+            if (currentValue > extentInfo.maxStretch)
+            {
+                force += distFromEnd * elasticProperties.end_k;
+            }
+            else
+            {
+                // Otherwise, add standard bidirectional magnetic/snapping force towards the end marker. (optional)
+                if (extentInfo.snapToMax)
+                {
+                    force += (distFromEnd) * elasticProperties.end_k * (1.0f - Mathf.Clamp01(Mathf.Abs(distFromEnd / elasticProperties.snap_radius)));
+                }
+            }
+
+            distFromEnd = extentInfo.minStretch - currentValue;
+            if (currentValue < extentInfo.minStretch)
+            {
+                force += distFromEnd * elasticProperties.end_k;
+            }
+            else
+            {
+                // Otherwise, add standard bidirectional magnetic/snapping force towards the end marker. (optional)
+                if (extentInfo.snapToMax)
+                {
+                    force += (distFromEnd) * elasticProperties.end_k * (1.0f - Mathf.Clamp01(Mathf.Abs(distFromEnd / elasticProperties.snap_radius)));
+                }
+            }
+
+            // Iterate over each snapping point, and apply forces as necessary.
+            foreach (float snappingPoint in extentInfo.snapPoints)
+            {
+                // Calculate distance from snapping point.
+                var distFromSnappingPoint = snappingPoint - currentValue;
+
+                // Snap force is calculated by multiplying the "-kx" factor by
+                // a clamped distance factor. This results in an overall
+                // hyperbolic profile to the force imparted by the snap point.
+                force += (distFromSnappingPoint) * elasticProperties.snap_k
+                          * (1.0f - Mathf.Clamp01(Mathf.Abs(distFromSnappingPoint / elasticProperties.snap_radius)));
+            }
+
+            // a = F/m
+            var accel = force / elasticProperties.mass;
+
+            // Integrate our acceleration over time.
+            currentVelocity += accel * deltaTime;
+            // Integrate our velocity over time.
+            currentValue += currentVelocity * deltaTime;
+
+            return currentValue;
+        }
+    }
+}
