c0bd19d5b537415872739d3852159fc58b7aeb6f
Finn Sinclair <finnnorth@gmail.com>
2020-05-28 15:19:25 -0800

    Work on drawstring concept

---
+++ Assets/TwoHandleManipulator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.UI
+{
+    public class TwoHandleManipulator : ObjectManipulator
+    {
+        
+    }
+}
+    
