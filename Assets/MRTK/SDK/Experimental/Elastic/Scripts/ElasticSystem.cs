8fca720cb5e3d1f4af291dddcde8caeba1c06fa7
Finn Sinclair <finnnorth@gmail.com>
2020-06-15 19:31:41 -0800

    Removing drawstring example scene, and updating code style

--- Assets/MRTK/SDK/Experimental/Elastic/Scripts/ElasticSystem.cs
+++ Assets/MRTK/SDK/Experimental/Elastic/Scripts/ElasticSystem.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Microsoft.MixedReality.Toolkit.Physics.Experimental
+namespace Microsoft.MixedReality.Toolkit.Experimental.Physics
 {
 
     /// <summary>
@@ -21,27 +21,27 @@ namespace Microsoft.MixedReality.Toolkit.Physics.Experimental
         /// specified as the norm of the n-dimensional extent
         /// </value>
         [SerializeField]
-        public float minStretch;
+        public float MinStretch;
 
         /// <value>
         /// Represents the upper bound of the extent,
         /// specified as the norm of the n-dimensional extent
         /// </value>
         [SerializeField]
-        public float maxStretch;
+        public float MaxStretch;
 
         /// <value>
         /// Whether the system, when approaching the upper bound,
         /// will treat the upper bound as a snap point.
         /// </value>
         [SerializeField]
-        public bool snapToMax;
+        public bool SnapToMax;
 
         /// <value>
         /// Points inside the extent to which the system will snap.
         /// </value>
         [SerializeField]
-        public T[] snapPoints;
+        public T[] SnapPoints;
     }
 
     /// <summary>
@@ -54,32 +54,32 @@ namespace Microsoft.MixedReality.Toolkit.Physics.Experimental
         /// Mass of the simulated oscillator element
         /// </value>
         [SerializeField]
-        public float mass;
+        public float Mass;
         /// <value>
         /// Hand spring constant
         /// </value>
         [SerializeField]
-        public float hand_k;
+        public float HandK;
         /// <value>
         /// End cap spring constant
         /// </value>
         [SerializeField]
-        public float end_k;
+        public float EndK;
         /// <value>
         /// Snap point spring constant
         /// </value>
         [SerializeField]
-        public float snap_k;
+        public float SnapK;
         /// <value>
         /// Extent at which snap points begin forcing the spring.
         /// </value>
         [SerializeField]
-        public float snap_radius;
+        public float SnapRadius;
         /// <value>
         /// Drag/damper factor, proportional to velocity.
         /// </value>
         [SerializeField]
-        public float drag;
+        public float Drag;
     }
 
     /// <summary>
@@ -118,6 +118,12 @@ namespace Microsoft.MixedReality.Toolkit.Physics.Experimental
         /// </summary>
         /// <returns>Current value of the elastic system</returns>
         public T GetCurrentValue() => currentValue;
+
+        /// <summary>
+        /// Query the elastic system for the current instantaneous velocity
+        /// </summary>
+        /// <returns>Current value of the elastic system</returns>
+        public T GetCurrentVelocity() => currentVelocity;
     }
 
     internal class LinearElasticSystem : ElasticSystem<float>
@@ -135,42 +141,42 @@ namespace Microsoft.MixedReality.Toolkit.Physics.Experimental
         public override float ComputeIteration(float forcingValue, float deltaTime)
         {
             // F = -kx - (drag * v)
-            var force = (forcingValue - currentValue) * elasticProperties.hand_k - elasticProperties.drag * currentVelocity;
+            var force = (forcingValue - currentValue) * elasticProperties.HandK - elasticProperties.Drag * currentVelocity;
 
             // Distance that the current stretch value is from the end limit.
-            float distFromEnd = extentInfo.maxStretch - currentValue;
+            float distFromEnd = extentInfo.MaxStretch - currentValue;
 
             // If we are extended beyond the end cap,
             // add one-sided force back to the center.
-            if (currentValue > extentInfo.maxStretch)
+            if (currentValue > extentInfo.MaxStretch)
             {
-                force += distFromEnd * elasticProperties.end_k;
+                force += distFromEnd * elasticProperties.EndK;
             }
             else
             {
                 // Otherwise, add standard bidirectional magnetic/snapping force towards the end marker. (optional)
-                if (extentInfo.snapToMax)
+                if (extentInfo.SnapToMax)
                 {
-                    force += (distFromEnd) * elasticProperties.end_k * (1.0f - Mathf.Clamp01(Mathf.Abs(distFromEnd / elasticProperties.snap_radius)));
+                    force += (distFromEnd) * elasticProperties.EndK * (1.0f - Mathf.Clamp01(Mathf.Abs(distFromEnd / elasticProperties.SnapRadius)));
                 }
             }
 
-            distFromEnd = extentInfo.minStretch - currentValue;
-            if (currentValue < extentInfo.minStretch)
+            distFromEnd = extentInfo.MinStretch - currentValue;
+            if (currentValue < extentInfo.MinStretch)
             {
-                force += distFromEnd * elasticProperties.end_k;
+                force += distFromEnd * elasticProperties.EndK;
             }
             else
             {
                 // Otherwise, add standard bidirectional magnetic/snapping force towards the end marker. (optional)
-                if (extentInfo.snapToMax)
+                if (extentInfo.SnapToMax)
                 {
-                    force += (distFromEnd) * elasticProperties.end_k * (1.0f - Mathf.Clamp01(Mathf.Abs(distFromEnd / elasticProperties.snap_radius)));
+                    force += (distFromEnd) * elasticProperties.EndK * (1.0f - Mathf.Clamp01(Mathf.Abs(distFromEnd / elasticProperties.SnapRadius)));
                 }
             }
 
             // Iterate over each snapping point, and apply forces as necessary.
-            foreach (float snappingPoint in extentInfo.snapPoints)
+            foreach (float snappingPoint in extentInfo.SnapPoints)
             {
                 // Calculate distance from snapping point.
                 var distFromSnappingPoint = snappingPoint - currentValue;
@@ -178,12 +184,12 @@ namespace Microsoft.MixedReality.Toolkit.Physics.Experimental
                 // Snap force is calculated by multiplying the "-kx" factor by
                 // a clamped distance factor. This results in an overall
                 // hyperbolic profile to the force imparted by the snap point.
-                force += (distFromSnappingPoint) * elasticProperties.snap_k
-                          * (1.0f - Mathf.Clamp01(Mathf.Abs(distFromSnappingPoint / elasticProperties.snap_radius)));
+                force += (distFromSnappingPoint) * elasticProperties.SnapK
+                          * (1.0f - Mathf.Clamp01(Mathf.Abs(distFromSnappingPoint / elasticProperties.SnapRadius)));
             }
 
             // a = F/m
-            var accel = force / elasticProperties.mass;
+            var accel = force / elasticProperties.Mass;
 
             // Integrate our acceleration over time.
             currentVelocity += accel * deltaTime;
