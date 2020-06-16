6ee37478ded992ac81351561362ad8ae6ff24659
Finn Sinclair <finnnorth@gmail.com>
2020-06-16 11:10:32 -0800

    Changed to internal scope + metafile

--- Assets/MRTK/SDK/Features/Utilities/Smoothing.cs
+++ Assets/MRTK/SDK/Features/Utilities/Smoothing.cs
@@ -9,7 +9,7 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
     /// <summary>
     /// Provides several utility functions for smoothing and lerping.
     /// </summary>
-    public class Smoothing {
+    internal class Smoothing {
 
         /// <summary>
         /// Smooths from source to goal, provided lerptime and a deltaTime.
@@ -19,7 +19,7 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
         /// <param name="lerpTime">Smoothing/lerp amount. Smoothing of 0 means no smoothing, and max value means no change at all.</param>
         /// <param name="deltaTime">Delta time. Usually would be set to Time.deltaTime</param>
         /// <returns>Smoothed value</returns>
-        public static Vector3 SmoothTo(Vector3 source, Vector3 goal, float lerpTime, float deltaTime)
+        internal static Vector3 SmoothTo(Vector3 source, Vector3 goal, float lerpTime, float deltaTime)
         {
             return Vector3.Lerp(source, goal, (lerpTime == 0f) ? 1f : 1f - Mathf.Pow(lerpTime, deltaTime));
         }
@@ -32,7 +32,7 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
         /// <param name="slerpTime">Smoothing/lerp amount. Smoothing of 0 means no smoothing, and max value means no change at all.</param>
         /// <param name="deltaTime">Delta time. Usually would be set to Time.deltaTime</param>
         /// <returns>Smoothed value</returns>
-        public static Quaternion SmoothTo(Quaternion source, Quaternion goal, float slerpTime, float deltaTime)
+        internal static Quaternion SmoothTo(Quaternion source, Quaternion goal, float slerpTime, float deltaTime)
         {
             return Quaternion.Slerp(source, goal, (slerpTime == 0f) ? 1f : 1f - Mathf.Pow(slerpTime, deltaTime));
         }
