c68836f2a11c6a22a0f41c5f36abafe422981ed4
Finn Sinclair <finnnorth@gmail.com>
2020-06-16 16:50:57 -0800

    Clarification on forcing functions/values

--- Assets/MRTK/SDK/Experimental/Elastic/Scripts/ElasticSystem.cs
+++ Assets/MRTK/SDK/Experimental/Elastic/Scripts/ElasticSystem.cs
@@ -114,7 +114,9 @@ namespace Microsoft.MixedReality.Toolkit.Experimental.Physics
         /// Update the internal state of the damped harmonic oscillator,
         /// given the forcing/desired value, returning the new value.
         /// </summary>
-        /// <param name="forcingValue">Input value, for example, a desired manipulation position.</param>
+        /// <param name="forcingValue">Forcing function, for example, a desired manipulation position.
+        /// See https://en.wikipedia.org/wiki/Forcing_function_(differential_equations). It is a non-time-dependent
+        /// input function to a differential equation; in our situation, it is the "input position" to the spring.</param>
         /// <param name="deltaTime">Amount of time that has passed since the last update.</param>
         /// <returns>The new value of the system.</returns>
         public abstract T ComputeIteration(T forcingValue, float deltaTime);
@@ -143,7 +145,9 @@ namespace Microsoft.MixedReality.Toolkit.Experimental.Physics
         /// <summary>
         /// Update the internal state of the damped harmonic oscillator, given the forcing/desired value.
         /// </summary>
-        /// <param name="forcingValue">Input value, for example, a desired manipulation position.</param>
+        /// <param name="forcingValue">Forcing function, for example, a desired manipulation position.
+        /// See https://en.wikipedia.org/wiki/Forcing_function_(differential_equations). It is a non-time-dependent
+        /// input function to a differential equation; in our situation, it is the "input position" to the spring.</param>
         /// <param name="deltaTime">Amount of time that has passed since the last update.</param>
         public override float ComputeIteration(float forcingValue, float deltaTime)
         {
