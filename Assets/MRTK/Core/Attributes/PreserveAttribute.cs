294564d769e3702c8f49cbbd461b967f5f947217
davidkline-ms <david.kline@microsoft.com>
2020-06-17 13:42:16 -0700

    add missing namespace

--- Assets/MRTK/Core/Attributes/PreserveAttribute.cs
+++ Assets/MRTK/Core/Attributes/PreserveAttribute.cs
@@ -3,23 +3,26 @@
 
 using System;
 
-/// <summary>
-/// Custom preserve attribute that is inheritable.
-/// </summary>
-[AttributeUsage(
-    AttributeTargets.Assembly | 
-    AttributeTargets.Class | 
-    AttributeTargets.Struct | 
-    AttributeTargets.Enum | 
-    AttributeTargets.Constructor | 
-    AttributeTargets.Method | 
-    AttributeTargets.Property | 
-    AttributeTargets.Field | 
-    AttributeTargets.Event | 
-    AttributeTargets.Interface | 
-    AttributeTargets.Delegate, 
-    Inherited = true)]
-public class MixedRealityToolkitPreserveAttribute : UnityEngine.Scripting.PreserveAttribute
+namespace Microsoft.MixedReality.Toolkit
 {
-    // No methods on PreserveAttribue for overriding.
+    /// <summary>
+    /// Custom preserve attribute that is inheritable.
+    /// </summary>
+    [AttributeUsage(
+        AttributeTargets.Assembly |
+        AttributeTargets.Class |
+        AttributeTargets.Struct |
+        AttributeTargets.Enum |
+        AttributeTargets.Constructor |
+        AttributeTargets.Method |
+        AttributeTargets.Property |
+        AttributeTargets.Field |
+        AttributeTargets.Event |
+        AttributeTargets.Interface |
+        AttributeTargets.Delegate,
+        Inherited = true)]
+    public class MixedRealityToolkitPreserveAttribute : UnityEngine.Scripting.PreserveAttribute
+    {
+        // No methods on PreserveAttribue for overriding.
+    }
 }
