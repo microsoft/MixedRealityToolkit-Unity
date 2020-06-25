7aabd2c6e5e5b9a99ef4c2f850157d00edd9515d
REDMOND\erfiscus <erfiscus@microsoft.com>
2020-06-25 10:18:28 -0700

    Added Touch Slider scene & script (initial commit)

---
+++ Assets/MRTK/SDK/Experimental/TouchSlider/TouchSlider.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.UI;
+using System;
+using UnityEngine;
+using TMPro;
+
+namespace Microsoft.MixedReality.Toolkit.Experimental.UI
+{
+    /// <summary>
+    /// This control enables touching a slider with one finger to set its value
+    /// </summary>
+    public class TouchSlider : MonoBehaviour
+    {
+        [Experimental]
+        [SerializeField]
+        private PinchSlider slider;
+        [SerializeField]
+        private TextMeshPro text;
+        public void UpdateSliderText()
+        {
+            text.text = slider.SliderValue.ToString();
+        }
+    }
+}
