03a1d31cfc194895aaba3d61812e37df675eadaf
REDMOND\erfiscus <erfiscus@microsoft.com>
2020-06-25 14:38:20 -0700

    Refined math for touching collider bounds

--- Assets/MRTK/SDK/Experimental/TouchSlider/TouchSlider.cs
+++ Assets/MRTK/SDK/Experimental/TouchSlider/TouchSlider.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Input;
 using System;
 using UnityEngine;
 using TMPro;
@@ -11,16 +12,56 @@ namespace Microsoft.MixedReality.Toolkit.Experimental.UI
     /// <summary>
     /// This control enables touching a slider with one finger to set its value
     /// </summary>
-    public class TouchSlider : MonoBehaviour
+    public class TouchSlider : MonoBehaviour, IMixedRealityTouchHandler
     {
         [Experimental]
         [SerializeField]
-        private PinchSlider slider = null;
+        private PinchSlider Slider = null;
         [SerializeField]
-        private TextMeshPro text = null;
+        private TextMeshPro SliderValue= null;
+        //[SerializeField]
+        //private BoxCollider TouchableCollider = null;
+        [SerializeField]
+        private TextMeshPro DebugText = null;
+        [SerializeField]
+        private float TouchableWidth = 0.135776976f;
+        [SerializeField]
+        private float Multiplier = 4f;
         public void UpdateSliderText()
         {
-            text.text = slider.SliderValue.ToString();
+            //SliderValue.text = Slider.SliderValue.ToString();
+        }
+        //void Start()
+        //{
+        //    DebugText.text = TouchableCollider.bounds.size.x.ToString();
+        //}
+        public void OnTouchStarted(HandTrackingInputEventData eventData)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnTouchCompleted(HandTrackingInputEventData eventData)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnTouchUpdated(HandTrackingInputEventData eventData)
+        {
+            float position = (eventData.InputData.x + TouchableWidth) * Multiplier;
+            float newPosition;
+            if(position < 0.1f)
+            {
+                newPosition = 0;
+            } else if(position > 0.95f)
+            {
+                newPosition = 1;
+            } else
+            {
+                newPosition = position;
+            }
+            Slider.SliderValue = newPosition;
+            SliderValue.text = (Mathf.Round(newPosition*100) / 100).ToString();
+            DebugText.text = position.ToString() + " | " + newPosition.ToString();
         }
     }
 }
