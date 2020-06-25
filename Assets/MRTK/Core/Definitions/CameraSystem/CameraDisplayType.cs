d397a0ed1f8ada8b9042fb5b4ae3102da833f860
davidkline-ms <david.kline@microsoft.com>
2020-06-24 17:16:35 -0700

    Normalize copyright and license header formats

--- Assets/MRTK/Core/Definitions/CameraSystem/CameraDisplayType.cs
+++ Assets/MRTK/Core/Definitions/CameraSystem/CameraDisplayType.cs
@@ -1,6 +1,6 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation.
 // Copyright(c) 2019 Takahiro Miyaura
-// Licensed under the MIT License. See LICENSE in the project root for license information.
+// Licensed under the MIT License.
 
 namespace Microsoft.MixedReality.Toolkit.CameraSystem
 {
