99b0f02b76fd7b5b13e8d9c8c35729af6e307f2e
RogPodge <roliu@microsoft.com>
2020-07-14 14:14:54 -0700

    made hands free input simulation more responsive

--- Assets/MRTK/Core/Providers/InputSimulation/SimulatedHandDataProvider.cs
+++ Assets/MRTK/Core/Providers/InputSimulation/SimulatedHandDataProvider.cs
@@ -29,7 +29,16 @@ namespace Microsoft.MixedReality.Toolkit.Input
         // Activate the pinch gesture
         // Pinch is a special gesture that triggers the Select and TriggerPress input actions
         // The pinch action doesn't occur until the gesture is completed.
-        public bool IsPinching => gesture == ArticulatedHandPose.GestureId.Pinch && gestureBlending == 1.0f;
+        public bool IsPinching
+        {
+            get
+            {
+                if (handedness == Handedness.None)
+                    return gesture == ArticulatedHandPose.GestureId.Pinch;
+                else
+                    return gesture == ArticulatedHandPose.GestureId.Pinch && gestureBlending == 1.0f;
+            }
+        }
 
         // Position of the hand in viewport space
         public Vector3 ViewportPosition = Vector3.zero;
