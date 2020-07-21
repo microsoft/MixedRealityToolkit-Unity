e1d741f549972e844a9f6a24e7eb8a1bd7788a81
Bernadette Thalhammer <bethalha@microsoft.com>
2020-07-21 22:58:29 +0100

    added missing namespace and license header

--- Assets/MRTK/SDK/Experimental/Features/UX/BoundsControl/BoundsControlInspector.cs
+++ Assets/MRTK/SDK/Experimental/Features/UX/BoundsControl/BoundsControlInspector.cs
@@ -1,157 +1,165 @@
-﻿using Microsoft.MixedReality.Toolkit.Input;
+﻿//
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+//
+
+using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Experimental.UI.BoundsControl;
 using Microsoft.MixedReality.Toolkit.Utilities.Editor;
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(BoundsControl), true)]
-[CanEditMultipleObjects]
-public class BoundsControlInspector : Editor
+namespace Microsoft.MixedReality.Toolkit.Experimental.Editor
 {
-    private SerializedProperty targetObject;
-    private SerializedProperty boundsOverride;
-    private SerializedProperty boundsCalculationMethod;
-    private SerializedProperty activationType;
-    private SerializedProperty controlPadding;
-    private SerializedProperty flattenAxis;
-
-    private SerializedProperty smoothingActive;
-    private SerializedProperty rotateLerpTime;
-    private SerializedProperty scaleLerpTime;
-
-    // configs
-    private SerializedProperty boxDisplayConfiguration;
-    private SerializedProperty linksConfiguration;
-    private SerializedProperty scaleHandlesConfiguration;
-    private SerializedProperty rotationHandlesConfiguration;
-    private SerializedProperty proximityEffectConfiguration;
-
-    // debug
-    private SerializedProperty hideElementsInHierarchyEditor;
-
-    // events
-    private SerializedProperty rotateStartedEvent;
-    private SerializedProperty rotateStoppedEvent;
-    private SerializedProperty scaleStartedEvent;
-    private SerializedProperty scaleStoppedEvent;
-
-    private BoundsControl boundsControl;
-
-    private static bool showBoxConfiguration = false;
-    private static bool showScaleHandlesConfiguration = false;
-    private static bool showRotationHandlesConfiguration = false;
-    private static bool showLinksConfiguration = false;
-    private static bool showProximityConfiguration = false;
-
-    private void OnEnable()
+    [CustomEditor(typeof(BoundsControl), true)]
+    [CanEditMultipleObjects]
+    public class BoundsControlInspector : UnityEditor.Editor
     {
-        boundsControl = (BoundsControl)target;
+        private SerializedProperty targetObject;
+        private SerializedProperty boundsOverride;
+        private SerializedProperty boundsCalculationMethod;
+        private SerializedProperty activationType;
+        private SerializedProperty controlPadding;
+        private SerializedProperty flattenAxis;
 
-        targetObject = serializedObject.FindProperty("targetObject");
-        activationType = serializedObject.FindProperty("activation");
-        boundsOverride = serializedObject.FindProperty("boundsOverride");
-        boundsCalculationMethod = serializedObject.FindProperty("boundsCalculationMethod");
-        flattenAxis = serializedObject.FindProperty("flattenAxis");
-        controlPadding = serializedObject.FindProperty("boxPadding");
+        private SerializedProperty smoothingActive;
+        private SerializedProperty rotateLerpTime;
+        private SerializedProperty scaleLerpTime;
 
-        smoothingActive = serializedObject.FindProperty("smoothingActive");
-        rotateLerpTime = serializedObject.FindProperty("rotateLerpTime");
-        scaleLerpTime = serializedObject.FindProperty("scaleLerpTime");
+        // configs
+        private SerializedProperty boxDisplayConfiguration;
+        private SerializedProperty linksConfiguration;
+        private SerializedProperty scaleHandlesConfiguration;
+        private SerializedProperty rotationHandlesConfiguration;
+        private SerializedProperty proximityEffectConfiguration;
 
-        boxDisplayConfiguration = serializedObject.FindProperty("boxDisplayConfiguration");
-        linksConfiguration = serializedObject.FindProperty("linksConfiguration");
-        scaleHandlesConfiguration = serializedObject.FindProperty("scaleHandlesConfiguration");
-        rotationHandlesConfiguration = serializedObject.FindProperty("rotationHandlesConfiguration");
-        proximityEffectConfiguration = serializedObject.FindProperty("handleProximityEffectConfiguration");
+        // debug
+        private SerializedProperty hideElementsInHierarchyEditor;
 
-        hideElementsInHierarchyEditor = serializedObject.FindProperty("hideElementsInInspector");
+        // events
+        private SerializedProperty rotateStartedEvent;
+        private SerializedProperty rotateStoppedEvent;
+        private SerializedProperty scaleStartedEvent;
+        private SerializedProperty scaleStoppedEvent;
 
-        rotateStartedEvent = serializedObject.FindProperty("rotateStarted");
-        rotateStoppedEvent = serializedObject.FindProperty("rotateStopped");
-        scaleStartedEvent = serializedObject.FindProperty("scaleStarted");
-        scaleStoppedEvent = serializedObject.FindProperty("scaleStopped");
-    }
+        private BoundsControl boundsControl;
 
-    public override void OnInspectorGUI()
-    {
-        if (target != null)
+        private static bool showBoxConfiguration = false;
+        private static bool showScaleHandlesConfiguration = false;
+        private static bool showRotationHandlesConfiguration = false;
+        private static bool showLinksConfiguration = false;
+        private static bool showProximityConfiguration = false;
+
+        private void OnEnable()
         {
-            // notification section - first thing to show in bounds control component
-            DrawRigidBodyWarning();
+            boundsControl = (BoundsControl)target;
 
-            // help url
-            InspectorUIUtility.RenderHelpURL(target.GetType());
+            targetObject = serializedObject.FindProperty("targetObject");
+            activationType = serializedObject.FindProperty("activation");
+            boundsOverride = serializedObject.FindProperty("boundsOverride");
+            boundsCalculationMethod = serializedObject.FindProperty("boundsCalculationMethod");
+            flattenAxis = serializedObject.FindProperty("flattenAxis");
+            controlPadding = serializedObject.FindProperty("boxPadding");
 
-            // data section
+            smoothingActive = serializedObject.FindProperty("smoothingActive");
+            rotateLerpTime = serializedObject.FindProperty("rotateLerpTime");
+            scaleLerpTime = serializedObject.FindProperty("scaleLerpTime");
+
+            boxDisplayConfiguration = serializedObject.FindProperty("boxDisplayConfiguration");
+            linksConfiguration = serializedObject.FindProperty("linksConfiguration");
+            scaleHandlesConfiguration = serializedObject.FindProperty("scaleHandlesConfiguration");
+            rotationHandlesConfiguration = serializedObject.FindProperty("rotationHandlesConfiguration");
+            proximityEffectConfiguration = serializedObject.FindProperty("handleProximityEffectConfiguration");
+
+            hideElementsInHierarchyEditor = serializedObject.FindProperty("hideElementsInInspector");
+
+            rotateStartedEvent = serializedObject.FindProperty("rotateStarted");
+            rotateStoppedEvent = serializedObject.FindProperty("rotateStopped");
+            scaleStartedEvent = serializedObject.FindProperty("scaleStarted");
+            scaleStoppedEvent = serializedObject.FindProperty("scaleStopped");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (target != null)
             {
-                EditorGUI.BeginChangeCheck();
+                // notification section - first thing to show in bounds control component
+                DrawRigidBodyWarning();
 
-                EditorGUILayout.PropertyField(targetObject);
-                EditorGUILayout.PropertyField(activationType);
-                EditorGUILayout.PropertyField(boundsOverride);
-                EditorGUILayout.PropertyField(boundsCalculationMethod);
-                EditorGUILayout.PropertyField(controlPadding);
-                EditorGUILayout.PropertyField(flattenAxis);
+                // help url
+                InspectorUIUtility.RenderHelpURL(target.GetType());
 
-
-                EditorGUILayout.PropertyField(smoothingActive);
-                EditorGUILayout.PropertyField(scaleLerpTime);
-                EditorGUILayout.PropertyField(rotateLerpTime);
-
-                EditorGUILayout.Space();
-                EditorGUILayout.LabelField(new GUIContent("Visuals", "Bounds Control Visual Configurations"), EditorStyles.boldLabel, GUILayout.ExpandWidth(true));
-                using (new EditorGUI.IndentLevelScope())
-                { 
-                    
-                    showBoxConfiguration = InspectorUIUtility.DrawScriptableFoldout<BoxDisplayConfiguration>(boxDisplayConfiguration, "Box Configuration", showBoxConfiguration);
-                    showScaleHandlesConfiguration = InspectorUIUtility.DrawScriptableFoldout<ScaleHandlesConfiguration>(scaleHandlesConfiguration, "Scale Handles Configuration", showScaleHandlesConfiguration);
-                    showRotationHandlesConfiguration = InspectorUIUtility.DrawScriptableFoldout<RotationHandlesConfiguration>(rotationHandlesConfiguration, "Rotation Handles Configuration", showRotationHandlesConfiguration);
-                    showLinksConfiguration = InspectorUIUtility.DrawScriptableFoldout<LinksConfiguration>(linksConfiguration, "Links Configuration", showLinksConfiguration);
-                    showProximityConfiguration = InspectorUIUtility.DrawScriptableFoldout<ProximityEffectConfiguration>(proximityEffectConfiguration, "Proximity Configuration", showProximityConfiguration);
-                }
-
-                EditorGUILayout.Space();
-                EditorGUILayout.LabelField(new GUIContent("Events", "Bounds Control Events"), EditorStyles.boldLabel, GUILayout.ExpandWidth(true));
+                // data section
                 {
-                    EditorGUILayout.PropertyField(rotateStartedEvent);
-                    EditorGUILayout.PropertyField(rotateStoppedEvent);
-                    EditorGUILayout.PropertyField(scaleStartedEvent);
-                    EditorGUILayout.PropertyField(scaleStoppedEvent);
-                }
+                    EditorGUI.BeginChangeCheck();
 
-                EditorGUILayout.Space();
-                EditorGUILayout.LabelField(new GUIContent("Debug", "Bounds Control Debug Section"), EditorStyles.boldLabel, GUILayout.ExpandWidth(true));
-                {
-                    EditorGUILayout.PropertyField(hideElementsInHierarchyEditor);
-                }
+                    EditorGUILayout.PropertyField(targetObject);
+                    EditorGUILayout.PropertyField(activationType);
+                    EditorGUILayout.PropertyField(boundsOverride);
+                    EditorGUILayout.PropertyField(boundsCalculationMethod);
+                    EditorGUILayout.PropertyField(controlPadding);
+                    EditorGUILayout.PropertyField(flattenAxis);
 
-                if (EditorGUI.EndChangeCheck())
-                {
-                    serializedObject.ApplyModifiedProperties();
+
+                    EditorGUILayout.PropertyField(smoothingActive);
+                    EditorGUILayout.PropertyField(scaleLerpTime);
+                    EditorGUILayout.PropertyField(rotateLerpTime);
+
+                    EditorGUILayout.Space();
+                    EditorGUILayout.LabelField(new GUIContent("Visuals", "Bounds Control Visual Configurations"), EditorStyles.boldLabel, GUILayout.ExpandWidth(true));
+                    using (new EditorGUI.IndentLevelScope())
+                    {
+
+                        showBoxConfiguration = InspectorUIUtility.DrawScriptableFoldout<BoxDisplayConfiguration>(boxDisplayConfiguration, "Box Configuration", showBoxConfiguration);
+                        showScaleHandlesConfiguration = InspectorUIUtility.DrawScriptableFoldout<ScaleHandlesConfiguration>(scaleHandlesConfiguration, "Scale Handles Configuration", showScaleHandlesConfiguration);
+                        showRotationHandlesConfiguration = InspectorUIUtility.DrawScriptableFoldout<RotationHandlesConfiguration>(rotationHandlesConfiguration, "Rotation Handles Configuration", showRotationHandlesConfiguration);
+                        showLinksConfiguration = InspectorUIUtility.DrawScriptableFoldout<LinksConfiguration>(linksConfiguration, "Links Configuration", showLinksConfiguration);
+                        showProximityConfiguration = InspectorUIUtility.DrawScriptableFoldout<ProximityEffectConfiguration>(proximityEffectConfiguration, "Proximity Configuration", showProximityConfiguration);
+                    }
+
+                    EditorGUILayout.Space();
+                    EditorGUILayout.LabelField(new GUIContent("Events", "Bounds Control Events"), EditorStyles.boldLabel, GUILayout.ExpandWidth(true));
+                    {
+                        EditorGUILayout.PropertyField(rotateStartedEvent);
+                        EditorGUILayout.PropertyField(rotateStoppedEvent);
+                        EditorGUILayout.PropertyField(scaleStartedEvent);
+                        EditorGUILayout.PropertyField(scaleStoppedEvent);
+                    }
+
+                    EditorGUILayout.Space();
+                    EditorGUILayout.LabelField(new GUIContent("Debug", "Bounds Control Debug Section"), EditorStyles.boldLabel, GUILayout.ExpandWidth(true));
+                    {
+                        EditorGUILayout.PropertyField(hideElementsInHierarchyEditor);
+                    }
+
+                    if (EditorGUI.EndChangeCheck())
+                    {
+                        serializedObject.ApplyModifiedProperties();
+                    }
                 }
             }
         }
-    }
 
-    private void DrawRigidBodyWarning()
-    {    
-        // check if rigidbody is attached - if so show warning in case input profile is not configured for individual collider raycast
-        Rigidbody rigidBody = boundsControl.GetComponent<Rigidbody>();
-
-        if (rigidBody != null)
+        private void DrawRigidBodyWarning()
         {
-            MixedRealityInputSystemProfile profile = Microsoft.MixedReality.Toolkit.CoreServices.InputSystem?.InputSystemProfile;
-            if (profile != null && profile.FocusIndividualCompoundCollider == false)
-            {
-                EditorGUILayout.Space();
-                // show warning and button to reconfigure profile
-                EditorGUILayout.HelpBox($"When using Bounds Control in combination with Rigidbody 'Focus Individual Compound Collider' must be enabled in Input Profile.", UnityEditor.MessageType.Warning);
-                if (GUILayout.Button($"Enable 'Focus Individual Compound Collider' in Input Profile"))
-                {
-                    profile.FocusIndividualCompoundCollider = true;
-                }
+            // check if rigidbody is attached - if so show warning in case input profile is not configured for individual collider raycast
+            Rigidbody rigidBody = boundsControl.GetComponent<Rigidbody>();
 
-                EditorGUILayout.Space();
+            if (rigidBody != null)
+            {
+                MixedRealityInputSystemProfile profile = Microsoft.MixedReality.Toolkit.CoreServices.InputSystem?.InputSystemProfile;
+                if (profile != null && profile.FocusIndividualCompoundCollider == false)
+                {
+                    EditorGUILayout.Space();
+                    // show warning and button to reconfigure profile
+                    EditorGUILayout.HelpBox($"When using Bounds Control in combination with Rigidbody 'Focus Individual Compound Collider' must be enabled in Input Profile.", UnityEditor.MessageType.Warning);
+                    if (GUILayout.Button($"Enable 'Focus Individual Compound Collider' in Input Profile"))
+                    {
+                        profile.FocusIndividualCompoundCollider = true;
+                    }
+
+                    EditorGUILayout.Space();
+                }
             }
         }
     }
