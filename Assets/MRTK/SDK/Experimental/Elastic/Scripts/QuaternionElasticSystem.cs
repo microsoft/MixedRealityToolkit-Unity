59d44aa6e06d22546704d67ddf56541dc88d1f26
Finn Sinclair <finnnorth@gmail.com>
2020-07-24 11:24:32 -0800

    Removed unused/unnecessary extension methods

--- Assets/MRTK/SDK/Experimental/Elastic/Scripts/QuaternionElasticSystem.cs
+++ Assets/MRTK/SDK/Experimental/Elastic/Scripts/QuaternionElasticSystem.cs
@@ -107,48 +107,58 @@ namespace Microsoft.MixedReality.Toolkit.Experimental.Physics
     }
     internal static class QuaternionExtras
     {
-        internal static Quaternion Lerp(Quaternion p, Quaternion q, float t, bool shortWay)
-        {
-            if (shortWay)
-            {
-                float dot = Quaternion.Dot(p, q);
-                if (dot < 0.0f)
-                    return Lerp(ScalarMultiply(p, -1.0f), q, t, true);
-            }
+        /// <summary>
+        /// An extended/improved version of the typical Quaternion lerp; this version
+        /// includes a "short way" argument, which controls whether the slerping algorithm
+        /// uses the shortest path or the naive path.
+        /// </summary>
+        /// <param name="p"> Beginning quaternion </param>
+        /// <param name="q"> Ending quaternion </param>
+        /// <param name="t"> Lerp factor </param>
+        /// <param name="shortWay"></param>
+        /// <returns></returns>
+        //internal static Quaternion Lerp(Quaternion p, Quaternion q, float t, bool shortWay)
+        //{
+        //    if (shortWay)
+        //    {
+        //        float dot = Quaternion.Dot(p, q);
+        //        if (dot < 0.0f)
+        //            return Lerp(ScalarMultiply(p, -1.0f), q, t, true);
+        //    }
 
-            Quaternion r = Quaternion.identity;
-            r.x = p.x * (1f - t) + q.x * (t);
-            r.y = p.y * (1f - t) + q.y * (t);
-            r.z = p.z * (1f - t) + q.z * (t);
-            r.w = p.w * (1f - t) + q.w * (t);
-            return r;
-        }
+        //    Quaternion r = Quaternion.identity;
+        //    r.x = p.x * (1f - t) + q.x * (t);
+        //    r.y = p.y * (1f - t) + q.y * (t);
+        //    r.z = p.z * (1f - t) + q.z * (t);
+        //    r.w = p.w * (1f - t) + q.w * (t);
+        //    return r;
+        //}
 
-        internal static Quaternion Slerp(Quaternion p, Quaternion q, float t, bool shortWay)
-        {
-            float dot = Quaternion.Dot(p, q);
-            if (shortWay)
-            {
-                if (dot < 0.0f)
-                    return Slerp(ScalarMultiply(p, -1.0f), q, t, true);
-            }
+        //internal static Quaternion Slerp(Quaternion p, Quaternion q, float t, bool shortWay)
+        //{
+        //    float dot = Quaternion.Dot(p, q);
+        //    if (shortWay)
+        //    {
+        //        if (dot < 0.0f)
+        //            return Slerp(ScalarMultiply(p, -1.0f), q, t, true);
+        //    }
 
-            float angle = Mathf.Acos(dot);
-            Quaternion first = ScalarMultiply(p, Mathf.Sin((1f - t) * angle));
-            Quaternion second = ScalarMultiply(q, Mathf.Sin((t) * angle));
-            float division = 1f / Mathf.Sin(angle);
-            return ScalarMultiply(Add(first, second), division);
-        }
+        //    float angle = Mathf.Acos(dot);
+        //    Quaternion first = ScalarMultiply(p, Mathf.Sin((1f - t) * angle));
+        //    Quaternion second = ScalarMultiply(q, Mathf.Sin((t) * angle));
+        //    float division = 1f / Mathf.Sin(angle);
+        //    return ScalarMultiply(Add(first, second), division);
+        //}
 
 
-        internal static Quaternion ScalarMultiply(Quaternion input, float scalar)
-        {
-            return new Quaternion(input.x * scalar, input.y * scalar, input.z * scalar, input.w * scalar);
-        }
+        //internal static Quaternion ScalarMultiply(Quaternion input, float scalar)
+        //{
+        //    return new Quaternion(input.x * scalar, input.y * scalar, input.z * scalar, input.w * scalar);
+        //}
 
-        internal static Quaternion Add(Quaternion p, Quaternion q)
-        {
-            return new Quaternion(p.x + q.x, p.y + q.y, p.z + q.z, p.w + q.w);
-        }
+        //internal static Quaternion Add(Quaternion p, Quaternion q)
+        //{
+        //    return new Quaternion(p.x + q.x, p.y + q.y, p.z + q.z, p.w + q.w);
+        //}
     }
 }
