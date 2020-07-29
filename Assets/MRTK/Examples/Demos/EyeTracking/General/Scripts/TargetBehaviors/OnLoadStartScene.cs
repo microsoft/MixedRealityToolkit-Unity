63a1b08ab1b5c155495bb586f6aa46efacb1a06b
Julia Schwarz <jschwarz@microsoft.com>
2020-07-29 12:55:53 -0700

    Add option to load scene only when on device

--- Assets/MRTK/Examples/Demos/EyeTracking/General/Scripts/TargetBehaviors/OnLoadStartScene.cs
+++ Assets/MRTK/Examples/Demos/EyeTracking/General/Scripts/TargetBehaviors/OnLoadStartScene.cs
@@ -1,11 +1,17 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Microsoft.MixedReality.Toolkit.Examples.Demos.EyeTracking
 {
+    public enum LoadOptions
+    {
+        LoadOnDeviceAndInEditor,
+        LoadOnlyOnDevice,
+    }
+
     /// <summary>
     /// When the button is selected, it triggers starting the specified scene.
     /// </summary>
@@ -16,9 +22,27 @@ namespace Microsoft.MixedReality.Toolkit.Examples.Demos.EyeTracking
         [Tooltip("Name of the scene to be loaded when the button is selected.")]
         private string SceneToBeLoaded = "";
 
+        [SerializeField]
+        [Tooltip("Option to only load the scene if running on the HoloLens device.")]
+        private LoadOptions LoadOption = LoadOptions.LoadOnlyOnDevice;
+
         public void Start()
         {
+            if (LoadOption == LoadOptions.LoadOnlyOnDevice)
+            {
+                LoadOnDevice();
+            }
+            else
+            {
+                LoadNewScene();
+            }
+        }
+
+        private void LoadOnDevice()
+        {
+#if WINDOWS_UWP
             LoadNewScene();
+#endif // (UNITY_WSA && DOTNETWINRT_PRESENT) || WINDOWS_UWP
         }
 
         private void LoadNewScene()
