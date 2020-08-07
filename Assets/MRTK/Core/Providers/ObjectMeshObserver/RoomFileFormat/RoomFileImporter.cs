c6f2e6275fe84e86172f20a9345fc56ea1f4cf48
Kurtis Eveleigh <kurtis.eveleigh@gmail.com>
2020-08-07 12:10:08 -0700

    Update asset importers

--- Assets/MRTK/Core/Providers/ObjectMeshObserver/RoomFileFormat/RoomFileImporter.cs
+++ Assets/MRTK/Core/Providers/ObjectMeshObserver/RoomFileFormat/RoomFileImporter.cs
@@ -3,8 +3,13 @@
 
 using System.Collections.Generic;
 using System.IO;
-using UnityEditor.Experimental.AssetImporters;
 using UnityEngine;
+
+#if UNITY_2020_2_OR_NEWER
+using UnityEditor.AssetImporters;
+#else
+using UnityEditor.Experimental.AssetImporters;
+#endif // UNITY_2020_2_OR_NEWER
 
 namespace Microsoft.MixedReality.Toolkit.SpatialObjectMeshObserver.RoomFile
 {
