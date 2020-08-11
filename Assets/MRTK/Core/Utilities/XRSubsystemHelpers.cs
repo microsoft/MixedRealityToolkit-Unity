a864bd38ff52d8f6a0dc2e6fefe14ecc465e550c
Kurtis Eveleigh <kurtis.eveleigh@gmail.com>
2020-08-10 17:01:52 -0700

    Update XRSubsystemHelpers.cs

--- Assets/MRTK/Core/Utilities/XRSubsystemHelpers.cs
+++ Assets/MRTK/Core/Utilities/XRSubsystemHelpers.cs
@@ -1,9 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Collections.Generic;
-
 #if UNITY_2019_3_OR_NEWER
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 #else
@@ -15,10 +14,13 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
     /// <summary>
     /// A helper class to provide easier access to active Unity XR SDK subsystems.
     /// </summary>
+    /// <remarks>These properties are only valid for the XR SDK pipeline.</remarks>
     public static class XRSubsystemHelpers
     {
         private static XRInputSubsystem inputSubsystem = null;
+#if UNITY_2019_3_OR_NEWER
         private static readonly List<XRInputSubsystem> XRInputSubsystems = new List<XRInputSubsystem>();
+#endif // UNITY_2019_3_OR_NEWER
 
         /// <summary>
         /// The XR SDK input subsystem for the currently loaded XR plug-in.
@@ -47,7 +49,9 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
         }
 
         private static XRMeshSubsystem meshSubsystem = null;
+#if UNITY_2019_3_OR_NEWER
         private static readonly List<XRMeshSubsystem> XRMeshSubsystems = new List<XRMeshSubsystem>();
+#endif // UNITY_2019_3_OR_NEWER
 
         /// <summary>
         /// The XR SDK mesh subsystem for the currently loaded XR plug-in.
@@ -76,7 +80,9 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
         }
 
         private static XRDisplaySubsystem displaySubsystem = null;
+#if UNITY_2019_3_OR_NEWER
         private static readonly List<XRDisplaySubsystem> XRDisplaySubsystems = new List<XRDisplaySubsystem>();
+#endif // UNITY_2019_3_OR_NEWER
 
         /// <summary>
         /// The XR SDK display subsystem for the currently loaded XR plug-in.
