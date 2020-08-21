08d4e9106870d693d8e120cd3ab3f73c4b67ada3
thalbern <bethalha@microsoft.com>
2020-08-21 13:45:47 +0100

    added ignore version folder for root version entry so default version will always fall back to root folder

--- scripts/docs/web/version.js
+++ scripts/docs/web/version.js
@@ -12,6 +12,8 @@ function createDropdown()
 		"releases/2.4.0"
 	];
 	
+	var ignoreDefaultInVersionFolder = true;
+	
 	//--------------------------------------
 	
 	// get web root path
@@ -49,12 +51,22 @@ function createDropdown()
 	{
 		if (versionArray[i] != currentVersionName)
 		{
-			createEntry(innerDiv, versionArray[i], rootDir+"version/"+versionArray[i]+"/README.html", false);
-			
-			// remember if our current version is also the default 
 			if (versionArray[i].localeCompare(defaultTitle) == 0)
 			{
-				isDefaultInVersionFolder = true;
+				if (ignoreDefaultInVersionFolder == false)
+				{
+					// remember if our current version is also the default and replaces the root folder entry
+					isDefaultInVersionFolder = true;
+					createEntry(innerDiv, versionArray[i], rootDir+"version/"+versionArray[i]+"/README.html", false);
+				}
+				else
+				{
+					// skip processing this entry - we'll add the root folder instead for this version
+				}
+			}
+			else
+			{
+				createEntry(innerDiv, versionArray[i], rootDir+"version/"+versionArray[i]+"/README.html", false);
 			}
 		}
 	}
