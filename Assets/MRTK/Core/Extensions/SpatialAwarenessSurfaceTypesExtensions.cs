0e0a387461c241b8f529e0626d32f3899aac1b88
davidkline-ms <david.kline@microsoft.com>
2020-08-24 15:02:03 -0700

    restore wip change accidentally committed

---
+++ Assets/MRTK/Core/Extensions/SpatialAwarenessSurfaceTypesExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Runtime.CompilerServices;
+
+#if ARSUBSYSTEMS_ENABLED
+using UnityEngine.XR.ARSubsystems;
+#endif // ARSUBSYSTEMS_ENABLED
+
+namespace Microsoft.MixedReality.Toolkit
+{
+    public static class SpatialAwarenessSurfaceTypeExtensions
+    {
+#if ARSUBSYSTEMS_ENABLED
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public static PlaneClassification ToPlaneClassification(this SpatialAwarenessSurfaceType)
+        {
+
+        }
+#endif // ARSUBSYSTEMS_ENABLED
+    }
+}
