b595b6a06ae1595a36cfc0e7c4fbc647883bfa38
RogPodge <roliu@microsoft.com>
2020-08-27 15:46:25 -0700

    updated asmdefs to play nice with the ci build pipelines

--- Assets/MRTK/Providers/Oculus/XRSDK/MRTK-Quest/Editor/MRTKQuestConfigurationChecker.cs
+++ Assets/MRTK/Providers/Oculus/XRSDK/MRTK-Quest/Editor/MRTKQuestConfigurationChecker.cs
@@ -54,15 +54,64 @@ namespace Microsoft.MixedReality.Toolkit.Providers.XRSDK.Oculus.Editor
         private static readonly string[] Definitions = { "OCULUSINTEGRATION_PRESENT" };
 
         /// <summary>
+        /// Detects if the Oculus Integration package is present and updates the AsmDefs with the appropriate definitions and references.
+        /// </summary>
+        [MenuItem("Mixed Reality Toolkit/Utilities/Oculus/Integrate Oculus Integration Unity Modules")]
+        private static void ConfigureOculusIntegration()
+        {
+            bool OculusIntegrationPresent = ReconcileOculusIntegrationDefine();
+
+            FileInfo[] oculusXRSDKAsmDefFile = FileUtilities.FindFilesInAssets("Microsoft.MixedReality.Toolkit.Providers.XRSDK.Oculus.asmdef");
+            FileInfo[] oculusXRSDKHandtrackingUtilsAsmDefFile = FileUtilities.FindFilesInAssets("Microsoft.MixedReality.Toolkit.XRSDK.Oculus.Handtracking.Utilities.asmdef");
+            FileInfo[] oculusXRSDKHandtrackingEditorAsmDefFile = FileUtilities.FindFilesInAssets("Microsoft.MixedReality.Toolkit.XRSDK.Oculus.Handtracking.Editor.asmdef");
+
+            List<FileInfo[]> oculusAsmDefFiles = new List<FileInfo[]>() { oculusXRSDKAsmDefFile, oculusXRSDKHandtrackingUtilsAsmDefFile, oculusXRSDKHandtrackingEditorAsmDefFile };
+
+            foreach (FileInfo[] oculusAsmDefFile in oculusAsmDefFiles)
+            {
+                // When MRTK is used through NuGet compiled assemblies, there will not be an asmdef file in the assets directory to configure.
+                if (oculusAsmDefFile.Length == 0)
+                {
+                    return;
+                }
+
+                AssemblyDefinition oculusAsmDef = AssemblyDefinition.Load(oculusAsmDefFile[0].FullName);
+
+                List<string> references = oculusAsmDef.References.ToList();
+
+                if (!OculusIntegrationPresent)
+                {
+                    Debug.Log("Oculus Integration package not detected, removing references from asmdefs");
+                    if (references.Contains("Oculus.VR"))
+                    {
+                        references.Remove("Oculus.VR");
+                    }
+                    if (references.Contains("Oculus.VR.Editor"))
+                    {
+                        references.Remove("Oculus.VR.Editor");
+                    }
+                    oculusAsmDef.References = references.ToArray();
+                }
+                else
+                {
+                    if(oculusAsmDefFile == oculusXRSDKAsmDefFile || oculusAsmDefFile == oculusXRSDKHandtrackingUtilsAsmDefFile)
+                        oculusAsmDef.AddReference("Oculus.VR");
+                    if (oculusAsmDefFile == oculusXRSDKHandtrackingEditorAsmDefFile)
+                        oculusAsmDef.AddReference("Oculus.VR.Editor");
+                }
+                oculusAsmDef.Save(oculusAsmDefFile[0].FullName);
+            }
+        }
+
+        /// <summary>
         /// Updates the assembly definitions to mark the Oculus Integration Asset as present or not present
         /// </summary>
         /// <returns>true if Assets/Oculus/OculusProjectConfig exists, false otherwise</returns>
-        [MenuItem("Mixed Reality Toolkit/Utilities/Oculus/Update Assembly Definitions")]
-        private static bool UpdateAssemblyDefinitions()
+        private static bool ReconcileOculusIntegrationDefine()
         {
             FileInfo[] files = FileUtilities.FindFilesInAssets(OculusIntegrationProjectConfig);
 
-            if (files.Length > 0) 
+            if (files.Length > 0)
             {
                 ScriptUtilities.AppendScriptingDefinitions(BuildTargetGroup.Android, Definitions);
                 ScriptUtilities.AppendScriptingDefinitions(BuildTargetGroup.Standalone, Definitions);
