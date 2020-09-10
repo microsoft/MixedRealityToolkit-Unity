0ae28f2acff7b98c98cc85631643265159df5f0e
CDiaz-MS <cadia@microsoft.com>
2020-09-10 10:06:10 -0700

    Remove override methods from Interactive Element Inspector

--- Assets/MRTK/SDK/Editor/Inspectors/UX/InteractiveElement/InteractiveElementInspector.cs
+++ Assets/MRTK/SDK/Editor/Inspectors/UX/InteractiveElement/InteractiveElementInspector.cs
@@ -11,16 +11,7 @@ namespace Microsoft.MixedReality.Toolkit.Editor
     /// </summary>
     [CustomEditor(typeof(InteractiveElement))]
     public class InteractiveElementInspector : BaseInteractiveElementInspector
-    {
-        protected override void OnEnable()
-        {
-            base.OnEnable();
-        }
-
-        public override void OnInspectorGUI()
-        {
-            // Interactive Element is a place holder class
-            base.OnInspectorGUI();
-        }
+    { 
+       // Interactive Element is a place holder class  
     }
 }
