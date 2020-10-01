338f6e3aa367336769704f583fe9881be1fdae75
mrtk-bld <mrtk-bld@microsoft.com>
2020-10-01 16:58:25 +0000

    Docs for commit 84454ecf5 (Update github.io documentation version list and default)

--- web/version.js
+++ web/version.js
@@ -1,7 +1,8 @@
 function createDropdown()
 {
 	// configurable values:
-	var defaultTitle = "releases/2.4.0"; // title in the dropdown for the root version of the docs - alternatively put a version from the version array as a default
+	var defaultTitle = "releases/2.5.0"; // title in the dropdown for the root version of the docs - alternatively put a version from the version array as a default
+	
 	// list of all versions in the version folder
 	var versionArray = [
 		"mrtk_development",
@@ -9,7 +10,8 @@ function createDropdown()
 		"releases/2.1.0",
 		"releases/2.2.0",
 		"releases/2.3.0",
-		"releases/2.4.0"
+		"releases/2.4.0",
+		"releases/2.5.0",
 	];
 	
 	var ignoreDefaultInVersionFolder = true;
