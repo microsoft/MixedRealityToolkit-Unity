e734f383bee03628a119dc27f05c80cbbdc5a543
RogPodge <roliu@microsoft.com>
2020-10-23 16:55:00 -0700

    fixed the Interactable script to properly initalize the IsToggled Field based on the inspector, tied the feature panel's state to the underlying profiles/application configurations

--- Assets/MRTK/SDK/Features/UX/Scripts/Utilities/ToggleHandVisualisation.cs
+++ Assets/MRTK/SDK/Features/UX/Scripts/Utilities/ToggleHandVisualisation.cs
@@ -9,22 +9,6 @@ namespace Microsoft.MixedReality.Toolkit.UI
     [AddComponentMenu("Scripts/MRTK/SDK/ToggleHandVisualisation")]
     public class ToggleHandVisualisation : MonoBehaviour
     {
-        public bool isHandMeshVisible = false;
-        public bool isHandJointVisible = false;
-
-        /// <summary>
-        /// Updates the hand tracking profile with the current local visualization settings
-        /// </summary>
-        private void UpdateHandVisibility()
-        {
-            MixedRealityHandTrackingProfile handTrackingProfile = CoreServices.InputSystem?.InputSystemProfile?.HandTrackingProfile;
-            if (handTrackingProfile != null)
-            {
-                handTrackingProfile.EnableHandMeshVisualization = isHandMeshVisible;
-                handTrackingProfile.EnableHandJointVisualization = isHandJointVisible;
-            }
-        }
-
         /// <summary>
         /// Toggles hand mesh visualization
         /// </summary>
