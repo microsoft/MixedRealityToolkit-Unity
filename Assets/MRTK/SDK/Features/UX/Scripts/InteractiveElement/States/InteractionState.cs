5c1af87c2fbaa5cffcf8db560255b2e5351a4b2e
Catherine Diaz <cadia@microsoft.com>
2020-10-29 11:37:47 -0700

    Change the event configuration to a serialized interface instead of a Scriptable Object

--- Assets/MRTK/SDK/Features/UX/Scripts/InteractiveElement/States/InteractionState.cs
+++ Assets/MRTK/SDK/Features/UX/Scripts/InteractiveElement/States/InteractionState.cs
@@ -1,13 +1,14 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License
 
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System;
 using UnityEngine;
 
 namespace Microsoft.MixedReality.Toolkit.UI.Interaction
 {
     /// <summary>
-    /// The container that represents a single state that is tracked within an Interactive Element.  The Tracked States 
-    /// scriptable object contains a list of Interaction States to track.
+    /// The container that represents a single Interaction State. This class is utilited in the BaseInteractiveElement MonoBehaviour.
     /// </summary>
     [System.Serializable]
     public class InteractionState
@@ -19,6 +20,7 @@ namespace Microsoft.MixedReality.Toolkit.UI.Interaction
         public InteractionState(string stateName)
         {
             Name = stateName;
+            SetEventConfiguration(Name);
         }
 
         [SerializeField]
@@ -44,45 +46,61 @@ namespace Microsoft.MixedReality.Toolkit.UI.Interaction
         public int Value
         {
             get => stateValue;
-            internal set => stateValue = value; 
-        }        
-        
+            internal set => stateValue = value;
+        }
+
         [SerializeField]
-        [Tooltip("The active value of the state. If the state is currently active the value will be 1, 0 if the " +
-            " state is not active.")]
+        [Tooltip("Whether or not the state is currently active.")]
         private bool active = false;
 
         /// <summary>
-        /// The active value of the state. If the state is currently active the value will be 1, 0 if the 
-        /// state is not active.
+        /// Whether or not the state is currently active.
         /// </summary>
         public bool Active
         {
             get => active;
-            internal set => active = value; 
+            internal set => active = value;
         }
 
-        [SerializeField]
-        [Tooltip("The event configuration of a state. The event configuration is an already defined scriptable object that contains the " +
-            "serialized Unity Events with specific event data for the state. For example, the FocusEvents contains serialized" +
-            "OnFocusOn and OnFocusOff Unity Events with Focus Event Data. These events are exposed in the inspector." +
-            "The event configuration of a state is null when an associated event configuration does not exist" +
-            "for the specific state. For example, the Default state does not have an associated event configuration therefore EventConfiguration " +
-            "will always be null.")]
-        private BaseInteractionEventConfiguration eventConfiguration = null;
+        [SerializeReference]
+        [Tooltip("The event configuration for this state. ")]
+        private IStateEventConfig eventConfiguration = null;
 
         /// <summary>
-        /// The event configuration of a state. The event configuration is an already defined scriptable object that contains the 
-        /// serialized Unity Events with specific event data for the state. For example, the FocusEvents contains serialized
-        /// OnFocusOn and OnFocusOff Unity Events with Focus Event Data. These events are exposed in the inspector.
-        /// The event configuration of a state is null when an associated event configuration does not exist
-        /// for the specific state. For example, the Default state does not have an associated event configuration therefore EventConfiguration 
-        /// will always be null.
+        /// The event configuration for this state. 
         /// </summary>
-        public BaseInteractionEventConfiguration EventConfiguration
+        public IStateEventConfig EventConfiguration
         {
             get => eventConfiguration;
             internal set => eventConfiguration = value;
+        }
+
+        // Set the event configuration for a new state
+        private void SetEventConfiguration(string stateName)
+        {
+            if (EventConfiguration == null)
+            {
+                BaseInteractionEventConfiguration eventConfiguration;
+
+                // Find matching event configuration by state name
+                var eventConfigTypes = TypeCacheUtility.GetSubClasses<BaseInteractionEventConfiguration>();
+                Type eventConfigType = eventConfigTypes.Find((type) => type.Name.StartsWith(stateName));
+
+                if (eventConfigType != null)
+                {
+                    // If a state has an associated event configuration class, then create an instance with the matching type
+                    eventConfiguration = Activator.CreateInstance(eventConfigType) as BaseInteractionEventConfiguration;
+                }
+                else
+                {
+                    // If the state does not have a specific event configuration class type, create the an instance of StateEvents.  
+                    // StateEvents is the default type for a state's event configuration when a matching event configuration type does not exist.
+                    eventConfiguration = Activator.CreateInstance(typeof(StateEvents)) as BaseInteractionEventConfiguration;
+                }
+
+                eventConfiguration.StateName = stateName;
+                EventConfiguration = eventConfiguration;
+            }
         }
     }
 }
