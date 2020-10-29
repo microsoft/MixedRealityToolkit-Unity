cca8abf44c8f58f4591c4a3a8a7a45258ced447b
Dominic Aglialoro <doaglial@microsoft.com>
2020-10-28 21:35:38 -0700

    Add Input Recording and Playback Services

---
+++ Assets/MRTK/Services/InputAnimation/InputRecordingBuffer.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Input
+{
+    /// <summary>
+    /// Container used to efficiently store a sequence of input animation keyframes while recording
+    /// </summary>
+    public class InputRecordingBuffer : IEnumerable<InputRecordingBuffer.Keyframe>
+    {
+        /// <summary>
+        /// The input state for a single frame
+        /// </summary>
+        public class Keyframe
+        {
+            public float Time;
+            public bool LeftTracked;
+            public bool RightTracked;
+            public bool LeftPinch;
+            public bool RightPinch;
+            public MixedRealityPose CameraPose;
+            public Ray GazeRay;
+            public Dictionary<TrackedHandJoint, MixedRealityPose> LeftJoints;
+            public Dictionary<TrackedHandJoint, MixedRealityPose> RightJoints;
+
+            public Keyframe(float time)
+            {
+                Time = time;
+                LeftJoints = new Dictionary<TrackedHandJoint, MixedRealityPose>();
+                RightJoints = new Dictionary<TrackedHandJoint, MixedRealityPose>();
+            }
+        }
+
+        /// <summary>
+        /// The time of the first keyframe in the buffer
+        /// </summary>
+        public float StartTime => keyframes.Peek().Time;
+        
+        private Keyframe currentKeyframe;
+        private Queue<Keyframe> keyframes;
+
+        public InputRecordingBuffer() => keyframes = new Queue<Keyframe>();
+
+        /// <summary>
+        /// Removes all keyframes from the buffer
+        /// </summary>
+        public void Clear()
+        {
+            keyframes.Clear();
+            currentKeyframe = null;
+        }
+
+        /// <summary>
+        /// Removes all keyframes before a given time
+        /// </summary>
+        public void RemoveBeforeTime(float time)
+        {
+            while (keyframes.Count > 0 && keyframes.Peek().Time < time)
+            {
+                keyframes.Dequeue();
+            }
+        }
+
+        /// <summary>
+        /// Sets the camera pose to be stored in the newest keyframe
+        /// </summary>
+        public void SetCameraPose(MixedRealityPose pose) => currentKeyframe.CameraPose = pose;
+
+        /// <summary>
+        /// Sets the eye gaze ray to be stored in the newest keyframe
+        /// </summary>
+        public void SetGazeRay(Ray ray) => currentKeyframe.GazeRay = ray;
+
+        /// <summary>
+        /// Sets the state of a given hand to be stored in the newest keyframe
+        /// </summary>
+        public void SetHandState(Handedness handedness, bool tracked, bool pinching)
+        {
+            if (handedness == Handedness.Left)
+            {
+                currentKeyframe.LeftTracked = tracked;
+                currentKeyframe.LeftPinch = pinching;
+            }
+            else
+            {
+                currentKeyframe.RightTracked = tracked;
+                currentKeyframe.RightPinch = pinching;
+            }
+        }
+
+        /// <summary>
+        /// Sets the pose of a given joint to be stored in the newest keyframe
+        /// </summary>
+        public void SetJointPose(Handedness handedness, TrackedHandJoint joint, MixedRealityPose pose)
+        {
+            if (handedness == Handedness.Left)
+            {
+                currentKeyframe.LeftJoints.Add(joint, pose);
+            }
+            else
+            {
+                currentKeyframe.RightJoints.Add(joint, pose);
+            }
+        }
+
+        /// <summary>
+        /// Creates a new, empty keyframe with a given time at the end of the buffer
+        /// </summary>
+        /// <returns>The index of the new keyframe</returns>
+        public int NewKeyframe(float time)
+        {
+            currentKeyframe = new Keyframe(time);
+            keyframes.Enqueue(currentKeyframe);
+
+            return keyframes.Count - 1;
+        }
+
+        /// <summary>
+        /// Returns a sequence of all keyframes in the buffer
+        /// </summary>
+        public IEnumerator<Keyframe> GetEnumerator() => keyframes.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ Assets/MRTK/Services/InputAnimation/InputRecordingService.cs
@@ -0,0 +1,405 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.MixedReality.Toolkit.Utilities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Input
+{
+    /// <summary>
+    /// Provides input recording into an internal buffer and exporting to files.
+    /// </summary>
+    [MixedRealityDataProvider(
+        typeof(IMixedRealityInputSystem),
+        (SupportedPlatforms)(-1), // Supported on all platforms
+        "Input Recording Service",
+        "Profiles/DefaultMixedRealityInputRecordingProfile.asset",
+        "MixedRealityToolkit.SDK",
+        true)]
+    public class InputRecordingService :
+        BaseInputDeviceManager,
+        IMixedRealityInputRecordingService
+    {
+        private static readonly int jointCount = Enum.GetNames(typeof(TrackedHandJoint)).Length;
+
+        public event Action OnRecordingStarted;
+        public event Action OnRecordingStopped;
+
+        /// <summary>
+        /// Service has been enabled.
+        /// </summary>
+        public bool IsEnabled { get; private set; } = false;
+
+        /// <inheritdoc />
+        public bool IsRecording { get; private set; } = false;
+
+        private bool useBufferTimeLimit = true;
+        /// <inheritdoc />
+        public bool UseBufferTimeLimit
+        {
+            get { return useBufferTimeLimit; }
+            set
+            {
+                if (useBufferTimeLimit && !value)
+                {
+                    // Start at buffer limit when making buffer unlimited
+                    unlimitedRecordingStartTime = StartTime;
+                }
+
+                useBufferTimeLimit = value;
+                
+                if (useBufferTimeLimit)
+                {
+                    PruneBuffer();
+                }
+            }
+        }
+
+        private float recordingBufferTimeLimit = 30.0f;
+        /// <inheritdoc />
+        public float RecordingBufferTimeLimit
+        {
+            get { return recordingBufferTimeLimit; }
+            set
+            {
+                recordingBufferTimeLimit = Mathf.Max(value, 0.0f);
+                
+                if (useBufferTimeLimit)
+                {
+                    PruneBuffer();
+                }
+            }
+        }
+
+        // Start time of recording if buffer is unlimited.
+        // Nullable to determine when time needs to be reset.
+        private float? unlimitedRecordingStartTime = null;
+        /// Start time of recording.
+        public float StartTime
+        {
+            get
+            {
+                if (unlimitedRecordingStartTime.HasValue)
+                {
+                    if (useBufferTimeLimit)
+                    {
+                        return Mathf.Max(unlimitedRecordingStartTime.Value, EndTime - recordingBufferTimeLimit);
+                    }
+                    else
+                    {
+                        return unlimitedRecordingStartTime.Value;
+                    }
+                }
+                
+                return EndTime;
+            }
+        }
+        
+        /// End time of recording.
+        public float EndTime { get; private set; }
+
+        /// The profile used for recording.
+        public MixedRealityInputRecordingProfile InputRecordingProfile
+        {
+            get
+            {
+                var profile = ConfigurationProfile as MixedRealityInputRecordingProfile;
+                
+                if (!profile)
+                {
+                    Debug.LogError("Profile for Input Recording Service must be a MixedRealityInputRecordingProfile");
+                }
+                
+                return profile;
+            }
+        }
+
+        private float frameRate;
+        private float frameInterval;
+        private float nextFrame;
+        private InputRecordingBuffer recordingBuffer = null;
+        private IMixedRealityGazeProvider eyeGazeProvider;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="registrar">The <see cref="IMixedRealityServiceRegistrar"/> instance that loaded the data provider.</param>
+        /// <param name="inputSystem">The <see cref="Microsoft.MixedReality.Toolkit.Input.IMixedRealityInputSystem"/> instance that receives data from this provider.</param>
+        /// <param name="name">Friendly name of the service.</param>
+        /// <param name="priority">Service priority. Used to determine order of instantiation.</param>
+        /// <param name="profile">The service's configuration profile.</param>
+        [Obsolete("This constructor is obsolete (registrar parameter is no longer required) and will be removed in a future version of the Microsoft Mixed Reality Toolkit.")]
+        public InputRecordingService(
+            IMixedRealityServiceRegistrar registrar,
+            IMixedRealityInputSystem inputSystem,
+            string name = null,
+            uint priority = DefaultPriority,
+            BaseMixedRealityProfile profile = null) : this( inputSystem, name, priority, profile)
+        {
+            Registrar = registrar;
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="inputSystem">The <see cref="Microsoft.MixedReality.Toolkit.Input.IMixedRealityInputSystem"/> instance that receives data from this provider.</param>
+        /// <param name="name">Friendly name of the service.</param>
+        /// <param name="priority">Service priority. Used to determine order of instantiation.</param>
+        /// <param name="profile">The service's configuration profile.</param>
+        public InputRecordingService(
+            IMixedRealityInputSystem inputSystem,
+            string name = null,
+            uint priority = DefaultPriority,
+            BaseMixedRealityProfile profile = null) : base(inputSystem, name, priority, profile)
+        { }
+
+        /// <inheritdoc />
+        public override void Enable()
+        {
+            IsEnabled = true;
+            recordingBuffer = new InputRecordingBuffer();
+        }
+
+        /// <inheritdoc />
+        public override void Disable()
+        {
+            IsEnabled = false;
+            recordingBuffer = null;
+            ResetStartTime();
+        }
+
+        /// <inheritdoc />
+        public void StartRecording()
+        {
+            eyeGazeProvider = CoreServices.InputSystem.GazeProvider;
+            IsRecording = true;
+            frameRate = InputRecordingProfile.FrameRate;
+            frameInterval = 1f / frameRate;
+            nextFrame = Time.time + frameInterval;
+            
+            if (UseBufferTimeLimit)
+            {
+                PruneBuffer();
+            }
+            if (!unlimitedRecordingStartTime.HasValue)
+            {
+                unlimitedRecordingStartTime = Time.time;
+            }
+
+            OnRecordingStarted?.Invoke();
+        }
+
+        /// <inheritdoc />
+        public void StopRecording()
+        {
+            IsRecording = false;
+            OnRecordingStopped?.Invoke();
+        }
+
+        /// <inheritdoc />
+        public override void LateUpdate()
+        {
+            if (IsEnabled && IsRecording && Time.time > nextFrame)
+            {
+                EndTime = Time.time;
+                nextFrame += frameInterval * (Mathf.Floor((Time.time - nextFrame) * frameRate) + 1f);
+                
+                if (UseBufferTimeLimit)
+                {
+                    PruneBuffer();
+                }
+
+                RecordKeyframe();
+            }
+        }
+
+        /// <inheritdoc />
+        public void DiscardRecordedInput()
+        {
+            if (IsEnabled)
+            {
+                recordingBuffer.Clear();
+                ResetStartTime();
+            }
+        }
+
+        /// <inheritdoc />
+        public string SaveInputAnimation(string directory = null) => SaveInputAnimation(InputAnimationSerializationUtils.GetOutputFilename(), directory);
+        
+        /// <inheritdoc />
+        public string SaveInputAnimation(string filename, string directory)
+        {
+            if (IsEnabled)
+            {
+                string path = Path.Combine(directory ?? Application.persistentDataPath, filename);
+
+                try
+                {
+                    using (Stream fileStream = File.Open(path, FileMode.Create))
+                    {
+                        PruneBuffer();
+                        
+                        var animation = InputAnimation.FromRecordingBuffer(recordingBuffer, InputRecordingProfile);
+                        
+                        Debug.Log($"Recording buffer saved to animation");
+                        animation.ToStream(fileStream, 0f);
+                        Debug.Log($"Recorded input animation exported to {path}");
+                    }
+                    
+                    return path;
+                }
+                catch (IOException ex)
+                {
+                    Debug.LogWarning(ex.Message);
+                }
+            }
+            
+            return "";
+        }
+        
+        /// <inheritdoc />
+        public Task<string> SaveInputAnimationAsync(string directory = null) => SaveInputAnimationAsync(InputAnimationSerializationUtils.GetOutputFilename(), directory);
+
+        /// <inheritdoc />
+        public async Task<string> SaveInputAnimationAsync(string filename, string directory)
+        {
+            if (IsEnabled)
+            {
+                string path = Path.Combine(directory ?? Application.persistentDataPath, filename);
+
+                try
+                {
+                    using (Stream fileStream = File.Open(path, FileMode.Create))
+                    {
+                        PruneBuffer();
+
+                        var animation = await Task.Run(() => InputAnimation.FromRecordingBuffer(recordingBuffer, InputRecordingProfile));
+                        
+                        Debug.Log($"Recording buffer saved to animation");
+
+                        await animation.ToStreamAsync(fileStream, 0f);
+                        
+                        Debug.Log($"Recorded input animation exported to {path}");
+                    }
+                    
+                    return path;
+                }
+                catch (IOException ex)
+                {
+                    Debug.LogWarning(ex.Message);
+                }
+            }
+            
+            return "";
+        }
+
+        private void ResetStartTime()
+        {
+            if (IsRecording)
+            {
+                unlimitedRecordingStartTime = Time.time;
+            }
+            else
+            {
+                unlimitedRecordingStartTime = null;
+            }
+        }
+
+        /// <summary>
+        /// Record a keyframe at the given time for the main camera and tracked input devices.
+        /// </summary>
+        private void RecordKeyframe()
+        {
+            float time = Time.time;
+            var profile = InputRecordingProfile;
+
+            recordingBuffer.NewKeyframe(time);
+
+            if (profile.RecordHandData)
+            {
+                RecordInputHandData(Handedness.Left);
+                RecordInputHandData(Handedness.Right);
+            }
+
+            MixedRealityPose cameraPose;
+            
+            if (profile.RecordCameraPose && CameraCache.Main)
+            {
+                cameraPose = new MixedRealityPose(CameraCache.Main.transform.position, CameraCache.Main.transform.rotation);
+                recordingBuffer.SetCameraPose(cameraPose);
+            }
+            else
+            {
+                cameraPose = new MixedRealityPose(Vector3.zero, Quaternion.identity);
+            }
+
+            if (profile.RecordEyeGaze)
+            {
+                if (eyeGazeProvider != null)
+                {
+                    recordingBuffer.SetGazeRay(new Ray(eyeGazeProvider.GazeOrigin, eyeGazeProvider.GazeDirection));
+                }
+                else 
+                {
+                    recordingBuffer.SetGazeRay(new Ray(cameraPose.Position, cameraPose.Forward));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Record a keyframe at the given time for a hand with the given handedness it is tracked.
+        /// </summary>
+        private void RecordInputHandData(Handedness handedness)
+        {
+            float time = Time.time;
+            var profile = InputRecordingProfile;
+
+            var hand = HandJointUtils.FindHand(handedness);
+            if (hand == null)
+            {
+                recordingBuffer.SetHandState(handedness, false, false);
+
+                return;
+            }
+
+            bool isTracked = (hand.TrackingState == TrackingState.Tracked);
+
+            // Extract extra information from current interactions
+            bool isPinching = false;
+            for (int i = 0; i < hand.Interactions?.Length; i++)
+            {
+                var interaction = hand.Interactions[i];
+                switch (interaction.InputType)
+                {
+                    case DeviceInputType.Select:
+                        isPinching = interaction.BoolData;
+                        break;
+                }
+            }
+
+            recordingBuffer.SetHandState(handedness, isTracked, isPinching);
+
+            if (isTracked)
+            {
+                for (int i = 0; i < jointCount; ++i)
+                {
+                    if (hand.TryGetJoint((TrackedHandJoint)i, out MixedRealityPose jointPose))
+                    {
+                        recordingBuffer.SetJointPose(handedness, (TrackedHandJoint)i, jointPose);
+                    }
+                }
+            }
+        }
+
+        /// Discard keyframes before the cutoff time.
+        private void PruneBuffer()
+        {
+            recordingBuffer.RemoveBeforeTime(StartTime);
+        }
+    }
+}
