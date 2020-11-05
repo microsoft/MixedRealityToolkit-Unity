53361e52425a891a20d47be51442f9a3441abdd8
Kurtis <kurtie@microsoft.com>
2020-11-05 10:23:33 -0800

    Remove extra whitespace

--- Assets/MRTK/Core/Providers/InputAnimation/InputAnimation.cs
+++ Assets/MRTK/Core/Providers/InputAnimation/InputAnimation.cs
@@ -44,7 +44,6 @@ namespace Microsoft.MixedReality.Toolkit.Input
         [SerializeField]
         private float duration = 0.0f;
         public float Duration => duration;
-        
         public class PoseCurves
         {
             public AnimationCurve PositionX = new AnimationCurve();
