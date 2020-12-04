30ee0ff8b8b4e812a6823331efa66c50c917cb53
Yoon Park <cre8ive.park@gmail.com>
2020-12-04 19:10:49 +0900

    Hand Menu polish - Accessibility - adding voice command and follow solver for hands-free scenario

---
+++ Assets/MRTK/Examples/Demos/HandTracking/Scripts/SolverTrackedTargetType.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Microsoft.MixedReality.Toolkit.Examples.Demos
+{
+    public class SolverTrackedTargetType : MonoBehaviour
+    {
+        [SerializeField]
+        private SolverHandler solverHandler;
+
+        public void ChangeTrackedTargetTypeHead()
+        {
+            solverHandler = gameObject.GetComponent<SolverHandler>();
+
+            if(solverHandler != null)
+            {
+                solverHandler.TrackedTargetType = TrackedObjectType.Head;
+            }
+        }
+
+        public void ChangeTrackedTargetTypeHandJoint()
+        {
+            solverHandler = gameObject.GetComponent<SolverHandler>();
+
+            if (solverHandler != null)
+            {
+                solverHandler.TrackedTargetType = TrackedObjectType.HandJoint;
+            }
+        }
+
+    }
+}
