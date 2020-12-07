58d0ffb71d74d3b95c836c7c8d353f2fd3a529d7
Kurtis <kurtie@microsoft.com>
2020-12-07 13:21:56 -0800

    Merge pull request #9005 from keveleigh/cherrypick-5c2bb63bdc71b6a20540bf6e022d6885d702dca7
    Cherry-pick pull request #8909 to 2.5.2

--- Assets/MRTK/Core/Definitions/Utilities/MixedRealityTransform.cs
+++ Assets/MRTK/Core/Definitions/Utilities/MixedRealityTransform.cs
@@ -108,6 +108,7 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
 
         #region IEqualityComparer Implementation
 
+        /// <inheritdoc />
         bool IEqualityComparer.Equals(object left, object right)
         {
             if (ReferenceEquals(null, left) || ReferenceEquals(null, right)) { return false; }
@@ -127,12 +128,12 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
             return obj is MixedRealityTransform && Equals((MixedRealityTransform)obj);
         }
 
+        /// <inheritdoc />
         int IEqualityComparer.GetHashCode(object obj)
         {
             return obj is MixedRealityTransform ? ((MixedRealityTransform)obj).GetHashCode() : 0;
         }
 
-        /// <inheritdoc />
         public override int GetHashCode()
         {
             return base.GetHashCode();
