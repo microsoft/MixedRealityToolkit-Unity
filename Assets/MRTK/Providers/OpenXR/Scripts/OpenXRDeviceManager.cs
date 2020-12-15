997e37a7c9e83d5077f76719d1fb7d0227d516fe
Kurtis <kurtie@microsoft.com>
2020-12-15 11:54:41 -0800

    Drop OpenXR scripts

---
+++ Assets/MRTK/Providers/OpenXR/Scripts/OpenXRDeviceManager.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.XRSDK.Input;
+using System;
+using UnityEngine.XR;
+
+namespace Microsoft.MixedReality.Toolkit.XRSDK.OpenXR
+{
+    [MixedRealityDataProvider(
+        typeof(IMixedRealityInputSystem),
+        (SupportedPlatforms)(-1),
+        "OpenXR XRSDK Device Manager")]
+    public class OpenXRDeviceManager : XRSDKDeviceManager
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="inputSystem">The <see cref="Microsoft.MixedReality.Toolkit.Input.IMixedRealityInputSystem"/> instance that receives data from this provider.</param>
+        /// <param name="name">Friendly name of the service.</param>
+        /// <param name="priority">Service priority. Used to determine order of instantiation.</param>
+        /// <param name="profile">The service's configuration profile.</param>
+        public OpenXRDeviceManager(
+            IMixedRealityInputSystem inputSystem,
+            string name = null,
+            uint priority = DefaultPriority,
+            BaseMixedRealityProfile profile = null) : base(inputSystem, name, priority, profile) { }
+
+        #region Controller Utilities
+
+        /// <summary>
+        /// The OpenXR plugin uses extensions to expose all possible data, which might be surfaced through multiple input devices.
+        /// This method is overridden to account for multiple input devices and reuse MRTK controllers if a match is found.
+        /// </summary>
+        protected override GenericXRSDKController GetOrAddController(InputDevice inputDevice)
+        {
+            // If this is a new input device, search if an existing input device has matching characteristics
+            if (!ActiveControllers.ContainsKey(inputDevice))
+            {
+                foreach (InputDevice device in ActiveControllers.Keys)
+                {
+                    if (((device.characteristics.HasFlag(InputDeviceCharacteristics.Controller) && inputDevice.characteristics.HasFlag(InputDeviceCharacteristics.Controller))
+                        || (device.characteristics.HasFlag(InputDeviceCharacteristics.HandTracking) && inputDevice.characteristics.HasFlag(InputDeviceCharacteristics.HandTracking)))
+                        && ((device.characteristics.HasFlag(InputDeviceCharacteristics.Left) && inputDevice.characteristics.HasFlag(InputDeviceCharacteristics.Left))
+                        || (device.characteristics.HasFlag(InputDeviceCharacteristics.Right) && inputDevice.characteristics.HasFlag(InputDeviceCharacteristics.Right))))
+                    {
+                        ActiveControllers.Add(inputDevice, ActiveControllers[device]);
+                        break;
+                    }
+                }
+            }
+
+            return base.GetOrAddController(inputDevice);
+        }
+
+        /// <inheritdoc />
+        protected override Type GetControllerType(SupportedControllerType supportedControllerType)
+        {
+            switch (supportedControllerType)
+            {
+                case SupportedControllerType.WindowsMixedReality:
+                    return typeof(WindowsMixedRealityOpenXRController);
+                case SupportedControllerType.ArticulatedHand:
+                    return typeof(WindowsMixedRealityOpenXRArticulatedHand);
+                default:
+                    return base.GetControllerType(supportedControllerType);
+            }
+        }
+
+        /// <inheritdoc />
+        protected override InputSourceType GetInputSourceType(SupportedControllerType supportedControllerType)
+        {
+            switch (supportedControllerType)
+            {
+                case SupportedControllerType.WindowsMixedReality:
+                    return InputSourceType.Controller;
+                case SupportedControllerType.ArticulatedHand:
+                    return InputSourceType.Hand;
+                default:
+                    return base.GetInputSourceType(supportedControllerType);
+            }
+        }
+
+        /// <inheritdoc />
+        protected override SupportedControllerType GetCurrentControllerType(InputDevice inputDevice)
+        {
+            if (inputDevice.characteristics.HasFlag(InputDeviceCharacteristics.HandTracking))
+            {
+                return SupportedControllerType.ArticulatedHand;
+            }
+
+            if (inputDevice.characteristics.HasFlag(InputDeviceCharacteristics.Controller))
+            {
+                return SupportedControllerType.WindowsMixedReality;
+            }
+
+            return base.GetCurrentControllerType(inputDevice);
+        }
+
+        #endregion Controller Utilities
+    }
+}
---
+++ Assets/MRTK/Providers/OpenXR/Scripts/WindowsMixedRealityOpenXRArticulatedHand.cs
@@ -0,0 +1,284 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.XRSDK.Input;
+using System;
+using System.Collections.Generic;
+using Unity.Profiling;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace Microsoft.MixedReality.Toolkit.XRSDK.OpenXR
+{
+    /// <summary>
+    /// Open XR + XR SDK implementation of Windows Mixed Reality articulated hands.
+    /// </summary>
+    [MixedRealityController(
+        SupportedControllerType.ArticulatedHand,
+        new[] { Handedness.Left, Handedness.Right })]
+    public class WindowsMixedRealityOpenXRArticulatedHand : GenericXRSDKController, IMixedRealityHand
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public WindowsMixedRealityOpenXRArticulatedHand(TrackingState trackingState, Handedness controllerHandedness, IMixedRealityInputSource inputSource = null, MixedRealityInteractionMapping[] interactions = null)
+                : base(trackingState, controllerHandedness, inputSource, interactions)
+        {
+            handDefinition = new ArticulatedHandDefinition(inputSource, controllerHandedness);
+        }
+
+        /// <inheritdoc />
+        public override MixedRealityInteractionMapping[] DefaultInteractions => handDefinition?.DefaultInteractions;
+
+        protected readonly Dictionary<TrackedHandJoint, MixedRealityPose> unityJointPoses = new Dictionary<TrackedHandJoint, MixedRealityPose>();
+        protected readonly ArticulatedHandDefinition handDefinition;
+
+        private static readonly HandFinger[] handFingers = Enum.GetValues(typeof(HandFinger)) as HandFinger[];
+        private readonly List<Bone> fingerBones = new List<Bone>();
+
+        private Vector3 currentPointerPosition = Vector3.zero;
+        private Quaternion currentPointerRotation = Quaternion.identity;
+        private MixedRealityPose currentPointerPose = MixedRealityPose.ZeroIdentity;
+
+        #region IMixedRealityHand Implementation
+
+        /// <inheritdoc/>
+        public bool TryGetJoint(TrackedHandJoint joint, out MixedRealityPose pose) => unityJointPoses.TryGetValue(joint, out pose);
+
+        #endregion IMixedRealityHand Implementation
+
+        /// <inheritdoc/>
+        public override bool IsInPointingPose => handDefinition.IsInPointingPose;
+
+        private static readonly ProfilerMarker UpdateControllerPerfMarker = new ProfilerMarker("[MRTK] WindowsMixedRealityOpenXRArticulatedHand.UpdateController");
+
+        /// <summary>
+        /// The OpenXR plug-in uses extensions to expose all possible data, which might be surfaced through multiple input devices.
+        /// This method is overridden to account for multiple input devices.
+        /// </summary>
+        /// <param name="inputDevice">The current input device to grab data from.</param>
+        public override void UpdateController(InputDevice inputDevice)
+        {
+            if (!Enabled) { return; }
+
+            if (Interactions == null)
+            {
+                Debug.LogError($"No interaction configuration for {GetType().Name}");
+                Enabled = false;
+            }
+
+            using (UpdateControllerPerfMarker.Auto())
+            {
+                if (inputDevice.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 _))
+                {
+                    base.UpdateController(inputDevice);
+                }
+                else
+                {
+                    UpdateHandData(inputDevice);
+
+                    for (int i = 0; i < Interactions?.Length; i++)
+                    {
+                        switch (Interactions[i].AxisType)
+                        {
+                            case AxisType.SixDof:
+                                UpdatePoseData(Interactions[i], inputDevice);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static readonly ProfilerMarker UpdateSingleAxisDataPerfMarker = new ProfilerMarker("[MRTK] WindowsMixedRealityOpenXRArticulatedHand.UpdateSingleAxisData");
+
+        /// <inheritdoc />
+        protected override void UpdateSingleAxisData(MixedRealityInteractionMapping interactionMapping, InputDevice inputDevice)
+        {
+            using (UpdateSingleAxisDataPerfMarker.Auto())
+            {
+                Debug.Assert(interactionMapping.AxisType == AxisType.SingleAxis);
+                // Update the interaction data source
+                switch (interactionMapping.InputType)
+                {
+                    case DeviceInputType.TriggerPress:
+                    case DeviceInputType.GripPress:
+                        if (inputDevice.TryGetFeatureValue(CommonUsages.grip, out float gripAmount))
+                        {
+                            interactionMapping.BoolData = Mathf.Approximately(gripAmount, 1.0f);
+                        }
+                        break;
+                    default:
+                        base.UpdateSingleAxisData(interactionMapping, inputDevice);
+                        return;
+                }
+
+                // If our value changed raise it.
+                if (interactionMapping.Changed)
+                {
+                    // Raise bool input system event if it's available
+                    if (interactionMapping.BoolData)
+                    {
+                        CoreServices.InputSystem?.RaiseOnInputDown(InputSource, ControllerHandedness, interactionMapping.MixedRealityInputAction);
+                    }
+                    else
+                    {
+                        CoreServices.InputSystem?.RaiseOnInputUp(InputSource, ControllerHandedness, interactionMapping.MixedRealityInputAction);
+                    }
+                }
+            }
+        }
+
+        private static readonly ProfilerMarker UpdateButtonDataPerfMarker = new ProfilerMarker("[MRTK] WindowsMixedRealityOpenXRArticulatedHand.UpdateButtonData");
+
+        /// <inheritdoc />
+        protected override void UpdateButtonData(MixedRealityInteractionMapping interactionMapping, InputDevice inputDevice)
+        {
+            using (UpdateButtonDataPerfMarker.Auto())
+            {
+                Debug.Assert(interactionMapping.AxisType == AxisType.Digital);
+
+                // Update the interaction data source
+                switch (interactionMapping.InputType)
+                {
+                    case DeviceInputType.Select:
+                        if (inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool buttonPressed))
+                        {
+                            interactionMapping.BoolData = buttonPressed;
+                        }
+                        break;
+                    default:
+                        base.UpdateButtonData(interactionMapping, inputDevice);
+                        return;
+                }
+
+                // If our value changed raise it.
+                if (interactionMapping.Changed)
+                {
+                    // Raise input system event if it's enabled
+                    if (interactionMapping.BoolData)
+                    {
+                        CoreServices.InputSystem?.RaiseOnInputDown(InputSource, ControllerHandedness, interactionMapping.MixedRealityInputAction);
+                    }
+                    else
+                    {
+                        CoreServices.InputSystem?.RaiseOnInputUp(InputSource, ControllerHandedness, interactionMapping.MixedRealityInputAction);
+                    }
+                }
+            }
+        }
+
+        private static readonly ProfilerMarker UpdatePoseDataPerfMarker = new ProfilerMarker("[MRTK] WindowsMixedRealityOpenXRArticulatedHand.UpdatePoseData");
+
+        /// <inheritdoc />
+        protected override void UpdatePoseData(MixedRealityInteractionMapping interactionMapping, InputDevice inputDevice)
+        {
+            using (UpdatePoseDataPerfMarker.Auto())
+            {
+                switch (interactionMapping.InputType)
+                {
+                    case DeviceInputType.IndexFinger:
+                        handDefinition?.UpdateCurrentIndexPose(interactionMapping);
+                        break;
+                    case DeviceInputType.SpatialPointer:
+                        if (inputDevice.TryGetFeatureValue(Input.CustomUsages.PointerPosition, out currentPointerPosition))
+                        {
+                            currentPointerPose.Position = MixedRealityPlayspace.TransformPoint(currentPointerPosition);
+                        }
+
+                        if (inputDevice.TryGetFeatureValue(Input.CustomUsages.PointerRotation, out currentPointerRotation))
+                        {
+                            currentPointerPose.Rotation = MixedRealityPlayspace.Rotation * currentPointerRotation;
+                        }
+
+                        interactionMapping.PoseData = currentPointerPose;
+
+                        // If our value changed raise it.
+                        if (interactionMapping.Changed)
+                        {
+                            // Raise input system event if it's enabled
+                            CoreServices.InputSystem?.RaisePoseInputChanged(InputSource, ControllerHandedness, interactionMapping.MixedRealityInputAction, interactionMapping.PoseData);
+                        }
+                        break;
+                    default:
+                        base.UpdatePoseData(interactionMapping, inputDevice);
+                        break;
+                }
+            }
+        }
+
+        private static readonly ProfilerMarker UpdateHandDataPerfMarker = new ProfilerMarker("[MRTK] WindowsMixedRealityOpenXRArticulatedHand.UpdateHandData");
+
+        /// <summary>
+        /// Update the hand data from the device.
+        /// </summary>
+        /// <param name="interactionSourceState">The InteractionSourceState retrieved from the platform.</param>
+        private void UpdateHandData(InputDevice inputDevice)
+        {
+            using (UpdateHandDataPerfMarker.Auto())
+            {
+                if (inputDevice.TryGetFeatureValue(CommonUsages.handData, out Hand hand))
+                {
+                    foreach (HandFinger finger in handFingers)
+                    {
+                        if (hand.TryGetRootBone(out Bone rootBone))
+                        {
+                            ReadHandJoint(TrackedHandJoint.Wrist, rootBone);
+                        }
+
+                        if (hand.TryGetFingerBones(finger, fingerBones))
+                        {
+                            for (int i = 0; i < fingerBones.Count; i++)
+                            {
+                                ReadHandJoint(ConvertToTrackedHandJoint(finger, i), fingerBones[i]);
+                            }
+                        }
+                    }
+
+                    handDefinition?.UpdateHandJoints(unityJointPoses);
+                }
+            }
+        }
+
+        private void ReadHandJoint(TrackedHandJoint trackedHandJoint, Bone bone)
+        {
+            bool positionAvailable = bone.TryGetPosition(out Vector3 position);
+            bool rotationAvailable = bone.TryGetRotation(out Quaternion rotation);
+
+            if (positionAvailable && rotationAvailable)
+            {
+                // We want input sources to follow the playspace, so fold in the playspace transform here to
+                // put the controller pose into world space.
+                position = MixedRealityPlayspace.TransformPoint(position);
+                rotation = MixedRealityPlayspace.Rotation * rotation;
+
+                unityJointPoses[trackedHandJoint] = new MixedRealityPose(position, rotation);
+            }
+        }
+
+        /// <summary>
+        /// Converts a Unity finger bone into an MRTK hand joint.
+        /// </summary>
+        /// <remarks>
+        /// For HoloLens 2, Unity provides four joints for the thumb and five joints for other fingers, in index order of metacarpal (0) to tip (4).
+        /// The wrist joint is provided as the hand root bone.
+        /// </remarks>
+        /// <param name="finger">The Unity classification of the current finger.</param>
+        /// <param name="index">The Unity index of the current finger bone.</param>
+        /// <returns>The current Unity finger bone converted into an MRTK joint.</returns>
+        private TrackedHandJoint ConvertToTrackedHandJoint(HandFinger finger, int index)
+        {
+            switch (finger)
+            {
+                case HandFinger.Thumb: return TrackedHandJoint.ThumbMetacarpalJoint + index;
+                case HandFinger.Index: return TrackedHandJoint.IndexMetacarpal + index;
+                case HandFinger.Middle: return TrackedHandJoint.MiddleMetacarpal + index;
+                case HandFinger.Ring: return TrackedHandJoint.RingMetacarpal + index;
+                case HandFinger.Pinky: return TrackedHandJoint.PinkyMetacarpal + index;
+                default: return TrackedHandJoint.None;
+            }
+        }
+    }
+}
