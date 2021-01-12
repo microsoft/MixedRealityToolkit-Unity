6b1b10b4d2369b2f28e01b0a60d170ec818921aa
Kurtis <kurtie@microsoft.com>
2021-01-12 11:55:45 -0800

    Null check / boolean logic rework

--- Assets/MRTK/Core/Definitions/Devices/MixedRealityControllerMappingProfile.cs
+++ Assets/MRTK/Core/Definitions/Devices/MixedRealityControllerMappingProfile.cs
@@ -214,8 +214,6 @@ namespace Microsoft.MixedReality.Toolkit.Input
             });
         }
 
-#endif // UNITY_EDITOR
-
         private static bool UsesCustomInteractionMapping(Type controllerType)
         {
             var attribute = MixedRealityControllerAttribute.Find(controllerType);
@@ -227,5 +225,6 @@ namespace Microsoft.MixedReality.Toolkit.Input
             var attribute = MixedRealityControllerAttribute.Find(controllerType);
             return attribute != null ? attribute.SupportedHandedness : Array.Empty<Handedness>();
         }
+#endif // UNITY_EDITOR
     }
 }
