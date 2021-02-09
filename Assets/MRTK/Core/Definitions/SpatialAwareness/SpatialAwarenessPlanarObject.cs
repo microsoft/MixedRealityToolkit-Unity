c2a5475e2f960dc2695a86f7b60d32d79ac9376f
davidkline-ms <david.kline@microsoft.com>
2021-02-08 19:35:12 -0800

    remove unused namespace

--- Assets/MRTK/Core/Definitions/SpatialAwareness/SpatialAwarenessPlanarObject.cs
+++ Assets/MRTK/Core/Definitions/SpatialAwareness/SpatialAwarenessPlanarObject.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Runtime.Remoting.Messaging;
 using UnityEngine;
 
 namespace Microsoft.MixedReality.Toolkit.SpatialAwareness
