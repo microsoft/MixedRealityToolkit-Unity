b5ed0a1170a6365f98aafd5ad155c75c52f1e27f
RogPodge <roliu@microsoft.com>
2021-02-24 17:00:28 -0800

    Made ggvpointer highlightable

--- Assets/MRTK/Examples/Demos/Input/Scenes/PrimaryPointer/PrimaryPointerHandlerExample.cs
+++ Assets/MRTK/Examples/Demos/Input/Scenes/PrimaryPointer/PrimaryPointerHandlerExample.cs
@@ -22,13 +22,21 @@ namespace Microsoft.MixedReality.Toolkit.Examples.Demos
         {
             if (CursorHighlight != null)
             {
-                // For this example scene, do not highlight a non-BaseControllerPointer if it becomes the primary pointer. 
-                // This is due to the non-standard way that controller pointers manage their respective cursors.
-                // In particular, the GGV pointer defers it's cursor management to the GazeProvider, which has it's own internal pointer definition as well
-                // In the future, the pointer/cursor relationship will be reworked and standardized to remove this awkward set of co-dependencies
-                if (newPointer != null && newPointer is BaseControllerPointer)
+                if (newPointer != null)
                 {
-                    Transform parentTransform = newPointer.BaseCursor.TryGetMonoBehaviour(out MonoBehaviour baseCursor) ? baseCursor.transform : null;
+                    Transform parentTransform = null;
+
+                    // For this example scene, use special logic if a GGVPointer becomes the primary pointer. 
+                    // In particular, the GGV pointer defers it's cursor management to the GazeProvider, which has it's own internal pointer definition as well
+                    // In the future, the pointer/cursor relationship will be reworked and standardized to remove this awkward set of co-dependencies
+                    if (newPointer is GGVPointer)
+                    {
+                        parentTransform = CoreServices.InputSystem.GazeProvider.GazePointer.BaseCursor.TryGetMonoBehaviour(out MonoBehaviour baseCursor) ? baseCursor.transform : null;
+                    }
+                    else
+                    {
+                        parentTransform = newPointer.BaseCursor.TryGetMonoBehaviour(out MonoBehaviour baseCursor) ? baseCursor.transform : null;
+                    }
 
                     // If there's no cursor try using the controller pointer transform instead
                     if (parentTransform == null)
