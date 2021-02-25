c1dc3e8e013f1761e9ea47323f995b054dfa206b
RogPodge <roliu@microsoft.com>
2021-02-24 16:47:00 -0800

    Added workaround for primary cursor scene, fixed input actions scene profile

--- Assets/MRTK/Examples/Demos/Input/Scenes/PrimaryPointer/PrimaryPointerHandlerExample.cs
+++ Assets/MRTK/Examples/Demos/Input/Scenes/PrimaryPointer/PrimaryPointerHandlerExample.cs
@@ -22,7 +22,11 @@ namespace Microsoft.MixedReality.Toolkit.Examples.Demos
         {
             if (CursorHighlight != null)
             {
-                if (newPointer != null)
+                // For this example scene, do not highlight a non-BaseControllerPointer if it becomes the primary pointer. 
+                // This is due to the non-standard way that controller pointers manage their respective cursors.
+                // In particular, the GGV pointer defers it's cursor management to the GazeProvider, which has it's own internal pointer definition as well
+                // In the future, the pointer/cursor relationship will be reworked and standardized to remove this awkward set of co-dependencies
+                if (newPointer != null && newPointer is BaseControllerPointer)
                 {
                     Transform parentTransform = newPointer.BaseCursor.TryGetMonoBehaviour(out MonoBehaviour baseCursor) ? baseCursor.transform : null;
 
