e440ec60da7a263ab1c2534f5b16be2449453b38
Kurtis <kurtie@microsoft.com>
2021-03-01 10:40:57 -0800

    Only update renderViewPortScale if an XR device is present

--- Assets/MRTK/Examples/Demos/ReadingMode/Scripts/ReadingModeSceneBehavior.cs
+++ Assets/MRTK/Examples/Demos/ReadingMode/Scripts/ReadingModeSceneBehavior.cs
@@ -4,7 +4,14 @@
 using Microsoft.MixedReality.Toolkit.CameraSystem;
 using Microsoft.MixedReality.Toolkit.UI;
 using UnityEngine;
+
+#if UNITY_2019_3_OR_NEWER
+using Microsoft.MixedReality.Toolkit.Utilities;
+#endif // UNITY_2019_3_OR_NEWER
+
+#if !UNITY_2020_1_OR_NEWER
 using UnityEngine.XR;
+#endif // !UNITY_2020_1_OR_NEWER
 
 namespace Microsoft.MixedReality.Toolkit.Examples.Demos.ReadingMode
 {
@@ -16,12 +23,31 @@ namespace Microsoft.MixedReality.Toolkit.Examples.Demos.ReadingMode
         [SerializeField]
         private PinchSlider renderViewportScaleSlider = null;
 
+        private float previousSliderValue = -1;
+
         private void Update()
         {
-            if (renderViewportScaleSlider != null)
+            if (renderViewportScaleSlider == null || renderViewportScaleSlider.SliderValue == previousSliderValue)
+            {
+                return;
+            }
+
+            previousSliderValue = renderViewportScaleSlider.SliderValue;
+
+#if UNITY_2019_3_OR_NEWER
+            if (XRSubsystemHelpers.DisplaySubsystem != null)
+            {
+                XRSubsystemHelpers.DisplaySubsystem.scaleOfAllViewports = renderViewportScaleSlider.SliderValue;
+                return;
+            }
+#endif // UNITY_2019_3_OR_NEWER
+
+#if !UNITY_2020_1_OR_NEWER
+            if (XRDevice.isPresent)
             {
                 XRSettings.renderViewportScale = renderViewportScaleSlider.SliderValue;
             }
+#endif // !UNITY_2020_1_OR_NEWER
         }
 
         public void EnableReadingMode()
