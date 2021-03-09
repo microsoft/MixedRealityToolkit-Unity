1f2cc3eb7909034f91e5a2d4effb6e194f478542
Yoon Park <cre8ive.park@gmail.com>
2021-03-08 22:39:26 -0800

    Adding Scene Understanding.

---
+++ Assets/MRTK/Examples/Common/Scripts/LoadProfiles.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Examples.Demos.EyeTracking
+{
+    /// <summary>
+    /// Automatically loads a given Mixed Reality Toolkit configuration profile when loading up the scene. 
+    /// </summary>
+    [AddComponentMenu("Scripts/MRTK/Examples/LoadProfiles")]
+    public class LoadProfiles : MonoBehaviour
+    {
+        [Tooltip("Mixed Reality Toolkit profile to load.")]
+        [SerializeField]
+        private MixedRealityToolkitConfigurationProfile configProfile = null;
+
+        public void LoadProfile()
+        {
+            if ((configProfile != null) && (MixedRealityToolkit.Instance != null))
+            {
+                MixedRealityToolkit.Instance.ActiveProfile = configProfile;
+                Debug.Log($"Loading new MRTK configuration profile: {configProfile.name}");
+                configProfile = null;
+            }
+        }
+    }
+}
---
+++ Assets/SceneSystemUnloadingCheck.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Experimental.SceneUnderstanding;
+using Microsoft.MixedReality.Toolkit.SceneSystem;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSystemUnloadingCheck : MonoBehaviour
+{
+    public DemoSceneUnderstandingController SUController;
+    private bool isCleared = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IMixedRealitySceneSystem sceneSystem = MixedRealityToolkit.Instance.GetService<IMixedRealitySceneSystem>();
+        sceneSystem.OnWillUnloadContent += HandleSceneOperation;
+    }
+
+    private void HandleSceneOperation(IEnumerable<string> obj)
+    {
+        if(isCleared == false)
+        {
+            SUController.ClearScene();
+            isCleared = true;
+        }
+    }
+}
