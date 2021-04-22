e4d11855bcceafcee056150c6c150abf64504523
RogPodge <roliu@microsoft.com>
2021-04-22 13:40:48 -0700

    fixing tracking origin mode check

--- Assets/MRTK/Core/Utilities/SceneContent/MixedRealitySceneContent.cs
+++ Assets/MRTK/Core/Utilities/SceneContent/MixedRealitySceneContent.cs
@@ -77,7 +77,7 @@ namespace Microsoft.MixedReality.Toolkit
 #if UNITY_2020_1_OR_NEWER
                     XRSubsystemHelpers.InputSubsystem != null && !XRSubsystemHelpers.InputSubsystem.GetTrackingOriginMode().HasFlag(TrackingOriginModeFlags.Unknown);
 #else
-                (XRSubsystemHelpers.InputSubsystem != null && !XRSubsystemHelpers.InputSubsystem.GetTrackingOriginMode().HasFlag(TrackingOriginModeFlags.Unknown)) ||
+                (XRSubsystemHelpers.InputSubsystem != null && XRSubsystemHelpers.InputSubsystem.GetTrackingOriginMode().HasFlag(TrackingOriginModeFlags.Floor)) ||
                 (XRDevice.isPresent && XRDevice.GetTrackingSpaceType() == TrackingSpaceType.RoomScale);
 #endif // UNITY_2020_1_OR_NEWER
 
