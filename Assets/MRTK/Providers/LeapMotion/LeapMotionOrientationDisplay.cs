391bf6de07489186d064424c84d8dfce13e72670
Jack Yang <jackyangzzh@gmail.com>
2021-04-24 20:11:15 -0500

    Display Leap Orientation dynamically through panel in example scene

---
+++ Assets/MRTK/Providers/LeapMotion/LeapMotionOrientationDisplay.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.LeapMotion.Input;
+using Microsoft.MixedReality.Toolkit.Input;
+using TMPro;
+
+namespace Microsoft.MixedReality.Toolkit.LeapMotion
+{
+    /// <summary>
+    /// Returns the orientation of Leap Motion Controller
+    /// </summary>
+    /// 
+    public class LeapMotionOrientationDisplay : MonoBehaviour
+    {
+        [SerializeField] MixedRealityToolkit toolkit;
+        [SerializeField] TextMeshProUGUI orientationText;
+        LeapMotionDeviceManagerProfile managerProfile;
+
+        // Update is called once per frame
+        private void Start()
+        {
+            if (GetLeapManager())
+            {
+                orientationText.text = "Orientation" + GetLeapManager().LeapControllerOrientation.ToString();
+            }
+            else
+            {
+                orientationText.text = "Orientation Unavailable";
+            }
+        }
+
+        private LeapMotionDeviceManagerProfile GetLeapManager()
+        {
+            if (!MixedRealityToolkit.Instance.ActiveProfile)
+            {
+                return null;
+            }
+            foreach (MixedRealityInputDataProviderConfiguration config in MixedRealityToolkit.Instance.ActiveProfile.InputSystemProfile.DataProviderConfigurations)
+            {
+                if (config.ComponentType == typeof(LeapMotionDeviceManager))
+                {
+                    managerProfile = (LeapMotionDeviceManagerProfile)config.Profile;
+                    return managerProfile;
+                }
+            }
+            return null;
+        }
+
+    }
+}
+
