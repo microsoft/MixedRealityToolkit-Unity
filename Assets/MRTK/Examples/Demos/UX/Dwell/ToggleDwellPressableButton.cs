d75057516dcbc97c3ac2ae164720a42538ba3451
Yoon Park <cre8ive.park@gmail.com>
2021-05-02 22:37:18 -0700

    Adding Volumetric UI Dwell examples.

---
+++ Assets/MRTK/Examples/Demos/UX/Dwell/ToggleDwellPressableButton.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.MixedReality.Toolkit.Input;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Microsoft.MixedReality.Toolkit.Dwell
+{
+    /// <summary>
+    /// Example script to demonstrate a toggle button using dwell
+    /// </summary>
+    [AddComponentMenu("Scripts/MRTK/Examples/ToggleDwellPressableButton")]
+    public class ToggleDwellPressableButton : BaseDwellPressableButton
+    {
+        [SerializeField]
+        private TextMeshPro dwellStatus = null;
+
+        [SerializeField]
+        private GameObject buttonBackground = null;
+
+        private bool isDwellEnabled = true;
+
+        [SerializeField]
+        private Color dwellOnColor = Color.white;
+
+        [SerializeField]
+        private Color dwellOffColor = Color.cyan;
+
+        [SerializeField]
+        private Color dwellIntendedColor = Color.cyan;
+
+        [SerializeField]
+        private float dwellVisualCancelDurationInFrames = 60;
+
+        private float cancelStartScale = 0;
+
+        private void Update()
+        {
+            if (IsDwelling)
+            {
+                float value = DwellHandler.DwellProgress;
+                dwellVisualImage.transform.localScale = new Vector3(value, value, value);
+            }
+            else if (!IsDwelling && dwellVisualImage.transform.localScale.x > 0)
+            {
+                float value = Mathf.Clamp(dwellVisualImage.transform.localScale.x - (cancelStartScale / dwellVisualCancelDurationInFrames), 0f, 1f);
+                dwellVisualImage.transform.localScale = new Vector3(value, value, value);
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void DwellIntended(IMixedRealityPointer pointer)
+        {
+            buttonBackground.GetComponent<MeshRenderer>().material.color = dwellIntendedColor;
+            dwellVisualImage.transform.localScale = Vector3.zero;
+        }
+
+        /// <inheritdoc/>
+        public override void DwellStarted(IMixedRealityPointer pointer)
+        {
+            base.DwellStarted(pointer);
+            buttonBackground.GetComponent<MeshRenderer>().material.color = dwellIntendedColor;
+        }
+
+        /// <inheritdoc/>
+        public override void DwellCanceled(IMixedRealityPointer pointer)
+        {
+            base.DwellCanceled(pointer);
+            buttonBackground.GetComponent<MeshRenderer>().material.color = isDwellEnabled ? this.dwellOnColor : this.dwellOffColor;
+            cancelStartScale = dwellVisualImage.transform.localScale.x;
+        }
+
+        /// <inheritdoc/>
+        public override void DwellCompleted(IMixedRealityPointer pointer)
+        {
+            base.DwellCompleted(pointer);
+            dwellVisualImage.transform.localScale = Vector3.zero;
+        }
+
+        /// <inheritdoc/>
+        public override void ButtonExecute()
+        {
+            isDwellEnabled = !isDwellEnabled;
+            dwellStatus.text = isDwellEnabled ? "On" : "Off";
+
+            // swap the button background and dwell visuals overlay color
+            buttonBackground.GetComponent<MeshRenderer>().material.color = isDwellEnabled ? this.dwellOnColor : this.dwellOffColor;
+            dwellVisualImage.GetComponentInChildren<MeshRenderer>().material.color = isDwellEnabled ? this.dwellOffColor : this.dwellOnColor;
+
+            var foundObjects = FindObjectsOfType<DwellHandler>();
+
+            foreach (var obj in foundObjects)
+            {
+                obj.enabled = isDwellEnabled;
+            }
+
+            DwellHandler.enabled = true;
+        }
+    }
+}
