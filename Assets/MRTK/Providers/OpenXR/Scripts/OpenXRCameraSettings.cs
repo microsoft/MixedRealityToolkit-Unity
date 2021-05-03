ca3f93f3585f20493fcc8509931a71e70966d1af
Kurtis <kurtie@microsoft.com>
2021-05-03 15:00:34 -0700

    Stash camera settings impl

---
+++ Assets/MRTK/Providers/OpenXR/Scripts/OpenXRCameraSettings.cs
@@ -0,0 +1,105 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.MixedReality.Toolkit.CameraSystem;
+using Microsoft.MixedReality.Toolkit.Utilities;
+
+#if UNITY_OPENXR
+using UnityEngine.XR.OpenXR;
+#endif // UNTIY_OPENXR
+
+namespace Microsoft.MixedReality.Toolkit.XRSDK.OpenXR
+{
+    /// <summary>
+    /// Camera settings provider for use with OpenXR and XR SDK.
+    /// </summary>
+    [MixedRealityDataProvider(
+        typeof(IMixedRealityCameraSystem),
+        (SupportedPlatforms)(-1),
+        "OpenXR Camera Settings",
+        "OpenXR/Profiles/DefaultOpenXRCameraSettingsProfile.asset",
+        "MixedRealityToolkit.Providers",
+        supportedUnityXRPipelines: SupportedUnityXRPipelines.XRSDK)]
+    public class OpenXRCameraSettings : BaseCameraSettingsProvider
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="cameraSystem">The instance of the camera system which is managing this provider.</param>
+        /// <param name="name">Friendly name of the provider.</param>
+        /// <param name="priority">Provider priority. Used to determine order of instantiation.</param>
+        /// <param name="profile">The provider's configuration profile.</param>
+        public OpenXRCameraSettings(
+            IMixedRealityCameraSystem cameraSystem,
+            string name = null,
+            uint priority = DefaultPriority,
+            BaseCameraSettingsProfile profile = null) : base(cameraSystem, name, priority, profile)
+        { }
+
+        private bool IsActiveLoader =>
+#if UNITY_OPENXR
+            LoaderHelpers.IsLoaderActive<OpenXRLoaderBase>();
+#else
+            false;
+#endif // UNITY_OPENXR
+
+#if UNITY_OPENXR
+        private static readonly bool IsReprojectionModeSupported = OpenXRRuntime.IsExtensionEnabled("XR_MSFT_composition_layer_reprojection_preview");
+#endif // UNTIY_OPENXR
+
+        private OpenXRCameraSettingsProfile Profile => ConfigurationProfile as OpenXRCameraSettingsProfile;
+
+        private OpenXRReprojectionUpdater reprojectionUpdater = null;
+
+        /// <inheritdoc />
+        public override void Enable()
+        {
+            if (!IsActiveLoader)
+            {
+                IsEnabled = false;
+                return;
+            }
+
+            base.Enable();
+            InitializeReprojectionUpdater();
+        }
+
+        /// <inheritdoc/>
+        public override void Disable()
+        {
+            UninitializeReprojectionUpdater();
+            base.Disable();
+        }
+
+        #region IMixedRealityCameraSettings
+
+        /// <inheritdoc/>
+        public override bool IsOpaque => XRSubsystemHelpers.DisplaySubsystem?.displayOpaque ?? true;
+
+        #endregion IMixedRealityCameraSettings
+
+        /// <summary>
+        /// Adds and initializes the reprojection updater component.
+        /// </summary>
+        private void InitializeReprojectionUpdater()
+        {
+            if (reprojectionUpdater == null && Profile != null)
+            {
+                reprojectionUpdater = CameraCache.Main.EnsureComponent<OpenXRReprojectionUpdater>();
+                reprojectionUpdater.ReprojectionMethod = Profile.ReprojectionMethod;
+            }
+        }
+
+        /// <summary>
+        /// Uninitializes and removes the reprojection updater component.
+        /// </summary>
+        private void UninitializeReprojectionUpdater()
+        {
+            if (reprojectionUpdater != null)
+            {
+                UnityObjectExtensions.DestroyObject(reprojectionUpdater);
+                reprojectionUpdater = null;
+            }
+        }
+    }
+}
