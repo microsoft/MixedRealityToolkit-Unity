dad9d1f184fb7807a847b6044027385baf5b032c
Kurtis <kurtie@microsoft.com>
2021-06-14 18:28:13 -0700

    Add editor window and menu item for displaying the current MRTK version

---
+++ Assets/MRTK/Core/Inspectors/Setup/MRTKVersionPopup.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.MixedReality.Toolkit.Utilities.Editor;
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Editor
+{
+    internal class MRTKVersionPopup : EditorWindow
+    {
+        private static MRTKVersionPopup window;
+        private static readonly Version MRTKVersion = typeof(MixedRealityToolkit).Assembly.GetName().Version;
+        private static readonly Vector2 WindowSize = new Vector2(300, 150);
+        private static readonly GUIContent Title = new GUIContent("Mixed Reality Toolkit");
+
+        [MenuItem("Mixed Reality/Toolkit/Show version...")]
+        private static void Init()
+        {
+            if (window != null)
+            {
+                window.Close();
+            }
+
+            window = CreateInstance<MRTKVersionPopup>();
+            window.titleContent = Title;
+            window.maxSize = WindowSize;
+            window.minSize = WindowSize;
+            window.ShowUtility();
+        }
+
+        private void OnGUI()
+        {
+            using (new EditorGUILayout.VerticalScope())
+            {
+                MixedRealityInspectorUtility.RenderMixedRealityToolkitLogo();
+
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    GUILayout.FlexibleSpace();
+                    EditorGUILayout.LabelField($"Version {MRTKVersion}", EditorStyles.wordWrappedLabel);
+                    GUILayout.FlexibleSpace();
+                }
+            }
+        }
+    }
+}
