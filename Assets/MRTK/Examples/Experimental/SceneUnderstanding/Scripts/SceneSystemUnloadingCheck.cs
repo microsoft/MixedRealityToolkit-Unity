a94ebb0a7f720e71130cf4be3424aef93331b6fb
Yoon Park <cre8ive.park@gmail.com>
2021-06-21 13:12:29 -0700

    File renaming.

---
+++ Assets/MRTK/Examples/Experimental/SceneUnderstanding/Scripts/SceneSystemUnloadingCheck.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Experimental.SceneUnderstanding;
+using Microsoft.MixedReality.Toolkit.SceneSystem;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Microsoft.MixedReality.Toolkit.Examples.Demos
+{
+    public class SceneSystemUnloadingCheck : MonoBehaviour
+    {
+        public DemoSceneUnderstandingController SUController;
+        private bool isCleared = false;
+
+        void Start()
+        {
+            IMixedRealitySceneSystem sceneSystem = MixedRealityToolkit.Instance.GetService<IMixedRealitySceneSystem>();
+            sceneSystem.OnWillUnloadContent += HandleSceneOperation;
+        }
+
+        private void HandleSceneOperation(IEnumerable<string> obj)
+        {
+            if (isCleared == false)
+            {
+                SUController.ClearScene();
+                isCleared = true;
+            }
+        }
+    }
+}
