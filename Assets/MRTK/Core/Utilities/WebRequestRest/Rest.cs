b2caad4e4b8fb4d0c4c00552c3b62f0966b3a782
Proton-V <vsevolodbelskiy@gmail.com>
2021-06-26 08:44:25 +0300

    Added waiting for webRequest text
    fixes freezing when receiving data

--- Assets/MRTK/Core/Utilities/WebRequestRest/Rest.cs
+++ Assets/MRTK/Core/Utilities/WebRequestRest/Rest.cs
@@ -254,31 +254,35 @@ namespace Microsoft.MixedReality.Toolkit.Utilities
             webRequest.disposeCertificateHandlerOnDispose = disposeCertificateHandlerOnDispose;
             await webRequest.SendWebRequest();
 
+            long responseCode = webRequest.responseCode;
+            byte[] downloadHandlerData = webRequest.downloadHandler?.data;
+            string downloadHandlerText = await Task
+                .Run(() => System.Text.Encoding.Default.GetString(downloadHandlerData));
+
 #if UNITY_2020_1_OR_NEWER
             if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError)
 #else
             if (webRequest.isNetworkError || webRequest.isHttpError)
 #endif // UNITY_2020_1_OR_NEWER
             {
-                if (webRequest.responseCode == 401) { return new Response(false, "Invalid Credentials", null, webRequest.responseCode); }
+                if (webRequest.responseCode == 401) { return new Response(false, "Invalid Credentials", null, responseCode); }
 
                 if (webRequest.GetResponseHeaders() == null)
                 {
-                    return new Response(false, "Device Unavailable", null, webRequest.responseCode);
+                    return new Response(false, "Device Unavailable", null, responseCode);
                 }
 
                 string responseHeaders = webRequest.GetResponseHeaders().Aggregate(string.Empty, (current, header) => $"\n{header.Key}: {header.Value}");
-                string downloadHandlerText = webRequest.downloadHandler?.text;
                 Debug.LogError($"REST Error: {webRequest.responseCode}\n{downloadHandlerText}{responseHeaders}");
-                return new Response(false, $"{responseHeaders}\n{downloadHandlerText}", webRequest.downloadHandler?.data, webRequest.responseCode);
+                return new Response(false, $"{responseHeaders}\n{downloadHandlerText}", downloadHandlerData, responseCode);
             }
             if (readResponseData)
             {
-                return new Response(true, webRequest.downloadHandler?.text, webRequest.downloadHandler?.data, webRequest.responseCode);
+                return new Response(true, downloadHandlerText, downloadHandlerData, responseCode);
             }
             else // This option can be used only if action will be triggered in the same scope as the webrequest
             {
-                return new Response(true, () => webRequest.downloadHandler?.text, () => webRequest.downloadHandler?.data, webRequest.responseCode);
+                return new Response(true, () => downloadHandlerText, () => downloadHandlerData, responseCode);
             }
         }
     }
