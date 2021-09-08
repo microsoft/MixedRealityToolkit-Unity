e220b933a477df18a8bc36ce1a17d79cb77d9fc3
Kurtis <kurtie@microsoft.com>
2021-09-08 14:39:18 -0700

    Fix speech command dropdown and update commands in ET example (#10197)
    * Add custom attribute and property drawer for rendering a dropdown of speech keywords
    
    * Update inspectors to use new utility
    
    * Change voice command from start/stop recording
    
    to "record eye gaze" and "end recording" to not clash with system keywords
    
    * Also check if availableKeywords is empty for valid keywords
    
    Co-authored-by: RogPodge <39840334+RogPodge@users.noreply.github.com>
    
    * Add summary
    
    and minor optimization
    
    Co-authored-by: RogPodge <39840334+RogPodge@users.noreply.github.com>

--- Assets/MRTK/Core/Inspectors/Utilities/InspectorUIUtility.cs
+++ Assets/MRTK/Core/Inspectors/Utilities/InspectorUIUtility.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEditor;
-using UnityEditor.Experimental.SceneManagement;
 using UnityEngine;
 
 namespace Microsoft.MixedReality.Toolkit.Utilities.Editor
