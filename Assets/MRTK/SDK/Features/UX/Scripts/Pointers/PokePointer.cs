13136037f179c28554836194514311b6fcf72028
Kurtis <kurtie@microsoft.com>
2022-05-16 16:08:54 -0700

    Update PokePointer.cs

--- Assets/MRTK/SDK/Features/UX/Scripts/Pointers/PokePointer.cs
+++ Assets/MRTK/SDK/Features/UX/Scripts/Pointers/PokePointer.cs
@@ -96,7 +96,6 @@ namespace Microsoft.MixedReality.Toolkit.Input
             set { pokeLayerMasks = value; }
         }
 
-
         [SerializeField]
         [Tooltip("Specify whether queries for touchable surfaces hit triggers.")]
         protected QueryTriggerInteraction triggerInteraction = QueryTriggerInteraction.UseGlobal;
