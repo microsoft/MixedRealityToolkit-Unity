f71daf23cd07b5bb5b25c6c9d91daf6d48f296c2
RogPodge <39840334+RogPodge@users.noreply.github.com>
2022-05-16 11:08:53 -0700

    Update Assets/MRTK/Core/Interfaces/InputSystem/IPointerPreferences.cs
    Co-authored-by: Kurtis <kurtie@microsoft.com>

--- Assets/MRTK/Core/Interfaces/InputSystem/IPointerPreferences.cs
+++ Assets/MRTK/Core/Interfaces/InputSystem/IPointerPreferences.cs
@@ -45,7 +45,7 @@ namespace Microsoft.MixedReality.Toolkit.Input
         /// gaze pointer actually cannot be referenced from here 
         /// since it's an internal class.
         /// 
-        /// This does not control the Gaze Provider's Behavior. Set CoreServices.InputSystem.GazeProvider.Enabled 
+        /// This does not control if the gaze provider is used for raycasting, just its use as an MRTK pointer. Set CoreServices.InputSystem.GazeProvider.Enabled 
         /// to false if you do not wish to use Gaze data in your project
         /// </summary>
         PointerBehavior GazePointerBehavior { get; set; }
