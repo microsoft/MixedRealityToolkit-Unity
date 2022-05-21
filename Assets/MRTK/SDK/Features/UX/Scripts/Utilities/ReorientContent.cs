1b157fb4d957aa0b8e0c299b873232f967037ede
Yoon Park <cre8ive.park@gmail.com>
2022-05-21 01:41:23 -0700

    Adding the scene reorientation functionality (#10100)
    * Adding ReorientContent() to MixedRealitySceneContent.cs Updating ToggleFeaturesPanel for Examples Hub to include reorient functionality.
    
    * Scene Content Height Fix
    
    * added align with head pose option to mrtk scene content, adjusted some scenes
    
    * Updating example scenes to have proper MixedRealitySceneContent object with (0,0,0) position and set Alignment Type 'Align with head pose'
    
    * Hub menu fix
    
    * fixing mistaken checked in files
    
    * remaining changes
    
    * whitespace
    
    * reverting unintended scene changes
    
    * whitespace fix
    
    * Update SurfaceMagnetismSpatialAwarenessExample.unity
    
    * updating profile
    
    * scene transition service now runs on quest/android
    
    * enabled teleport system
    
    * fixing CI complaints
    
    Co-authored-by: RogPodge <roliu@microsoft.com>
    Co-authored-by: Yoon Park <cre8ive@outlook.com>
    Co-authored-by: RogPodge <39840334+RogPodge@users.noreply.github.com>

---
+++ Assets/MRTK/SDK/Features/UX/Scripts/Utilities/ReorientContent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Utilities
+{
+    /// <summary>
+    /// Helper script to help recenter and rocient scene content in front of the user
+    /// </summary>
+    public class ReorientContent : MonoBehaviour
+    {
+        public void AdjustContentOrientation()
+        {
+            MixedRealitySceneContent sceneContent = GameObject.Find("MixedRealitySceneContent").GetComponent<MixedRealitySceneContent>();
+            sceneContent.ReorientContent();
+        }
+    }
+}
