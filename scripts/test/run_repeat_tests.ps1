2ea1e4a39cccde4f0dc0c1a5ee0e71cc6d8ca505
Kurtis <kurtie@microsoft.com>
2022-06-21 15:09:11 -0700

    Format additional scripts and update .gitignore for cross-branch compat (#10665)
    * Update .gitignore
    
    * Formatting

--- scripts/test/run_repeat_tests.ps1
+++ scripts/test/run_repeat_tests.ps1
@@ -7,32 +7,31 @@
 [CmdletBinding()]
 param (
     # Path to your Unity project
-    [Parameter(Position=0)]
-    [ValidateScript({Test-Path $_ -PathType Container})]
+    [Parameter(Position = 0)]
+    [ValidateScript({ Test-Path $_ -PathType Container })]
     [string]
     $projectPath = "$PSScriptRoot/../../",
     # Folder that will contain test results output and logs
     [string]
-    [ValidateScript({Test-Path $_ -PathType Container})]
+    [ValidateScript({ Test-Path $_ -PathType Container })]
     $outFolder = "$PSScriptRoot/out/",
     # Path to your Unity Executable
-    [ValidateScript({[System.IO.File]::Exists($_) -and $_.EndsWith(".exe") })]
+    [ValidateScript({ [System.IO.File]::Exists($_) -and $_.EndsWith(".exe") })]
     [string]
     $unityExePath = "C:\Program Files\Unity\Hub\Editor\2018.4.26f1\Editor\Unity.exe",
     # Optional test filter
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $false)]
     [string]
     $editorTestsFilter = "",
     # Number of times to repeat runs
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [int]
     $Times
 )
 
 $passThruParams = $PsBoundParameters
 $passThruParams.Remove("Times")
-for ($i = 0; $i -lt $Times; $i++) 
-{
+for ($i = 0; $i -lt $Times; $i++) {
     $user_i = $i + 1
     Write-Host -ForegroundColor Cyan "Run $user_i of $Times"
     Invoke-Expression $PSScriptRoot\run_playmode_tests.ps1 @passThruParams
