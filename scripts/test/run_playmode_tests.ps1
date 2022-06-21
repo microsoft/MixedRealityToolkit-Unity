4eba528e4a94f0604ce04916fc359bc744030d2c
Kurtis <kurtie@microsoft.com>
2022-06-21 13:26:05 -0700

    Update run_playmode_tests.ps1 (#10649)

--- scripts/test/run_playmode_tests.ps1
+++ scripts/test/run_playmode_tests.ps1
@@ -7,26 +7,25 @@
  #>
 param(
     # Path to your Unity project
-    [Parameter(Position=0)]
-    [ValidateScript({Test-Path $_ -PathType Container})]
+    [Parameter(Position = 0)]
+    [ValidateScript({ Test-Path $_ -PathType Container })]
     [string]
     $projectPath = "$PSScriptRoot/../../",
     # Folder that will contain test results output and logs
     [string]
-    [ValidateScript({Test-Path $_ -PathType Container})]
+    [ValidateScript({ Test-Path $_ -PathType Container })]
     $outFolder = "$PSScriptRoot/out/",
     # Path to your Unity Executable
-    [ValidateScript({[System.IO.File]::Exists($_) -and $_.EndsWith(".exe") })]
+    [ValidateScript({ [System.IO.File]::Exists($_) -and $_.EndsWith(".exe") })]
     [string]
     $unityExePath = "C:\Program Files\Unity\Hub\Editor\2018.4.26f1\Editor\Unity.exe",
     # Optional test filter
-    [Parameter(Mandatory=$false)]
+    [Parameter(Mandatory = $false)]
     [string]
     $editorTestsFilter = ""
 )
 $dateStr = Get-Date -format "yyyy_MM_dd-HHmmss"
-if (-not (Test-Path $outFolder))
-{
+if (-not (Test-Path $outFolder)) {
     New-Item -ItemType Directory $outFolder
 }
 $logPath = "$outFolder\playmode_tests_log-$dateStr.log"
@@ -44,7 +43,7 @@ $unityArgs = @(
     "-logFile $logPath",
     "-projectPath $projectPath",
     "-editorTestsFilter $editorTestsFilter"
-    )
+)
 Write-Host "Running command:"
 Write-Host $unityExePath ($unityArgs -Join " ")
 $handle = Start-Process -FilePath $unityExePath -PassThru -ArgumentList $unityArgs
@@ -55,8 +54,7 @@ Start-Process powershell -ArgumentList @(
 
 Write-Host "`nOpening new window to view test output..."
 Write-Host "Results will be printed here when the test completes"
-while (-not $handle.HasExited)
-{
+while (-not $handle.HasExited) {
     Start-Sleep 3
 }
 
@@ -67,14 +65,21 @@ Write-Host -ForegroundColor Cyan "Tests took: $($timer.Elapsed)"
 [xml]$cn = Get-Content $testResultPath
 $cnx = $cn["test-run"]
 Write-Host -ForegroundColor Cyan "passed: $($cnx.passed) failed: $($cnx.failed)"
-if ($cnx.failed -gt 0)
-{
+if ($cnx.failed -gt 0) {
     Write-Host ""
     Write-Host -ForegroundColor Cyan "Failed tests:"
     $testcases = $cnx.GetElementsByTagName("test-case")
     foreach ($item in $testcases) {
-        if($item.result -ne "Passed")
-        {
+        if ($item.result -eq "Failed") {
+            Write-Host "$($item.classname)::$($item.name)"
+        }
+    }
+
+    Write-Host ""
+    Write-Host -ForegroundColor Cyan "Skipped tests:"
+    $testcases = $cnx.GetElementsByTagName("test-case")
+    foreach ($item in $testcases) {
+        if ($item.result -eq "Skipped") {
             Write-Host "$($item.classname)::$($item.name)"
         }
     }
