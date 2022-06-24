c94ad4ed2776a1303c34d9dede0789e0c9c1b185
Kurtis <kurtie@microsoft.com>
2022-06-24 15:45:33 -0700

    Add initial CI and PR validation pipelines for MRTK3 (#10671)
    * Update .gitignore
    
    * Port test scripts
    
    * Fill out testables
    
    * Create README.md
    
    * Create ci.yaml
    
    * Create settings.yaml
    
    * upm
    
    * Create build-appx.yaml
    
    * Update .gitignore
    
    * Add build script
    
    * And unity build to pipelines
    
    * Add Unity yaml

---
+++ Tooling/Test/run_repeat_tests.ps1
@@ -0,0 +1,38 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+<#
+ # Runs the playmode tests in batch mode, repeatedly.
+ #>
+[CmdletBinding()]
+param (
+    # Path to your Unity project
+    [Parameter(Position = 0)]
+    [ValidateScript({ Test-Path $_ -PathType Container })]
+    [string]
+    $projectPath = "$PSScriptRoot/../../",
+    # Folder that will contain test results output and logs
+    [string]
+    [ValidateScript({ Test-Path $_ -PathType Container })]
+    $outFolder = "$PSScriptRoot/out/",
+    # Path to your Unity Executable
+    [ValidateScript({ [System.IO.File]::Exists($_) -and $_.EndsWith(".exe") })]
+    [string]
+    $unityExePath = "C:\Program Files\Unity\Hub\Editor\2020.3.35f1\Editor\Unity.exe",
+    # Optional test filter
+    [Parameter(Mandatory = $false)]
+    [string]
+    $editorTestsFilter = "",
+    # Number of times to repeat runs
+    [Parameter(Mandatory = $true)]
+    [int]
+    $Times
+)
+ 
+$passThruParams = $PsBoundParameters
+$passThruParams.Remove("Times")
+for ($i = 0; $i -lt $Times; $i++) {
+    $user_i = $i + 1
+    Write-Host -ForegroundColor Cyan "Run $user_i of $Times"
+    Invoke-Expression $PSScriptRoot\run_playmode_tests.ps1 @passThruParams
+}
---
+++ UnityProjects/MRTKDevTemplate/Assets/BuildAssets/BuildApp.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License.
+
+using System;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Examples.Build
+{
+    public static class BuildApp
+    {
+        private static string[] scenes = { "Assets/Scenes/HandInteractionExamples.unity" };
+        private static string buildPath = "build";
+
+        public static void StartCommandLineBuild()
+        {
+            ParseBuildCommandLine();
+
+            // We don't need stack traces on all our logs. Makes things a lot easier to read.
+            Application.SetStackTraceLogType(LogType.Log, StackTraceLogType.None);
+            Debug.Log($"Starting command line build for {EditorUserBuildSettings.activeBuildTarget}...");
+
+            BuildPlayerOptions options = new BuildPlayerOptions()
+            {
+                scenes = scenes,
+                locationPathName = buildPath,
+                targetGroup = EditorUserBuildSettings.selectedBuildTargetGroup,
+                target = EditorUserBuildSettings.activeBuildTarget,
+            };
+
+            bool success;
+            try
+            {
+                BuildReport buildReport = BuildPipeline.BuildPlayer(options);
+                success = buildReport != null && buildReport.summary.result == BuildResult.Succeeded;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Build Failed!\n{e.Message}\n{e.StackTrace}");
+                success = false;
+            }
+
+            Debug.Log($"Finished build... Build success? {success}");
+        }
+
+        private static void ParseBuildCommandLine()
+        {
+            string[] arguments = System.Environment.GetCommandLineArgs();
+
+            for (int i = 0; i < arguments.Length; ++i)
+            {
+                switch (arguments[i])
+                {
+                    case "-sceneList":
+                        scenes = SplitSceneList(arguments[++i]);
+                        break;
+                    case "-buildOutput":
+                        buildPath = arguments[++i];
+                        break;
+                }
+            }
+        }
+
+        private static string[] SplitSceneList(string sceneList)
+        {
+            return (from scene in sceneList.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    select scene.Trim()).ToArray();
+        }
+    }
+}
