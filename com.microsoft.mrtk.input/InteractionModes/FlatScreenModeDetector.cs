abbc3d7efe756c669745b11124077b7e914c91d0
RogPodge <roliu@microsoft.com>
2022-08-08 13:28:30 -0700

    adding license header

--- com.microsoft.mrtk.input/InteractionModes/FlatScreenModeDetector.cs
+++ com.microsoft.mrtk.input/InteractionModes/FlatScreenModeDetector.cs
@@ -1,4 +1,6 @@
-using System.Collections;
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
