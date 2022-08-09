1a6f1182a99bf44f091b6fcd6dcf66f544f6a327
Finn Sinclair <finnnorth@gmail.com>
2022-08-08 18:56:29 -0700

    PR feedback

--- com.microsoft.mrtk.input/Visualizers/RiggedHandVisualizer/RiggedHandMeshVisualizer.cs
+++ com.microsoft.mrtk.input/Visualizers/RiggedHandVisualizer/RiggedHandMeshVisualizer.cs
@@ -31,11 +31,14 @@ namespace Microsoft.MixedReality.Toolkit.Input
         public XRNode HandNode { get => handNode; set => handNode = value; }
 
         [SerializeField]
-        [Tooltip("When true, this visualizer will render rigged hands even on XR devices with transparent displays.")]
+        [Tooltip("When true, this visualizer will render rigged hands even on XR devices " +
+                 "with transparent displays. When false, the rigged hands will only render " +
+                 "on devices with opaque displays.")]
         private bool showHandsOnTransparentDisplays;
 
         /// <summary>
         /// When true, this visualizer will render rigged hands even on XR devices with transparent displays.
+        /// When false, the rigged hands will only render on devices with opaque displays.
         /// Usually, it's recommended not to show hand visualization on transparent displays as it can
         /// distract from the user's real hands, and cause a "double image" effect that can be disconcerting.
         /// </summary>
