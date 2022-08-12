ab5ef15a734f2c8ad85ef43ebf86ced29a8657e7
Finn Sinclair <finnnorth@gmail.com>
2022-08-11 21:27:00 -0700

    Hand mesh settings on the hand menu

--- UnityProjects/MRTKDevTemplate/Assets/Scripts/SampleSceneHandMenu.cs
+++ UnityProjects/MRTKDevTemplate/Assets/Scripts/SampleSceneHandMenu.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.UX;
 using Microsoft.MixedReality.Toolkit.Diagnostics;
@@ -23,6 +25,9 @@ namespace Microsoft.MixedReality.Toolkit.Examples.Demos
         private StatefulInteractable gazePinchToggle;
 
         [SerializeField]
+        private StatefulInteractable handMeshToggle;
+
+        [SerializeField]
         private StatefulInteractable perfOverlayToggle;
 
         [SerializeField]
@@ -31,13 +36,16 @@ namespace Microsoft.MixedReality.Toolkit.Examples.Demos
         [SerializeField]
         private StatefulInteractable nextSceneButton;
 
+
         [SerializeField]
         private TMP_Text sceneTitleLabel;
 
         [SerializeField]
         private GameObject profilerObject;
 
-        private void Awake()
+        private List<RiggedHandMeshVisualizer> handMeshes = new List<RiggedHandMeshVisualizer>();
+
+        private void Start()
         {
             rayToggle.OnClicked.AddListener(() => SetHandRaysActive(rayToggle.IsToggled));
             gazePinchToggle.OnClicked.AddListener(() => SetGazePinchActive(gazePinchToggle.IsToggled));
@@ -56,6 +64,11 @@ namespace Microsoft.MixedReality.Toolkit.Examples.Demos
                 // Removes button if no profiler found in scene.
                 perfOverlayToggle.gameObject.SetActive(false);
             }
+
+            handMeshes = new List<RiggedHandMeshVisualizer>(Object.FindObjectsOfType<RiggedHandMeshVisualizer>());
+            handMeshToggle.ForceSetToggled((handMeshes.Any((handMesh) => handMesh.IsShowingHands)));
+            handMeshToggle.OnClicked.AddListener(() => SetHandMeshActive(handMeshToggle.IsToggled));
+            
 
             previousSceneButton.OnClicked.AddListener(() => GoToPreviousScene());
             nextSceneButton.OnClicked.AddListener(() => GoToNextScene());
@@ -97,6 +110,32 @@ namespace Microsoft.MixedReality.Toolkit.Examples.Demos
             if (profilerObject != null)
             {
                 profilerObject.SetActive(value);
+            }
+        }
+
+        void SetHandMeshActive(bool value)
+        {
+            if (handMeshes.Count == 0)
+            {
+                handMeshes = new List<RiggedHandMeshVisualizer>(Object.FindObjectsOfType<RiggedHandMeshVisualizer>());
+            }
+
+            if (handMeshes.Count == 0) {
+                handMeshToggle.enabled = false;
+                return;
+            }
+
+            foreach (var handMesh in handMeshes)
+            {
+                if (value)
+                {
+                    handMesh.ShowHandsOnTransparentDisplays = true;
+                    handMesh.enabled = true;
+                }
+                else
+                {
+                    handMesh.enabled = false;
+                }
             }
         }
 
