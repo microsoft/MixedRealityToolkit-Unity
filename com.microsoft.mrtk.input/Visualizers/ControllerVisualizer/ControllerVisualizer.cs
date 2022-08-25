3ea68467e16075a4869c11a59dc798eb683b5408
Roger Liu <roliu@microsoft.com>
2022-08-25 11:38:43 -0700

    Almost final touches on the controller visualizer, Only thing remaining is to load models on startup

--- com.microsoft.mrtk.input/Visualizers/ControllerVisualizer/ControllerVisualizer.cs
+++ com.microsoft.mrtk.input/Visualizers/ControllerVisualizer/ControllerVisualizer.cs
@@ -8,6 +8,10 @@ using UnityEngine.InputSystem.Utilities;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityInputSystem = UnityEngine.InputSystem;
+using InputAction = UnityEngine.InputSystem.InputAction;
+using InputActionType = UnityEngine.InputSystem.InputActionType;
+using System.Threading.Tasks;
+using System;
 
 namespace Microsoft.MixedReality.Toolkit.Input
 {
@@ -37,11 +41,13 @@ namespace Microsoft.MixedReality.Toolkit.Input
         // The controller usages we want the input device to have;
         private InternedString targetUsage;
 
-        // The characteristics we want to check to ensure that the input device is for the appropriate hand.
-        private InputDeviceCharacteristics handednessCharacteristic;
+        private GameObject platformLoadedGameObject;
+        private GameObject platformLoadedGameObjectRoot;
+        private GameObject fallbackGameObject;
 
-        // The controller characteristics we want the input device to have;
-        private InputDeviceCharacteristics validControllerCharacteristiscs = InputDeviceCharacteristics.HeldInHand & InputDeviceCharacteristics.TrackedDevice;
+        [SerializeField]
+        private UnityInputSystem.InputActionProperty myAction;
+        private InputAction controllerDetectedAction => myAction.action;
 
         /// <inheritdoc />
         protected bool IsControllerTracked => xrController.currentControllerState.inputTrackingState.HasPositionAndRotation();
@@ -58,154 +64,118 @@ namespace Microsoft.MixedReality.Toolkit.Input
                     controllerModelProvider = ControllerModel.Left;
                     xrController = lookups[0].LeftHandController;
                     targetUsage = UnityInputSystem.CommonUsages.LeftHand;
-                    handednessCharacteristic = InputDeviceCharacteristics.Left;
                     break;
                 case XRNode.RightHand:
                     controllerModelProvider = ControllerModel.Right;
                     xrController = lookups[0].RightHandController;
                     targetUsage = UnityInputSystem.CommonUsages.RightHand;
-                    handednessCharacteristic = InputDeviceCharacteristics.Right;
                     break;
                 default:
                     break;
             }
 
-            // This process may change in the future as unity updates its input subsystem.
-            // In the future, there will be a different way of distinguishing between phsyical controllers
-            // and tracked hands, forgoing the UnityEngine.XR.InputDevices route
-            UnityInputSystem.InputSystem.onDeviceChange += CheckToShowVisuals;
-            InputDevices.deviceConnected += CheckToShowVisuals;
-            InputDevices.deviceDisconnected += RemoveVisuals;
+            if (controllerDetectedAction == null) { return; }
+            controllerDetectedAction.started += RenderControllerVisuals;
+            controllerDetectedAction.canceled += RemoveControllerVisuals;
+            controllerDetectedAction.Enable();
         }
 
         protected void OnDisable()
         {
-            UnityInputSystem.InputSystem.onDeviceChange -= CheckToShowVisuals;
-            InputDevices.deviceConnected -= CheckToShowVisuals;
-            InputDevices.deviceDisconnected -= RemoveVisuals;
-
-            // Hide the controller model
-            xrController.hideControllerModel = true;
+            if (controllerDetectedAction == null) { return; }
+            controllerDetectedAction.Disable();
+            controllerDetectedAction.started -= RenderControllerVisuals;
+            controllerDetectedAction.canceled -= RemoveControllerVisuals;
+        }
+        private void RenderControllerVisuals(InputAction.CallbackContext context)
+        {
+            RenderControllerVisuals(context.control.device);
         }
 
-        private void CheckToShowVisuals(UnityInputSystem.InputDevice inputDevice, UnityInputSystem.InputDeviceChange change)
+        private void RenderControllerVisuals(UnityInputSystem.InputDevice inputDevice)
         {
+            // This process may change in the future as unity updates its input subsystem.
+            // In the future, there will be a different way of distinguishing between phsyical controllers
+            // and tracked hands, forgoing the UnityEngine.XR.InputDevices route
+
             // Upon detecting a generic input device with the appropriate usages, load or remove the controller visuals
             // when appropriate
             if (inputDevice is UnityInputSystem.XR.XRController xrInputDevice && xrInputDevice.usages.Contains(targetUsage))
             {
-                switch (change)
+                // Fallback visuals are only used if NO hand joints are detected
+                // OR the input device is specifically a simulated controller that is in the MotionController Simulation Mode.
+                bool useFallbackVisuals;
+                bool isSimulatedController;
+                if (xrInputDevice is MRTKSimulatedController simulatedController)
                 {
-                    case UnityInputSystem.InputDeviceChange.Added:
-                    case UnityInputSystem.InputDeviceChange.Reconnected:
-                    case UnityInputSystem.InputDeviceChange.Enabled:
-                    case UnityInputSystem.InputDeviceChange.UsageChanged:
-                    case UnityInputSystem.InputDeviceChange.ConfigurationChanged:
-                    case UnityInputSystem.InputDeviceChange.SoftReset:
-                    case UnityInputSystem.InputDeviceChange.HardReset:
-                        // Clear the exiting visuals before proceding to try to render new ones
-                        ClearControllerVisuals();
-
-                        // Fallback visuals are only used if NO hand joints are detected
-                        // OR the input device is specifically a simulated controller that is in the MotionController Simulation Mode.
-                        bool useFallbackVisuals = !HandsUtils.GetSubsystem().TryGetJoint(TrackedHandJoint.Palm, handNode, out _);
-                        bool isSimulatedController = false;
-                        if (xrInputDevice is MRTKSimulatedController simulatedController)
-                        {
-                            isSimulatedController = simulatedController.SimulationMode == ControllerSimulationMode.MotionController;
-                            useFallbackVisuals |= isSimulatedController;
-                        }
-
-                        InstantiateControllerVisuals(!isSimulatedController, useFallbackVisuals);
-                        break;
-                    case UnityInputSystem.InputDeviceChange.Disabled:
-                    case UnityInputSystem.InputDeviceChange.Removed:
-                    case UnityInputSystem.InputDeviceChange.Disconnected:
-                        ClearControllerVisuals();
-                        break;
+                    useFallbackVisuals = simulatedController.SimulationMode == ControllerSimulationMode.MotionController;
+                    isSimulatedController = true;
                 }
+                else
+                {
+                    useFallbackVisuals = !HandsUtils.GetSubsystem().TryGetJoint(TrackedHandJoint.Palm, handNode, out _);
+                    isSimulatedController = false;
+                }
+                InstantiateControllerVisuals(!isSimulatedController, useFallbackVisuals);
             }
         }
 
-        private void CheckToShowVisuals(InputDevice inputDevice)
-        {
-            if ((inputDevice.characteristics & handednessCharacteristic) == handednessCharacteristic &&
-                (inputDevice.characteristics & validControllerCharacteristiscs) == validControllerCharacteristiscs)
-            {
-                // Clear the exiting visuals before proceding to try to render new ones
-                ClearControllerVisuals();
-
-                // Instantiate the controller visuals if this input device matches the characteristics for a valid controller.
-                InstantiateControllerVisuals(true, true);
-            }
-        }
-
-        private void RemoveVisuals(InputDevice inputDevice)
-        {
-            if ((inputDevice.characteristics & handednessCharacteristic) == handednessCharacteristic &&
-                (inputDevice.characteristics & validControllerCharacteristiscs) == validControllerCharacteristiscs)
-            {
-                // Clear the controller visuals if they have been previously instantiated
-                ClearControllerVisuals();
-            }
-        }
 
         private GameObject ControllerGameObject;
         /// <summary>
         /// Tries to instantiate controller visuals for the specified hand node.
         /// </summary>
+        /// <param name="usePlatformVisuals">Whether or not to try to load visuals from the platform provider</param>
         /// <param name="useFallbackVisuals">Whether or not to use the fallback controller visuals</param>
         private async void InstantiateControllerVisuals(bool usePlatformVisuals, bool useFallbackVisuals)
         {
-            // Initialize the ControllerGameObject if it has not yet been initialized
-            // First try to load the controller model from the platform
-            if (ControllerGameObject == null)
+            // Disable any pre-existing controller models before trying to render new ones.
+            platformLoadedGameObject?.SetActive(false);
+            platformLoadedGameObjectRoot?.SetActive(false);
+            fallbackGameObject?.SetActive(false);
+
+            // Try to load the controller model from the platform
+            if (usePlatformVisuals && controllerModelProvider != null)
             {
-                if (usePlatformVisuals && controllerModelProvider != null)
+                platformLoadedGameObject = await ControllerModelLoader.TryGenerateControllerModelFromPlatformSDK(controllerModelProvider);
+                if (platformLoadedGameObject != null)
                 {
-                    GameObject PlatformLoadedGameObject = await ControllerModelLoader.TryGenerateControllerModelFromPlatformSDK(controllerModelProvider);
-                    if (PlatformLoadedGameObject != null)
+                    // Platform models are "rotated" 180 degrees due to the forward vector for a controller pointing towards the user.
+                    // We need to rotate these models in order to have them pointing in the correct direction on device.
+                    if (platformLoadedGameObjectRoot == null)
                     {
-                        // Platform models are "rotated" 180 degrees due to the forward vector for a controller pointing towards the user.
-                        // We need to rotate these models in order to have them pointing in the correct direction on device
-                        GameObject rotationAdjustedGameObject = new GameObject(PlatformLoadedGameObject.name + " Root");
-                        PlatformLoadedGameObject.transform.parent = rotationAdjustedGameObject.transform;
-                        PlatformLoadedGameObject.transform.SetPositionAndRotation(rotationAdjustedGameObject.transform.position, rotationAdjustedGameObject.transform.rotation * Quaternion.Euler(0, 180, 0));
-                        ControllerGameObject = rotationAdjustedGameObject;
+                        platformLoadedGameObjectRoot = new GameObject("Platform Model Root");
                     }
+                    platformLoadedGameObject.transform.parent = platformLoadedGameObjectRoot.transform;
+                    platformLoadedGameObject.transform.SetPositionAndRotation(platformLoadedGameObjectRoot.transform.position, platformLoadedGameObjectRoot.transform.rotation * Quaternion.Euler(0, 180, 0));
+
+                    ControllerGameObject = platformLoadedGameObjectRoot;
+                }
+            }
+
+            // If the ControllerGameObject is still not initialized after this, then use the fallback model if told to
+            if (useFallbackVisuals && ControllerGameObject == null)
+            {
+                if (fallbackGameObject == null)
+                {
+                    fallbackGameObject = Instantiate(fallbackControllerModel);
                 }
 
-                // If the ControllerGameObject is still not initialized after this, then use the fallback model if told to
-                if (ControllerGameObject == null && useFallbackVisuals)
-                {
-                    ControllerGameObject = Instantiate(fallbackControllerModel);
-                }
+                ControllerGameObject = fallbackGameObject;
             }
 
             if (ControllerGameObject != null)
             {
+                ControllerGameObject.SetActive(usePlatformVisuals || useFallbackVisuals);
                 ControllerGameObject.transform.parent = transform;
                 ControllerGameObject.transform.SetPositionAndRotation(transform.position, transform.rotation);
             }
         }
 
-        /// <summary>
-        /// Clears the controller visuals if they have been instantiated
-        /// </summary>
-        private void ClearControllerVisuals()
+        private void RemoveControllerVisuals(InputAction.CallbackContext obj)
         {
-            if (ControllerGameObject != null)
-            {
-                Destroy(ControllerGameObject);
-            }
+            ControllerGameObject.SetActive(false);
         }
 
-        public void Update()
-        {
-            if(ControllerGameObject != null && ControllerGameObject.activeSelf != IsControllerTracked)
-            {
-                ControllerGameObject.SetActive(IsControllerTracked);
-            }
-        }
     }
 }
