be0caa4f419bf03f3e6c6589af7bf1edd51b513a
Finn Sinclair <finnnorth@gmail.com>
2022-10-04 15:50:00 -0700

    Fixing ObjManip HostTransform bugs (#11075)
    * Re-init constraintManager when HostTransform is assigned to
    
    * Fixing HostTransform bug
    
    * Fixing weird abs thing
    
    * Fixing brainfart

--- com.microsoft.mrtk.core/Utilities/Extensions/VectorExtensions.cs
+++ com.microsoft.mrtk.core/Utilities/Extensions/VectorExtensions.cs
@@ -169,9 +169,9 @@ namespace Microsoft.MixedReality.Toolkit
         /// <param name="v2">The second vector.</param>
         /// <param name="distanceTolerance">The maximum distance that will cause this to return true.</param>
         /// <returns>True if the distance between the two vectors is within the tolerance, false otherwise.</returns>
-        public static bool CloseEnough(Vector3 v1, Vector3 v2, float distanceTolerance)
+        public static bool CloseEnoughTo(this Vector3 v1, Vector3 v2, float distanceTolerance = 0.001f)
         {
-            return Mathf.Abs(Vector3.Distance(v1, v2)) < distanceTolerance;
+            return Vector3.Distance(v1, v2) < distanceTolerance;
         }
 
         /// <summary>
@@ -179,7 +179,7 @@ namespace Microsoft.MixedReality.Toolkit
         /// </summary>
         /// <param name="source">The source <see href="https://docs.unity3d.com/ScriptReference/Vector3.html">Vector3</see> to be mapped to sphere</param>
         /// <param name="radius">This is a <see cref="float"/> for the radius of the sphere</param>
-        public static Vector3 SphericalMapping(Vector3 source, float radius)
+        public static Vector3 SphericalMapping(this Vector3 source, float radius)
         {
             float circ = 2f * Mathf.PI * radius;
 
@@ -199,7 +199,7 @@ namespace Microsoft.MixedReality.Toolkit
         /// </summary>
         /// <param name="source">The source <see href="https://docs.unity3d.com/ScriptReference/Vector3.html">Vector3</see> to be mapped to cylinder</param>
         /// <param name="radius">This is a <see cref="float"/> for the radius of the cylinder</param>
-        public static Vector3 CylindricalMapping(Vector3 source, float radius)
+        public static Vector3 CylindricalMapping(this Vector3 source, float radius)
         {
             float circ = 2f * Mathf.PI * radius;
 
@@ -223,7 +223,7 @@ namespace Microsoft.MixedReality.Toolkit
         /// <param name="totalRows">The total rows as a <see cref="int"/> for the radial calculation</param>
         /// <param name="column">The current column as a <see cref="int"/> for the radial calculation</param>
         /// <param name="totalColumns">The total columns as a <see cref="int"/> for the radial calculation</param>
-        public static Vector3 RadialMapping(Vector3 source, float radialRange, float radius, int row, int totalRows, int column, int totalColumns)
+        public static Vector3 RadialMapping(this Vector3 source, float radialRange, float radius, int row, int totalRows, int column, int totalColumns)
         {
             float radialCellAngle = radialRange / totalColumns;
 
@@ -244,7 +244,7 @@ namespace Microsoft.MixedReality.Toolkit
         /// </summary>
         /// <param name="source">The source <see href="https://docs.unity3d.com/ScriptReference/Vector3.html">Vector3</see> to be mapped to cylinder</param>
         /// <param name="radius">This is a <see cref="float"/> for the radius of the cylinder</param>
-        public static Vector3 ScatterMapping(Vector3 source, float radius)
+        public static Vector3 ScatterMapping(this Vector3 source, float radius)
         {
             source.x = UnityEngine.Random.Range(-radius, radius);
             source.y = UnityEngine.Random.Range(-radius, radius);
