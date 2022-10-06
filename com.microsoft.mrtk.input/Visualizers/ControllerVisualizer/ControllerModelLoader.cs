a7a9a42f36a40f64b1f900773a5ea921ae9f4561
Roger Liu <roliu@microsoft.com>
2022-10-06 11:05:58 -0700

    fixed import ordering lint complaints

--- com.microsoft.mrtk.input/Visualizers/ControllerVisualizer/ControllerModelLoader.cs
+++ com.microsoft.mrtk.input/Visualizers/ControllerVisualizer/ControllerModelLoader.cs
@@ -2,10 +2,9 @@
 // Licensed under the MIT License.
 
 using System.Collections.Generic;
-using UnityEngine;
-using System.Threading.Tasks;
-using UnityEngine.InputSystem.XR;
 using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
 
