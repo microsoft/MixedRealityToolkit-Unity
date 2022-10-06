ba838e75a82ba9e5159641a9ccac4e12fbd000ad
Roger Liu <roliu@microsoft.com>
2022-10-06 13:26:10 -0700

    Porting remaining MRTK2 constraints for completeness

---
+++ com.microsoft.mrtk.spatialmanipulation/Constraints/MaintainApparentSizeConstraint.cs
@@ -0,0 +1,45 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.SpatialManipulation
+{
+    /// <summary>
+    /// Component for setting the min/max scale values for ObjectManipulator
+    /// or BoundsControl
+    /// </summary>
+    public class MaintainApparentSizeConstraint : TransformConstraint
+    {
+        #region Properties
+
+        private float initialDist;
+
+        public override TransformFlags ConstraintType => TransformFlags.Scale;
+
+        #endregion Properties
+
+        #region Public Methods
+
+        /// <inheritdoc />
+        public override void Initialize(MixedRealityTransform worldPose)
+        {
+            base.Initialize(worldPose);
+
+            initialDist = (worldPose.Position - Camera.main.transform.position).magnitude;
+        }
+
+        /// <summary>
+        /// Constrains scale such that the object's apparent size at manipulation
+        /// start does not change when the object is moved towards and away from 
+        /// the head.
+        /// </summary>
+        public override void ApplyConstraint(ref MixedRealityTransform transform)
+        {
+            float dist = (transform.Position - Camera.main.transform.position).magnitude;
+            transform.Scale = (dist / initialDist) * worldPoseOnManipulationStart.Scale;
+        }
+
+        #endregion Public Methods
+    }
+}
