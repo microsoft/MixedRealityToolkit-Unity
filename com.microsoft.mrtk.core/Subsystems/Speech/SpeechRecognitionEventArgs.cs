eb5095d004c2afbf6762fce095c28ce6187a527e
MaxWang-MS <68253937+MaxWang-MS@users.noreply.github.com>
2022-10-31 19:25:48 -0700

    Add SpeechRecognitionSubsystem to MRTK3

---
+++ com.microsoft.mrtk.core/Subsystems/Speech/SpeechRecognitionEventArgs.cs
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.MixedReality.Toolkit.Subsystems
+{
+    /// <summary>
+    /// Event data associated with the result of speech recognition.
+    /// </summary>
+    public class SpeechRecognitionResultEventArgs
+    {
+        /// <summary>
+        /// The result of speech recognition.
+        /// </summary>
+        public string Result { get; private set; }
+
+        /// <summary>
+        /// The confidence of the speech recognition result on a scale of 0 to 1.
+        /// Null when the confidence is not available.
+        /// </summary>
+        public float? Confidence { get; private set; }
+
+        /// <summary>
+        /// Construct the <c>SpeechRecognitionResultEventArgs</c>.
+        /// </summary>
+        public SpeechRecognitionResultEventArgs(string result, float? confidence)
+        {
+            Confidence = confidence;
+            Result = result;
+        }
+    }
+
+    /// <summary>
+    ///  Event data associated with the session of speech recognition.
+    /// </summary>
+    public class SpeechRecognitionSessionEventArgs
+    {
+        /// <summary>
+        /// The reason for this speech recognition session event.
+        /// </summary>
+        public string Reason { get; private set; }
+
+        /// <summary>
+        /// Construct the <c>SpeechRecognitionSessionEventArgs</c>.
+        /// </summary>
+        public SpeechRecognitionSessionEventArgs(string reason)
+        {
+            Reason = reason;
+        }
+    }
+}
---
+++ com.microsoft.mrtk.windowsspeech/Subsystems/WindowsSpeechRecognitionSubsystem.cs
@@ -0,0 +1,195 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.MixedReality.Toolkit.Subsystems;
+using UnityEngine;
+using UnityEngine.Scripting;
+
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_WSA
+using UnityEngine.Windows.Speech;
+#endif // UNITY_STANDALONE_WIN || UNITY_WSA || UNITY_EDITOR_WIN
+
+namespace Microsoft.MixedReality.Toolkit.Speech.Windows
+{
+    [Preserve]
+    [MRTKSubsystem(
+        Name = "com.microsoft.mixedreality.windowsspeechrecognition",
+        DisplayName = "MRTK Windows SpeechRecognition Subsystem",
+        Author = "Microsoft",
+        ProviderType = typeof(WindowsSpeechRecognitionProvider),
+        SubsystemTypeOverride = typeof(WindowsSpeechRecognitionSubsystem))]
+    public class WindowsSpeechRecognitionSubsystem : SpeechRecognitionSubsystem
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        static void Register()
+        {
+            // Fetch subsystem metadata from the attribute.
+            var cinfo = XRSubsystemHelpers.ConstructCinfo<WindowsSpeechRecognitionSubsystem, SpeechRecognitionSubsystemCinfo>();
+
+            if (!Register(cinfo))
+            {
+                Debug.LogError($"Failed to register the {cinfo.Name} subsystem.");
+            }
+        }
+
+        [Preserve]
+        class WindowsSpeechRecognitionProvider : Provider
+        {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_WSA
+            private DictationRecognizer dictationRecognizer;
+            private ConfidenceLevel currentConfidenceLevel = ConfidenceLevel.Medium;
+#endif
+
+            /// <summary>
+            /// Constructor of WindowsSpeechRecognitionProvider.
+            /// </summary>
+            public WindowsSpeechRecognitionProvider()
+            {
+#if !(UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_WSA)
+                Debug.LogError("Cannot create WindowsSpeechRecognitionSubsystem because WindowsSpeechRecognitionSubsystem is only supported on Windows Editor, Standalone Windows and UWP.");
+#endif
+            }
+
+
+            /// <inheritdoc/>
+            public override void Destroy()
+            {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_WSA
+                if (dictationRecognizer != null)
+                {
+                    DeregisterDictationEvents();
+                    dictationRecognizer.Dispose();
+                    dictationRecognizer = null;
+                }
+#endif
+            }
+
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_WSA
+            /// <summary>
+            /// Start speech recognition using the specified ConfidenceLevel.
+            /// </summary>
+            public void StartRecognition(ConfidenceLevel confidenceLevel)
+            {
+                if (dictationRecognizer == null || currentConfidenceLevel != confidenceLevel)
+                {
+                    Destroy();
+                    currentConfidenceLevel = confidenceLevel;
+                    dictationRecognizer = new DictationRecognizer(confidenceLevel);
+                }
+                if (dictationRecognizer.Status != SpeechSystemStatus.Running)
+                {
+                    dictationRecognizer.Start();
+                    // Deregister first to prevent duplicate
+                    DeregisterDictationEvents();
+                    RegisterDictationEvents();
+                }
+            }
+#endif
+
+            #region ISpeechRecognitionSubsystem implementation
+
+            /// <inheritdoc/>
+            public override void StartRecognition()
+            {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_WSA
+                StartRecognition(currentConfidenceLevel);
+#else
+                Debug.LogError("Cannot call StartRecognition because WindowsSpeechRecognitionSubsystem is only supported on Windows Editor, Standalone Windows and UWP.");
+#endif
+            }
+
+            /// <inheritdoc/>
+            public override void StopRecognition()
+            {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_WSA
+                if (dictationRecognizer == null)
+                {
+                    dictationRecognizer.Stop();
+                }
+#else
+                Debug.LogError("Cannot call StopRecognition because WindowsSpeechRecognitionSubsystem is only supported on Windows Editor, Standalone Windows and UWP.");
+#endif
+            }
+
+            #endregion ISpeechRecognitionSubsystem implementation
+
+            #region Helpers
+
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || UNITY_WSA
+
+            private void RegisterDictationEvents()
+            {
+                dictationRecognizer.DictationHypothesis += DictationRecognizer_DictationHypothesis;
+                dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
+                dictationRecognizer.DictationComplete += DictationRecognizer_DictationComplete;
+                dictationRecognizer.DictationError += DictationRecognizer_DictationError;
+            }
+
+            private void DeregisterDictationEvents()
+            {
+                dictationRecognizer.DictationHypothesis -= DictationRecognizer_DictationHypothesis;
+                dictationRecognizer.DictationResult -= DictationRecognizer_DictationResult;
+                dictationRecognizer.DictationComplete -= DictationRecognizer_DictationComplete;
+                dictationRecognizer.DictationError -= DictationRecognizer_DictationError;
+            }
+
+            /// <summary>
+            /// This event is fired while the user is talking. As the recognizer listens, it provides text of what it's heard so far.
+            /// </summary>
+            /// <param name="text">The currently hypothesized recognition.</param>
+            private void DictationRecognizer_DictationHypothesis(string text)
+            {
+                SpeechRecognitionResultEventArgs eventArgs = new(text, null);
+                OnRecognizing(eventArgs);
+            }
+
+            /// <summary>
+            /// This event is fired after the user pauses, typically at the end of a sentence. The full recognized string is returned here.
+            /// </summary>
+            /// <param name="text">The text that was heard by the recognizer.</param>
+            /// <param name="confidence">A representation of how confident (rejected, low, medium, high) the recognizer is of this recognition.</param>
+            private void DictationRecognizer_DictationResult(string text, ConfidenceLevel confidence)
+            {
+                SpeechRecognitionResultEventArgs eventArgs = new(text, ConfidenceLevelToFloat(confidence));
+                OnRecognized(eventArgs);
+            }
+
+            /// <summary>
+            /// This event is fired when the recognizer stops, whether from StartRecording() being called, a timeout occurring, or some other error.
+            /// Typically, this will simply return "Complete".
+            /// </summary>
+            /// <param name="cause">An enumerated reason for the session completing.</param>
+            private void DictationRecognizer_DictationComplete(DictationCompletionCause cause)
+            {
+                SpeechRecognitionSessionEventArgs eventArgs = new(cause.ToString());
+                OnRecognitionFinished(eventArgs);
+            }
+
+            /// <summary>
+            /// This event is fired when an error occurs.
+            /// </summary>
+            /// <param name="error">The string representation of the error reason.</param>
+            /// <param name="hresult">The int representation of the hresult.</param>
+            private void DictationRecognizer_DictationError(string error, int hresult)
+            {
+                SpeechRecognitionSessionEventArgs eventArgs = new(error.ToString() + "\nHRESULT: " + hresult);
+                OnRecognitionFaulted(eventArgs);
+            }
+
+            private float ConfidenceLevelToFloat(ConfidenceLevel level)
+            {
+                return level switch
+                {
+                    ConfidenceLevel.Rejected => 0,
+                    ConfidenceLevel.Low => 0.25f,
+                    ConfidenceLevel.Medium => 0.5f,
+                    ConfidenceLevel.High => 0.75f,
+                    _ => 0,
+                };
+            }
+#endif
+
+            #endregion Helpers
+        }
+    }
+}
