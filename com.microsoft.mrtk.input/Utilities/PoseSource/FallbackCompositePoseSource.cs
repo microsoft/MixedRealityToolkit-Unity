e91cccd3be7bcb7dab0a8212469fbc16816462d8
Kurtis <kurtie@microsoft.com>
2022-11-07 22:34:21 -0800

    Add migration path

--- com.microsoft.mrtk.input/Utilities/PoseSource/FallbackCompositePoseSource.cs
+++ com.microsoft.mrtk.input/Utilities/PoseSource/FallbackCompositePoseSource.cs
@@ -11,17 +11,22 @@ namespace Microsoft.MixedReality.Toolkit.Input
     /// which successfully returns a pose.
     /// </summary>
     [Serializable]
-    public class FallbackCompositePoseSource : IPoseSource
+    public class FallbackCompositePoseSource : IPoseSource, ISerializationCallbackReceiver
     {
         [SerializeReference]
         [InterfaceSelector]
         [Tooltip("An ordered list of pose sources to query.")]
-        private IPoseSource[] poseSources;
+        private IPoseSource[] poseSourceList;
+
+        [SerializeField]
+        [Tooltip("An ordered list of pose sources to query.")]
+        [Obsolete, HideInInspector]
+        private PoseSourceWrapper[] poseSources;
 
         /// <summary>
         /// An ordered list of pose sources to query.
         /// </summary>
-        protected IPoseSource[] PoseSources { get => poseSources; set => poseSources = value; }
+        protected IPoseSource[] PoseSources { get => poseSourceList; set => poseSourceList = value; }
 
         /// <summary>
         /// Tries to get a pose from each pose source in order, returning the result of the first pose source
@@ -29,9 +34,9 @@ namespace Microsoft.MixedReality.Toolkit.Input
         /// </summary>
         public bool TryGetPose(out Pose pose)
         {
-            for (int i = 0; i < poseSources.Length; i++)
+            for (int i = 0; i < poseSourceList.Length; i++)
             {
-                IPoseSource currentPoseSource = poseSources[i];
+                IPoseSource currentPoseSource = poseSourceList[i];
                 if (currentPoseSource != null && currentPoseSource.TryGetPose(out pose))
                 {
                     return true;
@@ -40,6 +45,34 @@ namespace Microsoft.MixedReality.Toolkit.Input
 
             pose = Pose.identity;
             return false;
+        }
+
+        [Obsolete]
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            if (poseSources != null && poseSources.Length > 0)
+            {
+                poseSourceList = new IPoseSource[poseSources.Length];
+
+                for (int i = 0; i < poseSources.Length; i++)
+                {
+                    PoseSourceWrapper poseSource = poseSources[i];
+                    poseSourceList[i] = poseSource.source;
+                }
+
+                poseSources = null;
+            }
+        }
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize() { }
+
+        [Serializable, Obsolete]
+        private struct PoseSourceWrapper
+        {
+            [SerializeReference]
+            [InterfaceSelector]
+            [Tooltip("The pose source we are trying to get the pose of")]
+            public IPoseSource source;
         }
     }
 }
