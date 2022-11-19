d05ca1376089919e7f8b219ed2e13f7360bdc46d
Finn Sinclair <finnnorth@gmail.com>
2022-11-18 16:11:46 -0800

    Fixing metas/adding legacy Dialogs

---
+++ com.microsoft.mrtk.uxcore/LegacyDialog/DialogButton.cs
@@ -0,0 +1,98 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.UX.Deprecated
+{
+    /// <summary>
+    /// Handling click event and dismiss dialog
+    /// </summary>
+    [Obsolete("Legacy Dialog is deprecated. Please migrate to the new Dialog. See uxcore/LegacyDialog/README.md")]
+    public class DialogButton : MonoBehaviour
+    {
+        [SerializeField, Tooltip("The TextMeshPro component for displaying text on the button itself.")]
+        private TMP_Text buttonText;
+
+        /// <summary>
+        /// The TextMeshPro component for displaying text on the button itself.
+        /// </summary>
+        [Obsolete("Legacy Dialog is deprecated. Please migrate to the new Dialog. See uxcore/LegacyDialog/README.md")]
+        public TMP_Text ButtonText
+        {
+            get => buttonText;
+            set => buttonText = value;
+        }
+
+        /// <summary>
+        /// A reference to the Dialog that this button is on.
+        /// </summary>
+        [Obsolete("Legacy Dialog is deprecated. Please migrate to the new Dialog. See uxcore/LegacyDialog/README.md")]
+        public Dialog ParentDialog { get; set; }
+
+        /// <summary>
+        /// The type description of the button.
+        /// </summary>
+        [Obsolete("Legacy Dialog is deprecated. Please migrate to the new Dialog. See uxcore/LegacyDialog/README.md")]
+        public DialogButtonTypes ButtonTypeEnum => DialogProperty.Convert(ButtonContext.ButtonType);
+
+        [SerializeField, Tooltip("The type description of the button.")]
+        private DialogButtonContext buttonContext;
+
+        /// <summary>
+        /// The context, including type and an optional label, of the button.
+        /// </summary>
+        [Obsolete("Legacy Dialog is deprecated. Please migrate to the new Dialog. See uxcore/LegacyDialog/README.md")]
+        public DialogButtonContext ButtonContext => buttonContext;
+
+        private void OnValidate()
+        {
+            SetButtonContext(ButtonContext);
+        }
+
+        /// <summary>
+        /// Event handler that runs when button is clicked.
+        /// Dismisses the parent dialog.
+        /// </summary>
+        [Obsolete("Legacy Dialog is deprecated. Please migrate to the new Dialog. See uxcore/LegacyDialog/README.md")]
+        public void OnButtonClicked()
+        {
+            if (ParentDialog != null)
+            {
+                ParentDialog.Property.ResultContext = ButtonContext;
+                ParentDialog.Dismiss();
+            }
+        }
+
+        [Obsolete("Use SetLabel or SetButtonContext instead.")]
+        public void SetTitle(string title) => SetLabel(title);
+
+        /// <summary>
+        /// Sets the label text on the button.
+        /// </summary>
+        /// <param name="label">New label text for the button.</param>
+        [Obsolete("Legacy Dialog is deprecated. Please migrate to the new Dialog. See uxcore/LegacyDialog/README.md")]
+        public void SetLabel(string label)
+        {
+            buttonContext.Label = label;
+
+            if (ButtonText != null)
+            {
+                ButtonText.text = !string.IsNullOrWhiteSpace(buttonContext.Label) ? buttonContext.Label : buttonContext.ButtonType.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Sets the button context and the label text on the button.
+        /// </summary>
+        /// <param name="buttonContext">New button context for this button.</param>
+        [Obsolete("Legacy Dialog is deprecated. Please migrate to the new Dialog. See uxcore/LegacyDialog/README.md")]
+        public void SetButtonContext(DialogButtonContext buttonContext)
+        {
+            this.buttonContext = buttonContext;
+            SetLabel(buttonContext.Label);
+        }
+    }
+}
---
+++ com.microsoft.mrtk.uxcore/LegacyDialog/DialogButtonTypes.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Microsoft.MixedReality.Toolkit.UX.Deprecated
+{
+    /// <summary>
+    /// The style of button on a dialog.
+    /// </summary>
+    [Obsolete("Legacy Dialog is deprecated. Please migrate to the new Dialog. See uxcore/LegacyDialog/README.md")]
+    public enum DialogButtonType
+    {
+        None = 0,
+        Close = 1,
+        Confirm = 2,
+        Cancel = 3,
+        Accept = 4,
+        Yes = 5,
+        No = 6,
+        OK = 7,
+    }
+
+    /// <summary>
+    /// The style of button on a dialog.
+    /// </summary>
+    [Flags]
+    [Obsolete("Legacy Dialog is deprecated. Please migrate to the new Dialog. See uxcore/LegacyDialog/README.md")]
+    public enum DialogButtonTypes
+    {
+        None = 0 << 0,
+        Close = 1 << 0,
+        Confirm = 1 << 1,
+        Cancel = 1 << 2,
+        Accept = 1 << 3,
+        Yes = 1 << 4,
+        No = 1 << 5,
+        OK = 1 << 6,
+    }
+}
