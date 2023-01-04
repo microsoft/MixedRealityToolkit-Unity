c66298c0a917fb24bb2fd901930fe0a9a66de7a0
David Kline (ANALOG) <David.Kline@microsoft.com>
2023-01-04 11:13:43 -0800

    fix assembly version to match file/package

--- com.microsoft.mrtk.accessibility/AssemblyInfo.cs
+++ com.microsoft.mrtk.accessibility/AssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Runtime.CompilerServices;
 // The AssemblyVersion attribute is checked-in and is recommended not to be changed often.
 // https://docs.microsoft.com/troubleshoot/visualstudio/general/assembly-version-assembly-file-version
 // AssemblyFileVersion and AssemblyInformationalVersion are added by pack-upm.ps1 to match the current MRTK build version.
-[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
