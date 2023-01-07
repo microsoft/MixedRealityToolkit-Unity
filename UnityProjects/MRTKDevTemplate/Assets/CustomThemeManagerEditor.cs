0e6a056b2bf7343b223277cd165f5b223ba2b499
Finn Sinclair <finnnorth@gmail.com>
2023-01-06 22:53:11 -0800

    Testing out new Theming

---
+++ UnityProjects/MRTKDevTemplate/Assets/CustomThemeManagerEditor.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Theming;
+using Microsoft.MixedReality.Toolkit.EditorTools;
+
+[UnityEditor.CustomEditor(typeof(ThemeManager))]
+[UnityEditor.CanEditMultipleObjects]
+public class CustomThemeManagerEditor : MRTKEditorBase
+{
+}
