3a0266d33ac8e231c00e0ea0288eeff4aba06892
Kurtis <kurtie@microsoft.com>
2023-01-17 17:20:48 -0800

    Update AssemblyInfo

--- com.microsoft.mrtk.buildwindow/BuildAndDeploy/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-[assembly: System.Reflection.AssemblyVersion("2.8.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion("2.8.0.0")]
-
-[assembly: System.Reflection.AssemblyProduct("Microsoft® Mixed Reality Toolkit")]
-[assembly: System.Reflection.AssemblyCopyright("Copyright © Microsoft Corporation")]
