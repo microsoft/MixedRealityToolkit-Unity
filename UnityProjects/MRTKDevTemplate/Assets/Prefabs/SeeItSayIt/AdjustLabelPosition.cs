c136e0defab4ae1a3ae2bae412ff75a350f59b01
Marlena Klein <marlenaklein@microsoft.com>
2023-04-03 12:49:24 -0700

    Add namespace

--- UnityProjects/MRTKDevTemplate/Assets/Prefabs/SeeItSayIt/AdjustLabelPosition.cs
+++ UnityProjects/MRTKDevTemplate/Assets/Prefabs/SeeItSayIt/AdjustLabelPosition.cs
@@ -1,28 +1,38 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 using System;
 using UnityEngine;
 
-public class AdjustLabelPosition : MonoBehaviour
+namespace Microsoft.MixedReality.Toolkit.Examples.Demos
 {
 
-    [SerializeField]
-    private Transform label;
-
-    private Vector2 _lastSize;
-
-    public void Update()
+    /// <summary>
+    /// Script to adjust the position of a another GameObject when this GameObject resizes.
+    /// EX Used to position see-it say-it labels when non-canvas button backplate resizes. 
+    /// </summary>
+    public class AdjustLabelPosition : MonoBehaviour
     {
-        // Check if the size of the backplate has changed
-        if (gameObject.transform.lossyScale != (Vector3)_lastSize)
+
+        [SerializeField]
+        private Transform label;
+
+        private Vector2 _lastSize;
+
+        public void Update()
         {
-            // Calculate the new position of the label based on the current size of the backplate
-            float childOffset = ((gameObject.transform.lossyScale.y) / 2f * -1);
+            // Check if the size of the backplate has changed
+            if (gameObject.transform.lossyScale != (Vector3)_lastSize)
+            {
+                // Calculate the new position of the label based on the current size of the backplate
+                float childOffset = ((gameObject.transform.lossyScale.y) / 2f * -1);
 
-            label.localPosition = new Vector3(label.localPosition.x, childOffset, label.localPosition.z);
+                label.localPosition = new Vector3(label.localPosition.x, childOffset, label.localPosition.z);
 
 
-            // Store the current size of the button for the next frame
-            _lastSize = gameObject.transform.lossyScale;
+                // Store the current size of the backplate for the next frame
+                _lastSize = gameObject.transform.lossyScale;
+            }
         }
     }
-
 }
