3dbc70fcf7ede0bde4aaf88e2964fa4180d860f7
Marlena Klein <marlenaklein@microsoft.com>
2023-04-04 17:12:31 -0700

    Update adjustment script

--- UnityProjects/MRTKDevTemplate/Assets/Scripts/AdjustLabelPosition.cs
+++ UnityProjects/MRTKDevTemplate/Assets/Scripts/AdjustLabelPosition.cs
@@ -1,38 +1,27 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System;
 using UnityEngine;
 
 namespace Microsoft.MixedReality.Toolkit.Examples.Demos
 {
 
     /// <summary>
-    /// Script to adjust the position of a another GameObject when this GameObject resizes.
-    /// EX Used to position see-it say-it labels when non-canvas button backplate resizes. 
+    /// Script to adjust the position of this GameObject when another GameObject resizes.
+    /// Used to position see-it say-it labels when non-canvas button backplate resizes. 
     /// </summary>
     public class AdjustLabelPosition : MonoBehaviour
     {
 
         [SerializeField]
-        private Transform label;
+        [Tooltip("The object whose dimensions controls the placement of this object.")]
+        private Transform control;
 
-        private Vector2 _lastSize;
-
-        public void Update()
+        private void Awake()
         {
-            // Check if the size of the backplate has changed
-            if (gameObject.transform.lossyScale != (Vector3)_lastSize)
-            {
-                // Calculate the new position of the label based on the current size of the backplate
-                float childOffset = ((gameObject.transform.lossyScale.y) / 2f * -1);
-
-                label.localPosition = new Vector3(label.localPosition.x, childOffset, label.localPosition.z);
-
-
-                // Store the current size of the backplate for the next frame
-                _lastSize = gameObject.transform.lossyScale;
-            }
+            // Calculate the new position of the label based on the current size of the backplate
+            float childOffset = ((control.lossyScale.y) / 2f * -1);
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, childOffset, gameObject.transform.localPosition.z);
         }
     }
 }
