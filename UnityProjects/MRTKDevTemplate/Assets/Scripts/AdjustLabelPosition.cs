f7a9671b6f29bc131d15b38e897fb82da01bdd65
Marlena Klein <marlenaklein@microsoft.com>
2023-04-05 11:57:23 -0700

    Nit style

--- UnityProjects/MRTKDevTemplate/Assets/Scripts/AdjustLabelPosition.cs
+++ UnityProjects/MRTKDevTemplate/Assets/Scripts/AdjustLabelPosition.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 
 namespace Microsoft.MixedReality.Toolkit.Examples.Demos
 {
-
     /// <summary>
     /// Script to adjust the position of this GameObject when another GameObject resizes.
     /// Used to position see-it say-it labels when non-canvas button backplate resizes. 
     /// </summary>
     public class AdjustLabelPosition : MonoBehaviour
     {
-
         [SerializeField]
         [Tooltip("The object whose dimensions controls the placement of this object.")]
         private Transform control;
