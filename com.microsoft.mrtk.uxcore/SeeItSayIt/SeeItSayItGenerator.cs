7d7f4ca3bd678ed757a9af93b14b4a854dbc4a97
Marlena Klein <marlenaklein@microsoft.com>
2023-04-10 11:15:19 -0700

    Fix Z-position  for label poke out

--- com.microsoft.mrtk.uxcore/SeeItSayIt/SeeItSayItGenerator.cs
+++ com.microsoft.mrtk.uxcore/SeeItSayIt/SeeItSayItGenerator.cs
@@ -11,6 +11,9 @@ namespace Microsoft.MixedReality.Toolkit.UX
         [SerializeField]
         private Transform PositionControl;
 
+        [SerializeField]
+        private bool IsCanvas;
+
         private void Awake()
         {
             PressableButton pressablebutton = gameObject.GetComponent<PressableButton>();
@@ -18,19 +21,21 @@ namespace Microsoft.MixedReality.Toolkit.UX
             {
 #if MRTK_INPUT_PRESENT && MRTK_SPEECH_PRESENT
                 GameObject label = Instantiate(SeeItSayItPrefab, transform, false);
-                RectTransform canvasTransform = label.GetComponent<RectTransform>();
 
-                if (canvasTransform != null)
+                if (IsCanvas && label.transform.childCount > 0)
                 {
                     RectTransform labelTransform = label.transform.GetChild(0).gameObject.GetComponent<RectTransform>();
-                    if (labelTransform != null)
+                    RectTransform controlTransform = PositionControl.gameObject.GetComponent<RectTransform>();
+                    RectTransform canvasTransform = label.GetComponent<RectTransform>();
+                    if (labelTransform != null && canvasTransform != null && controlTransform != null)
                     {
-                        labelTransform.anchoredPosition = new Vector2(canvasTransform.rect.width / 2f, canvasTransform.rect.height / 2f + (PositionControl.gameObject.GetComponent<RectTransform>().rect.height /  2f * -1) - 10f);
+                        labelTransform.anchoredPosition3D = new Vector3(canvasTransform.rect.width / 2f, canvasTransform.rect.height / 2f + (controlTransform.rect.height /  2f * -1) - 10f, -10f);
+
                     }
                 }
                 else
                 {
-                    label.transform.localPosition = new Vector3(PositionControl.localPosition.x, (PositionControl.lossyScale.y / 2f * -1) -.007f, PositionControl.localPosition.z);
+                    label.transform.localPosition = new Vector3(PositionControl.localPosition.x, (PositionControl.lossyScale.y / 2f * -1) -.007f, PositionControl.localPosition.z - .01f);
                 }
 
                 foreach (Transform child in label.transform)
