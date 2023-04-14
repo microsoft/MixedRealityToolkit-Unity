4e1a5eb4d07023526c07e69b65e97a4a8d04b2ce
Marlena Klein <125413876+marlenaklein-msft@users.noreply.github.com>
2023-04-14 16:42:16 -0700

    [MRTK3] Port Enabled SeeItSayIt Labels (#11466)
    - Adds a SeeItSayItLabelEnabler script which enables See-It Say-It labels at runtime, if voice commands are enabled for a button and the speech and input packages are installed.
    
    - Adds anims to uxcomponents and uxcomponents.noncanvas, and adds a StateVisualizer component to canvas and non-canvas See-It Say-It label prefabs, to show See-It Say-It label on active hover.
    
    - Updates the canvas and non-canvas button prefabs (Empty Button and Pressable Button_128x32mm_TextOnly) to add See-It Say-It Labels (disabled by default) and the SeeItSayItLabelEnabler. 
    
    - Updates the SeeItSayIt Example scene to use the SeeItSayItLabelEnabler component to enable labels at runtime. 
    
    - Adds unit tests for canvas and non-canvas label prefabs, as well as SeeItSayItLabelEnabler script.
    
    - Adds null checks to StateVisualizer.

---
+++ com.microsoft.mrtk.uxcomponents/Tests/Runtime/SeeItSayItCanvasTests.cs
@@ -0,0 +1,132 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections;
+using System.Threading.Tasks;
+using Microsoft.MixedReality.Toolkit.Core.Tests;
+using Microsoft.MixedReality.Toolkit.Input.Tests;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Microsoft.MixedReality.Toolkit.UX.Runtime.Tests
+{
+    /// <summary>
+    /// Tests for the Canvas See-It Say-It label.
+    /// </summary>
+    public class SeeItSayItCanvasTests : BaseRuntimeInputTests
+    {
+        // SeeItSayItLabel/SeeItSayItLabel-Canvas.prefab
+        private const string SeeItSayItLabelGuid = "d9e84b5a8037fd946aa503a059fee93f";
+        private static readonly string SeeItSayItLabelPath = AssetDatabase.GUIDToAssetPath(SeeItSayItLabelGuid);
+
+        // Button/Prefabs/Empty Button.prefab
+        private const string EmptyButtonGuid = "b85e005d231192249b7077b40a4d4e45";
+        private static readonly string EmptyButtonPath = AssetDatabase.GUIDToAssetPath(EmptyButtonGuid);
+
+        [UnityTest]
+        public IEnumerator TestSeeItSayItLabelInstantiate()
+        {
+            GameObject testLabel = InstantiatePrefab(SeeItSayItLabelPath);
+            yield return null;
+
+            StateVisualizer labelStateVisualizerComponent = testLabel.GetComponent<StateVisualizer>();
+            Assert.IsNotNull(labelStateVisualizerComponent, "State visualizer component exists on label");
+            Assert.AreEqual(testLabel.transform.childCount, 1, "Label has one child GameObject");
+
+            Object.Destroy(testLabel);
+            // Wait for a frame to give Unity a change to actually destroy the object
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator TestEmptyButtonSeeItSayItLabelEnabler()
+        {
+            GameObject testButton = InstantiatePrefab(EmptyButtonPath);
+            yield return null;
+
+            SeeItSayItLabelEnabler labelEnablerComponent = testButton.GetComponent<SeeItSayItLabelEnabler>();
+            Assert.IsNotNull(labelEnablerComponent, "SeeItSayIt enabler component exists on empty button prefab");
+
+            Object.Destroy(testButton);
+            // Wait for a frame to give Unity a change to actually destroy the object
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator TestLabelEnabledOnHover()
+        {
+            GameObject testButton = InstantiatePrefab(EmptyButtonPath);
+            yield return null;
+
+            // Ensure that there is a label 
+            GameObject generatedLabel = GameObject.Find("SeeItSayItLabel-Canvas");
+            if (generatedLabel == null)
+            {
+                generatedLabel = InstantiateChildPrefab(SeeItSayItLabelPath, testButton.transform);
+            }
+
+            // and label child (the part that is enabled and disabled on hover)
+            GameObject labelChild = null;
+            if (generatedLabel.transform.childCount >= 1)
+            {
+                labelChild = generatedLabel.transform.GetChild(0).gameObject;
+                labelChild.SetActive(false);
+            }
+
+            // No hover initially -- label should be disabled
+            Assert.IsTrue(labelChild?.activeInHierarchy == false, "The label is disabled when the button is not hovered.");
+
+            // Move hand to hover the object, wait for the animation to play
+            yield return HoverButtonWithHand(testButton.transform.position);
+            yield return RuntimeTestUtilities.WaitForFixedUpdates(frameCount: 50);
+            Assert.IsTrue(labelChild?.activeInHierarchy == true, "The label is enabled when the button is hovered.");
+
+            // Move hand away from the object
+            yield return ReleaseButtonWithHand(testButton.transform.position);
+            yield return RuntimeTestUtilities.WaitForUpdates();
+            Assert.IsTrue(labelChild?.activeInHierarchy == false, "The label is disabled when the button is not hovered.");
+
+            Object.Destroy(testButton);
+            // Wait for a frame to give Unity a change to actually destroy the object
+            yield return null;
+        }
+
+        private GameObject InstantiatePrefab(string prefabPath)
+        {
+            Object pressableButtonPrefab = AssetDatabase.LoadAssetAtPath(prefabPath, typeof(Object));
+            GameObject testGO = Object.Instantiate(pressableButtonPrefab) as GameObject;
+
+            return testGO;
+        }
+
+        private GameObject InstantiateChildPrefab(string prefabPath, Transform parent)
+        {
+            Object pressableButtonPrefab = AssetDatabase.LoadAssetAtPath(prefabPath, typeof(Object));
+            GameObject testGO = Object.Instantiate(pressableButtonPrefab, parent) as GameObject;
+
+            return testGO;
+        }
+
+        /// <summary>
+        /// Move the hand forward to the button
+        /// </summary>
+        private IEnumerator HoverButtonWithHand(Vector3 buttonPosition)
+        {
+            TestHand hand = new TestHand(Handedness.Right);
+            yield return hand.Show(Vector3.zero);
+            yield return hand.MoveTo(buttonPosition, 15);
+        }
+
+        /// <summary>
+        /// Move the hand away from the button
+        /// </summary>
+        private IEnumerator ReleaseButtonWithHand(Vector3 buttonPosition)
+        {
+            TestHand hand = new TestHand(Handedness.Right);
+            yield return hand.MoveTo(buttonPosition + new Vector3(0.0f, 0, -0.05f));
+            yield return hand.Hide();
+        }
+    }
+}
---
+++ com.microsoft.mrtk.uxcore/SeeItSayIt/SeeItSayItLabelEnabler.cs
@@ -0,0 +1,98 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using TMPro;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.UX
+{
+    [RequireComponent(typeof(PressableButton))]
+    [AddComponentMenu("MRTK/UX/See It Say It Label")]
+    public class SeeItSayItLabelEnabler : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("The GameObject for the see-it say-it label to be enabled.")]
+        private GameObject seeItSayItLabel;
+
+        /// <summary>
+        /// The GameObject for the see-it say-it label to be enabled.
+        /// </summary>
+        public GameObject SeeItSayItLabel
+        {
+            get => seeItSayItLabel;
+            set => seeItSayItLabel = value;
+        }
+
+        [SerializeField]
+        [Tooltip("The Transform that the label will be dynamically positioned off of. Empty by default. If positioning a Canvas label, this must be a RectTransform.")]
+        private Transform positionControl;
+
+        /// <summary>
+        /// The Transform that the label will be dynamically positioned off of. Empty by default. If positioning a Canvas label, this must be a RectTransform.
+        /// </summary>
+        public Transform PositionControl
+        {
+            get => positionControl;
+            set => positionControl = value;
+        }
+
+        private float canvasOffset = -10f;
+        private float nonCanvasOffset = -.004f;
+
+        private void Start()
+        {
+            // Check if voice commands are enabled for this button
+            PressableButton pressablebutton = gameObject.GetComponent<PressableButton>();
+            if (pressablebutton != null && pressablebutton.AllowSelectByVoice)
+            {
+                // Check if input and speech packages are present
+#if MRTK_INPUT_PRESENT && MRTK_SPEECH_PRESENT
+                SeeItSayItLabel.SetActive(true);
+
+                // Children must be disabled so that they are not initially visible 
+                foreach (Transform child in SeeItSayItLabel.transform)
+                {
+                    child.gameObject.SetActive(false);
+                }
+
+                // Set the label text to reflect the speech recognition keyword
+                string keyword = pressablebutton.SpeechRecognitionKeyword;
+                if (keyword != null)
+                {
+                    TMP_Text labelText = SeeItSayItLabel.GetComponentInChildren<TMP_Text>(true);
+                    if (labelText != null)
+                    {
+                        labelText.text = $"Say '{keyword}'";
+                    }
+                }
+
+                // If a Transform is specified, use it to reposition the object dynamically
+                if (positionControl != null)
+                {
+                    // The control RectTransform used to position the label's height
+                    RectTransform controlRectTransform = PositionControl.gameObject.GetComponent<RectTransform>();
+
+                    // If PositionControl is a RectTransform, reposition label relative to Canvas button
+                    if (controlRectTransform != null &&  SeeItSayItLabel.transform.childCount > 0)
+                    {
+                        // The parent RectTransform used to center the label
+                        RectTransform canvasTransform = SeeItSayItLabel.GetComponent<RectTransform>();
+
+                        // The child RectTransform used to set the final position of the label 
+                        RectTransform labelTransform = SeeItSayItLabel.transform.GetChild(0).gameObject.GetComponent<RectTransform>();
+
+                        if (labelTransform != null && canvasTransform != null)
+                        {
+                            labelTransform.anchoredPosition3D = new Vector3(canvasTransform.rect.width / 2f, canvasTransform.rect.height / 2f + (controlRectTransform.rect.height /  2f * -1) + canvasOffset, canvasOffset);
+                        }
+                    }
+                    else
+                    {
+                        SeeItSayItLabel.transform.localPosition = new Vector3(PositionControl.localPosition.x, (PositionControl.lossyScale.y / 2f * -1) + nonCanvasOffset, PositionControl.localPosition.z + nonCanvasOffset);
+                    }
+                }
+#endif
+            }
+        }
+    }
+}
