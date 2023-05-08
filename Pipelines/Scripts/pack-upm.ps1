dd8cd9891bb373a705e9225e995f7125b5948bc9
REDMOND\shaynie <shaynie@microsoft.com>
2023-05-08 08:40:51 -0700

    Modify the CI pipeline to produce packages with a datecode and an optional prerelease tag.

--- Pipelines/Scripts/pack-upm.ps1
+++ Pipelines/Scripts/pack-upm.ps1
@@ -16,6 +16,9 @@
     The fourth digit (revision) for the full version.
 .PARAMETER PreviewTag
     The tag to append after the version, including the preview number (e.g. "internal.0" or "pre.100")
+.PARAMETER ReleasePackages
+    An array of the package names that are approved for release.  If the package isn't in this array, it will get labeled -Pre
+
 #>
 param(
     [Parameter(Mandatory = $true)]
@@ -30,15 +33,18 @@ param(
     [string]$BuildNumber,
 
     [ValidatePattern("[A-Za-z]+\.\d+[\.\d+]*")]
-    [string]$PreviewTag
-)
+    [string]$PreviewTag,
+    
+    [string]$DateCode,
 
+    [string]$DateCodeRevision,
+
+    [string]$ReleasePackages
+)
+$releasePkgs = $ReleasePackages.Split(",")
+$versionHash = @{}
 $ProjectRoot = Resolve-Path -Path $ProjectRoot
 
-if ($PreviewTag) {
-    $PreviewTag = "-$PreviewTag"
-    Write-Output "Version preview: $PreviewTag"
-}
 
 if ($BuildNumber) {
     $BuildNumber = ".$BuildNumber"
@@ -50,6 +56,8 @@ if (-not (Test-Path $OutputDirectory -PathType Container)) {
 
 $OutputDirectory = Resolve-Path -Path $OutputDirectory
 Write-Output "OutputDirectory: $OutputDirectory"
+
+Write-Output "Preview packages: $releasePkgs"
 
 try {
     Push-Location $OutputDirectory
@@ -84,17 +92,36 @@ try {
         }
 
         if ($parseVersion) {
-            $inlineVersion = Select-String '"version" *: *"([0-9.]+)-?[a-zA-Z0-9.]*' -InputObject (Get-Content -Path $_)
+            $inlineVersion = Select-String '"version" *: *"([0-9.]+)(-?[a-zA-Z0-9.]*)' -InputObject (Get-Content -Path $_)
             $Version = $inlineVersion.Matches.Groups[1].Value
+            $suffix = $inlineVersion.Matches.Groups[2].Value
+
+        }
+        
+        if (!$releasePkgs.Contains( $packageName )) {
+            $preview = "$PreviewTag."
+            Write-Output "Version preview: $preview"
+        } else {
+            $preview = ""
         }
 
-        Write-Output "Patching package version to $Version$PreviewTag"
-        ((Get-Content -Path $_ -Raw) -Replace '("version": |"com\.microsoft\.mrtk\.\w+" *: *)"(?:[0-9.]+|%version%)-?[a-zA-Z0-9.]*', "`$1`"$Version$PreviewTag") | Set-Content -Path $_ -NoNewline
+        $buildTag = "$preview$DateCode.$DateCodeRevision"
+        Write-Output "buildTag: $buildTag"
+
+
+        $versionHash[$packageName]=$Version
+
+        Write-Output " Version: $Version"
+        Write-Output " suffix:  $suffix"
+
+        Write-Output "Patching package version to $Version-$buildTag"
+        ((Get-Content -Path $_ -Raw) -Replace '("version": )"(?:[0-9.]+|%version%)-?[a-zA-Z0-9.]*', "`$1`"$Version-$buildTag") | Set-Content -Path $_ -NoNewline
 
         Write-Output "Patching assembly version to $Version$BuildNumber"
         Get-ChildItem -Path $packagePath/AssemblyInfo.cs -Recurse | ForEach-Object {
+            (Get-Content -Path $_ -Raw) -Replace '\[assembly:.AssemblyVersion\(.*', "[assembly: AssemblyVersion(`"$Version.0`")]`r" | Set-Content -Path $_ -NoNewline
             Add-Content -Path $_ -Value "[assembly: AssemblyFileVersion(`"$Version$BuildNumber`")]"
-            Add-Content -Path $_ -Value "[assembly: AssemblyInformationalVersion(`"$Version$PreviewTag`")]"
+            Add-Content -Path $_ -Value "[assembly: AssemblyInformationalVersion(`"$Version-$buildTag`")]"
         }
 
         Write-Output "Packing $packageFriendlyName"
