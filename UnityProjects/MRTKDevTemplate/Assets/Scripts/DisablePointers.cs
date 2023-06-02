3b6af8fde8b7fe87f79da6f8e6b6f0387ea4b8b6
Vicky Voinova <vicky.voinova@unity3d.com>
2023-06-01 21:35:48 -0400

    Setup Hololens and VR modes, hooked up the toggles and buttons

--- UnityProjects/MRTKDevTemplate/Assets/Scripts/DisablePointers.cs
+++ UnityProjects/MRTKDevTemplate/Assets/Scripts/DisablePointers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Microsoft.MixedReality.Toolkit.UX;
 using Microsoft.MixedReality.Toolkit.Subsystems;
+using UnityEngine.XR;
 
 namespace Microsoft.MixedReality.Toolkit.Examples
 {
@@ -10,7 +11,7 @@ namespace Microsoft.MixedReality.Toolkit.Examples
     /// Example script that turns pointers on and off
     /// by activating and deactivating the interactors.
     /// </summary>
-    [RequireComponent(typeof(PointerBehaviorControls))]
+    [RequireComponent(typeof(InteractorBehaviorControls))]
     [AddComponentMenu("MRTK/Examples/Disable Pointers")]
     public class DisablePointers : MonoBehaviour
     {
@@ -20,19 +21,101 @@ namespace Microsoft.MixedReality.Toolkit.Examples
         public PressableButton HandRayToggle;
         public PressableButton ControllerRayToggle;
 
-        void Start()
+        public PressableButton VRToggle;
+        public PressableButton HololensToggle;
+
+        InteractorBehaviorControls interactorBehaviorControls;
+
+        private void Awake()
+        {
+            interactorBehaviorControls = GetComponent<InteractorBehaviorControls>();
+        }
+
+        private void Start()
         {
             ResetExample();
         }
 
-        public void ResetExample()
+        private void OnEnable()
         {
-            
+            interactorBehaviorControls.onControllerRayToggled += OnControllerRayToggled;
+            interactorBehaviorControls.onGazeToggled += OnGazeToggled;
+            interactorBehaviorControls.onGrabToggled += OnGrabToggled;
+            interactorBehaviorControls.onHandRayToggled += OnHandRayToggled;
+            interactorBehaviorControls.onPokeToggled += OnPokeToggled;
         }
 
-        void Update()
+        private void OnDisable()
         {
-            
+            interactorBehaviorControls.onControllerRayToggled -= OnControllerRayToggled;
+            interactorBehaviorControls.onGazeToggled -= OnGazeToggled;
+            interactorBehaviorControls.onGrabToggled -= OnGrabToggled;
+            interactorBehaviorControls.onHandRayToggled -= OnHandRayToggled;
+            interactorBehaviorControls.onPokeToggled -= OnPokeToggled;
+        }
+
+        public void ResetExample()
+        {
+            List<InputDevice> motionControllers = new List<InputDevice>();
+            InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.HeldInHand, motionControllers);
+
+            if(motionControllers.Count > 0)
+            {
+                SetVRModeActive();
+            }
+            else if(XRSubsystemHelpers.HandsAggregator != null)
+            {
+                SetHololensModeActive();
+            }
+        }
+
+        public void SetVRModeActive()
+        {
+            interactorBehaviorControls.SetVR();
+
+            SetToggleActive(VRToggle, true);
+            SetToggleActive(HololensToggle, false);
+        }
+
+        public void SetHololensModeActive()
+        {
+            interactorBehaviorControls.SetHololens();
+
+            SetToggleActive(VRToggle, false);
+            SetToggleActive(HololensToggle, true);
+        }
+
+        private void OnPokeToggled(bool isActive)
+        {
+            SetToggleActive(PokeToggle, isActive);
+        }
+
+        private void OnHandRayToggled(bool isActive)
+        {
+            SetToggleActive(HandRayToggle, isActive);
+        }
+
+        private void OnGrabToggled(bool isActive)
+        {
+            SetToggleActive(GrabToggle, isActive);
+        }
+
+        private void OnGazeToggled(bool isActive)
+        {
+            SetToggleActive(GazeToggle, isActive);
+        }
+
+        private void OnControllerRayToggled(bool isActive)
+        {
+            SetToggleActive(ControllerRayToggle, isActive);
+        }
+
+        private void SetToggleActive(PressableButton toggle, bool isActive)
+        {
+            if(toggle.IsToggled != isActive)
+            {
+                toggle.ForceSetToggled(isActive, false);
+            }
         }
     }
 }
--- UnityProjects/MRTKDevTemplate/Assets/Scripts/InteractorBehaviorControls.cs
+++ UnityProjects/MRTKDevTemplate/Assets/Scripts/InteractorBehaviorControls.cs
@@ -1,4 +1,5 @@
 using Microsoft.MixedReality.Toolkit.Input;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,11 +11,14 @@ namespace Microsoft.MixedReality.Toolkit.Examples
     /// Utility class to control <see cref="Microsoft.MixedReality.Toolkit.Input.PointerBehavior"/> of pointers.
     /// Hook up buttons to the public functions to turn rays on and off.
     /// </summary>
-    [AddComponentMenu("Scripts/MRTK/SDK/PointerBehaviorControls")]
-    public class PointerBehaviorControls : MonoBehaviour
+    [AddComponentMenu("Scripts/MRTK/SDK/InteractorBehaviorControls")]
+    public class InteractorBehaviorControls : MonoBehaviour
     {
         [SerializeField]
         private InteractionModeManager interactionModeManager;
+
+        [SerializeField]
+        private XRInteractionManager interactionManager;
 
         [SerializeField]
         private XRBaseInteractor[] handRaysInteractors;
@@ -31,29 +35,58 @@ namespace Microsoft.MixedReality.Toolkit.Examples
         [SerializeField]
         private XRBaseInteractor gazeInteractor;
 
+        public event Action<bool> onHandRayToggled;
+        public event Action<bool> onControllerRayToggled;
+        public event Action<bool> onGrabToggled;
+        public event Action<bool> onPokeToggled;
+        public event Action<bool> onGazeToggled;
+
+        public void SetHololens()
+        {
+            SetHandPokeActive(true);
+            SetHandGrabActive(true);
+            SetHandRayActive(true);
+            SetControllerRayActive(false);
+            SetGazeActive(true);
+        }
+
+        public void SetVR()
+        {
+            SetHandPokeActive(false);
+            SetHandGrabActive(false);
+            SetHandRayActive(false);
+            SetControllerRayActive(true);
+            SetGazeActive(false);
+        }
+
         public void SetGazeActive(bool isActive)
         {
             ToggleInteractor(gazeInteractor, isActive);
+            onGazeToggled?.Invoke(isActive);
         }
 
         public void SetHandPokeActive(bool isActive)
         {
             ToggleInteractors(pokeInteractors, isActive);
+            onPokeToggled?.Invoke(isActive);
         }
 
         public void SetHandGrabActive(bool isActive)
         {
             ToggleInteractors(grabInteractors, isActive);
+            onGrabToggled?.Invoke(isActive);
         }
 
         public void SetControllerRayActive(bool isActive)
         {
             ToggleInteractors(controllerRayInteractors, isActive);
+            onControllerRayToggled?.Invoke(isActive);
         }
 
         public void SetHandRayActive(bool isActive)
         {
             ToggleInteractors(handRaysInteractors, isActive);
+            onHandRayToggled?.Invoke(isActive);
         }
 
         private void ToggleInteractors(XRBaseInteractor[] interactors, bool isActive)
