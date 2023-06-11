8b1386f7b0f0827b6a20b7f8664ed9ac6556ae91
REDMOND\shaynie <shaynie@microsoft.com>
2023-06-11 10:47:58 -0700

    Save perf scene work

---
+++ UnityProjects/MRTKDevTemplate/Assets/Performance/ModelSpawner.cs
@@ -0,0 +1,220 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.IO;
+using System.Text;
+
+public class ModelSpawner : MonoBehaviour
+{
+    [Serializable]
+    public struct SpawnTest
+    {
+        public string TestName;
+        public GameObject ModelToSpawn;
+        public float ModelSize;
+        public GameObject Parent;
+        public int StepCount;
+        public bool Instantiate;
+    }
+
+    [SerializeField]
+    private GameObject model;
+
+    [SerializeField]
+    private TextMeshProUGUI label;
+
+    [SerializeField]
+    private TextMeshProUGUI framerateText;
+
+    [SerializeField]
+    private float secondsBetweenFramerateUpdates = 0.25f;
+
+    [SerializeField]
+    private GameObject canvasParent;
+
+    [SerializeField]
+    private float offset = 0;
+
+    [SerializeField]
+    private int columns = 20;
+
+    [SerializeField]
+    private int rows = 10;
+
+    [SerializeField]
+    private int startCount = 0;
+
+    //[SerializeField]
+    private int targetLowFramerate = 60;
+
+    [SerializeField]
+    public SpawnTest[] Tests;
+
+    private float secondsSinceLastFramerateUpdate = 0.0f;
+    private int currentCount = 0;
+    private List<GameObject> models = new List<GameObject>();
+    private bool allTestsComplete = false;
+    StreamWriter writer;
+    //StringBuilder resultsStringBuilder = new StringBuilder(8192);
+    string filePath;
+    private int frameCount = 0;
+    private readonly System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+    private float samplePeriod = 0.1f;
+    public float FrameRate = 0f;
+    private int currentTestIdx = -1;
+    private SpawnTest currentTest;
+    private int yRank = 0;
+    private float yOffset = 0.0f;
+    private int zRank = 0;
+    private float zOffset = 0.0f;
+
+    private void Start()
+    {
+        if (columns == 0)
+        {
+            columns = 20;
+        }
+
+    }
+
+    private void OnDestroy()
+    {
+        stopwatch.Stop();
+    }
+
+    //private void WriteResults()
+    //{
+    //    Debug.Log($"Results sb size: {resultsStringBuilder.Length}");
+    //    filePath = $"{Application.persistentDataPath}\\{currentTest.TestName}.csv";
+    //    using (StreamWriter sw = new StreamWriter(filePath))
+    //    {
+    //        sw.Write(resultsStringBuilder.ToString());
+    //        sw.Flush();
+    //    }
+
+    //    resultsStringBuilder.Clear();
+    //}
+
+    public void StartNextTest()
+    {
+        SetModelCount(0);
+
+        currentTestIdx++;
+        if (currentTestIdx >= Tests.Length)
+        {
+            Debug.Log("Finished all tests");
+            allTestsComplete = true;
+            return;
+        }
+
+        currentTest = Tests[currentTestIdx];
+
+
+        stopwatch.Start();
+    }
+
+    private void LateUpdate()
+    {
+        secondsSinceLastFramerateUpdate += Time.deltaTime;
+        FrameRate = (int)(1.0f / Time.smoothDeltaTime);
+        if (secondsSinceLastFramerateUpdate >= secondsBetweenFramerateUpdates)
+        {
+            framerateText.text = FrameRate.ToString();
+            secondsSinceLastFramerateUpdate = 0;
+        }
+    }
+
+    public void AddModel()
+    {
+        SetModelCount(currentCount + 1);
+    }
+
+    public void RemoveModel()
+    {
+        SetModelCount(currentCount - 1);
+    }
+
+    private int frameWait = 10;
+    private int lowFramerateFramecount = 0;
+
+    private void Update()
+    {
+        if (currentTestIdx < 0 || allTestsComplete)
+        {
+            return;
+        }
+
+        if (FrameRate < targetLowFramerate)
+        {
+            lowFramerateFramecount++;
+        }
+
+        if (currentCount < 800 && lowFramerateFramecount < 20 )
+        {
+            //resultsStringBuilder.Append(currentCount);
+            //resultsStringBuilder.Append(",");
+            //resultsStringBuilder.AppendLine(FrameRate.ToString());
+
+            if (frameWait == 0)
+            {
+                int cachedCount = currentCount;
+                if (currentTest.Instantiate)
+                {
+                    SetModelCount(0);
+                }
+                cachedCount += currentTest.StepCount;
+                SetModelCount(cachedCount);
+                frameWait = 10;
+            }
+            else
+                frameWait--;
+        }
+        else
+        {
+            //WriteResults();
+            Debug.Log($"Test complete after {currentCount} objects, wrote data to {filePath}");
+            StartNextTest();
+        }
+    }
+
+    public void SetModelCount(int count)
+    {
+        if (count < currentCount)
+        {
+            // delete models
+            while (count < currentCount && models.Count > 0)
+            {
+                Destroy(models[models.Count - 1]);
+                models.RemoveAt(models.Count - 1);
+                currentCount--;
+            }
+        }
+        else if (count > currentCount)
+        {
+            // spawn models
+            var modelParentTransform = currentTest.Parent == null ? canvasParent.transform : currentTest.Parent.transform;
+
+            while (count > currentCount)
+            {
+                var m = Instantiate(model);
+                if (currentTest.Parent != null)
+                {
+                    m.transform.parent = modelParentTransform;
+                    m.transform.localScale = Vector3.one;
+                }
+
+                zRank = currentCount / (rows * columns);
+                zOffset = zRank * offset;
+                yRank = (int)(currentCount / columns);
+                yOffset = (yRank % rows) * offset;
+                m.transform.localPosition = new Vector3((currentCount % columns) * offset, yOffset, zOffset);
+                models.Add(m);
+                currentCount++;
+            }
+        }
+
+        label.text = $"Count: {currentCount}";
+    }
+}
--- UnityProjects/MRTKDevTemplate/Assets/Performance/PerfEvaluationSceneManager.cs
+++ UnityProjects/MRTKDevTemplate/Assets/Performance/PerfEvaluationSceneManager.cs
@@ -1,16 +1,20 @@
+using Microsoft.MixedReality.Profiling;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PerfEvaluationSceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject objectToInstantiate;
+
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
