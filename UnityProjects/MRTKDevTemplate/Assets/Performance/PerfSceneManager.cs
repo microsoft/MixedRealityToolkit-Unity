9e4921f98ae701e54fb3b9799da8d5c5e2a90e8a
REDMOND\shaynie <shaynie@microsoft.com>
2023-06-11 20:50:10 -0700

    Add comments and change starttest method name

--- UnityProjects/MRTKDevTemplate/Assets/Performance/PerfSceneManager.cs
+++ UnityProjects/MRTKDevTemplate/Assets/Performance/PerfSceneManager.cs
@@ -1,105 +1,124 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+/// <summary>
+/// Manager for the performance evaluation scene.
+/// It instantiates gameobjects until the FPS drops below a set
+/// limit, currently 50 fps.
+/// </summary>
 public class PerfSceneManager : MonoBehaviour
 {
+    // Reference to the description panel.  The description is disabled when the test is run.
     [SerializeField]
     private GameObject descriptionPanel;
 
+    // Reference to the gameobject to spawn.
     [SerializeField]
     private GameObject model;
 
+    // TextmeshPro text field for the number of objects instantiated.
     [SerializeField]
     private TextMeshProUGUI countText;
 
+    // TextmeshPro text field for displaying the current framerate
     [SerializeField]
     private TextMeshProUGUI framerateText;
 
+    // TextmeshPro text field for displaying the results of the test.
     [SerializeField]
     private TextMeshProUGUI resultsText;
 
+    // How many seconds between updates of the framerate text.
     [SerializeField]
     private float secondsBetweenFramerateUpdates = 0.25f;
 
+    // Parent for the instantiated objects.
     [SerializeField]
     private GameObject canvasParent;
 
+    // Distance between the instantiated objects.
     [SerializeField]
     private float offset = 0;
 
+    // How many columns to use to display the instantiated objects.
     [SerializeField]
     private int columns = 20;
 
+    // How many rows to use to display the instantiated objects.
     [SerializeField]
     private int rows = 10;
 
+    // The test runs until it drops below this framerate.
     [SerializeField]
-    private int startCount = 0;
-
-    //[SerializeField]
     private int targetLowFramerate = 50;
 
+    // tracking field for the framerate text update.
     private float secondsSinceLastFramerateUpdate = 0.0f;
+
+    // Current object count.
     private int currentCount = 0;
-    private List<GameObject> models = new List<GameObject>();
+
+    // List for tracking the instantiated objects.
+    private List<GameObject> testObjects = new List<GameObject>();
+
+    // Boolean for tracking the end of the test.
     private bool testComplete = true;
-    private readonly System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
-    private float samplePeriod = 0.1f;
-    public float FrameRate = 0f;
+
+    // The current framerate.
+    private float frameRate = 0f;
+
+    // Which column is being filled.
     private int yRank = 0;
+
+    // y-Axis local distance for the current instantiated object.
     private float yOffset = 0.0f;
+
+    // Which rank in the z axis is being filled.
     private int zRank = 0;
+
+    // x-Axis local distance for the current instantiated object.
     private float zOffset = 0.0f;
+
+    // How many frames before instantiating the next object.
+    private int frameWait = 10;
+
+    // How many frames have had the framerate below the target.
+    private int lowFramerateFramecount = 0;
 
     private void Start()
     {
+        // prevent divide by zero
         if (columns == 0)
         {
             columns = 20;
         }
     }
 
-    private void OnDestroy()
+    public void StartTest()
     {
-        stopwatch.Stop();
-    }
-
-
-    public void StartNextTest()
-    {
+        // Trigger the test
         descriptionPanel.SetActive(false);
         resultsText.text = string.Empty;
         lowFramerateFramecount = 0;
         SetModelCount(0);
         testComplete = false;
-
-        stopwatch.Start();
     }
 
     private void LateUpdate()
     {
+        // Framerate calculations.
         secondsSinceLastFramerateUpdate += Time.deltaTime;
-        FrameRate = (int)(1.0f / Time.smoothDeltaTime);
+        frameRate = (int)(1.0f / Time.smoothDeltaTime);
         if (secondsSinceLastFramerateUpdate >= secondsBetweenFramerateUpdates)
         {
-            framerateText.text = FrameRate.ToString();
+            framerateText.text = frameRate.ToString();
             secondsSinceLastFramerateUpdate = 0;
         }
     }
-
-    public void AddModel()
-    {
-        SetModelCount(currentCount + 1);
-    }
-
-    public void RemoveModel()
-    {
-        SetModelCount(currentCount - 1);
-    }
-
-    private int frameWait = 10;
-    private int lowFramerateFramecount = 0;
 
     private void Update()
     {
@@ -108,7 +127,7 @@ public class PerfSceneManager : MonoBehaviour
             return;
         }
 
-        if (FrameRate < targetLowFramerate)
+        if (frameRate < targetLowFramerate)
         {
             lowFramerateFramecount++;
         }
@@ -139,17 +158,16 @@ public class PerfSceneManager : MonoBehaviour
         if (count < currentCount)
         {
             // delete models
-            while (count < currentCount && models.Count > 0)
+            while (count < currentCount && testObjects.Count > 0)
             {
-                Destroy(models[models.Count - 1]);
-                models.RemoveAt(models.Count - 1);
+                Destroy(testObjects[testObjects.Count - 1]);
+                testObjects.RemoveAt(testObjects.Count - 1);
                 currentCount--;
             }
         }
         else if (count > currentCount)
         {
             // spawn object
-
             while (count > currentCount)
             {
                 var m = Instantiate(model);
@@ -162,7 +180,7 @@ public class PerfSceneManager : MonoBehaviour
                 yRank = (int)(currentCount / columns);
                 yOffset = (yRank % rows) * offset;
                 m.transform.localPosition = new Vector3((currentCount % columns) * offset, yOffset, zOffset);
-                models.Add(m);
+                testObjects.Add(m);
                 currentCount++;
             }
         }
