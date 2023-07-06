b66ab752b8702caea3db6b1238724d3841ee1a0e
REDMOND\shaynie <shaynie@microsoft.com>
2023-07-06 11:35:58 -0700

    Change 'Unknown' display type value to be 0

--- com.microsoft.mrtk.core/Utilities/DisplayType.cs
+++ com.microsoft.mrtk.core/Utilities/DisplayType.cs
@@ -11,18 +11,18 @@ namespace Microsoft.MixedReality.Toolkit
     public enum DisplayType
     {
         /// <summary>
+        /// An unknown display type.
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
         /// A see-through display that allows the physical world to be viewed.
         /// </summary>
-        Transparent = 0,
+        Transparent = 1,
 
         /// <summary>
         /// A display that blocks viewing of the physical world.
         /// </summary>
-        Opaque = 1,
-
-        /// <summary>
-        /// An unknown display type.
-        /// </summary>
-        Unknown = 2,
+        Opaque = 2,
     }
 }
