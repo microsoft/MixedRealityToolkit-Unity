2cdfb27920e72cb47a00c4174f17191130fd3e73
Marlena Klein <marlenaklein@microsoft.com>
2023-07-07 11:20:21 -0700

    Add active check

--- com.microsoft.mrtk.core/Utilities/ComponentCache.cs
+++ com.microsoft.mrtk.core/Utilities/ComponentCache.cs
@@ -16,7 +16,6 @@ namespace Microsoft.MixedReality.Toolkit
         /// <summary>
         /// Finds the first instance of an object of the given type. 
         /// </summary>
-        /// <param name="includeInactive">Specifies whether the search should include inactive objects.</param>
         /// <returns>
         /// The first instance of an object of the given type. 
         /// </returns>
@@ -29,14 +28,12 @@ namespace Microsoft.MixedReality.Toolkit
         /// <summary>
         /// Finds the first instance of an object of the given type. 
         /// </summary>
-        /// <param name="includeInactive">Specifies whether the search should include inactive objects.</param>
-        /// <param name="result">A reference of the given type, which will be set to the first instance of an object if one is found.</param>
         /// <returns>
         /// True if an instance was found, and false otherwise. 
         /// </returns>
         public static bool TryFindFirstActiveInstance(out T result)
         {
-            if (cacheFirstInstance == null)
+            if (cacheFirstInstance == null || !cacheFirstInstance.gameObject.activeInHierarchy)
             {
                 cacheFirstInstance = Object.FindFirstObjectByType<T>();
             }
