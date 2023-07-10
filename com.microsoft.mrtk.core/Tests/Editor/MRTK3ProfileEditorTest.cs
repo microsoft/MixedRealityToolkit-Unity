9d58d789203d404f60ae566e16816dec0c78daf7
Adam Mollis <amollis@microsoft.com>
2023-07-10 12:59:04 -0700

    Fixing bug in mrtk profile editor after property removal

---
+++ com.microsoft.mrtk.core/Tests/Editor/MRTK3ProfileEditorTest.cs
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using NUnit.Framework;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.TestTools;
+using UnityEngine.UIElements;
+
+namespace Microsoft.MixedReality.Toolkit.Core.Tests.EditMode
+{
+    internal class MRTK3ProfileEditorTest
+    {
+        /// <summary>
+        /// Verify that the <see cref="MRTK3ProfileEditor"/> window can be opened.
+        /// </summary>
+        [UnityTest]
+        public IEnumerator InspectorWindowSmokeTest()
+        {
+            var window = SettingsService.OpenProjectSettings("Project/MRTK3");
+            Assert.IsNotNull(window, "The MRTK Profile editor window should have been opened");
+
+            var panel = window.rootVisualElement.Query<IMGUIContainer>(className: "settings-panel-imgui-container").First();
+            Assert.IsNotNull(panel, "There should have hand a settings panel created for the MRTK Profile.");
+
+            yield return null;
+
+            //Assert.AreEqual(0, logWatcher.FailureCount, "There should have been zero failures logged when showing the MRTK Profile editor window");
+        }
+    }
+}
