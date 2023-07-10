d4ed81c54e915ace7f030a9a4a5305d03f75e000
Adam Mollis <amollis@microsoft.com>
2023-07-10 15:07:17 -0700

    Disabling unit test in batch mode

--- com.microsoft.mrtk.core/Tests/Editor/MRTK3ProfileEditorTest.cs
+++ com.microsoft.mrtk.core/Tests/Editor/MRTK3ProfileEditorTest.cs
@@ -4,6 +4,7 @@
 using NUnit.Framework;
 using System.Collections;
 using UnityEditor;
+using UnityEngine;
 using UnityEngine.TestTools;
 using UnityEngine.UIElements;
 
@@ -14,16 +15,26 @@ namespace Microsoft.MixedReality.Toolkit.Core.Tests.EditMode
         /// <summary>
         /// Verify that the <see cref="MRTK3ProfileEditor"/> window can be opened.
         /// </summary>
+        /// <remarks>
+        /// This test is disabled when executing in batch mode, since it requires a Unity graphic device.
+        /// </remarks>
         [UnityTest]
         public IEnumerator InspectorWindowSmokeTest()
         {
-            var window = SettingsService.OpenProjectSettings("Project/MRTK3");
-            Assert.IsNotNull(window, "The MRTK Profile editor window should have been opened");
+            if (Application.isBatchMode)
+            {
+                yield return null;
+            }
+            else
+            {
+                var window = SettingsService.OpenProjectSettings("Project/MRTK3");
+                Assert.IsNotNull(window, "The MRTK Profile editor window should have been opened");
 
-            var panel = window.rootVisualElement.Query<IMGUIContainer>(className: "settings-panel-imgui-container").First();
-            Assert.IsNotNull(panel, "There should have hand a settings panel created for the MRTK Profile.");
+                var panel = window.rootVisualElement.Query<IMGUIContainer>(className: "settings-panel-imgui-container").First();
+                Assert.IsNotNull(panel, "There should have hand a settings panel created for the MRTK Profile.");
 
-            yield return null;
+                yield return null;
+            }
         }
     }
 }
